67111d04bcbb63b8bcd4310acfa4a64cb1cde7c3
Wannte <T.wannte@gmail.com>
2020-12-10 02:45:38 +0900

    feat(Line): 노선에 역정보를 저장하고, 추가하는 로직 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,28 @@
 package subway.domain;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations = new LinkedList<>();
 
-    public Line(String name) {
+    public Line(String name, Station start, Station end) {
         this.name = name;
+        this.stations.add(start);
+        this.stations.add(end);
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public void add(int index, Station station) {
+        stations.add(index, station);
+//TODO catch (IndexOutOfBoundsException e) error handling
+    }
 }
