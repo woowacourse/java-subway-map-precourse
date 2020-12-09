46220d1fa30322353475ff3e2ada08b836adf9f4
sakjung <58401309+sakjung@users.noreply.github.com>
2020-12-09 21:35:04 +0900

    feat(InputView): Add feature to get input values

--- src/main/java/subway/controller/MainScreenController.java
+++ src/main/java/subway/controller/MainScreenController.java
@@ -1,9 +1,13 @@
 package subway.controller;
 
+import subway.view.InputView;
 import subway.view.OutputView;
 
+import java.util.Scanner;
+
 public class MainScreenController {
-	public static void showMainScreen() {
+	public static void showMainScreen(Scanner scanner) {
+		InputView.getInput(scanner);
 		OutputView.printMainScreen();
 	}
 }
--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,4 +1,9 @@
 package subway.view;
 
+import java.util.Scanner;
+
 public class InputView {
+	public static String getInput(Scanner scanner) {
+		return scanner.nextLine();
+	}
 }
