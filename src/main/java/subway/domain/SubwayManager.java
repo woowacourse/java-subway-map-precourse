11d3ee16624ea16452974c6a78e9172ccbd49b3f
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-09 21:39:58 +0900

    feat: 역을 제거하는 기능 추가

--- src/main/java/subway/domain/SubwayManager.java
+++ src/main/java/subway/domain/SubwayManager.java
@@ -7,4 +7,9 @@ public class SubwayManager {
         StationRepository.addStation(new Station(name));
         OutputView.printRegisteredStationMessage();
     }
+
+    public void removeStation(String name) {
+        StationRepository.deleteStation(name);
+        OutputView.printRemovedStationMessage();
+    }
 }
