7869d3fd6893e866fc8ec5cbf56ca776843c176d
PaperCrafter <leejonghee342@gmail.com>
2020-12-10 02:25:41 +0900

    feat(ControllerState): add common feature abstract class for controllers

---
+++ src/main/java/subway/controller/ControllerState.java
@@ -0,0 +1,24 @@
+package subway.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public abstract class ControllerState {
+    protected static Map<String, Integer> featureRequiredInputNumMap = new HashMap<>();
+
+    public abstract void printMain();
+
+    public abstract void doFeature(String feature, int order, String param);
+
+    public boolean isValidFeature(String feature) {
+        if(Optional.of(featureRequiredInputNumMap.get(feature)).isPresent()){
+            return true;
+        }
+        return false;
+    }
+
+    public int getRequiredInputNum(String feature){
+        return featureRequiredInputNumMap.get(feature);
+    }
+}
