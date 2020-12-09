5602873f989ccbfdb02359eb041b12ac689ebf85
sakjung <58401309+sakjung@users.noreply.github.com>
2020-12-09 22:28:35 +0900

    feat(StationController): Add feature to create station

--- src/main/java/subway/controller/StationController.java
+++ src/main/java/subway/controller/StationController.java
@@ -1,12 +1,22 @@
 package subway.controller;
 
+import subway.domain.Station;
+import subway.domain.StationRepository;
 import subway.view.InputView;
 import subway.view.OutputView;
 
 import java.util.Scanner;
 
 public class StationController {
-	public static void showStationScreen() {
+	private static final String REGISTER_MESSAGE = "## 등록할 역 이름을 입력하세요.";
+
+	private static void showStationScreen() {
 		OutputView.printStationScreen();
+	}
+
+	private static void createStation(Scanner scanner) {
+		System.out.println(REGISTER_MESSAGE);
+		String name = InputView.getInput(scanner);
+		StationRepository.addStation(new Station(name));
 	}
 }
