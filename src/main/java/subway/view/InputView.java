034955609cd2d4550f89b73d722b71a47e55f99d
RinSabbit <celena1004@naver.com>
2020-12-10 04:15:18 +0900

    refactor: InputView 에서 출력 관선 변수 및 함수들을 OutputView로 이동

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,79 +1,19 @@
 package subway.view;
 
 import java.util.Scanner;
-import subway.exception.SubwayCustomException;
-import subway.utils.ValidateUtils;
 
 /**
  * 입력값을 받는 클래스
  */
 public class InputView {
 
-    private static final String MENU_PREFIX = "## ";
-    private static final String MAIN_MENU_MESSAGE = "메인화면 \n"
-        + "1. 역 관리 \n"
-        + "2. 노선 관리 \n"
-        + "3. 구간 관리 \n"
-        + "4. 지하철 노선도 출력 \n"
-        + "Q. 종료\n";
-    private static final String STATION = "역";
-    private static final String LINE = "노선";
-    private static final String LINE_OR_STATION_MENU_MESSAGE = "%s 관리 화면 \n"
-        + "1. %s 등록 \n"
-        + "2. %s 삭제 \n"
-        + "3. %s 조회 \n"
-        + "B. 돌아가기 \n";
-    private static final String MENU_CATEGORY_CHOICE_MESSAGE = "원하는 기능을 선택하세요.";
-    private static final String INPUT_SECTION_NAME_MESSAGE = "역이름을 입력하세요.";
-    private static final String INPUT_SECTION_POSITION_MESSAGE = "순서를 입력하세요.";
-    private static final String INPUT_SECTION_LINE_MESSAGE = "노선을 입력하세요.";
-    private static final String REMOVE_SECTION_LINE_MESSAGE = "삭제할 구간의 노선을 입력하세요.";
-    private static final String REMOVE_SECTION_NAME_MESSAGE = "삭제할 구간의 역을 입력하세요.";
-    private static final String INPUT_LINE_NAME_MESSAGE = "등록할 노선의 이름을 입력하세요.";
-    private static final String INPUT_STATION_NAME_MESSAGE = "등록할 역 이름을 입력하세요.";
-    private static final String REMOVE_STATION_NAME_MESSAGE = "삭제할 역 이름을 입력하세요.";
-    private static final String INPUT_LINE_START_STATION_NAME_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
-    private static final String INPUT_LINE_END_STATION_NAME_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
     private final Scanner scanner;
-    private final ValidateUtils validateUtils;
 
     public InputView(Scanner scanner) {
         this.scanner = scanner;
-        validateUtils = new ValidateUtils();
     }
 
-    public String showMain() {
-        System.out.println(MENU_PREFIX + MAIN_MENU_MESSAGE);
-        return chooseCategory();
-    }
-
-    public String chooseCategory() {
-        System.out.println(MENU_PREFIX + MENU_CATEGORY_CHOICE_MESSAGE);
-        try {
-            String input = scanner.nextLine();
-            //validateUtils.isValid(input); // 메뉴에 있는 카테고리인지 확인
-            return input.toUpperCase();
-        } catch (SubwayCustomException exception) {
-            System.out.println(exception.getMessage());
-            return chooseCategory();
-        }
-    }
-
-    public String chooseLineOrStationMenu(String input) {
-        if (input.equals("1")) {
-            System.out.printf(MENU_PREFIX + LINE_OR_STATION_MENU_MESSAGE, LINE, LINE, LINE, LINE);
-            return chooseCategory();
-        }
-        if (input.equals("2")) {
-            System.out.printf(MENU_PREFIX + LINE_OR_STATION_MENU_MESSAGE, STATION, STATION, STATION,
-                STATION);
-            return chooseCategory();
-        }
-        return null;
-    }
-
-    public String insertNewStation(){
-        System.out.println(INPUT_STATION_NAME_MESSAGE);
+    public String inputValue() {
         return scanner.nextLine();
     }
 }
--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -11,27 +11,100 @@ import subway.domain.Station;
 public class OutputView {
 
     private static final String INFORMATION_PREFIX = "[INFO] ";
-    private static final String INPUT_LINE_CHECK_MESSAGE = "지하철 노선이 등록되었습니다.";
-    private static final String REMOVE_LINE_CHECK_MESSAGE = "지하철 노선이 삭제되었습니다.";
-    private static final String SHOW_LINE_LIST_MESSAGE = "노선 목록";
+    private static final String ERROR_PREFIX = "[ERROR] ";
     private static final String SECTION_LINE = "---";
-    private static final String INPUT_SECTION_CHECK_MESSAGE = "구간이 등록되었습니다.";
-    private static final String REMOVE_SECTION_CHECK_MESSAGE = "구간이 삭제되었습니다.";
+    private static final String MENU_PREFIX = "## ";
+    private static final String MAIN_MENU_MESSAGE = "메인화면 \n"
+        + "1. 역 관리 \n"
+        + "2. 노선 관리 \n"
+        + "3. 구간 관리 \n"
+        + "4. 지하철 노선도 출력 \n"
+        + "Q. 종료\n";
+    private static final String STATION_MENU_MESSAGE = "역 관리 화면 \n"
+        + "1. 역 등록 \n"
+        + "2. 역 삭제 \n"
+        + "3. 역 조회 \n"
+        + "B. 돌아가기 \n";
+    private static final String LINE_MENU_MESSAGE = "노선 관리 화면 \n"
+        + "1. 노선 등록 \n"
+        + "2. 노선 삭제 \n"
+        + "3. 노선 조회 \n"
+        + "B. 돌아가기 \n";
+    private static final String MENU_CATEGORY_CHOICE_MESSAGE = "원하는 기능을 선택하세요.";
+
+    private static final String INPUT_STATION_NAME_MESSAGE = "등록할 역 이름을 입력하세요.";
+    private static final String INPUT_STATION_CHECK_MESSAGE = "지하철 역이 등록되었습니다.";
+    private static final String REMOVE_STATION_NAME_MESSAGE = "삭제할 역 이름을 입력하세요.";
     private static final String REMOVE_STATION_CHECK_MESSAGE = "지하철 역이 삭제되었습니다.";
     private static final String SHOW_STATION_LIST_MESSAGE = "역 목록";
-    private static final String INPUT_STATION_CHECK_MESSAGE = "지하철 역이 등록되었습니다.";
 
-    public void showInfoMessage(String message) {
+    private static final String INPUT_LINE_NAME_MESSAGE = "등록할 노선의 이름을 입력하세요.";
+    private static final String INPUT_LINE_START_STATION_NAME_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
+    private static final String INPUT_LINE_END_STATION_NAME_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
+    private static final String INPUT_LINE_CHECK_MESSAGE = "지하철 노선이 등록되었습니다.";
+    private static final String REMOVE_LINE_NAME_MESSAGE = "삭제할 노선 이름을 입력하세요.";
+    private static final String REMOVE_LINE_CHECK_MESSAGE = "지하철 노선이 삭제되었습니다.";
+    private static final String SHOW_LINE_LIST_MESSAGE = "노선 목록";
+
+    private static final String INPUT_SECTION_LINE_MESSAGE = "노선을 입력하세요.";
+    private static final String INPUT_SECTION_NAME_MESSAGE = "역이름을 입력하세요.";
+    private static final String INPUT_SECTION_POSITION_MESSAGE = "순서를 입력하세요.";
+    private static final String INPUT_SECTION_CHECK_MESSAGE = "구간이 등록되었습니다.";
+    private static final String REMOVE_SECTION_LINE_MESSAGE = "삭제할 구간의 노선을 입력하세요.";
+    private static final String REMOVE_SECTION_NAME_MESSAGE = "삭제할 구간의 역을 입력하세요.";
+    private static final String REMOVE_SECTION_CHECK_MESSAGE = "구간이 삭제되었습니다.";
+
+    public static void showInfoMessage(String message) {
         System.out.println(INFORMATION_PREFIX + message);
     }
 
-    public void showStationList(List<Station> stations) {
+    public static String showErrorMessage(String message) {
+        System.out.println(ERROR_PREFIX + message);
+        return message;
+    }
+
+    public void showMainMenu() {
+        System.out.println(MENU_PREFIX + MAIN_MENU_MESSAGE);
+        System.out.println(MENU_PREFIX + MENU_CATEGORY_CHOICE_MESSAGE);
+    }
+
+    public static void showStationMenu() {
+        System.out.println(MENU_PREFIX + STATION_MENU_MESSAGE);
+        System.out.println(MENU_PREFIX + MENU_CATEGORY_CHOICE_MESSAGE);
+    }
+
+    public void showLineMenu() {
+        System.out.println(MENU_PREFIX + LINE_MENU_MESSAGE);
+        System.out.println(MENU_PREFIX + MENU_CATEGORY_CHOICE_MESSAGE);
+    }
+
+    public static void guideInputStation() {
+        System.out.println(MENU_PREFIX + INPUT_STATION_NAME_MESSAGE);
+    }
+
+    public static void doneInsertStation() {
+        showInfoMessage(INPUT_STATION_CHECK_MESSAGE);
+    }
+
+    public static void guideRemoveStation() {
+        System.out.println(MENU_PREFIX + REMOVE_STATION_NAME_MESSAGE);
+    }
+
+    public static void doneRemoveStation() {
+        showInfoMessage(REMOVE_STATION_CHECK_MESSAGE);
+    }
+
+    public static void showStationList(List<Station> stations) {
         System.out.println(INFORMATION_PREFIX + SHOW_STATION_LIST_MESSAGE);
         stations.forEach(station -> showInfoMessage(station.getName()));
     }
 
-    public void doneInsertStation(){
-        showInfoMessage(INPUT_STATION_CHECK_MESSAGE);
+    public void doneInsertLine() {
+        showInfoMessage(INPUT_LINE_CHECK_MESSAGE);
+    }
+
+    public void doneRemoveLine() {
+        showInfoMessage(REMOVE_LINE_CHECK_MESSAGE);
     }
 
     public void showLineList(List<Line> lines) {
@@ -46,7 +119,7 @@ public class OutputView {
         System.out.println();
     }
 
-    public void showSubwaySections() {
+    public void showSubwayLineMap() {
         LineRepository.lines().forEach(this::showSection);
     }
 
