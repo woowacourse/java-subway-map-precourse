85a4d50ec3ed78bf4eb38bb0d3eb06c3a47719b5
PaperCrafter <leejonghee342@gmail.com>
2020-12-10 02:48:16 +0900

    docs(README): add main menu implementation requirements
    add implementation requirment of main menu switching state

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -4,12 +4,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
 
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
+    }
+
+    public static Line getLine(String name){
+        return lines.stream()
+                .filter(line -> line.getName() != name)
+                .collect(Collectors.toList())
+                .get(0);
     }
 
     public static void addLine(Line line) {
