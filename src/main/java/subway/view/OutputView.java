42d2c7e53a9af38f0200a86248ab71a0faf20fc1
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-09 17:04:07 +0900

    feat: 노선 관리 화면 출력 추가

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -41,4 +41,12 @@ public class OutputView {
     public static void printRemovedStationMessage() {
         System.out.println(NEW_LINE + "[INFO] 지하철 역이 삭제되었습니다.");
     }
+
+    public static void printLineScene() {
+        System.out.println(NEW_LINE + "## 노선 관리 화면");
+        System.out.println("1. 노선 등록");
+        System.out.println("2. 노선 삭제");
+        System.out.println("3. 노선 조회");
+        System.out.println("B. 돌아가기");
+    }
 }
