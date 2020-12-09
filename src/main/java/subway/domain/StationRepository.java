6989e59fec51ca56c490a74596fde730fe3413f1
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-09 17:46:19 +0900

    feat: 초기 역 정보 등록

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -8,6 +8,16 @@ import java.util.Objects;
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
 
+    static {
+        stations.add(new Station("교대역"));
+        stations.add(new Station("강남역"));
+        stations.add(new Station("역삼역"));
+        stations.add(new Station("남부터미널역"));
+        stations.add(new Station("양재역"));
+        stations.add(new Station("양재시민의숲역"));
+        stations.add(new Station("매봉역"));
+    }
+
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
     }
