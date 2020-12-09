c71b4244a7c6ea98dbcdba90a9baa59e994bb5c9
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-09 23:17:21 +0900

    feat: 기능 실행 관련 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,14 @@
 package subway;
 
+import subway.domain.SubwayManager;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        SubwayManager subwayManager = new SubwayManager();
+        subwayManager.startProgram(scanner);
     }
 }
