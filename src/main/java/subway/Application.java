50de51c6b72347d67dd0ca3baa57569b13b87ac1
soongjamm <kimstz0303@gmail.com>
2020-12-09 21:37:41 +0900

    refactor: Application 클래스에 잘못 입력된 코드 수정

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,16 +1,12 @@
 package subway;
 
-import subway.view.InputView;
-
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
-        InputView inputView = new InputView(scanner);
-        String inputTest = inputView.mainInput();
-        System.out.println(inputTest);
-
+        Controller controller = new Controller(scanner);
+        controller.run();
     }
 }
