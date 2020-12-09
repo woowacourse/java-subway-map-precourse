fa7c2541d8f429f28580b375d88c1aa951249081
Taehwa Lee <xoghk0321@gmail.com>
2020-12-09 23:15:30 +0900

    feat: 지하철 노선 생성 시 상행 종점역, 하행 종점역을 추가로 입력받는다.

--- src/main/java/subway/domain/line/Line.java
+++ src/main/java/subway/domain/line/Line.java
@@ -1,23 +1,28 @@
 package subway.domain.line;
 
+import java.util.Arrays;
+import java.util.List;
 import subway.domain.line.exception.ShorterThanMinLineNameException;
+import subway.domain.station.Station;
 
 public class Line {
 
     public static final int MIN_NAME_SIZE = 2;
 
     private String name;
+    private List<Station> stations;
 
-    private Line(String name) {
+    private Line(String name, List<Station> stations) {
         this.name = name;
+        this.stations = stations;
     }
 
-    public static Line from(String name) {
+    public static Line of(String name, Station upstreamStation, Station downstreamStation) {
         if (name.length() < MIN_NAME_SIZE) {
             throw new ShorterThanMinLineNameException(name);
         }
 
-        return new Line(name);
+        return new Line(name, Arrays.asList(upstreamStation, downstreamStation));
     }
 
     public String getName() {
--- src/test/java/subway/domain/line/LineTest.java
+++ src/test/java/subway/domain/line/LineTest.java
@@ -5,23 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import subway.domain.line.exception.ShorterThanMinLineNameException;
+import subway.domain.station.Station;
 
 @DisplayName("지하철 노선(Line)에 대한 테스트")
 class LineTest {
+
+    final Station upstreamStation = Station.from("test1");
+    final Station downstreamStation = Station.from("test2");
 
     @DisplayName("지하철 노선 이름은 2글자 이상이어야 한다.")
     @Test
     void shorterThanMinLineNameException() {
         final String name = "a";
 
-        assertThrows(ShorterThanMinLineNameException.class, () -> Line.from(name));
+        assertThrows(ShorterThanMinLineNameException.class,
+            () -> Line.of(name, upstreamStation, downstreamStation));
     }
 
     @DisplayName("지하철 노선을 생성할 수 있다.")
     @Test
     void create() {
         final String name = "test";
-        final Line line = Line.from(name);
+        final Line line = Line.of(name, upstreamStation, downstreamStation);
 
         assertNotNull(line);
     }
