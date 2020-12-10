f281cca3d12597103552a6576997e41d9a2dbd01
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-12-10 22:42:24 +0900

    feat(StationService): 역 등록,삭제,조회 입력 및 출력처리 구현

---
+++ src/main/java/subway/service/StationService.java
@@ -0,0 +1,53 @@
+package subway.service;
+
+import subway.domain.Station;
+import subway.domain.StationRepository;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+public class StationService {
+    private static final String SELECT_STATION_TEXT="## 역 목록";
+    private static final String DELETE_STATION_ERROR_TEXT="삭제할 수 없습니다.";
+    private static final String INSERT_SUCCESS="지하철 역이 등록되었습니다.";
+    private static StationService stationService;
+
+    private StationService(){}
+
+    public static StationService getInstance(){
+        if(stationService==null){
+            stationService=new StationService();
+        }
+        return stationService;
+    }
+
+    public boolean insert(){
+        String name=InputView.getStationName();
+        Station station=new Station(name);
+        boolean result=StationRepository.addStation(station);
+        if(result){
+            OutputView.printInfo(INSERT_SUCCESS);
+            System.out.println();
+        }
+        return result;
+    }
+
+    public boolean delete(){
+        String name=InputView.getDeleteStationName();
+        boolean result=StationRepository.deleteStation(name);
+        if(!result){
+            OutputView.printError(DELETE_STATION_ERROR_TEXT);
+        }
+        return result;
+    }
+
+    public boolean find(){
+        System.out.println(SELECT_STATION_TEXT);
+        StationRepository.printAllStation();
+        System.out.println();
+        return true;
+    }
+
+    public boolean back(){
+        return true;
+    }
+}
