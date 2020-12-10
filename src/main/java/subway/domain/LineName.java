5d03430ef4c9dbd781fb161cc6f110a35cf71f1c
naru <hsik0225@gmail.com>
2020-12-10 13:54:00 +0900

    feat(line-name): 라인 이름 관리 역할을 가진 LineName 클래스 생성

---
+++ src/main/java/subway/domain/LineName.java
@@ -0,0 +1,37 @@
+package subway.domain;
+
+import java.util.Objects;
+
+import subway.validator.LineNameValidator;
+
+public class LineName {
+
+    public static final int LENGTH_LOWER_BOUND = 2;
+
+    public static final int LENGTH_UPPER_BOUND = 10;
+
+    private final String name;
+
+    public LineName(String name) {
+        new LineNameValidator().validate(name);
+
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { return true; }
+        if (!(o instanceof LineName)) { return false; }
+        LineName lineName = (LineName) o;
+        return Objects.equals(getName(), lineName.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
+}
