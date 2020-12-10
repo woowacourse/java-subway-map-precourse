51caf26fddcd7cfc57d36a3a77296cc3d64dbef8
taelee <taelee@student.42seoul.kr>
2020-12-10 17:34:45 +0900

    feat(StationRepository): initialize station list

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -6,7 +6,9 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+
         SubwayProgram newSubwayProgram = new SubwayProgram(scanner);
         newSubwayProgram.run();
+
     }
 }
--- src/main/java/subway/SubwayProgram.java
+++ src/main/java/subway/SubwayProgram.java
@@ -1,9 +1,14 @@
 package subway;
 
+import subway.domain.*;
+
 import java.util.Scanner;
+import java.util.List;
 
 public class SubwayProgram {
     private final Scanner scanner;
+    private StationRepository newStationRepository;
+    private LineRepository newLineRepository;
 
     public SubwayProgram(Scanner scanner) {
         this.scanner = scanner;
@@ -12,8 +17,10 @@ public class SubwayProgram {
     public void run() {
         MainMenu mainMenu = new MainMenu(scanner);
         int mainMenuNumber = mainMenu.inputAndNext();
-
+        newStationRepository = new StationRepository();
+//        System.out.println(StationRepository.stations().get(0).getName());
     }
+
 
 
 }
