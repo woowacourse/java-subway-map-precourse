2515cd9ab24b9a9407e2abd0852eeacc8b7b2dee
naru <hsik0225@gmail.com>
2020-12-10 14:05:43 +0900

    feat(line-repository): 노선 등록 시 추가되었는지 여부를 반환하는 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,29 @@
 package subway.domain;
 
+import java.util.Objects;
+
 public class Line {
-    private String name;
+
+    private final LineName name;
 
     public Line(String name) {
-        this.name = name;
+        this.name = new LineName(name);
     }
 
     public String getName() {
-        return name;
+        return name.getName();
     }
 
-    // 추가 기능 구현
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { return true; }
+        if (!(o instanceof Line)) { return false; }
+        Line line = (Line) o;
+        return Objects.equals(getName(), line.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
 }
--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -1,19 +1,19 @@
 package subway.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class LineRepository {
-    private static final List<Line> lines = new ArrayList<>();
+    private static final Set<Line> lines = new LinkedHashSet<>();
 
-    public static List<Line> lines() {
-        return Collections.unmodifiableList(lines);
+    public static Set<Line> lines() {
+        return Collections.unmodifiableSet(lines);
     }
 
-    public static void addLine(Line line) {
-        lines.add(line);
+    public static boolean addLine(Line line) {
+        return lines.add(line);
     }
 
     public static boolean deleteLineByName(String name) {
