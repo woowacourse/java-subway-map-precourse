63a5dba618d5b9415aab8e90f383991a9c7f0d5f
RinSabbit <celena1004@naver.com>
2020-12-10 17:34:33 +0900

    feat: 저장소에서 역 또는 노선을 검색하여 반환하는 기능 구현

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import subway.exception.SubwayCustomException;
 
 public class LineRepository {
 
     private static final List<Line> lines = new ArrayList<>();
+    public static final String NOT_VALID_LINE_EXCEPTION_MESSAGE = "존재하지 않는 노선입니다.";
 
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
@@ -19,5 +21,12 @@ public class LineRepository {
 
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static Line searchLineByName(String name){
+        return lines.stream()
+            .filter(line -> Objects.equals(line.getName(), name))
+            .findFirst()
+            .orElseThrow(()->new SubwayCustomException(NOT_VALID_LINE_EXCEPTION_MESSAGE));
     }
 }
--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import subway.exception.SubwayCustomException;
 
 public class StationRepository {
 
     private static final List<Station> stations = new ArrayList<>();
+    public static final String NOT_VALID_STATION_EXCEPTION_MESSAGE = "존재하지 않는 역입니다.";
 
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
@@ -19,6 +21,13 @@ public class StationRepository {
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    public static Station searchStationByName(String name){
+        return stations.stream()
+            .filter(station -> Objects.equals(station.getName(), name))
+            .findFirst()
+            .orElseThrow(()->new SubwayCustomException(NOT_VALID_STATION_EXCEPTION_MESSAGE));
     }
 
 }
