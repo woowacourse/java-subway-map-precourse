22eb1dabf1af642d7ad9179e9e275ef9203f7274
doorisopen <audauddl2@gmail.com>
2020-12-11 00:27:13 +0900

    test: 역을 검색한다
    - 역 검색에 성공한다
    - 역 검색에 실패한다

--- src/test/java/subway/domain/StationTest.java
+++ src/test/java/subway/domain/StationTest.java
@@ -3,6 +3,7 @@ package subway.domain;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,5 +20,37 @@ class StationTest {
         //then
         List<Station> stations = StationRepository.stations();
         assertThat(stations.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void 역_검색을_성공한다() throws Exception {
+        //given
+        Station station = new Station("서울역");
+        StationRepository.addStation(station);
+
+        //when
+        List<Station> stations = StationRepository.stations();
+        Optional<Station> findStation = stations.stream()
+                .filter(st -> st.getName().equals("서울역"))
+                .findAny();
+
+        //then
+        assertThat(findStation.get()).isEqualTo(station);
+    }
+
+    @Test
+    public void 역_검색을_실패한다() throws Exception {
+        //given
+        Station station = new Station("서울역");
+        StationRepository.addStation(station);
+
+        //when
+        List<Station> stations = StationRepository.stations();
+        Optional<Station> findStation = stations.stream()
+                .filter(st -> st.getName().equals("부산역"))
+                .findAny();
+
+        //then
+        assertThat(findStation.get()).isNotEqualTo(station);
     }
 }
