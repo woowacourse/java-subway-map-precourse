2daed73eff742a691810a680e6d153444d699fd8
483759 <483759@naver.com>
2020-12-11 00:51:45 +0900

    refactor(Application) : add enum MainFunction

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,32 +1,44 @@
 package subway;
 
+import com.sun.tools.javac.Main;
 import java.util.Scanner;
 import subway.utils.InputUtils;
 import subway.utils.PrintUtils;
 
 public class Application {
-    private static final char mainQuit = 'Q';
+
+    enum MainFunction {
+        STATION_MANAGEMENT('1'), LINE_MANAGEMENT('2'), SECTION_MANAGEMENT('3'), SUBWAY_MAP(
+            '4'), QUIT('Q');
+
+        final private char menu;
+
+        private MainFunction(char menu){this.menu=menu;}
+
+        public char getMenu(){return menu;}
+
+    }
 
     private static PrintUtils printUtils;
     private static InputUtils inputUtils;
 
     public static void main(String[] args) {
-        char selectMainMenu;
-
         final Scanner scanner = new Scanner(System.in);
         printUtils = new PrintUtils();
         inputUtils = new InputUtils();
+        char mainFunction;
 
-        while(true) {
-            selectMainMenu=mainMenu();
-            if(selectMainMenu==mainQuit)
+        while (true) {
+            mainFunction = mainMenu();
+            if (mainFunction == MainFunction.QUIT.getMenu()) {
                 break;
+            }
         }
     }
 
-    private static char mainMenu(){
+    private static char mainMenu() {
         printUtils.printMainMenu();
         printUtils.printSelectFunction();
-        return inputUtils.inputFunctionSelect(4,mainQuit);
+        return inputUtils.inputFunctionSelect(4, MainFunction.QUIT.getMenu());
     }
 }
