12192f22aa86b2ad84728b96ac60d14384d0f7bf
tributetothemoon <kkm97351@gmail.com>
2020-12-11 03:56:27 +0900

    feat(OutputView): 메인 메뉴 출력 기능 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,13 +1,43 @@
+/**
+ * Application.java
+ * 메인 화면에서 기능을 선택할 수 있도록 합니다.
+ *
+ * @author
+ * Kimun Kim, github.com/tributetothemoon
+ */
+
 package subway;
 
 import subway.view.InputView;
+import subway.view.OutputView;
 
 import java.util.Scanner;
 
 public class Application {
+    private static final String STATION_MANAGEMENT = "1";
+    private static final String LINE_MANAGEMENT = "2";
+    private static final String SECTION_MANAGEMENT = "3";
+    private static final String SHOW_SUBWAY_LINE = "4";
+    private static final String QUIT = "Q";
+
+    private static String menu;
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
         InputView.setScanner(scanner);
+        run();
+    }
+
+    private static void run() {
+        do{
+            OutputView.showMainMenu();
+            // menu = InputView.getMainMenuSelection(); //무조건 올바른 값이 넘어오도록 설정
+            // runSelectedMenuFunction();
+        }while(!menu.equals(QUIT));
+    }
+
+    private static void runSelectedMenuFunction() {
+
     }
 }
--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,10 +1,43 @@
 package subway.view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class OutputView {
     private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String STATION_MANAGEMENT = "역 관리";
+    private static final String LINE_MANAGEMENT = "노선 관리";
+    private static final String SECTION_MANAGEMENT = "구간 관리";
+    private static final String SUBWAY_MAP = "지하철 노선도 출력";
+    private static final String MENU_FORMAT = "%s. %s";
+    private static final String QUIT = "종료";
+    private static final int MENU_START_INDEX = 1;
+    private static final int MAIN_MENU_LENGTH = 4;
+
+    private static List<String> mainMenu = new ArrayList<>(Arrays.asList(
+            STATION_MANAGEMENT, LINE_MANAGEMENT, SECTION_MANAGEMENT, SUBWAY_MAP, QUIT
+    ));
+
+    private static List<String> mainMenuSelections = new ArrayList<>();
+
+    static {
+        IntStream.rangeClosed(MENU_START_INDEX, MAIN_MENU_LENGTH)
+                .mapToObj(Integer::toString)
+                .forEach(mainMenuSelections::add);
+        mainMenuSelections.add("Q");
+    }
 
     public static void showErrorMessage(Exception e) {
         System.out.println(ERROR_PREFIX + e.getMessage());
     }
 
+    public static void showMainMenu() {
+        Iterator<String> menuIterator = mainMenu.iterator();
+        for (String index : mainMenuSelections) {
+            System.out.println(String.format(MENU_FORMAT, index, menuIterator.next()));
+        }
+    }
 }
