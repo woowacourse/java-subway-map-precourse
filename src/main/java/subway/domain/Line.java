7014ef63f8b6f1e1d82cbba82d65536f10c5f36b
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-12-11 01:20:13 +0900

    feat(Line): Line인스턴스에 역을 추가하는 메서드 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,9 +1,22 @@
 package subway.domain;
 
+import subway.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Line {
+    private static final Integer MIN_LINE_NAME_LENGTH=2;
+    private static final Integer MIN_STATION_NAME_LENGTH=2;
+    private static final Integer MIN_ARRAY_INDEX=0;
+
+    private List<Station> stations;
     private String name;
 
     public Line(String name) {
+        this.stations=new LinkedList<>();
         this.name = name;
     }
 
@@ -12,4 +25,23 @@ public class Line {
     }
 
     // 추가 기능 구현
+    public boolean addStation(int i,String name){
+        if(i<MIN_ARRAY_INDEX||i>stations.size()){
+            return false;
+        }
+        if(name.length()<MIN_STATION_NAME_LENGTH){
+            return false;
+        }
+        stations.add(i,new Station(name));
+        return true;
+    }
+
+    public void addAllStation(String...names){
+        Arrays.stream(names)
+                .forEach(name->stations.add(new Station(name)));
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
 }
