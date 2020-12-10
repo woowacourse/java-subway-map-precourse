f0e660095c0cef774c7d67f32cec548ab37bc515
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-12-11 01:13:14 +0900

    feat(Application): main 클래스

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,17 @@
 package subway;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.TrainRouteMap;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        TrainRouteMap trainRouteMap=new TrainRouteMap(scanner);
+        trainRouteMap.run();
     }
 }
