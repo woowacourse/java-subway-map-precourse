72f8e171bd4b064d301410adb01a63ba8580dd6b
sakjung <58401309+sakjung@users.noreply.github.com>
2020-12-10 15:44:25 +0900

    feat: Add features to validate line name

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,12 +1,18 @@
 package subway.domain;
 
+import subway.view.StationMessages;
+
 import java.util.List;
 
 public class Line {
+    public static final int NAME_LENGTH_LOWER_BOUND = 2;
+
     private String name;
     private List<Station> stations;
 
-    public Line(String name) {
+    public Line(String name) throws IllegalArgumentException {
+        validateDuplicate(name);
+        validateNameLength(name);
         this.name = name;
     }
 
@@ -14,4 +20,21 @@ public class Line {
         return name;
     }
 
+    private boolean hasName(String name) {
+        return LineRepository.lines().stream()
+                .map(Line::getName)
+                .anyMatch(stationName -> stationName.equals(name));
+    }
+
+    private void validateDuplicate(String name) throws IllegalArgumentException {
+        if (hasName(name)) {
+            throw new IllegalArgumentException(StationMessages.DUPLICATE_NAME_ERROR.getMessage());
+        }
+    }
+
+    private void validateNameLength(String name) throws IllegalArgumentException {
+        if (name.length() < NAME_LENGTH_LOWER_BOUND) {
+            throw new IllegalArgumentException(StationMessages.NAME_LENGTH_ERROR.getMessage());
+        }
+    }
 }
