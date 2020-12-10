8084924e7b91a8da8f558632c5e8c96ff830a300
483759 <483759@naver.com>
2020-12-10 20:53:08 +0900

    feat(PrintUtils) : implement printMainMenu

--- src/main/java/utils/PrintUtils.java
+++ src/main/java/utils/PrintUtils.java
@@ -8,4 +8,12 @@ package utils;
  */
 public class PrintUtils {
 
+    public void printMainMenu(){
+        System.out.println("## 메인 화면");
+        System.out.println("1. 역 관리");
+        System.out.println("2. 노선 관리");
+        System.out.println("3. 구간 관리");
+        System.out.println("4. 지하철 노선도 출력");
+        System.out.println("Q. 종료");
+    }
 }
