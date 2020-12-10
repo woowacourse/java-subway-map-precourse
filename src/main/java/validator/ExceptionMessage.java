543eb403e73bf565720c421b5a7a1329e2ed6b96
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-11 01:28:54 +0900

    feat: 예러 출력문 수정

--- src/main/java/validator/ExceptionMessage.java
+++ src/main/java/validator/ExceptionMessage.java
@@ -1,8 +1,8 @@
 package validator;
 
 public class ExceptionMessage {
-    public static final String IS_NOT_VALID_FUNCTION = "[ERROR] 유효한 기능이 아닙니다";
-    public static final String INPUT_OVER_TWO = "[ERROR] 역 이름은 2자 이상이여야합니다.";
-    public static final String STATION_NAME_EXISTS = "[ERROR] 동일한 이름의 역이 존재합니다.";
+    public static final String IS_NOT_VALID_FUNCTION = "[ERROR] 선택할 수 없는 기능입니다";
+    public static final String INPUT_OVER_TWO = "[ERROR] 역 이름은 2글자 이상이여야 합니다.";
+    public static final String STATION_NAME_EXISTS = "[ERROR] 이미 등록된 역 이름입니다.";
     public static final String NOT_EXIST_STATION = "[ERROR] 삭제할 역이 존재하지 않습니다.";
 }
