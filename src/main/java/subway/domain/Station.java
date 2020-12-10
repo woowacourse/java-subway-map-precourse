d6d9118d7663198098d8a6fcdc50d85e3cad205f
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-12-10 22:58:15 +0900

    feat(Station): 역 이름 길이 예외처리

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,6 +1,12 @@
 package subway.domain;
 
+import subway.view.OutputView;
+
+
+
 public class Station {
+    private static final String NAME_LENGTH_ERROR="역 이름은 2글자 이상입니다.";
+    private static final Integer MIN_NAME_LENGTH=2;
     private String name;
 
     public Station(String name) {
@@ -12,4 +18,7 @@ public class Station {
     }
 
     // 추가 기능 구현
+    public static boolean isValidName(String name){
+        return name.length()>=MIN_NAME_LENGTH;
+    }
 }
