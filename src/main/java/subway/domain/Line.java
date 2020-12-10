5b9c159da1957c2f97adb98d0adc6ebfb4db67f8
JinseongHwang <eddy5360@naver.com>
2020-12-10 16:52:21 +0900

    feat: 노선이름, 상행종점, 하행종점, 등록된 역 인스턴스 변수 생성

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,37 @@
 package subway.domain;
 
+import java.util.LinkedList;
+
 public class Line {
     private String name;
+    private String northboundTerminal;
+    private String southboundTerminal;
+    private LinkedList<String> containedStation = new LinkedList<String>();
 
-    public Line(String name) {
+    public Line(String name, String northboundTerminal, String southboundTerminal) {
         this.name = name;
+        this.northboundTerminal = northboundTerminal;
+        this.southboundTerminal = southboundTerminal;
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    public String getNorthboundTerminal() {
+        return northboundTerminal;
+    }
+
+    public String getSouthboundTerminal() {
+        return southboundTerminal;
+    }
+
+    public LinkedList<String> getContainedStation() {
+        return containedStation;
+    }
+
+    public void appendStation(String stationName) {
+        containedStation.add(stationName);
+    }
+
 }
