90de06230b2fa936e12470eaef8562718d6576f6
KJunseo <max9106@naver.com>
2020-12-11 00:31:29 +0900

    feat: 지하철 노선 초기 세팅 기능

---
+++ src/main/java/subway/line/LineInitialization.java
@@ -0,0 +1,37 @@
+package subway.line;
+
+import subway.station.Station;
+import utils.StringSeparation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LineInitialization {
+    private static final String lineNumberTwo = "2호선, 교대역, 강남역, 역삼역";
+    private static final String lineNumberThree = "3호선, 교대역, 남부터미널역, 양재역, 매봉역";
+    private static final String dxLine = "신분당선, 강남역, 양재역, 양재시민의숲역";
+    private static final List<String> basicLineList = Arrays.asList(lineNumberTwo, lineNumberThree, dxLine);
+
+    public static void setBasicLines() {
+        for (String lineComponent : basicLineList) {
+            List<String> stations = Arrays.asList(StringSeparation.stringToArray(lineComponent));
+            String lineName = stations.get(0);
+            EachLineStations lineStations = new EachLineStations(getStationList(stations, lineName));
+            Line line = new Line(lineName, lineStations);
+            LineRepository.addLine(line);
+        }
+    }
+
+    private static List<Station> getStationList(List<String> lineComponent, String lineName) {
+        List<Station> stations = new ArrayList<>();
+
+        for (String stationName : lineComponent) {
+            if (!stationName.equals(lineName)) {
+                stations.add(new Station(stationName));
+            }
+        }
+
+        return stations;
+    }
+}
