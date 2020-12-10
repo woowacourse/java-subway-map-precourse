b7cca06fcc39b73be3026b8c7ba78f65cd8d41d8
KJunseo <max9106@naver.com>
2020-12-10 23:10:57 +0900

    feat: 지하철 역 초기 세팅 기능

---
+++ src/main/java/subway/station/StationInitialization.java
@@ -0,0 +1,18 @@
+package subway.station;
+
+import utils.StringSeparation;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StationInitialization {
+    private static final String basicStationsName = "교대역, 강남역, 역삼역, 남부터미널역, 양재역, 양재시민의숲역, 매봉역";
+    private static final List<String> basicStations = Arrays.asList(StringSeparation.stringToArray(basicStationsName));
+
+    public static void setBasicStations() {
+        for (String name : basicStations) {
+            Station station = new Station(name);
+            StationRepository.addStation(station);
+        }
+    }
+}
