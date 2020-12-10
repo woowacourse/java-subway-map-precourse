d73865747feea8e1e0a6d92c2b039a0f54b9c842
483759 <483759@naver.com>
2020-12-10 21:38:01 +0900

    feat(InputUtils) : add InputUtils class

---
+++ src/main/java/subway/utils/InputUtils.java
@@ -0,0 +1,17 @@
+package subway.utils;
+
+import java.util.Scanner;
+
+/**
+ * 프로그램의 입력 부분을 담당하는 클래스
+ *
+ * @author 483759@naver.com / 윤이진
+ * @version 1.0 2020/12/10
+ */
+public class InputUtils {
+    private final Scanner scanner;
+
+    public InputUtils(){
+        scanner = new Scanner(System.in);
+    }
+}
