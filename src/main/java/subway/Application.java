d9121bd062d8b439b10954d1283daa3a1641075e
483759 <483759@naver.com>
2020-12-10 21:26:07 +0900

    feat(Application) : implement mainMenu

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,21 @@
 package subway;
 
 import java.util.Scanner;
+import utils.PrintUtils;
 
 public class Application {
+
+    private static PrintUtils printUtils;
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        printUtils = new PrintUtils();
+
+        mainMenu();
+    }
+
+    public static void mainMenu(){
+        printUtils.printMainMenu();
+        printUtils.printSelectFunction();
     }
 }
