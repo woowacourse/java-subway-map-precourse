98ba1addc8351a23f4b61aab558e9dfbb9ead5a7
tributetothemoon <kkm97351@gmail.com>
2020-12-11 03:04:36 +0900

    feat(Station): 이름 길이를 검증하고 객체를 생성하는 로직 작성
    - 이름이 2글자 미만인 경우는 생성할 수 없다는 검증 로직을 구현했습니다.

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,9 +1,15 @@
 package subway.domain;
 
 public class Station {
+    private static final int MIN_LENGTH = 2;
+    private static final String INVALID_LENGTH_FORMAT = "이름은 %d 글자 이상이어야 합니다.";
+
     private String name;
 
     public Station(String name) {
+        if (name.length() < MIN_LENGTH) {
+            throw new IllegalArgumentException(String.format(INVALID_LENGTH_FORMAT, MIN_LENGTH));
+        }
         this.name = name;
     }
 
