4703539be959397a5b4fad1189306a2b49773a61
KJunseo <max9106@naver.com>
2020-12-11 00:28:43 +0900

    feat: 각 호선에 포함된 지하철역의 리스트를 나타내는 클래스 추가

---
+++ src/main/java/subway/line/EachLineStations.java
@@ -0,0 +1,13 @@
+package subway.line;
+
+import subway.station.Station;
+
+import java.util.List;
+
+public class EachLineStations {
+    private List<Station> stations;
+
+    public EachLineStations(List<Station> stations) {
+        this.stations = stations;
+    }
+}
