f2872f23a3b5450ce00c686cdda05b50ae6f0896
tributetothemoon <kkm97351@gmail.com>
2020-12-11 03:25:12 +0900

    feat(StationRepository): 등록된 역을 삭제하는 로직 구현
    - 삭제하는 과정에서 해당 역이 없는 경우, 예외를 발생시킵니다.

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class StationRepository {
     private static final String ERROR_ALREADY_EXIST= "이미 등록된 역입니다.";
+    private static final String ERROR_NOT_EXIST= "등록되지 않은 역입니다.";
 
     private static final List<Station> stations = new ArrayList<>();
 
@@ -28,6 +29,10 @@ public class StationRepository {
     }
 
     public static boolean deleteStation(String name) {
-        return stations.removeIf(station -> Objects.equals(station.getName(), name));
+        boolean removed = stations.removeIf(station -> Objects.equals(station.getName(), name));
+        if (!removed) {
+            throw new IllegalArgumentException(ERROR_NOT_EXIST);
+        }
+        return true;
     }
 }
