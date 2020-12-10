6b9a5e3a60891f55b827f07c7b7034b9ccf2737b
tributetothemoon <kkm97351@gmail.com>
2020-12-11 08:34:08 +0900

    feat(Line): 인스턴스 변수로 List<Station> 추가, 노선에 역 추가 기능 구현
    - 각 노선은 자신의 역들을 가져야하므로, 인스턴스 변수로 List<Station>을 추가했습니다.
    - Station리스트에 Station을 추가하는 로직을 추가했습니다. 추가 과정에서 이미 노선에 등록된 역인지에 대한 검증을 실행합니다.

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,16 +1,29 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private static final String INVALID_LENGTH_FORMAT = "이름은 %d 글자 이상이어야 합니다.";
+    private static final String ERROR_ALREADY_EXIST = "이미 노선에 등록된 역입니다.";
     private static final int MIN_LENGTH = 2;
 
     private String name;
+    private List<Station> stations;
 
     public Line(String name) {
         if (name.length() < MIN_LENGTH) {
             throw new IllegalArgumentException(String.format(INVALID_LENGTH_FORMAT, MIN_LENGTH));
         }
         this.name = name;
+        this.stations = new ArrayList<>();
+    }
+
+    public void addStation(Station station) {
+        if (stations.contains(station)) {
+            throw new IllegalArgumentException(ERROR_ALREADY_EXIST);
+        }
+        stations.add(station);
     }
 
     public String getName() {
