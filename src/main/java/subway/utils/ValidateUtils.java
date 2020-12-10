15650a83ab2bc9c0520c238da03755c1d5624009
483759 <483759@naver.com>
2020-12-10 22:28:09 +0900

    feat(ValidateUtils) : implement isValidMenuSelect

--- src/main/java/subway/utils/ValidateUtils.java
+++ src/main/java/subway/utils/ValidateUtils.java
@@ -8,4 +8,14 @@ package subway.utils;
  */
 public class ValidateUtils {
 
+    public boolean isValidMenuSelect(String selectMenu, int bound, char quit) {
+        if (selectMenu.length() > 1) {
+            return false;
+        }
+        char selectChar = selectMenu.charAt(0);
+        if (selectChar == quit || (selectChar >= '1' && selectChar <= bound+'0')) {
+            return true;
+        }
+        return false;
+    }
 }
