9540f7d98b5542a7711af56d912870dc4bb5e8d3
opdshe <opdshe@naver.com>
2020-12-11 15:13:47 +0900

    feat: 역 관련 기능을 입력받는 기능

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_STATION_NAME_FOR_REGISTRATION = "등록할 역 이름을 입력하세요.";
     private static final String INPUT_STATION_NAME_FOR_REMOVAL = "삭제할 역 이름을 입력하세요.";
+    private static final String INPUT_FUNCTION = "원하는 기능을 선택하세요.";
 
     private InputView() {
     }
@@ -17,6 +18,11 @@ public class InputView {
 
     public static String getStationNameForRemoval() {
         OutputView.printGuideMessage(INPUT_STATION_NAME_FOR_REMOVAL);
+        return SCANNER.nextLine();
+    }
+
+    public static String getStationFunction() {
+        OutputView.printGuideMessage(INPUT_FUNCTION);
         return SCANNER.nextLine();
     }
 }
