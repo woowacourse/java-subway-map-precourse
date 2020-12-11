ecee44f8b0f4f475cc4170d0e3798330a9b0a7ec
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-11 22:10:20 +0900

    feat: 구간 등록된 Station 을 관리하기 위한 클래스 생성

---
+++ src/main/java/subway/domain/SectionStation.java
@@ -0,0 +1,11 @@
+package subway.domain;
+
+public class SectionStation {
+    private Station station;
+    private int index;
+
+    public SectionStation(Station station, int index) {
+        this.station = station;
+        this.index = index;
+    }
+}
