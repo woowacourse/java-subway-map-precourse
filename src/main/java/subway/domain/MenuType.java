eea9e4cc54ae2af46b5d9cd3b6e4e181b957a7d5
doorisopen <audauddl2@gmail.com>
2020-12-11 16:44:10 +0900

    feat: 메뉴 범위 추가

--- src/main/java/subway/domain/MenuType.java
+++ src/main/java/subway/domain/MenuType.java
@@ -1,20 +1,24 @@
 package subway.domain;
 
 public enum MenuType {
+    MAIN_MENU_RANGE("4"),
     MAIN_STATION("1"),
     MAIN_LINE_STATION("2"),
     MAIN_SECTION("3"),
     MAIN_PRINT_LINE_STATION( "4"),
     MAIN_QUIT("Q"),
 
+    STATION_MENU_RANGE("3"),
     STATION_ADD("1"),
     STATION_DELETE("2"),
     STATION_SEARCH("3"),
 
+    LINE_STATION_MENU_RANGE("3"),
     LINE_STATION_ADD("1"),
     LINE_STATION_DELETE("2"),
     LINE_STATION_SEARCH("3"),
 
+    SECTION_MENU_RANGE("2"),
     SECTION_ADD("1"),
     SECTION_DELETE("2"),
     BACK("B");
