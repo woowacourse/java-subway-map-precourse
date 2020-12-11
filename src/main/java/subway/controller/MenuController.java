177dd5eb48f1ea4904091c1a42d7891adfea8bea
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-12 03:48:01 +0900

    feat: 입력된 메인기능 기호 유효성 검사 메소드 구현

--- src/main/java/subway/controller/MenuController.java
+++ src/main/java/subway/controller/MenuController.java
@@ -3,9 +3,12 @@ package subway.controller;
 import subway.view.InputView;
 import subway.view.OutputView;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class MenuController {
+    private static final List<String> MAIN_MENU_SIGN = Arrays.asList(new String[] {"1","2","3","4","Q"});
 
     public static String selectMainMenu(Scanner scanner) {
         OutputView.printMainScreen();
@@ -16,5 +19,9 @@ public class MenuController {
     private static String selectMenu(Scanner scanner) {
         OutputView.printMenuSelectScreen();
         return InputView.getInput(scanner);
+    }
+
+    public static boolean isValidMenu(String menu) {
+        return MAIN_MENU_SIGN.contains(menu);
     }
 }
