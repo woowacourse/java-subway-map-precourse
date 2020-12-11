a6b50cfc8b5dc72eb4f3f42101bda040a7eee0ff
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-12 01:01:21 +0900

    feat: 프로그램 실행전 생성된 구간 추가

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -12,6 +12,19 @@ public class LineRepository {
         lines.add(new Line("2호선"));
         lines.add(new Line("3호선"));
         lines.add(new Line("신분당선"));
+
+        lines.get(0).initializeSectionStation(
+                StationRepository.findStationByName("교대역"), StationRepository.findStationByName("역삼역"));
+        lines.get(0).addStation(StationRepository.findStationByName("강남역"), 1);
+
+        lines().get(1).initializeSectionStation(
+                StationRepository.findStationByName("교대역"), StationRepository.findStationByName("매봉역"));
+        lines().get(1).addStation(StationRepository.findStationByName("남부터미널역"), 1);
+        lines().get(1).addStation(StationRepository.findStationByName("양재역"), 2);
+
+        lines.get(2).initializeSectionStation(
+                StationRepository.findStationByName("강남역"), StationRepository.findStationByName("양재시민의숲역"));
+        lines.get(2).addStation(StationRepository.findStationByName("양재역"), 1);
     }
 
     public static List<Line> lines() {
