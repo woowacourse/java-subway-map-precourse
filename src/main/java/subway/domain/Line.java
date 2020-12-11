f4981d502a84b2ef8c4fe8c9eb1d9fa87f42a953
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-12-11 15:57:13 +0900

    [추가]  노선 생성자에서 노선이 포함하고 있는 지하철 역들을 지정하는 부분도 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -7,8 +7,9 @@ public class Line {
     private String name;
     private List<Station> stations = new ArrayList<>();
 
-    public Line(String name) {
+    public Line(String name, List<Station> stations) {
         this.name = name;
+        this.stations = stations;
     }
 
     public String getName() {
--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -37,4 +37,5 @@ public class LineRepository {
         }
         return false;
     }
+
 }
