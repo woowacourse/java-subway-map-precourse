f42a48b961c196a417d0cc497d785920465f18c1
doorisopen <audauddl2@gmail.com>
2020-12-11 16:51:33 +0900

    feat: 역(Station)의 비즈니스 로직 클래스 추가
    - feat: 역을 등록한다.

---
+++ src/main/java/subway/service/StationService.java
@@ -0,0 +1,37 @@
+package subway.service;
+
+import subway.domain.LineStationRepository;
+import subway.domain.MenuType;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
+import java.util.Scanner;
+
+import static subway.view.InputView.inputAddStationNameRequestMessage;
+import static subway.view.OutputView.printAddStationSuccessMessage;
+
+public class StationService {
+
+    public void selectStationManagementMenu(Scanner scanner, String menu, LineStationRepository lineStation) {
+        if(menu.equals(MenuType.STATION_ADD.getKey())) {
+            addStation(scanner);
+        }
+        if(menu.equals(MenuType.STATION_DELETE.getKey())) {
+        }
+        if(menu.equals(MenuType.STATION_SEARCH.getKey())) {
+        }
+        if(menu.equals(MenuType.BACK.getKey())) {
+            return;
+        }
+    }
+
+    private void addStation(Scanner scanner) {
+        inputAddStationNameRequestMessage();
+        String name = scanner.nextLine();
+        //validation
+        //1.이름의 길이가 2이상인지 검증한다
+        //2.중복된 역 이름이 존재하는지 검증한다
+        StationRepository.addStation(new Station(name));
+        printAddStationSuccessMessage();
+    }
+}
