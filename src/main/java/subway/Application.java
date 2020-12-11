6a734f6f4a08e354411575f708763b9d16e28968
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-11 21:18:52 +0900

    feat(OutputView) : 역 관리, 노선 관리, 구간 관리 화면 출력

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -13,14 +13,15 @@ public class Application {
 
         while (true) {
             OutputView.printMainFunctions();
-            String mainFunctionsNumber = InputView.inputFunctionNumber(scanner);
+            String mainFunctionNumber = InputView.inputFunctionNumber(scanner);
             // 메인 화면에서 종료
-            if (mainFunctionsNumber.equals("Q")) {
+            if (mainFunctionNumber.equals("Q")) {
                 break;
             }
-            Functions mainFunctions = machine.selectMainFunctions(mainFunctionsNumber);
+            Functions mainFunctions = machine.selectMainFunctions(mainFunctionNumber);
 
             // 서브 기능
+            OutputView.printFunctions(mainFunctionNumber);
             String functionNumber = InputView.inputFunctionNumber(scanner);
             Function function = mainFunctions.selectFunction(functionNumber);
             System.out.println(function);
--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -14,4 +14,52 @@ public class OutputView {
     private static void printEmptyLine() {
         System.out.println();
     }
+
+    public static void printFunctions(String mainFunctionNumber) {
+        if (mainFunctionNumber.equals("1")) {
+            printStationFunctions();
+            return;
+        }
+        if (mainFunctionNumber.equals("2")) {
+            printLineFunctions();
+            return;
+        }
+        if (mainFunctionNumber.equals("3")) {
+            printSectionFunctions();
+            return;
+        }
+        if (mainFunctionNumber.equals("4")) {
+            printSubwayMap();
+            return;
+        }
+    }
+
+    private static void printStationFunctions() {
+        System.out.println("## 역 관리 화면");
+        System.out.println("1. 역 등록");
+        System.out.println("2. 역 삭제");
+        System.out.println("3. 역 조회");
+        System.out.println("B. 돌아가기");
+        printEmptyLine();
+    }
+
+    private static void printLineFunctions() {
+        System.out.println("## 노선 관리 화면");
+        System.out.println("1. 노선 등록");
+        System.out.println("2. 노선 삭제");
+        System.out.println("3. 노선 조회");
+        System.out.println("B. 돌아가기");
+        printEmptyLine();
+    }
+
+    private static void printSectionFunctions() {
+        System.out.println("## 구간 관리 화면");
+        System.out.println("1. 구간 등록");
+        System.out.println("2. 구간 삭제");
+        System.out.println("B. 돌아가기");
+    }
+
+    private static void printSubwayMap() {
+        System.out.println("## 지하철 노선도");
+    }
 }
