f31932ae8097c19335d2823a9b30176359055b79
Naeun <knae11@naver.com>
2020-12-11 22:51:45 +0900

    feat: 역조회 기능추가, 역등록 확인문구 추가

--- src/main/java/subway/controller/StationSystem.java
+++ src/main/java/subway/controller/StationSystem.java
@@ -1,6 +1,7 @@
 package subway.controller;
 
 import java.util.Scanner;
+import javax.sound.sampled.Line.Info;
 import subway.domain.MenuItemsRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
@@ -32,24 +33,32 @@ public class StationSystem {
     private void runSystemByInput(String input) {
         if (input.equals("1")) {
             addStation();
-            System.out.println("역 등록");
         }
         if (input.equals("2")) {
+            removeStation();
             System.out.println("역 삭제");
         }
         if (input.equals("3")) {
-            System.out.println("역 조회");
+            lookupStations();
         }
+    }
+
+    private void lookupStations() {
+        for (Station station : StationRepository.stations()) {
+            InfoMessage.printName(station.getName());
+        }
+    }
+
+    private void removeStation() {
     }
 
     private void addStation() {
         String name = stationInputManager.getStationName("등록할");
         Station station = new Station(name);
         StationRepository.addStation(station);
-        for( Station what : StationRepository.stations()){
-            InfoMessage.printInfo(what.getName());
-        }
+        InfoMessage.printStationAdded();
 
     }
+
 
 }
--- src/main/java/subway/view/InfoMessage.java
+++ src/main/java/subway/view/InfoMessage.java
@@ -2,12 +2,16 @@ package subway.view;
 
 public class InfoMessage {
     private static final String INFO_PREFIX= "[INFO] ";
+    private static final String STATION_ADDED= "지하철 역이 등록되었습니다.";
+
     private InfoMessage(){
     }
 
-    public static void printInfo(String message){
-        System.out.println(INFO_PREFIX+message);
+    public static void printName(String name){
+        System.out.println(INFO_PREFIX+name);
     }
-
+    public static void printStationAdded(){
+        System.out.println(INFO_PREFIX+STATION_ADDED);
+    }
 
 }
