77b49e9d67bb223e11099c73a96648206767e67e
doorisopen <audauddl2@gmail.com>
2020-12-11 21:46:53 +0900

    test: 입력값 길이가 2 이상인지 검증한다

--- src/test/java/subway/utils/InputValidationTest.java
+++ src/test/java/subway/utils/InputValidationTest.java
@@ -24,4 +24,16 @@ class InputValidationTest {
         assertThat(isContainsQ).isTrue();
         assertThat(isContainsB).isFalse();
     }
+
+    @Test
+    public void 입력값_길이가_2_이상인지_검증한다() throws Exception {
+        //given
+        String name = "서울역";
+        String name2 = "중역";
+        //when
+
+        //then
+        assertThat(name.length()).isGreaterThan(1);
+        assertThat(name2.length()).isGreaterThan(1);
+    }
 }
