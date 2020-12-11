25cb23493966fd921918d204ad0d5ae4ebd7c325
perenok <perenoke@gmail.com>
2020-12-11 12:42:54 +0900

    feat(View): implement printMain

---
+++ src/main/java/subway/view/View.java
@@ -0,0 +1,14 @@
+package subway.view;
+
+
+public class View {
+    public void printMain(){
+        System.out.println("## 메인 화면");
+        System.out.println("1. 역 관리");
+        System.out.println("2. 노선 관리");
+        System.out.println("3. 구간 관리");
+        System.out.println("4. 지하철 노선도 출력");
+        System.out.println("Q. 종료");
+    }
+
+}
