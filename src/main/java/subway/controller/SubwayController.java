9c77e8c72821d6305a3712af92c80fbd1d0f0c29
daeun <leede0418@likelion.org>
2020-12-11 20:31:30 +0900

    feat(controller): initialize stations, lines
    - [초기 설정] 지하철 역을 초기 설정하는 기능 구현
    - [초기 설정] 지하철 노선을 초기 설정하는 기능 구현

--- src/main/java/subway/controller/SubwayController.java
+++ src/main/java/subway/controller/SubwayController.java
@@ -1,9 +1,17 @@
 package subway.controller;
 
+import subway.service.LineService;
+import subway.service.StationService;
 import subway.view.OutputView;
 
 public class SubwayController {
     public static void runSubway() {
+        initializeSubway();
         OutputView.printMainScreen();
+    }
+
+    public static void initializeSubway() {
+        StationService.initializeStations();
+        LineService.initializeLines();
     }
 }
