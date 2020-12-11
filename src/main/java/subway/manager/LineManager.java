81dc10531a9ca15f7fdab54d506d01610bae8421
JinseongHwang <eddy5360@naver.com>
2020-12-11 18:45:40 +0900

    feat: 각 화면들을 관리하는 Manager패키지 생성

---
+++ src/main/java/subway/manager/LineManager.java
@@ -0,0 +1,23 @@
+package subway.manager;
+
+import view.InputView;
+import view.OutputView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LineManager {
+
+    private static final List<String> choices = Arrays.asList("1", "2", "3", "B");
+
+    public static void execute() {
+        OutputView.printLineManagement();
+        String command = InputView.getFunctionSelect(choices);
+        nextProcedure(command);
+    }
+
+    public static void nextProcedure(String command) {
+        // 다른 manager execute 호출
+    }
+
+}
