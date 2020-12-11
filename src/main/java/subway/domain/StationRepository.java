7b06c8e5be1f86cfbeece2f86b476b9fde463a7d
Hyeon9mak <jinha3507@gmail.com>
2020-12-11 17:32:39 +0900

    feat: add Station object validate method
    add validate Station object last name 역

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -7,23 +7,40 @@ import java.util.Objects;
 
 public class StationRepository {
     private static final String WHITE_SPACE = " ";
-    private static final int MINIMUM_NAME_LENGTH = 2;
+    private static final int EMPTY_STATIONS = 0;
+    private static final int LAST_INDEX = 1;
+    private static final int MINIMUM_NAME_LENGTH = 3;
+    private static final String RULE_STATION_NAME = "역";
 
     private static final List<Station> stations = new ArrayList<>();
 
     public static List<Station> stations() {
+        validateStationsEmpty();
         return Collections.unmodifiableList(stations);
     }
 
     public static void addStation(String name) {
+        validateNameLastCharacter(name);
         validateNameContainWhitepace(name);
-        validateNameDuplicate(name);
         validateNameLength(name);
+        validateNameDuplicate(name);
         stations.add(new Station(name));
     }
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    private static void validateStationsEmpty() {
+        if (stations.size() == EMPTY_STATIONS) {
+            throw new IllegalArgumentException("등록 되어 있는 지하철 역이 없습니다.");
+        }
+    }
+
+    private static void validateNameLastCharacter(String name) {
+        if (!name.substring(name.length()-LAST_INDEX).equals(RULE_STATION_NAME)){
+            throw new IllegalArgumentException("지하철 역 이름은 \"역\"으로 끝나야 합니다.");
+        }
     }
 
     private static void validateNameContainWhitepace(String name) {
@@ -32,16 +49,16 @@ public class StationRepository {
         }
     }
 
+    private static void validateNameLength(String name) {
+        if (name.length() < MINIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException("지하철 역 이름은 2글자 이상 + \"역\"으로 이루어져야 합니다.");
+        }
+    }
+
     private static void validateNameDuplicate(String name) {
         if (stations.stream()
             .anyMatch(station -> station.getName().equals(name))) {
             throw new IllegalArgumentException("동일한 지하철 역 이름이 존재합니다.");
-        }
-    }
-
-    private static void validateNameLength(String name) {
-        if (name.length() < MINIMUM_NAME_LENGTH) {
-            throw new IllegalArgumentException("지하철 역 이름은 2글자 이상이어야 합니다.");
         }
     }
 }
