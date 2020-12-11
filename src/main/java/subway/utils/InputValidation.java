a0f00e84ef8efe0efbacae95e53f5bb38d8c4a22
doorisopen <audauddl2@gmail.com>
2020-12-11 21:51:08 +0900

    feat: 입력값 길이가 2 이상인지 검증한다

--- src/main/java/subway/utils/InputValidation.java
+++ src/main/java/subway/utils/InputValidation.java
@@ -3,11 +3,17 @@ package subway.utils;
 import java.util.List;
 
 public class InputValidation {
-
+    private static final int NAME_MIN_LENGTH = 2;
     public void validateMenuRange(List<String> menuRange, String menu) {
         boolean isContains = menuRange.contains(menu);
         if(!isContains) {
             throw new IllegalArgumentException("[ERROR] 선택할 수 없는 기능입니다.");
+        }
+    }
+
+    public void validateNameLengthIsMoreThan2(String name) {
+        if(name.length() < NAME_MIN_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 이름의 길이는 최소 2이상이어야 합니다.");
         }
     }
 }
