1d137140757fc3aed91e4cf2533292b5fd7f6c4e
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-12 01:47:45 +0900

    feat(Line) : 노선 이름은 2글자 이상이어야 하는 예외처리 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -3,12 +3,20 @@ package subway.domain;
 import java.util.List;
 
 public class Line {
+    private static final int MIN_LENGTH_OF_LINE_NAME = 2;
     private String name;
     private List<Station> stations;
 
     public Line(String name, List<Station> stations) {
+        validateLineName(name);
         this.name = name;
         this.stations = stations;
+    }
+
+    private void validateLineName(String name) {
+        if (name.length() < MIN_LENGTH_OF_LINE_NAME) {
+            throw new IllegalArgumentException("[ERROR] 지하철 노선 이름은 2글자 이상이어야 합니다.");
+        }
     }
 
     public String getName() {
@@ -23,6 +31,7 @@ public class Line {
         }
         return false;
     }
+
 
     @Override
     public String toString() {
