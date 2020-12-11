564cdc351d51afb41319f23d2772023bfb2f0e6f
yangfriendship <yangfriendship@naver.com>
2020-12-12 02:38:38 +0900

    feat (InputView) : 사용자에게 입력값을 받을 InputView 생성 및 테스트

---
+++ src/main/java/subway/view/InputView.java
@@ -0,0 +1,30 @@
+package subway.view;
+
+import subway.exception.NoInitialScannerException;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private Scanner scanner;
+    private static InputView instance;
+
+    private InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public static void initScanner(Scanner scanner) {
+        instance = new InputView(scanner);
+    }
+
+    public static InputView getInstance() {
+
+        if (instance == null) {
+            throw new NoInitialScannerException();
+        }
+
+        return instance;
+    }
+
+
+}
