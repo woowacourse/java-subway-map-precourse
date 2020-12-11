63a50fc345ec02777198e15485adc373150aa7ca
RinSabbit <celena1004@naver.com>
2020-12-11 21:43:48 +0900

    refactor: OutputView 클래스의 상수 String을 따로 분리

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,70 +1,22 @@
 package subway.view;
 
+import static subway.view.TextFixtures.*;
 import subway.domain.Line;
 import subway.domain.LineRepository;
 import subway.domain.StationRepository;
+import subway.exception.SubwayCustomException;
 
 /**
  * 결과에 따른 출력을 하는 클래스
  */
 public class OutputView {
 
-    private static final String INFORMATION_PREFIX = "[INFO] ";
-    private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String SECTION_LINE = "---";
-    private static final String MENU_PREFIX = "## ";
-    private static final String MAIN_MENU_MESSAGE = "메인화면 \n"
-        + "1. 역 관리 \n"
-        + "2. 노선 관리 \n"
-        + "3. 구간 관리 \n"
-        + "4. 지하철 노선도 출력 \n"
-        + "Q. 종료\n";
-    private static final String STATION_MENU_MESSAGE = "역 관리 화면 \n"
-        + "1. 역 등록 \n"
-        + "2. 역 삭제 \n"
-        + "3. 역 조회 \n"
-        + "B. 돌아가기 \n";
-    private static final String LINE_MENU_MESSAGE = "노선 관리 화면 \n"
-        + "1. 노선 등록 \n"
-        + "2. 노선 삭제 \n"
-        + "3. 노선 조회 \n"
-        + "B. 돌아가기 \n";
-    private static final String SECTION_MENU_MESSAGE = "구간 관리 화면\n"
-        + "1. 구간 등록\n"
-        + "2. 구간 삭제\n"
-        + "B. 돌아가기\n";
-    private static final String MENU_CATEGORY_CHOICE_MESSAGE = "원하는 기능을 선택하세요.";
-    private static final String INPUT_STATION_NAME_MESSAGE = "등록할 역 이름을 입력하세요.";
-    private static final String INPUT_STATION_CHECK_MESSAGE = "지하철 역이 등록되었습니다.";
-    private static final String REMOVE_STATION_NAME_MESSAGE = "삭제할 역 이름을 입력하세요.";
-    private static final String REMOVE_STATION_CHECK_MESSAGE = "지하철 역이 삭제되었습니다.";
-    private static final String SHOW_STATION_LIST_MESSAGE = "역 목록";
-
-    private static final String INPUT_LINE_NAME_MESSAGE = "등록할 노선의 이름을 입력하세요.";
-    private static final String INPUT_LINE_START_STATION_NAME_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
-    private static final String INPUT_LINE_END_STATION_NAME_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
-    private static final String INPUT_LINE_CHECK_MESSAGE = "지하철 노선이 등록되었습니다.";
-    private static final String REMOVE_LINE_NAME_MESSAGE = "삭제할 노선 이름을 입력하세요.";
-    private static final String REMOVE_LINE_CHECK_MESSAGE = "지하철 노선이 삭제되었습니다.";
-    private static final String SHOW_LINE_LIST_MESSAGE = "노선 목록";
-
-    private static final String INPUT_SECTION_LINE_MESSAGE = "노선을 입력하세요.";
-    private static final String INPUT_SECTION_STATION_MESSAGE = "역이름을 입력하세요.";
-    private static final String INPUT_SECTION_POSITION_MESSAGE = "순서를 입력하세요.";
-    private static final String INPUT_SECTION_CHECK_MESSAGE = "구간이 등록되었습니다.";
-    private static final String REMOVE_SECTION_LINE_MESSAGE = "삭제할 구간의 노선을 입력하세요.";
-    private static final String REMOVE_SECTION_STATION_MESSAGE = "삭제할 구간의 역을 입력하세요.";
-    private static final String REMOVE_SECTION_CHECK_MESSAGE = "구간이 삭제되었습니다.";
-
-    private static final String SUBWAY_LINE_MAP = "지하철 노선도";
-
     private static void showInfoMessage(String message) {
         System.out.println(INFORMATION_PREFIX + message);
     }
 
-    public static String showErrorMessage(String message) {
-        System.out.println(ERROR_PREFIX + message);
-        return message;
+    public static void showErrorMessage(SubwayCustomException exception) {
+        System.out.println(ERROR_PREFIX + exception.getMessage() + System.lineSeparator());
     }
 
     private static void showMenuMessage(String message) {
---
+++ src/main/java/subway/view/TextFixtures.java
@@ -0,0 +1,54 @@
+package subway.view;
+
+public class TextFixtures {
+
+    public static final String INFORMATION_PREFIX = "[INFO] ";
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String SECTION_LINE = "---";
+    public static final String MENU_PREFIX = "## ";
+    public static final String MAIN_MENU_MESSAGE = "메인화면 \n"
+        + "1. 역 관리 \n"
+        + "2. 노선 관리 \n"
+        + "3. 구간 관리 \n"
+        + "4. 지하철 노선도 출력 \n"
+        + "Q. 종료\n";
+    public static final String STATION_MENU_MESSAGE = "역 관리 화면 \n"
+        + "1. 역 등록 \n"
+        + "2. 역 삭제 \n"
+        + "3. 역 조회 \n"
+        + "B. 돌아가기 \n";
+    public static final String LINE_MENU_MESSAGE = "노선 관리 화면 \n"
+        + "1. 노선 등록 \n"
+        + "2. 노선 삭제 \n"
+        + "3. 노선 조회 \n"
+        + "B. 돌아가기 \n";
+    public static final String SECTION_MENU_MESSAGE = "구간 관리 화면\n"
+        + "1. 구간 등록\n"
+        + "2. 구간 삭제\n"
+        + "B. 돌아가기\n";
+    public static final String MENU_CATEGORY_CHOICE_MESSAGE = "원하는 기능을 선택하세요.";
+    public static final String INPUT_STATION_NAME_MESSAGE = "등록할 역 이름을 입력하세요.";
+    public static final String INPUT_STATION_CHECK_MESSAGE = "지하철 역이 등록되었습니다.";
+    public static final String REMOVE_STATION_NAME_MESSAGE = "삭제할 역 이름을 입력하세요.";
+    public static final String REMOVE_STATION_CHECK_MESSAGE = "지하철 역이 삭제되었습니다.";
+    public static final String SHOW_STATION_LIST_MESSAGE = "역 목록";
+
+    public static final String INPUT_LINE_NAME_MESSAGE = "등록할 노선의 이름을 입력하세요.";
+    public static final String INPUT_LINE_START_STATION_NAME_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
+    public static final String INPUT_LINE_END_STATION_NAME_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
+    public static final String INPUT_LINE_CHECK_MESSAGE = "지하철 노선이 등록되었습니다.";
+    public static final String REMOVE_LINE_NAME_MESSAGE = "삭제할 노선 이름을 입력하세요.";
+    public static final String REMOVE_LINE_CHECK_MESSAGE = "지하철 노선이 삭제되었습니다.";
+    public static final String SHOW_LINE_LIST_MESSAGE = "노선 목록";
+
+    public static final String INPUT_SECTION_LINE_MESSAGE = "노선을 입력하세요.";
+    public static final String INPUT_SECTION_STATION_MESSAGE = "역이름을 입력하세요.";
+    public static final String INPUT_SECTION_POSITION_MESSAGE = "순서를 입력하세요.";
+    public static final String INPUT_SECTION_CHECK_MESSAGE = "구간이 등록되었습니다.";
+    public static final String REMOVE_SECTION_LINE_MESSAGE = "삭제할 구간의 노선을 입력하세요.";
+    public static final String REMOVE_SECTION_STATION_MESSAGE = "삭제할 구간의 역을 입력하세요.";
+    public static final String REMOVE_SECTION_CHECK_MESSAGE = "구간이 삭제되었습니다.";
+
+    public static final String SUBWAY_LINE_MAP = "지하철 노선도";
+
+}
