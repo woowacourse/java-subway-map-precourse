e5345bb66f8db659eaa61586f80ec81121aedc69
doorisopen <audauddl2@gmail.com>
2020-12-11 22:16:20 +0900

    feat: 중복된 역 이름이 존재하는지 검증한다

--- src/main/java/subway/utils/InputValidation.java
+++ src/main/java/subway/utils/InputValidation.java
@@ -1,9 +1,13 @@
 package subway.utils;
 
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
 import java.util.List;
 
 public class InputValidation {
     private static final int NAME_MIN_LENGTH = 2;
+
     public void validateMenuRange(List<String> menuRange, String menu) {
         boolean isContains = menuRange.contains(menu);
         if(!isContains) {
@@ -14,6 +18,13 @@ public class InputValidation {
     public void validateNameLengthIsMoreThan2(String name) {
         if(name.length() < NAME_MIN_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 이름의 길이는 최소 2이상이어야 합니다.");
+        }
+    }
+
+    public void validateStationNameIsDuplicate(String name) {
+        Station findStation = StationRepository.findStation(name).orElse(null);
+        if(findStation.equals(null)) {
+            throw new IllegalArgumentException("[ERROR] 이미 등록된 역 이름입니다.");
         }
     }
 }
