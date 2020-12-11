b74621a53839f499c3d6976c3f38658e1018bbba
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-11 23:23:35 +0900

    feat(Station) : 역 등록 시 이름이 2글자 이상이어야 하는 기능 추가

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,14 +1,22 @@
 package subway.domain;
 
 public class Station {
+    private static final int MIN_LENGTH_OF_STATION_NAME = 2;
     private String name;
 
     public Station(String name) {
+        validateStationName(name);
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validateStationName(String name) {
+        if (name.length() < MIN_LENGTH_OF_STATION_NAME) {
+            throw new IllegalArgumentException("역의 이름은 2글자 이상이어야 합니다.");
+        }
     }
 
     // 추가 기능 구현
