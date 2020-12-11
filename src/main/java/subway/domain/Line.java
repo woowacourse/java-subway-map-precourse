b07cf339fc0e9e51a50de9394c717ad85fcfba44
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-11 22:21:46 +0900

    feat : 사전 등록 정보로 초기 설정하기

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,10 +1,14 @@
 package subway.domain;
 
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations;
 
-    public Line(String name) {
+    public Line(String name, List<Station> stations) {
         this.name = name;
+        this.stations = stations;
     }
 
     public String getName() {
--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -1,12 +1,32 @@
 package subway.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
+
+    static {
+        lines.add(new Line("2호선", new ArrayList<Station>(Arrays.asList(
+            new Station("교대역"),
+            new Station("강남역"),
+            new Station("역삼역"))
+        )));
+        lines.add(new Line("2호선", new ArrayList<Station>(Arrays.asList(
+            new Station("교대역"),
+            new Station("남부터미널역"),
+            new Station("양재역"),
+            new Station("매봉역"))
+        )));
+        lines.add(new Line("신분당선", new ArrayList<Station>(Arrays.asList(
+            new Station("강남역"),
+            new Station("양재역"),
+            new Station("양재시민의숲역"))
+        )));
+    }
 
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
