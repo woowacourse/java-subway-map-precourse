9eb791ea22940ff6ed4a2b38a11e155ee1c5c296
SB <woltaryksb1@gmail.com>
2020-12-12 00:24:51 +0900

    feat: 구간을 더하고 삭제하는 기능 추가

--- src/main/java/subway/Board.java
+++ src/main/java/subway/Board.java
@@ -16,6 +16,8 @@ public class Board {
     private final static String LINE_ADD_MESSAGE = INFO + " 노선을 추가했습니다.";
     private final static String LINE_REMOVED_MESSAGE = INFO + " 노선을 삭제했습니다.";
 
+    private final static String SECTION_ADD_MESSAGE = INFO + " 구간을 등록했습니다.";
+
     private final static String ERROR = "[ERROR]";
 
     private final static String MINIMUM_LENGTH_ERROR = ERROR + " 이름이 너무 짧습니다.";
@@ -27,7 +29,9 @@ public class Board {
 
     private final static String SAME_STATION_IN_SECTION_ERROR = ERROR + " 이미 같은 역이 있습니다.";
     private final static String TERMINAL_DOES_NOT_EXIST_ERROR = ERROR + " 상행선 또는 하행선이 없는 역입니다.";
+    private final static String SECTION_MINIMUM_SIZE_ERROR = ERROR + " 구간을 더 이상 삭제할 수 없습니다.";
     private final static String SAME_TERMINAL_ERROR = ERROR + " 상행선과 하행선이 같은 역입니다.";
+    private final static String SECTION_INDEX_ERROR = ERROR + " 순서의 범위가 맞지 않습니다.";
 
     private final static int MIN_NAME_LENGTH = 2;
 
@@ -85,6 +89,33 @@ public class Board {
         System.out.println(LINE_REMOVED_MESSAGE);
     }
 
+    public boolean addSection(String lineName, String stationName, int index) {
+        if (!checkLineExist(lineName) || !checkStationExist(stationName)) {
+            return false;
+        }
+
+        Line line = LineRepository.findLineByName(lineName);
+        if (!checkValidSectionIndex(line, index) || checkStationDuplicate(line, stationName)) {
+            return false;
+        }
+        line.addStation(index, stationName);
+        System.out.println(SECTION_ADD_MESSAGE);
+        return true;
+    }
+
+    public boolean removeSection(String lineName, String stationName) {
+        if (!checkLineExist(lineName) || !checkStationExist(stationName)) {
+            return false;
+        }
+
+        Line line = LineRepository.findLineByName(lineName);
+        if (checkSectionRemovable(line)) {
+            return false;
+        }
+        line.removeStationByName(stationName);
+        return true;
+    }
+
     private boolean checkLineExist(String name) {
         if (!LineRepository.contains(name)) {
             System.out.println(NOT_EXIST_LINE_ERROR);
@@ -97,6 +128,30 @@ public class Board {
         if (!StationRepository.contains(name)) {
             System.out.println(NOT_EXIST_STATION_ERROR);
             return false;
+        }
+        return true;
+    }
+
+    private boolean checkSectionRemovable(Line line) {
+        if (!line.canRemove()) {
+            System.out.println(SECTION_MINIMUM_SIZE_ERROR);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkStationDuplicate(Line line, String stationName) {
+        if (line.contains(stationName)) {
+            System.out.println(SAME_STATION_IN_SECTION_ERROR);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkValidSectionIndex(Line line, int index) {
+        if (!line.isValidSection(index)) {
+           System.out.println(SECTION_INDEX_ERROR);
+           return false;
         }
         return true;
     }
