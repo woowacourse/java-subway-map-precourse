7d954d73b4bf57923f372d783b7ec32bcae6e6a2
doorisopen <audauddl2@gmail.com>
2020-12-11 22:28:03 +0900

    feat: 입력한 역 이름이 존재하는지 검증한다
    - refactor: 결과 검증 코드 수정
    - style: 코드 컨벤션 적용

--- src/main/java/subway/utils/InputValidation.java
+++ src/main/java/subway/utils/InputValidation.java
@@ -4,27 +4,35 @@ import subway.domain.Station;
 import subway.domain.StationRepository;
 
 import java.util.List;
+import java.util.Objects;
 
 public class InputValidation {
     private static final int NAME_MIN_LENGTH = 2;
 
     public void validateMenuRange(List<String> menuRange, String menu) {
         boolean isContains = menuRange.contains(menu);
-        if(!isContains) {
+        if (!isContains) {
             throw new IllegalArgumentException("[ERROR] 선택할 수 없는 기능입니다.");
         }
     }
 
     public void validateNameLengthIsMoreThan2(String name) {
-        if(name.length() < NAME_MIN_LENGTH) {
+        if (name.length() < NAME_MIN_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 이름의 길이는 최소 2이상이어야 합니다.");
         }
     }
 
     public void validateStationNameIsDuplicate(String name) {
         Station findStation = StationRepository.findStation(name).orElse(null);
-        if(findStation.equals(null)) {
+        if (Objects.equals(findStation, null)) {
             throw new IllegalArgumentException("[ERROR] 이미 등록된 역 이름입니다.");
+        }
+    }
+
+    public void validateStationNameIsContains(String name) {
+        Station findStation = StationRepository.findStation(name).orElse(null);
+        if (Objects.equals(findStation, null)) {
+            throw new IllegalArgumentException("[ERROR] 존재하지 않는 역입니다.");
         }
     }
 }
