4efab9e8ca2600cec841c5b7c48ded1d6bb5dd2a
김주원 <kjw11077@naver.com>
2020-12-11 13:16:58 +0900

    feat(view): InputView 구현
    입력값에 따라 적절한 컨트롤러에게 메시지 전달

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,12 @@
 package subway;
 
 import java.util.Scanner;
+import subway.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        InputView inputView = new InputView(scanner);
     }
 }
