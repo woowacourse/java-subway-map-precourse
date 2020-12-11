3eca1f0bf07a13851dbb16902500125cb11552f0
doorisopen <audauddl2@gmail.com>
2020-12-11 11:02:13 +0900

    feat: 노선의 비즈니스 로직 추가
    - 노선을 등록한다
    - 등록된 노선의 정보를 반환한다
    - 등록된 Line 의 Station 정보를 반환한다.
    - 등록된 노선의 개수를 반환한다.
    - 등록된 Line 의 Station 개수를 반환한다.

--- src/main/java/subway/domain/LineStationRepository.java
+++ src/main/java/subway/domain/LineStationRepository.java
@@ -1,5 +1,6 @@
 package subway.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,5 +9,29 @@ public class LineStationRepository {
 
     public LineStationRepository(Map<Line, List<Station>> lineStation) {
         this.lineStation = lineStation;
+    }
+
+    public void addLineStation(Line line, Station station) {
+        add(line, station);
+    }
+
+    private void add(Line K, Station V){
+        lineStation.computeIfAbsent(K, Ve->new ArrayList<Station>()).add(V);
+    }
+
+    public Map<Line, List<Station>> getLineStation() {
+        return lineStation;
+    }
+
+    public List<Station> getStationsOfLine(Line line) {
+        return lineStation.get(line);
+    }
+
+    public int getLineStationSize() {
+        return lineStation.size();
+    }
+
+    public int getStationSizeOfLine(Line line) {
+        return lineStation.get(line).size();
     }
 }
