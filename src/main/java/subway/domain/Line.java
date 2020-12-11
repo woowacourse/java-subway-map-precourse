a01644b8e4279cac0ef54a4746e9a682cc6d1f71
sor <bosl95@naver.com>
2020-12-12 00:03:47 +0900

    feat : [초기설정] 지하철 기본 데이터 생성 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -2,14 +2,24 @@ package subway.domain;
 
 public class Line {
     private String name;
+    private List<String> stations = new ArrayList<>();
 
-    public Line(String name) {
+    public Line(String name, String firstStation, String lastStation) {
         this.name = name;
+        stations.add(firstStation);
+        stations.add(lastStation);
     }
 
     public String getName() {
         return name;
     }
 
+    public List<String> getStations() {
+        return Collections.unmodifiableList(stations);
+    }
+
+    public void add(int index, String sectionStations) {
+        stations.add(index, sectionStations);
+    }
     // 추가 기능 구현
 }
--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -8,6 +8,25 @@ import java.util.Objects;
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
 
+    public LineRepository() {
+        init();
+    }
+
+    private void init() {
+        Line line2 = new Line("2호선", "교대역", "역삼역");
+        line2.add(1, "강남역");
+        lines.add(line2);
+
+        Line line3 = new Line("3호선", "교대역", "매봉역");
+        line3.add(1, "양재역");
+        line3.add(1, "남부터미널역");
+        lines.add(line3);
+
+        Line bundangLine = new Line("신분당선", "강남역", "양재시민의숲역");
+        bundangLine.add(1, "양재역");
+        lines.add(bundangLine);
+    }
+
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
     }
