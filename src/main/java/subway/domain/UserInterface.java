0aa691ed2544d0f3166d16662189c01c1290e7e7
Seonho Kim <zxzx235@naver.com>
2020-12-11 23:27:27 +0900

    refactor(UserInterface): method 가입력값을 리턴하도록 변경

--- src/main/java/subway/domain/UserInterface.java
+++ src/main/java/subway/domain/UserInterface.java
@@ -3,33 +3,33 @@ package subway.domain;
 import java.util.Scanner;
 
 public class UserInterface {
-    public static void mainMenu(Scanner scanner) {
+    public static String mainMenu(Scanner scanner) {
         Print.mainMenu();
-        String input = scanner.next();
+        return scanner.next();
     }
 
-    public static void stationSetting(Scanner scanner) {
+    public static String stationSetting(Scanner scanner) {
         Print.stationSetting();
-        String input = scanner.next();
+        return scanner.next();
     }
 
-    public static void lineSetting(Scanner scanner) {
+    public static String lineSetting(Scanner scanner) {
         Print.lineSetting();
-        String input = scanner.next();
+        return scanner.next();
     }
 
-    public static void sectionSetting(Scanner scanner) {
+    public static String sectionSetting(Scanner scanner) {
         Print.sectionSetting();
-        String input = scanner.next();
+        return scanner.next();
     }
 
     public static void subwayMap(Scanner scanner) {
         Print.subwayMap();
+        mainMenu(scanner);
     }
 
     public static String getStationName(Scanner scanner) {
         Print.getStationName();
-        String input = scanner.next();
-        return Exception.passNameExceptionTest(input);
+        return scanner.next();
     }
 }
