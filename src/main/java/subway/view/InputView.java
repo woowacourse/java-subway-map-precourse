e60c8ab6de481b3176e64e7d03b3bfaafc3ba5d1
opdshe <opdshe@naver.com>
2020-12-11 18:08:31 +0900

    feat: 등록할 노선의 상행 종점역과 하행 종점역 입력받는 기능

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -8,6 +8,8 @@ public class InputView {
     private static final String INPUT_STATION_NAME_FOR_REMOVAL = "삭제할 역 이름을 입력하세요.";
     private static final String INPUT_FUNCTION = "원하는 기능을 선택하세요.";
     private static final String INPUT_LINE_NAME_FOR_REGISTRATION = "등록할 노선 이름을 입력하세요.";
+    private static final String INPUT_TOP_STATION = "등록할 노선의 상행 종점역 이름을 입력하세요.";
+    private static final String INPUT_BOTTOM_STATION = "등록할 노선의 하행 종점역 이름을 입력하세요.";
 
     private InputView() {
     }
@@ -29,6 +31,16 @@ public class InputView {
 
     public static String getLineNameForRegistration() {
         OutputView.printGuideMessage(INPUT_LINE_NAME_FOR_REGISTRATION);
+        return SCANNER.nextLine();
+    }
+
+    public static String getTopStation() {
+        OutputView.printGuideMessage(INPUT_TOP_STATION);
+        return SCANNER.nextLine();
+    }
+
+    public static String getBottomStation() {
+        OutputView.printGuideMessage(INPUT_BOTTOM_STATION);
         return SCANNER.nextLine();
     }
 }
