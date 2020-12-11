71133db400c9130de5ad24e744f7efd2c05f22bf
PaperCrafter <leejonghee342@gmail.com>
2020-12-11 23:50:09 +0900

    feat(StationManagementViewComponent): add view componet
    added view components realtede to station management view

--- src/main/java/subway/view/component/StationManagementViewComponent.java
+++ src/main/java/subway/view/component/StationManagementViewComponent.java
@@ -6,8 +6,33 @@ public class StationManagementViewComponent {
             "2. 역 삭제\n" +
             "3. 역 조회\n" +
             "B. 돌아가기";
+    private static final String finishPrefixComponent ="[INFO] ";
+    private static final String registerStationComponent = "## 등록할 역 이름을 입력하세요.";
+    private static final String registerStationFinishComponent = "지하철 역이 등록되었습니다.";
+    private static final String removeStationComponent ="## 삭제할 역 이름을 입력하세요.";
+    private static final String removeStationFinishComponent = "지하철 역이 삭제되었습니다.";
 
     public static String getMenuComponent(){
         return menuComponent;
+    }
+
+    public static String getFinishPrefixComponent() {
+        return finishPrefixComponent;
+    }
+
+    public static String getRegisterStationComponent(){
+        return registerStationComponent;
+    }
+
+    public static String getRegisterStationFinishComponent(){
+        return finishPrefixComponent + registerStationFinishComponent;
+    }
+
+    public static String getRemoveStationComponent(){
+        return removeStationComponent;
+    }
+
+    public static String getRemoveStationFinishComponent(){
+        return finishPrefixComponent + removeStationFinishComponent;
     }
 }
