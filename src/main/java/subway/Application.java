f2a62f2ed25e2599859b687763eab959e190b876
Wannte <T.wannte@gmail.com>
2020-12-11 20:28:33 +0900

    chore(Application): Application 기능 초기화

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,20 +1,11 @@
 package subway;
 
-import subway.domain.LineRepository;
-import subway.domain.Station;
-import subway.domain.StationRepository;
-
-import subway.domain.*;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         Settings.init();
-        StationRepository.printStations();
-        LineRepository.printLines();
     }
 }
 
