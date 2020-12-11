5f2f8a7c0281b5a31f1a73a5af1863b1dd1af98e
Wannte <T.wannte@gmail.com>
2020-12-11 21:48:44 +0900

    chore(Application): 실행 로직 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,11 +1,15 @@
 package subway;
 
+import subway.view.View;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         Settings.init();
+        View view = new View(scanner);
+        view.main();
     }
 }
 
