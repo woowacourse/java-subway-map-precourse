69690531563bdfb04bbdea89fc4f80ef10d6c6a3
yangfriendship <yangfriendship@naver.com>
2020-12-12 01:51:53 +0900

    feat (LineName) : 노선 이름에 대한 검증로직 추가

--- src/main/java/subway/domain/name/LineName.java
+++ src/main/java/subway/domain/name/LineName.java
@@ -1,9 +1,15 @@
 package subway.domain.name;
 
 
+import subway.exception.InvalidLineNameException;
+
 import java.util.Objects;
 
 public class LineName implements Comparable<LineName> {
+
+    private static final int ONE_INDEX = 1;
+    private static final int NAME_MIN_LENGTH = 3;  //마지막 "역"까지 포함해서 3글자 이상
+    private static final char NAME_SUFFIX = '선';
 
     private String name;
 
@@ -13,6 +19,26 @@ public class LineName implements Comparable<LineName> {
 
     public static LineName of(String name) {
         return new LineName(name);
+    }
+
+    private static void validate(String name) {
+
+        if (name.length() > NAME_MIN_LENGTH) {
+            throw new InvalidLineNameException(name);
+        }
+
+        if (!containSuffix(name)) {
+            throw new InvalidLineNameException(name);
+        }
+
+    }
+
+    private static boolean containSuffix(String name) {
+        return name.charAt(LastIndexOf(name)) == NAME_SUFFIX;
+    }
+
+    private static int LastIndexOf(String name) {
+        return name.length() - ONE_INDEX;
     }
 
     @Override
