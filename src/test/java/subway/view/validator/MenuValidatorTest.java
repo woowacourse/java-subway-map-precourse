0652c2fb5bcb3622cf509d5e3e9196ab515d82d4
kyle456 <kyle2123@naver.com>
2020-12-11 20:03:58 +0900

    test: 메뉴 입력 예외 처리 테스트 코드 추가

---
+++ src/test/java/subway/view/validator/MenuValidatorTest.java
@@ -0,0 +1,37 @@
+package subway.view.validator;
+
+import org.junit.Test;
+
+public class MenuValidatorTest {
+    @Test(expected = MenuSelectionException.class)
+    public void testMainMenuSelection() {
+        MenuValidator.validateMenuSelection("Main", "5");
+        MenuValidator.validateMenuSelection("Main", " ");
+        MenuValidator.validateMenuSelection("Main", "+~");
+        MenuValidator.validateMenuSelection("Main", "q");
+    }
+    
+    @Test(expected = MenuSelectionException.class)
+    public void testStationMenuSelection() {
+        MenuValidator.validateMenuSelection("Station", "4");
+        MenuValidator.validateMenuSelection("Station", " ");
+        MenuValidator.validateMenuSelection("Station", "+~");
+        MenuValidator.validateMenuSelection("Station", "b");
+    }
+    
+    @Test(expected = MenuSelectionException.class)
+    public void testLineMenuSelection() {
+        MenuValidator.validateMenuSelection("Line", "4");
+        MenuValidator.validateMenuSelection("Line", " ");
+        MenuValidator.validateMenuSelection("Line", "+~");
+        MenuValidator.validateMenuSelection("Line", "b");
+    }
+    
+    @Test(expected = MenuSelectionException.class)
+    public void testSectionMenuSelection() {
+        MenuValidator.validateMenuSelection("Section", "3");
+        MenuValidator.validateMenuSelection("Section", " ");
+        MenuValidator.validateMenuSelection("Section", "+~");
+        MenuValidator.validateMenuSelection("Section", "b");
+    }
+}
