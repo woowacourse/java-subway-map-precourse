be731969fa04f13f4564b3e1d31b24f1d8d3dc54
JinseongHwang <eddy5360@naver.com>
2020-12-12 00:02:42 +0900

    feat: MainManager에 의해 실행되도록 변경

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,15 +1,10 @@
 package subway;
 
-import view.InputView;
-
-import java.util.Scanner;
+import subway.manager.MainManager;
 
 public class Application {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
         Initialization.set();
-        InputView inputView = new InputView(scanner);
-        inputView.executeProgram();
-        scanner.close();
+        MainManager.execute();
     }
 }
