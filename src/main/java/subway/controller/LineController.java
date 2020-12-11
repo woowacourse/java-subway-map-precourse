9d5493fbdb7f0049d10ca40c1cc78ecb066054d9
PaperCrafter <leejonghee342@gmail.com>
2020-12-12 00:17:45 +0900

    feat(LineController): add line controller

---
+++ src/main/java/subway/controller/LineController.java
@@ -0,0 +1,22 @@
+package subway.controller;
+
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.view.SectionManagementViewState;
+
+import java.util.List;
+import java.util.Optional;
+
+public class LineController {
+    private static LineController lineController;
+
+    private LineController(){
+    }
+
+    public static synchronized LineController getLineController(){
+        if(!Optional.ofNullable(lineController).isPresent()){
+            lineController = new LineController();
+        }
+        return lineController;
+    }
+}
