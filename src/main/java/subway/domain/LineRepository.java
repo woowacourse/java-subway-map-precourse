019607d0a1898985953f727259b7be559351d7a6
eoc940 <hgk0000@naver.com>
2020-12-12 00:55:06 +0900

    feat: 노선 등록, 삭제 기능 구현

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -42,10 +42,17 @@ public class LineRepository {
     }
 
     public static void addLine(Line line) {
+        if (lines.contains(line)) {
+            return;
+        }
         lines.add(line);
     }
 
     public static boolean deleteLineByName(String name) {
+        if (fixedLines.removeIf(line -> Objects.equals(line.getName(), name))) {
+            // 삭제할 수 없는 노선입니다. 메시지 출력
+            return false;
+        }
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
     }
 }
