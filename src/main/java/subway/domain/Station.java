ed073ec4e7b51e2e5c0cc6d157bd72ed8456e8a5
JinseongHwang <eddy5360@naver.com>
2020-12-11 18:41:54 +0900

    feat: Station객체가 상행, 하행종점과 속해있는 노선 정보들을 저장할 수 있게 함

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,15 +1,58 @@
 package subway.domain;
 
+import java.util.HashSet;
+
 public class Station {
+
     private String name;
+    private boolean northboundTerminal;
+    private boolean southboundTerminal;
+    private HashSet<String> linesIncludedStation = new HashSet<String>();
 
     public Station(String name) {
         this.name = name;
+        this.northboundTerminal = false;
+        this.southboundTerminal = false;
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    public boolean isNorthboundTerminal() {
+        return northboundTerminal;
+    }
+
+    public boolean isSouthboundTerminal() {
+        return southboundTerminal;
+    }
+
+    public void registerToNorthbound() {
+        northboundTerminal = true;
+    }
+
+    public void deleteFromNorthbound() {
+        northboundTerminal = false;
+    }
+
+    public void registerToSouthbound() {
+        southboundTerminal = true;
+    }
+
+    public void deleteFromSouthbound() {
+        southboundTerminal = false;
+    }
+
+    public void registerIn(String lineName) {
+        linesIncludedStation.add(lineName);
+    }
+
+    public void deleteFrom(String lineName) {
+        linesIncludedStation.remove(lineName);
+    }
+
+    public boolean includedInSomeLines() {
+        return !linesIncludedStation.isEmpty();
+    }
+
 }
