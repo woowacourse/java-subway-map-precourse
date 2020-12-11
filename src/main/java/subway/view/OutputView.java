1bb3c9e2610662daaf8435a6b76928e9020f4950
yangfriendship <yangfriendship@naver.com>
2020-12-12 03:33:01 +0900

    feat (OuptView) : printMenu에 타이틀도 출력하도록 변경 및 상수 정리

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -9,8 +9,10 @@ import java.util.List;
 public class OutputView {
 
     private static final String INFO_PREFIX = "[INFO] ";
-    private static final String PRINT_STATIONS_TITLE = "## 역목록";
-    private static final String PRINT_LINES_TITLE = "## 노선 목록";
+    private static final String INPUT_PREFIX = "## ";
+    private static final String PRINT_STATIONS_TITLE = INPUT_PREFIX + "역목록";
+    private static final String PRINT_LINES_TITLE = INPUT_PREFIX + "노선 목록";
+    private static final String MENU_SUFFIX = "화면";
 
     private static void addBlankLine() {
         System.out.println();
@@ -35,12 +37,11 @@ public class OutputView {
     }
 
     public static void printMenu(Menu menu) {
-
+        System.out.println(INPUT_PREFIX + menu.getTitle() + MENU_SUFFIX);
         for (Menu value : menu.getValues()) {
             System.out.println(value.toString());
         }
         addBlankLine();
-
     }
 
 }
