f9ac3903cb17e37674890c789cd05c3cfd712152
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-11 17:50:06 +0900

    feat(OutputView) : 메인 화면 출력 기능 추가

---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,17 @@
+package subway.view;
+
+public class OutputView {
+    public static void printMainFunctions() {
+        System.out.println("## 메인화면");
+        System.out.println("1. 역 관리");
+        System.out.println("2. 노선 관리");
+        System.out.println("3. 구간 관리");
+        System.out.println("4. 지하철 노선도 출력");
+        System.out.println("Q. 종료");
+        printEmptyLine();
+    }
+
+    private static void printEmptyLine() {
+        System.out.println();
+    }
+}
