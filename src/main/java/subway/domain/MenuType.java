480b03acb8bd2699c6b3b7f3777461aace294228
doorisopen <audauddl2@gmail.com>
2020-12-11 15:24:54 +0900

    feat: 메뉴 타입 추가

---
+++ src/main/java/subway/domain/MenuType.java
@@ -0,0 +1,31 @@
+package subway.domain;
+
+public enum MenuType {
+    MAIN_STATION("1"),
+    MAIN_LINE_STATION("2"),
+    MAIN_SECTION("3"),
+    MAIN_PRINT_LINE_STATION( "4"),
+    MAIN_QUIT("Q"),
+
+    STATION_ADD("1"),
+    STATION_DELETE("2"),
+    STATION_SEARCH("3"),
+
+    LINE_STATION_ADD("1"),
+    LINE_STATION_DELETE("2"),
+    LINE_STATION_SEARCH("3"),
+
+    SECTION_ADD("1"),
+    SECTION_DELETE("2"),
+    BACK("B");
+
+    private String key;
+
+    MenuType(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}
