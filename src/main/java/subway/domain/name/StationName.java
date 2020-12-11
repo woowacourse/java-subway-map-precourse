7f5ee7349ba822ef23d52cb96dd8a8651010e417
yangfriendship <yangfriendship@naver.com>
2020-12-11 23:25:10 +0900

    feat (StationName) : 지하철 역 이름 생성

---
+++ src/main/java/subway/domain/name/StationName.java
@@ -0,0 +1,40 @@
+package subway.domain.name;
+
+import java.util.Objects;
+
+public class StationName implements Comparable<StationName> {
+
+    private String name;
+
+    private StationName(String name) {
+        this.name = name;
+    }
+
+    public static StationName of(String name) {
+        return new StationName(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StationName that = (StationName) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public int compareTo(StationName o) {
+        return this.name.compareTo(o.name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
