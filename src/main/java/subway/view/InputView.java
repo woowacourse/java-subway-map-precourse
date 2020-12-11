e1a4f2a1a3f1f85681b6fe603376a7a473905013
opdshe <opdshe@naver.com>
2020-12-11 13:37:01 +0900

    feat: 삭제할 지하철 역 이름을 입력받는 기능

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_STATION_NAME_FOR_REGISTRATION = "등록할 역 이름을 입력하세요.";
+    private static final String INPUT_STATION_NAME_FOR_REMOVAL = "삭제할 역 이름을 입력하세요.";
 
     private InputView() {
     }
 
     public static String getStationNameForRegistration() {
         OutputView.printGuideMessage(INPUT_STATION_NAME_FOR_REGISTRATION);
+        return SCANNER.nextLine();
+    }
+
+    public static String getStationNameForRemoval() {
+        OutputView.printGuideMessage(INPUT_STATION_NAME_FOR_REMOVAL);
         return SCANNER.nextLine();
     }
 }
