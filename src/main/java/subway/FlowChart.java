599aa4a36bbb3403993c417e9a2170d6eeebbff4
Seonho Kim <zxzx235@naver.com>
2020-12-13 00:31:47 +0900

    feat: 메인 화면과 관리화면 출력 및 입력 받는 기능 추가

--- src/main/java/subway/FlowChart.java
+++ src/main/java/subway/FlowChart.java
@@ -21,6 +21,10 @@ public class FlowChart {
         Menu stationPage = new Menu(Constant.STATION_MENU_TITLE, Constant.stationMenuItemList());
         if(mainInput.equals("1")){
             String stationInput = stationPage.load(scanner);
+            stationAdd(scanner, stationInput);
+            stationDelete(scanner, stationInput);
+            stationLookUp(scanner, stationInput);
+            back(scanner, stationInput);
         }
     }
 
@@ -28,6 +32,10 @@ public class FlowChart {
         Menu linePage = new Menu(Constant.LINE_MENU_TITLE, Constant.lineMenuItemList());
         if(mainInput.equals("2")){
             String lineInput = linePage.load(scanner);
+            lineAdd(scanner, lineInput);
+            lineDelete(scanner, lineInput);
+            lineLookUp(scanner, lineInput);
+            back(scanner, lineInput);
         }
     }
 
@@ -35,6 +43,9 @@ public class FlowChart {
         Menu sectionPage = new Menu(Constant.SECTION_MENU_TITLE, Constant.sectionMenuItemList());
         if(mainInput.equals("3")){
             String sectionInput = sectionPage.load(scanner);
+            sectionAdd(scanner, sectionInput);
+            sectionDelete(scanner, sectionInput);
+            back(scanner, sectionInput);
         }
     }
 
@@ -49,6 +60,76 @@ public class FlowChart {
     public static void quit(String mainInput) {
         if(mainInput.equals("Q")){
             System.exit(0);
+        }
+    }
+
+    public static void back(Scanner scanner, String input) {
+        if(input.equals("B")){
+            main(scanner);
+        }
+    }
+
+    public static void stationAdd(Scanner scanner, String stationInput) {
+        if(stationInput.equals("1")){
+            System.out.println("stationAdd");
+            System.out.println();
+            main(scanner);
+        }
+    }
+
+    public static void stationDelete(Scanner scanner, String stationInput) {
+        if(stationInput.equals("2")){
+            System.out.println("stationDelete");
+            System.out.println();
+            main(scanner);
+        }
+    }
+
+    public static void stationLookUp(Scanner scanner, String stationInput) {
+        if(stationInput.equals("3")){
+            System.out.println("stationLookUp");
+            System.out.println();
+            main(scanner);
+        }
+    }
+
+    public static void lineAdd(Scanner scanner, String lineInput) {
+        if(lineInput.equals("1")){
+            System.out.println("lineAdd");
+            System.out.println();
+            main(scanner);
+        }
+    }
+
+    public static void lineDelete(Scanner scanner, String lineInput) {
+        if(lineInput.equals("2")){
+            System.out.println("lineDelete");
+            System.out.println();
+            main(scanner);
+        }
+    }
+
+    public static void lineLookUp(Scanner scanner, String lineInput) {
+        if(lineInput.equals("3")){
+            System.out.println("lineLookUp");
+            System.out.println();
+            main(scanner);
+        }
+    }
+
+    public static void sectionAdd(Scanner scanner, String sectionInput) {
+        if(sectionInput.equals("1")){
+            System.out.println("sectionAdd");
+            System.out.println();
+            main(scanner);
+        }
+    }
+
+    public static void sectionDelete(Scanner scanner, String sectionInput) {
+        if(sectionInput.equals("2")){
+            System.out.println("sectionDelete");
+            System.out.println();
+            main(scanner);
         }
     }
 }
