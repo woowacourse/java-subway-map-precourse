d621d82422f3e6c5eb2c3b7d14e4bc0a9c9a59ae
tributetothemoon <kkm97351@gmail.com>
2020-12-13 01:46:55 +0900

    fix(InputView): 프로그램 실행 후 각 기능 화면마다 최소 1번의 유효값이 입력되지  않으면 종료되는 버그 수정
    이전 업데이트 중 공백 입력시 재입력을 요구하지 않도록 변경하였는데, 이 때 각 컨트롤러마다 가지고 있는 menu 인스턴스가 초기화되지 못해 예외를
    잡지 못하고 종료되는 버그가 있었습니다. enum타입 특성상 null이 아닌 마땅한 더미 데이터를 넣을 수 있는 것이 없다고 생각해 다시 공백입력에 한해서만 View에서 예외를 처리하고 재입력을 요구하도록 변경했습니다.

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -7,17 +7,12 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String ERROR_NONE_INPUT_VALUE = "입력값이 없습니다.";
-    private static final String SHARP_PREFIX = "## ";
     private static final String MENU_SELECTION = "원하는 기능을 선택하세요.";
 
     private static Scanner scanner;
 
     public static void setScanner(Scanner scanner) {
         InputView.scanner = scanner;
-    }
-
-    public static void showSelectionMessage() {
-        System.out.println(SHARP_PREFIX + MENU_SELECTION);
     }
 
     private static String deleteWhiteSpaces(String string) {
@@ -32,21 +27,27 @@ public class InputView {
     }
 
     public static Selection getSelection(Selections selections) {
-        showSelectionMessage();
-        String string = deleteWhiteSpaces(scanner.nextLine());
-        isNotEmptyStringOrThrowException(string);
-
-        newLine();
-        return selections.searchByValue(string);
+        try{
+            String input = getStringWithMessage(MENU_SELECTION);
+            newLine();
+            return selections.searchByValue(input);
+        } catch (Exception e) {
+            OutputView.showErrorMessage(e);
+            return getSelection(selections);
+        }
     }
 
     public static String getStringWithMessage(String requestMessage) {
-        System.out.println(SHARP_PREFIX + requestMessage);
-        String name = deleteWhiteSpaces(scanner.nextLine());
-        isNotEmptyStringOrThrowException(name);
-
-        newLine();
-        return name;
+        try {
+            OutputView.printWithSharpPrefix(requestMessage);
+            String string = deleteWhiteSpaces(scanner.nextLine());
+            isNotEmptyStringOrThrowException(string);
+            newLine();
+            return string;
+        }catch (Exception e) {
+            OutputView.showErrorMessage(e);
+            return getStringWithMessage(requestMessage);
+        }
     }
 
     private static void newLine() {
