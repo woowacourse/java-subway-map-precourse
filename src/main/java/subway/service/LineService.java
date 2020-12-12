f3cf320635273263d3fef5e455b4cb7111daa11b
bepoz <kangsy763@naver.com>
2020-12-13 02:30:36 +0900

    feat: 지하철 노선 이름은 2글자 이상이어야 한다

--- src/main/java/subway/service/LineService.java
+++ src/main/java/subway/service/LineService.java
@@ -8,11 +8,15 @@ import subway.view.OutputView;
 import static subway.view.OutputView.*;
 
 public class LineService {
+    private final String LINE_NAME_LENGTH_WARN = "노선 이름은 2글자 이상이어야 합니다.\n";
     private final String ASK_ADD_LINE_NAME = "등록할 노선 이름을 입력하세요.";
 
     public boolean addLine(InputView inputView) {
         askMessage(ASK_ADD_LINE_NAME);
         String lineName = inputView.inputName();
+        if (lineName.length() < 2) {
+            warnMessage(LINE_NAME_LENGTH_WARN);
+        }
         //validate
         return LineRepository.addLine(new Line(lineName));
     }
