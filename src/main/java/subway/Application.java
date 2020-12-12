84618e577c89e71212090c876fc63b841980163d
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-12 21:47:34 +0900

    refactor(Application) : 메인 화면에서 기능에 없는 값을 입력 받을 경우 예외 처리하는 부분 리팩토링

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -2,6 +2,7 @@ package subway;
 
 import java.util.Scanner;
 import subway.domain.LineRepository;
+import subway.domain.Machine;
 import subway.domain.StationRepository;
 import subway.domain.function.MainFunction;
 import subway.view.InputView;
@@ -11,16 +12,12 @@ public class Application {
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+
+        // TODO - 디버깅용 (개발 완료 후 지우기)
         System.out.println(LineRepository.lines());
         System.out.println(StationRepository.stations());
 
-        while (true) {
-            OutputView.printMainFunctions();
-            String mainFunctionNumber = InputView.inputFunctionNumber(scanner);
-            if (mainFunctionNumber.equals("Q")) {
-                break;
-            }
-            MainFunction.getMainFunctionByNumber(mainFunctionNumber).operate(scanner);
-        }
+        Machine machine = new Machine();
+        machine.start(scanner);
     }
 }
---
+++ src/main/java/subway/domain/Machine.java
@@ -0,0 +1,24 @@
+package subway.domain;
+
+import java.util.Scanner;
+import subway.domain.function.MainFunction;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+public class Machine {
+    public void start(Scanner scanner) {
+        try {
+            OutputView.printMainFunctions();
+            String mainFunctionNumber = InputView.inputFunctionNumber(scanner);
+            if (mainFunctionNumber.equals("Q")) {
+                start(scanner);
+                return;
+            }
+            MainFunction.getMainFunctionByNumber(mainFunctionNumber).operate(scanner);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            start(scanner);
+        }
+
+    }
+}
