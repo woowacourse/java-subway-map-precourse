466b5db81cc0a34f44419030f878b7d5a785ba28
Seonho Kim <zxzx235@naver.com>
2020-12-12 14:02:33 +0900

    refactor(Print): 같은 기능을 가진 method 합침

--- src/main/java/subway/domain/Print.java
+++ src/main/java/subway/domain/Print.java
@@ -51,16 +51,8 @@ public class Print {
         System.out.printf(Constant.INFO + Constant.DONE_MESSAGE + "%n", target, AddOrDelete);
     }
 
-    public static void setLineToAddSection() {
-        System.out.printf(Constant.HASH + Constant.ENTER_LINE + "%n");
-    }
-
-    public static void setStationToAddSection() {
-        System.out.printf(Constant.HASH + Constant.ENTER_STATION + "%n");
-    }
-
-    public static void setOrderToAddSection() {
-        System.out.printf(Constant.HASH + Constant.ENTER_ORDER + "%n");
+    public static void hashAnd(String enterContent) {
+        System.out.printf(Constant.HASH + enterContent + "%n");
     }
 
     public static void setLineOrStationToDeleteSection(String lineOrStation) {
