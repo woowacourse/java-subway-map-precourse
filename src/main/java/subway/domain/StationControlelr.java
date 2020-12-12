d74d77357c8a8705d0e950793959eb2c2f597994
naru <hsik0225@gmail.com>
2020-12-13 04:14:52 +0900

    feat(output-view): 역 삭제 완료 출력 기능 구현

--- src/main/java/subway/domain/StationControlelr.java
+++ src/main/java/subway/domain/StationControlelr.java
@@ -30,6 +30,6 @@ public class StationControlelr {
 
         stationRepository.removeStation(stationName);
 
-        outputView.printStationSaved();
+        outputView.printStationRemoved();
     }
 }
--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -8,8 +8,14 @@ public class OutputView {
 
     public static final String STATION_SAVED = "지하철 역이 등록되었습니다.";
 
+    public static final String STATION_REMOVED = "지하철 역이 삭제되었습니다.";
+
     public void printStationSaved() {
         printSuccessMessage(STATION_SAVED);
+    }
+
+    public void printStationRemoved() {
+        printSuccessMessage(STATION_REMOVED);
     }
 
     private PrintStream printSuccessMessage(String message) {
