f67f7c2690a837c9b8b046d81a0413db7c1337bf
Seonho Kim <zxzx235@naver.com>
2020-12-12 22:30:52 +0900

    feat: loopSectionPage method 추가
    구간 관리 화면을 보여주는 기능 추가.

--- src/main/java/subway/FlowChart.java
+++ src/main/java/subway/FlowChart.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class FlowChart {
     public static void start(Scanner scanner){
         Menu mainPage = new Menu(Constant.MAIN_MENU_TITLE, Constant.mainMenuItemList());
-        Menu sectionPage = new Menu(Constant.SECTION_MENU_TITLE, Constant.sectionMenuItemList());
 
         while (true) {
             String mainInput = mainPage.load(scanner);
@@ -16,7 +15,7 @@ public class FlowChart {
                 loopLinePage(scanner);
             }
             if(mainInput.equals(Constant.mainMenuItemList().get(2).substring(0, 1))){
-                sectionPage.load(scanner);
+                loopSectionPage(scanner);
             }
             if(mainInput.equals(Constant.mainMenuItemList().get(3).substring(0, 1))){
                 System.out.println("지하철 노선도가 들어갈 자리");
@@ -51,13 +50,13 @@ public class FlowChart {
         while (true){
             String lineInput = linePage.load(scanner);
             if(lineInput.equals(Constant.lineMenuItemList().get(0).substring(0, 1))){
-                // 역 등록.
+                // 노선 등록.
             }
             if(lineInput.equals(Constant.lineMenuItemList().get(1).substring(0, 1))){
-                // 역 삭제.
+                // 노선 삭제.
             }
             if(lineInput.equals(Constant.lineMenuItemList().get(2).substring(0, 1))){
-                // 역 조회.
+                // 노선 조회.
             }
             if(lineInput.equals(Constant.lineMenuItemList().get(3).substring(0, 1))){
                 break;
@@ -65,5 +64,20 @@ public class FlowChart {
         }
     }
 
+    public static void loopSectionPage(Scanner scanner){
+        Menu sectionPage = new Menu(Constant.SECTION_MENU_TITLE, Constant.sectionMenuItemList());
+        while (true){
+            String sectionInput = sectionPage.load(scanner);
+            if(sectionInput.equals(Constant.sectionMenuItemList().get(0).substring(0, 1))){
+                // 구간 등록.
+            }
+            if(sectionInput.equals(Constant.sectionMenuItemList().get(1).substring(0, 1))){
+                // 구간 삭제.
+            }
+            if(sectionInput.equals(Constant.sectionMenuItemList().get(2).substring(0, 1))){
+                break;
+            }
+        }
+    }
 
 }
