2f1a95e497b6649ae896f58edce6c7e7da6020d2
bluewood <bluewood.truth@gmail.com>
2020-12-12 09:49:23 +0900

    feat: MVC 패턴의 기본 뼈대 작성
    view와 controller를 추가함.
    Application.main()에서 프로그램을 실행하는 기능을 추가함.

---
+++ src/main/java/subway/controller/SubwayMapController.java
@@ -0,0 +1,11 @@
+package subway.controller;
+
+import java.util.Scanner;
+
+import subway.view.InputView;
+
+public class SubwayMapController {
+    public static void run(Scanner scanner) {
+        InputView.setScanner(scanner);
+    }
+}
---
+++ src/main/java/subway/view/InputView.java
@@ -0,0 +1,11 @@
+package subway.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static Scanner scanner;
+
+    public static void setScanner(Scanner scanner) {
+        InputView.scanner = scanner;
+    }
+}
