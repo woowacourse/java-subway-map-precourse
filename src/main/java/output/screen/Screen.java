a37fea9d1bac9749c6159c891e5cf33fa81465a0
Ji-Ha <xodhkd36@gmail.com>
2020-12-12 18:08:57 +0900

    style(Screen.java): 문자열 추가

--- src/main/java/output/screen/Screen.java
+++ src/main/java/output/screen/Screen.java
@@ -1,12 +1,15 @@
 package output.screen;
 
+import input.Input;
+
 public interface Screen {
     public final static String HEAD = "## ";
     public final static String SPACE = " ";
     public final static String POINT = ".";
     public final static String ENTER = "\n";
     public final static String MANAGEMENT = "관리";
+    public final static String SELECT_FUNCTION = "원하는 기능을 선택하세요.";
 
     void visualize();
-    void logic();
+    void logic(Input input);
 }
