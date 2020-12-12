5f5a9ee12e8449d8acc5be0d5da9e337f3353a82
Seonho Kim <zxzx235@naver.com>
2020-12-13 00:36:05 +0900

    refactor: 사용하지 않는 메소드 삭제

--- src/main/java/subway/FlowChart.java
+++ src/main/java/subway/FlowChart.java
@@ -3,10 +3,6 @@ package subway;
 import java.util.Scanner;
 
 public class FlowChart {
-    public static void start(Scanner scanner) {
-        main(scanner);
-    }
-
     public static void main(Scanner scanner) {
         Menu mainPage = new Menu(Constant.MAIN_MENU_TITLE, Constant.mainMenuItemList());
         String mainInput = mainPage.load(scanner);
