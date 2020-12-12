841c6bb6dbfacd2302eafc9e8bddcfbac3233fd0
김경록 <goodboy3421@gmail.com>
2020-12-13 04:09:27 +0900

    refactor: 누락된 에러 메시지 추가 (중복된 노선이름을 입력했을 경우 예외 발생)

--- src/main/java/subway/domain/line/service/LineService.java
+++ src/main/java/subway/domain/line/service/LineService.java
@@ -6,6 +6,8 @@ import subway.domain.line.model.LineRepository;
 import java.util.List;
 
 public class LineService {
+    private static final String NOT_DUPLICATION_LINE_NAME_MESSAGE = "[ERROR] 중복된 노선 이름은 등록할 수 없습니다.";
+
     public static List<Line> findAll() {
         return LineRepository.lines();
     }
@@ -21,7 +23,7 @@ public class LineService {
                 .anyMatch(savedLine -> savedLine.isEqualTo(line));
 
         if (isDuplicated) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NOT_DUPLICATION_LINE_NAME_MESSAGE);
         }
     }
 
