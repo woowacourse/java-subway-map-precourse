49fc18ed180307de79cf1e843acfad1c47d30a86
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-12 21:42:24 +0900

    메인 화면과 역 관리 화면 출력 기능 추가

---
+++ src/main/java/subway/domain/printScreen.java
@@ -0,0 +1,30 @@
+package subway.domain;
+
+public class printScreen {
+
+    public static void printMainScreen() {
+        System.out.println("## 메인 화면");
+        System.out.println("1. 역 관리");
+        System.out.println("2. 노선 관리");
+        System.out.println("3. 구간 관리");
+        System.out.println("4. 지하철 노선도 출력");
+        System.out.println("Q. 종료\n");
+        printSelectFunction();
+    }
+
+    public static void printStationManagementScreen() {
+        System.out.println("1. 역 등록");
+        System.out.println("2. 역 삭제");
+        System.out.println("3. 역 조회");
+        System.out.println("B. 돌아가기\n");
+        printSelectFunction();
+    }
+
+    public static void printSelectFunction() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+    }
+
+    public static void printInputStationName() {
+        System.out.println("## 등록할 역 이름을 입력하세요.");
+    }
+}
