34dc8a98597633416211c07b337b4e474f71cf0e
bluewood <bluewood.truth@gmail.com>
2020-12-12 19:22:30 +0900

    feat: 노선 초기 설정 추가

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -11,6 +11,12 @@ import subway.exception.NullLineException;
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
 
+    static {
+        lines.add(Line.createLineWithStationInitializers("2호선", "교대역", "강남역", "역삼역"));
+        lines.add(Line.createLineWithStationInitializers("3호선", "교대역", "남부터미널역", "양재역", "매봉역"));
+        lines.add(Line.createLineWithStationInitializers("신분당선", "강남역", "양재역", "양재시민의숲역"));
+    }
+
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
     }
