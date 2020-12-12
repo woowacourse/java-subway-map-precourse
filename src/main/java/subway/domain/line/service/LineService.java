bbd8459ad7c7a7a7c4ec3d0a66a0499b2e4c46bb
김경록 <goodboy3421@gmail.com>
2020-12-13 03:23:26 +0900

    feat: 지하철 노선의 목록을 조회하는 기능

---
+++ src/main/java/subway/domain/line/service/LineService.java
@@ -0,0 +1,12 @@
+package subway.domain.line.service;
+
+import subway.domain.line.model.Line;
+import subway.domain.line.model.LineRepository;
+
+import java.util.List;
+
+public class LineService {
+    public static List<Line> findAll() {
+        return LineRepository.lines();
+    }
+}
---
+++ src/test/java/subway/domain/line/service/LineServiceTest.java
@@ -0,0 +1,46 @@
+package subway.domain.line.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import subway.domain.line.model.Line;
+import subway.domain.line.model.LineRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class LineServiceTest {
+
+    @DisplayName("지하철 노선의 목록을 조회하는 기능을 테스트한다")
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "1호선,2호선,3호선:3", "신분당선,분당선,경의선,공항철도:4"
+            }, delimiter = ':'
+    )
+    void testFindAll(String input, int lineNumber) {
+        //given
+        String[] lineNames = input.split(",");
+        Arrays.stream(lineNames)
+                .map(Line::new)
+                .forEach(LineRepository::addLine);
+
+        //when
+        List<Line> lines = LineService.findAll();
+
+        //then
+        List<Line> expectedLines = Arrays.stream(lineNames)
+                .map(Line::new)
+                .collect(Collectors.toList());
+        assertAll(
+                () -> assertThat(lines)
+                        .usingElementComparatorOnFields("name")
+                        .containsAll(expectedLines),
+                () -> assertThat(lines).hasSize(lineNumber)
+        );
+    }
+}
