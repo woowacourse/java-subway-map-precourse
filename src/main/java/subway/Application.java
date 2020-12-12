b8e3f582792c19bb3128d834d0650e9c5e6960c7
JinseongHwang <eddy5360@naver.com>
2020-12-12 17:07:41 +0900

    feat: 프로그램 시작하면 Power.on()메서드 실행하도록 변경

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,9 +1,12 @@
 package subway;
 
+import subway.initialize.Initialization;
 import subway.manager.MainManager;
+import utils.Power;
 
 public class Application {
     public static void main(String[] args) {
+        Power.on();
         Initialization.set();
         MainManager.execute();
     }
