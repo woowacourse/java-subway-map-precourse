dfff94a4286471a0482a1dd84861cd810a59350e
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-12 21:44:05 +0900

    각 기능 번호 상수 추가

--- src/main/java/subway/domain/Constants.java
+++ src/main/java/subway/domain/Constants.java
@@ -2,4 +2,10 @@ package subway.domain;
 
 public class Constants {
     public final static int MIN_NAME_LENGTH = 2;
+    public final static String FUNCTION_1 = "1";
+    public final static String FUNCTION_2 = "2";
+    public final static String FUNCTION_3 = "3";
+    public final static String FUNCTION_4 = "4";
+    public final static String FUNCTION_Q = "Q";
+    public final static String FUNCTION_B = "B";
 }
