76399f79a3fd2954461e0ddf0332202a6ba91c0b
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-12 19:52:53 +0900

    역 리스트 반환 메소드 이름 변경, 역 추가할 때 길이 2 이상, 중복체크 확인 추가

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -8,15 +8,23 @@ import java.util.Objects;
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
 
-    public static List<Station> stations() {
+    public static List<Station> retrieveStation() {
         return Collections.unmodifiableList(stations);
     }
 
     public static void addStation(Station station) {
-        stations.add(station);
+        //길이 2이상, 중복 체크
+        if (checkNameLength(station.getName()) && !retrieveStation().contains(station)) {
+            stations.add(station);
+        }
     }
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+
+    private static boolean checkNameLength(String name) {
+        return name.length() >= Constants.MIN_NAME_LENGTH;
     }
 }
