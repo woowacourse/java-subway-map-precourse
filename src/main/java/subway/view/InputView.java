a50a898d92f5286f0053a53596f7a62145ff64ba
NewWisdom <wlgp2500@gmail.com>
2020-12-12 20:43:21 +0900

    feat(input-view): '역'으로 끝나지 않는 역 이름 입력 예외 처리 추가

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -11,9 +11,11 @@ public class InputView {
     private static final String INVALID_FUNCTION_ERROR_MESSAGE = "선택할 수 없는 기능입니다.";
     private static final String NOT_LETTER_ERROR_MESSAGE = "이름은 문자여야 합니다.";
     private static final String INVALID_LENGTH_ERROR_MESSAGE = "이름은 2자 이상이어야 합니다.";
+    private static final String INVALID_STATION_LAST_CHAR = "이름의 마지막 글자는 '역'으로 끝나야 합니다.";
     private static final String EXISTING_STATION_ERROR_MESSAGE = "이미 등록된 역 이름입니다.";
     private static final String REGEX_KOREAN = "^[가-힣]*$";
     private static final int MIN_NAME_LENGTH = 2;
+    private static final char STATION_LAST_CHAR = '역';
 
 
     private final Scanner scanner;
@@ -44,18 +46,21 @@ public class InputView {
         if (isInvalidLength(station)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + INVALID_LENGTH_ERROR_MESSAGE);
         }
+        if (isInvalidLastChar(station)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + INVALID_STATION_LAST_CHAR);
+        }
         if (isExistingStation(station)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + EXISTING_STATION_ERROR_MESSAGE);
         }
         return station;
     }
 
-    private static boolean isNotKorean(String value) {
-        return !Pattern.matches(REGEX_KOREAN, value);
+    private static boolean isNotKorean(String station) {
+        return !Pattern.matches(REGEX_KOREAN, station);
     }
 
-    private static boolean isInvalidLength(String value) {
-        return value.length() < MIN_NAME_LENGTH;
+    private static boolean isInvalidLength(String station) {
+        return station.length() < MIN_NAME_LENGTH;
     }
 
     private static boolean isExistingStation(String station) {
@@ -65,5 +70,9 @@ public class InputView {
                         s -> s.getName()
                                 .equals(station)
                 );
+    }
+
+    private static boolean isInvalidLastChar(String station) {
+        return station.charAt(station.length() - 1) != STATION_LAST_CHAR;
     }
 }
