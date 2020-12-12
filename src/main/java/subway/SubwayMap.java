4e9db8e2c7feee91a357359be72f14153041dc74
yangfriendship <yangfriendship@naver.com>
2020-12-12 22:30:01 +0900

    feat (SubwayMap) : SubwayMap

---
+++ src/main/java/subway/SubwayMap.java
@@ -0,0 +1,57 @@
+package subway;
+
+import subway.exception.MenuNotFountException;
+import subway.menu.MainMenu;
+import subway.menu.Menu;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+import java.util.Scanner;
+
+public class SubwayMap {
+
+    private static Menu state = MainMenu.QUIT;
+    private InputView inputView;
+
+    public SubwayMap(Scanner scanner) {
+        InputView.initScanner(scanner);
+        this.inputView = InputView.getInstance();
+    }
+
+    public void run() {
+
+        while (checkState()) {
+
+            printMenu();
+
+            selectMenu();
+
+            menuRen();
+        }
+    }
+
+
+    private boolean checkState() {
+        return state != null;
+    }
+
+    private void selectMenu() {
+
+        String input = inputView.selectMenu();
+
+        try {
+            this.state = state.change(input);
+        } catch (MenuNotFountException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void menuRen() {
+        this.state = state.run();
+    }
+
+    private void printMenu() {
+        OutputView.printMenu(state);
+    }
+
+}
