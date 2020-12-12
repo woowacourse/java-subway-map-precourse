bff808135ee174d24593619676fce1031a6a889a
opdshe <opdshe@naver.com>
2020-12-12 12:40:34 +0900

    style: getStationFunction 메서드 getFunction으로 이름 변경 (재사용을 위해)

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String INPUT_FUNCTION = "원하는 기능을 선택하세요.";
     private static final String INPUT_STATION_NAME_FOR_REGISTRATION = "등록할 역 이름을 입력하세요.";
     private static final String INPUT_STATION_NAME_FOR_REMOVAL = "삭제할 역 이름을 입력하세요.";
-    private static final String INPUT_FUNCTION = "원하는 기능을 선택하세요.";
     private static final String INPUT_LINE_NAME_FOR_REGISTRATION = "등록할 노선 이름을 입력하세요.";
     private static final String INPUT_TOP_STATION = "등록할 노선의 상행 종점역 이름을 입력하세요.";
     private static final String INPUT_BOTTOM_STATION = "등록할 노선의 하행 종점역 이름을 입력하세요.";
@@ -25,7 +25,7 @@ public class InputView {
         return SCANNER.nextLine();
     }
 
-    public static String getStationFunction() {
+    public static String getFunction() {
         OutputView.printGuideMessage(INPUT_FUNCTION);
         return SCANNER.nextLine();
     }
