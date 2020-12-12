d4984a30a47abc0778461ff2aac04c7ba353d174
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-12 17:44:07 +0900

    feat: 세부기능 입력받는 메소드 구현

--- src/main/java/subway/controller/MenuController.java
+++ src/main/java/subway/controller/MenuController.java
@@ -10,11 +10,28 @@ import java.util.Scanner;
 
 public class MenuController {
     private static final List<String> MAIN_MENU_SIGN = Arrays.asList(new String[] {"1","2","3","4","Q"});
+    private static final List<String> STATION_MANAGEMENT_MENU_SIGN = Arrays.asList(new String[] {"1","2","3","B"});
+    private static final List<String> LINE_MANAGEMENT_MENU_SIGN = Arrays.asList(new String[] {"1","2","3","B"});
+    private static final List<String> EDGE_MANAGEMENT_MENU_SIGN = Arrays.asList(new String[] {"1","2","B"});
 
     public static String scanMainMenu(Scanner scanner) {
         OutputView.printMainScreen();
         return scanValidMenu(scanner, MAIN_MENU_SIGN);
+    }
 
+    public static String scanStationManagementMenu(Scanner scanner) {
+        OutputView.printStationManagementScreen();
+        return scanValidMenu(scanner, STATION_MANAGEMENT_MENU_SIGN);
+    }
+
+    public static String scanLineManagementMenu(Scanner scanner) {
+        OutputView.printLineManagementScreen();
+        return scanValidMenu(scanner, LINE_MANAGEMENT_MENU_SIGN);
+    }
+
+    public static String scanEdgeManagementMenu(Scanner scanner) {
+        OutputView.printEdgeManagementScreen();
+        return scanValidMenu(scanner, EDGE_MANAGEMENT_MENU_SIGN);
     }
 
     private static String scanValidMenu(Scanner scanner, List<String> signs) {
