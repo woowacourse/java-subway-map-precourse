3f0ef5e3a4c6a810bd1d4bcd265f824ea79f7592
Ji-Ha <xodhkd36@gmail.com>
2020-12-12 18:09:39 +0900

    feat(MainScreen.java): 1,2,3,Q 기능 추가

--- src/main/java/output/screen/MainScreen.java
+++ src/main/java/output/screen/MainScreen.java
@@ -1,12 +1,12 @@
 package output.screen;
 
+import input.Input;
 import output.Menu;
 
 public class MainScreen implements Screen{
     public final static String MAIN_SCREEN_MESSAGE = "메인 화면";
-    public final static String SUBWAY_MAP = "4. 지하철 노선도 출력";
-    public final static String QUIT = "Q. 종료";
     public final static int SET_NUMBER = 1;
+    private Menu selectedMenu;
 
     @Override
     public void visualize() {
@@ -14,12 +14,30 @@ public class MainScreen implements Screen{
         for (Menu menu : Menu.values()) {
             System.out.println((menu.ordinal() + SET_NUMBER) + POINT + SPACE + menu.getMenuName() + SPACE + MANAGEMENT);
         }
-        System.out.println(SUBWAY_MAP);
-        System.out.println(QUIT + ENTER);
     }
 
     @Override
-    public void logic() {
+    public void logic(Input input) {
+        System.out.println(HEAD + SELECT_FUNCTION);
+        String answer = input.inputFunction(); //입력.
+        System.out.println();
+        setSelectedScreen(answer);
+    }
 
+    private void setSelectedScreen(String answer){
+        if(answer.equals("1")){
+            selectedMenu = Menu.STATION;
+        }else if(answer.equals("2")){
+            selectedMenu = Menu.LINE;
+        }else if(answer.equals("3")){
+            selectedMenu = Menu.ROUTE;
+        }else if(answer.equals("4")){
+            selectedMenu = Menu.SUBWAY_MAP;
+        }else if(answer.equals("Q")){
+            selectedMenu = Menu.QUIT;
+        }
+    }
+    public Menu getSelectedScreen(){
+        return selectedMenu;
     }
 }
