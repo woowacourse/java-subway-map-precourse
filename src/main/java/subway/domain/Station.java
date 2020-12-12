9d406503b4f7bc2f3b2e89ab501c1ed2c7fd3864
hybeom0720 <hybeom@gmail.com>
2020-12-12 23:56:20 +0900

    feat(Station) : 지하철 역 기능 구현
    수정:
    - 지하철 역 생성 시 오류에 대한 메서드 추가
    - 지하철 역 이름 비교 메서드 추가

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,9 +1,12 @@
 package subway.domain;
 
 public class Station {
+    private static final int NAME_LIMIT_LENGTH = 2;
+    private static final String LENGTH_ERROR_MESSAGE = "[ERROR] 역의 이름은 2글자 이상이어야 합니다.";
     private String name;
 
     public Station(String name) {
+        checkNameLength(name);
         this.name = name;
     }
 
@@ -11,5 +14,16 @@ public class Station {
         return name;
     }
 
-    // 추가 기능 구현
+    public boolean compareName(String compareName) {
+        if (name.equals(compareName)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void checkNameLength(String name) {
+        if (name.length() < NAME_LIMIT_LENGTH) {
+            throw new IllegalArgumentException(LENGTH_ERROR_MESSAGE);
+        }
+    }
 }
