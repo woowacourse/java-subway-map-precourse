c12d8183db67c90e19d715a1a4c61091d172a836
Seonho Kim <zxzx235@naver.com>
2020-12-12 23:48:52 +0900

    refactor: while을 사용하지 않고 구현할 수 있도록 구조 변경

--- src/main/java/subway/FlowChart.java
+++ src/main/java/subway/FlowChart.java
@@ -4,15 +4,45 @@ import java.util.Scanner;
 
 public class FlowChart {
     public static void start(Scanner scanner) {
+        main(scanner);
+    }
+
+    public static void main(Scanner scanner) {
         Menu mainPage = new Menu(Constant.MAIN_MENU_TITLE, Constant.mainMenuItemList());
-        Menu stationPage = new Menu(Constant.STATION_MENU_TITLE, Constant.stationMenuItemList());
-        Menu linePage = new Menu(Constant.LINE_MENU_TITLE, Constant.lineMenuItemList());
-        Menu sectionPage = new Menu(Constant.SECTION_MENU_TITLE, Constant.sectionMenuItemList());
-
         String mainInput = mainPage.load(scanner);
+        station(mainInput);
+        line(mainInput);
+        section(mainInput);
+        map(mainInput);
+        quit(mainInput);
+    }
+
+    public static void station(String mainInput) {
+        Menu stationPage = new Menu(Constant.STATION_MENU_TITLE, Constant.stationMenuItemList());
         if(mainInput.equals("1")){
-
         }
+    }
 
+    public static void line(String mainInput) {
+        Menu linePage = new Menu(Constant.LINE_MENU_TITLE, Constant.lineMenuItemList());
+        if(mainInput.equals("2")){
+        }
+    }
+
+    public static void section(String mainInput) {
+        Menu sectionPage = new Menu(Constant.SECTION_MENU_TITLE, Constant.sectionMenuItemList());
+        if(mainInput.equals("3")){
+        }
+    }
+
+    public static void map(String mainInput) {
+        if(mainInput.equals("4")){
+        }
+    }
+
+    public static void quit(String mainInput) {
+        if(mainInput.equals("Q")){
+            System.exit(0);
+        }
     }
 }
