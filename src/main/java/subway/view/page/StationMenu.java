bcdb6bc44a5e1f62b5ea36307f11b35192aee8da
ecsimsw <ecsimsw@gmail.com>
2020-12-13 08:54:54 +0900

    feat : StationMenu에서 선택한 기능에 해당하는 메소드 호출

--- src/main/java/subway/view/page/StationMenu.java
+++ src/main/java/subway/view/page/StationMenu.java
@@ -16,42 +16,53 @@ public class StationMenu {
         stationController = StationController.getInstance(scanner);
         printMenu();
         Menu selected = getMenuSelection(scanner);
+        selected.execute();
     }
 
-    public static void printMenu(){
+    public static void printMenu() {
         Arrays.stream(Menu.values())
-                .map(Menu -> Menu.getMenuName()+"\n")
+                .map(Menu -> Menu.getMenuName() + "\n")
                 .forEach(OutputView::printMsg);
     }
 
-    public static Menu getMenuSelection(Scanner scanner){
+    public static Menu getMenuSelection(Scanner scanner) {
         OutputView.printMsg("원하는 기능을 선택하세요.");
         return Menu.getSelection(InputView.getInput(scanner));
     }
 
-    private enum Menu{
-        REGISTER_STATION("1", "1. 역 등록"),
-        DELETE_STATION("2", "2. 역 삭제"),
-        PRINT_STATIONS("3", "3. 역 조회"),
-        BACK("B", "B. 돌아가기");
+    public static void printGoBack() {
+        OutputView.printInfoMsg("이전 메뉴로 돌아갑니다.");
+    }
+
+    private enum Menu {
+        REGISTER_STATION("1", "1. 역 등록", () -> stationController.addStation()),
+        DELETE_STATION("2", "2. 역 삭제", () -> stationController.deleteStation()),
+        PRINT_STATIONS("3", "3. 역 조회", () -> stationController.printStations()),
+        BACK("B", "B. 돌아가기", () -> printGoBack());
 
         private String userInput;
         private String menuName;
+        private Runnable action;
 
-        Menu(String userInput, String menuName){
+        Menu(String userInput, String menuName, Runnable action) {
             this.userInput = userInput;
             this.menuName = menuName;
+            this.action = action;
         }
 
-        public String getMenuName(){
+        public String getMenuName() {
             return menuName;
         }
 
-        public static Menu getSelection(String input){
+        public void execute() {
+            action.run();
+        }
+
+        public static Menu getSelection(String input) {
             return Arrays.stream(values())
                     .filter(menu -> menu.userInput.equals(input))
                     .findFirst()
-                    .orElseThrow(()-> new IllegalArgumentException("적절하지 않은 입력입니다."));
+                    .orElseThrow(() -> new IllegalArgumentException("적절하지 않은 입력입니다."));
         }
     }
 }
