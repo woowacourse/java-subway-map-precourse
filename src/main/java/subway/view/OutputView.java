f4197820399bc1e2027b6a7e5a5d0f591cbcdf70
bluewood <bluewood.truth@gmail.com>
2020-12-12 11:10:55 +0900

    feat: 메뉴를 출력하는 기능 추가

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,5 +1,30 @@
 package subway.view;
 
+import subway.domain.menu.Menu;
+import subway.domain.menu.MenuItem;
+
 public class OutputView {
+    private static final String MESSAGE_FORMAT = "## %s%n";
+    private static final String ERROR_FORMAT = "[ERROR] %s%n";
+    private static final String MENU_ITEM_FORMAT = "%s. %s%n";
+
+    public static void printMenu(Menu menu) {
+        printMessage(menu.getTitle());
+        for (MenuItem menuItem : menu) {
+            printMenuItem(menuItem);
+        }
+    }
+
+    private static void printMenuItem(MenuItem menuItem) {
+        System.out.printf(MENU_ITEM_FORMAT, menuItem.getKey(), menuItem.getName());
+    }
     
+    private static void printMessage(String message) {
+        printEmptyLine();
+        System.out.printf(MESSAGE_FORMAT, message);
+    }
+
+    private static void printEmptyLine() {
+        System.out.println();
+    }
 }
