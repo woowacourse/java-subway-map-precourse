8152066aa35e5faa4a9f8388fee9be7e464f2d5f
Injun Son <ison@sfu.ca>
2020-12-12 22:07:25 +0900

    feat: 메인화면 출력 구현

---
+++ src/main/java/subway/SubwayManager.java
@@ -0,0 +1,41 @@
+package subway;
+
+import java.util.Scanner;
+import subway.domain.StationRepository;
+
+public class SubwayManager {
+
+    private static final String WANT_QUIT_CODE = "Q";
+    private static final String MAIN_TITLE = "## 메인 화면";
+    private static final String MANAGE_STATION_OPTION = "1. 역 관리";
+    private static final String MANAGE_LINE_OPTION = "2. 노선 관리";
+    private static final String MANAGE_SECTION_OPTION = "3. 구간 관리";
+    private static final String PRINT_MAP_OPTION = "4. 지하철 노선도 출력";
+    private static final String QUIT_OPTION = "Q. 종료";
+    private static final String ASK_OPTION_MESSAGE = "## 원하는 기능을 선택하세요.";
+    private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String ERROR_MESSAGE = "1~4 또는 Q 옵션 중 하나를 입력하세요";
+    private static final int OPTION_MIN = 1;
+    private static final int OPTION_MAX = 4;
+    private static String userOption = "";
+    public static StationRepository stationRepository = new StationRepository();
+
+    public static void manage(Scanner scanner) {
+        while (!userOption.equals(WANT_QUIT_CODE)) {
+            printMainScreen();
+            userOption = getUserOption(scanner);
+            callOptionMenu(userOption, scanner);
+        }
+    }
+
+    public static void printMainScreen() {
+        System.out.println(MAIN_TITLE);
+        System.out.println(MANAGE_STATION_OPTION);
+        System.out.println(MANAGE_LINE_OPTION);
+        System.out.println(MANAGE_SECTION_OPTION);
+        System.out.println(PRINT_MAP_OPTION);
+        System.out.println(QUIT_OPTION);
+        System.out.println();
+    }
+
+}
