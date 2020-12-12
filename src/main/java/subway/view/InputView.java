6f56c94be009df9fca7f05129163fc88cf41c6ea
ecsimsw <ecsimsw@gmail.com>
2020-12-13 07:32:19 +0900

    feat : 기본 입출력 방식 지정

---
+++ src/main/java/subway/view/InputView.java
@@ -0,0 +1,11 @@
+package subway.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private InputView(){}
+
+    public static String getInput(Scanner scanner){
+        return scanner.nextLine();
+    }
+}
---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,9 @@
+package subway.view;
+
+public class OutputView {
+    private OutputView(){}
+
+    public static void printMsg(Object msg){
+        System.out.print(msg);
+    }
+}
