59694066d72a1f8b0234c9828cbd260b019b1baf
bepoz <kangsy763@naver.com>
2020-12-12 23:30:27 +0900

    refactor: 메세지 출력 방식 변경
    물음을 위한 통합 메세지 메서드를 생성하였다

--- src/main/java/subway/service/StationService.java
+++ src/main/java/subway/service/StationService.java
@@ -4,14 +4,15 @@ import subway.domain.Station;
 import subway.repository.StationRepository;
 import subway.view.InputView;
 
-import static subway.view.OutputView.askStationName;
+import static subway.view.OutputView.askMessage;
 import static subway.view.OutputView.warnMessage;
 
 public class StationService {
     private final String STATION_NAME_LENGTH_WARN = "역 이름은 2글자 이상이어야 합니다.\n";
+    private static final String ASK_STATION_NAME_MESSAGE = "등록할 역 이름을 입력하세요.\n";
 
     public boolean addStation(InputView inputView) {
-        askStationName();
+        askMessage(ASK_STATION_NAME_MESSAGE);
         String stationName = inputView.inputStationName();
         if (stationName.length() < 2) {
             warnMessage(STATION_NAME_LENGTH_WARN);
--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -2,14 +2,13 @@ package subway.view;
 
 public class OutputView {
     private static final String SHARP_PREFIX = "## ";
-    private static final String ASK_STATION_NAME_MESSAGE = "등록할 역 이름을 입력하세요.\n";
     private static final String ERROR_PREFIX = "[ERROR] ";
 
     public static void warnMessage(String warnMessage) {
         System.out.println(ERROR_PREFIX + warnMessage);
     }
 
-    public static void askStationName() {
-        System.out.println(SHARP_PREFIX + ASK_STATION_NAME_MESSAGE);
+    public static void askMessage(String askMessage) {
+        System.out.println(SHARP_PREFIX + askMessage);
     }
 }
