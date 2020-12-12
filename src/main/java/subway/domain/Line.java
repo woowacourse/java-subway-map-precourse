be6d9566afccb89f0b18acf9c0538ca455ac361c
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-13 05:22:29 +0900

    노선 클래스에 상행 하행 종점역 변수 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -2,9 +2,13 @@ package subway.domain;
 
 public class Line {
     private String name;
+    private String firstStation;
+    private String lastStation;
 
-    public Line(String name) {
+    public Line(String name, String firstStation, String lastStation) {
         this.name = name;
+        this.firstStation = firstStation;
+        this.lastStation = lastStation;
     }
 
     public String getName() {
