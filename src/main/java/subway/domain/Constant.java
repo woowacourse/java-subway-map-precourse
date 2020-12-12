4725dae30f0d23b3a3ebda676099644739375c2b
Seonho Kim <zxzx235@naver.com>
2020-12-12 13:21:26 +0900

    fix(Constant): 상수값 변경

--- src/main/java/subway/domain/Constant.java
+++ src/main/java/subway/domain/Constant.java
@@ -34,7 +34,7 @@ public class Constant {
     public static final String PRINT_SUBWAY_MAP = "지하철 노선도 출력";
     public static final String SETTING = " 관리";
     public static final String ADD = "등록";
-    public static final String DELETE = " 삭제";
+    public static final String DELETE = "삭제";
     public static final String LOOKUP = " 조회";
     public static final String QUIT = "종료";
     public static final String BACK = "돌아가기";
