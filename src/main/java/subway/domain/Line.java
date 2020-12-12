f5a4cc21d4bb90e9acda375b656569574495bbd1
김주원 <kjw11077@naver.com>
2020-12-12 21:01:03 +0900

    feat(Line): Line 도메인, 레포지토리 수정

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,50 @@
 package subway.domain;
 
-public class Line {
-    private String name;
+import java.util.Arrays;
+import java.util.List;
+import subway.exception.DuplicatedStationInOneLineException;
+import subway.exception.LineNameLengthException;
 
-    public Line(String name) {
+public class Line {
+
+    private final String name;
+    private final List<Station> sections;
+
+    private Line(String name, List<Station> sections) {
         this.name = name;
+        this.sections = sections;
+    }
+
+    public static Line of(String name, Station firstStation, Station lastStation) {
+        validateLength(name);
+        validateDuplicatedStations(firstStation, lastStation);
+        return new Line(name, Arrays.asList(firstStation, lastStation));
+    }
+
+    public static Line getLineByName(String name) {
+        return LineRepository.findByName(name);
+    }
+
+    public void save() {
+        LineRepository.addLine(this);
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    private static void validateLength(String name) {
+        if (name.length() < 2) {
+            throw new LineNameLengthException(name);
+        }
+        if (5 < name.length()) {
+            throw new LineNameLengthException(name);
+        }
+    }
+
+    private static void validateDuplicatedStations(Station firstStation, Station lastStation) {
+        if (firstStation.equals(lastStation)) {
+            throw new DuplicatedStationInOneLineException(firstStation.getName());
+        }
+    }
 }
--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import subway.exception.CannotFindLineByNameException;
+import subway.exception.LineAlreadyExistsException;
 
 public class LineRepository {
 
@@ -17,10 +19,33 @@ public class LineRepository {
     }
 
     public static void addLine(Line line) {
+        validateDuplicated(line);
         lines.add(line);
+    }
+
+    private static void validateDuplicated(Line line) {
+        if (exists(line.getName())) {
+            throw new LineAlreadyExistsException(line.getName());
+        }
     }
 
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static Line findByName(String name) {
+        return lines.stream()
+                .filter(line -> line.getName().equals(name))
+                .findAny()
+                .orElseThrow(() -> new CannotFindLineByNameException(name));
+    }
+
+    public static boolean exists(String name) {
+        return lines.stream()
+                .anyMatch(line -> line.getName().equals(name));
+    }
+
+    public static void deleteAll() {
+        lines.clear();
     }
 }
