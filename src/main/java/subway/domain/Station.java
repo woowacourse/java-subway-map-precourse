df4d2a524c3d19a607372b5355be20ac3f5b8ec7
JinseongHwang <eddy5360@naver.com>
2020-12-12 17:00:37 +0900

    feat: Station객체가 종점에 대한 정보를 가지지 않도록 변경

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -5,42 +5,14 @@ import java.util.HashSet;
 public class Station {
 
     private String name;
-    private boolean northboundTerminal;
-    private boolean southboundTerminal;
     private HashSet<String> linesIncludedStation = new HashSet<String>();
 
     public Station(String name) {
         this.name = name;
-        this.northboundTerminal = false;
-        this.southboundTerminal = false;
     }
 
     public String getName() {
         return name;
-    }
-
-    public boolean isNorthboundTerminal() {
-        return northboundTerminal;
-    }
-
-    public boolean isSouthboundTerminal() {
-        return southboundTerminal;
-    }
-
-    public void registerToNorthbound() {
-        northboundTerminal = true;
-    }
-
-    public void deleteFromNorthbound() {
-        northboundTerminal = false;
-    }
-
-    public void registerToSouthbound() {
-        southboundTerminal = true;
-    }
-
-    public void deleteFromSouthbound() {
-        southboundTerminal = false;
     }
 
     public void registerIn(String lineName) {
