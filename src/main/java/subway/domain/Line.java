f05f41dae236a5de12311475863685734c638dec
Ji-Ha <xodhkd36@gmail.com>
2020-12-12 18:36:20 +0900

    feat(Line.java): 노선에 필요한 역들을 저장하기 위한 LineStations 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,12 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
+
+    private static final List<Station> lineStations = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -12,4 +17,7 @@ public class Line {
     }
 
     // 추가 기능 구현
+    public void addLineStation(Station station){
+        lineStations.add(station);
+    }
 }
