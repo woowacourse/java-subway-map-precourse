dade2f180160a77cd4f1c031db9e430461744ded
jaeseongDev <qkrwotjd1445@naver.com>
2020-12-13 01:54:49 +0900

    feat : 공백으로 입력한 지하철 및 노선 이름에 대해서 예외 처리 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -3,6 +3,7 @@ package subway.domain;
 import java.util.List;
 
 public class Line {
+
     private static final int MIN_LENGTH_OF_LINE_NAME = 2;
     private String name;
     private List<Station> stations;
@@ -14,8 +15,9 @@ public class Line {
     }
 
     public static void validateLineName(String name) {
-        if (name.length() < MIN_LENGTH_OF_LINE_NAME) {
-            throw new IllegalArgumentException("[ERROR] 지하철 노선 이름은 2글자 이상이어야 합니다.");
+        if (name.trim().length() < MIN_LENGTH_OF_LINE_NAME) {
+            throw new IllegalArgumentException(
+                "[ERROR] 지하철 노선 이름은 2글자 이상이어야 합니다. (문자열의 양 끝에 있는 공백 제외한 이름이 2글자 이상이어야 합니다.)");
         }
     }
 
--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -3,6 +3,7 @@ package subway.domain;
 import java.util.Objects;
 
 public class Station {
+
     private static final int MIN_LENGTH_OF_STATION_NAME = 2;
     private String name;
 
@@ -16,8 +17,9 @@ public class Station {
     }
 
     private void validateStationName(String name) {
-        if (name.length() < MIN_LENGTH_OF_STATION_NAME) {
-            throw new IllegalArgumentException("[ERROR] 역의 이름은 2글자 이상이어야 합니다.");
+        if (name.trim().length() < MIN_LENGTH_OF_STATION_NAME) {
+            throw new IllegalArgumentException(
+                "[ERROR] 역의 이름은 2글자 이상이어야 합니다. (문자열의 양 끝에 있는 공백 제외한 이름이 2글자 이상이어야 합니다.)");
         }
     }
 
