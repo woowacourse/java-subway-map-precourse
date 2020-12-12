7a4178c5bc9d90824653baf2c979c65036d7faf6
bepoz <kangsy763@naver.com>
2020-12-13 03:33:43 +0900

    feat: 노선 추가 테스트

--- src/test/java/subway/repository/LineRepositoryTest.java
+++ src/test/java/subway/repository/LineRepositoryTest.java
@@ -13,7 +13,7 @@ class LineRepositoryTest {
     @DisplayName("역이 노선에 등록했는지 확인할 수 있다")
     public void isExistStationInLineTest() throws Exception{
         //given
-        Line line = new Line("2호선");
+        Line line = new Line("2호선", null, null);
         Station station = new Station("잠실역");
 
         //when
@@ -23,7 +23,16 @@ class LineRepositoryTest {
         //then
         boolean result = LineRepository.isStationExistInLine(station);
         assertTrue(result);
+    }
 
+    @Test
+    @DisplayName("중복 이름의 노선은 등록될 수 없다")
+    public void duplicateLineAddTest() throws Exception{
+        Line line = new Line("bepoz", null, null);
+        LineRepository.addLine(line);
+
+        Line line2 = new Line("bepoz", null, null);
+        assertFalse(LineRepository.addLine(line2));
     }
 
 }
