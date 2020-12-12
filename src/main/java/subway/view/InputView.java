8eff52ec073fcfeda3d61230cea075ea6f371462
NewWisdom <wlgp2500@gmail.com>
2020-12-12 14:58:23 +0900

    feat(input-view): 입력한 기능 번호 검증 기능 추가

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -4,17 +4,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String ERROR_MESSAGE_HEADER = "[ERROR] ";
+    private static final String INVALID_FUNCTION_ERROR_MESSAGE = "선택할 수 없는 기능입니다.";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
         this.scanner = scanner;
     }
 
-    public String getInput(List<String> functionIndexList){
-        return validateInput(functionIndexList, scanner.nextLine());
+    public String getInputFunctionIndex(List<String> functionIndexList){
+        return validateInputFunctionIndex(functionIndexList, scanner.nextLine());
     }
 
-    public String validateInput(List<String> functionIndexList, String str) {
-        return str;
+    public String validateInputFunctionIndex(List<String> functionIndexList, String functionIndex) {
+        if (!functionIndexList.contains(functionIndex)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + INVALID_FUNCTION_ERROR_MESSAGE);
+        }
+        return functionIndex;
     }
 }
