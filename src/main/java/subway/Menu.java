f4a7592e83facdf1cbd65ea557be438e8940058d
Seonho Kim <zxzx235@naver.com>
2020-12-12 16:39:18 +0900

    fix: 구조 변경을 위해 모두 삭제

---
+++ src/main/java/subway/Menu.java
@@ -0,0 +1,15 @@
+package subway;
+
+public class Menu {
+    static final int ITEM_LENGTH = 4;
+    static final String QUIT = "Q. 종료";
+    String[] itemList = {"역 관리", "노선 관리", "구간 관리", "지하철 노선도 출력"};
+    
+    public static void main() {
+        
+    }
+
+    public static void print() {
+
+    }
+}
