149aff676caae6e5a490a7619bd1ed61a067aea3
ecsimsw <ecsimsw@gmail.com>
2020-12-13 08:19:27 +0900

    feat : 메인 메뉴 입력 시, 선택 페이지 실행 구현

--- src/main/java/subway/view/page/MainMenu.java
+++ src/main/java/subway/view/page/MainMenu.java
@@ -5,13 +5,19 @@ import subway.view.OutputView;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class MainMenu {
+    private static boolean isEnd = false;
+
     private MainMenu(){}
 
     public static void run(Scanner scanner){
-        printMenu();
-        Menu selected = getMenuSelection(scanner);
+        do{
+            printMenu();
+            Menu selected = getMenuSelection(scanner);
+            selected.execute(scanner);
+        }while(!isEnd);
     }
 
     public static void printMenu(){
@@ -25,23 +31,37 @@ public class MainMenu {
         return Menu.getSelection(InputView.getInput(scanner));
     }
 
+    public static void setEnd(){
+        isEnd = true;
+    }
+
+    public static void printSubwayMap(){
+
+    }
+
     private enum Menu {
-        STATION_MENU("1", "1. 역 관리"),
-        LINE_MENU("2", "2. 노선 관리"),
-        SECTION_MENU("3", "3. 구간 관리"),
-        PRINT_MAP("4", "4. 지하철 노선도 출력"),
-        QUIT("Q", "Q. 종료");
+        STATION_MENU("1", "1. 역 관리", (scanner)-> StationMenu.run(scanner)),
+        LINE_MENU("2", "2. 노선 관리", (scanner)-> LineMenu.run(scanner)),
+        SECTION_MENU("3", "3. 구간 관리", (scanner)-> SectionMenu.run(scanner)),
+        PRINT_MAP("4", "4. 지하철 노선도 출력", (scanner)-> printSubwayMap()),
+        QUIT("Q", "Q. 종료", (scanner)-> setEnd());
 
         private String userInput;
         private String menuName;
+        private Consumer<Scanner> nextMove;
 
-        Menu(String userInput, String menuName){
+        Menu(String userInput, String menuName, Consumer<Scanner> nextMove){
             this.userInput = userInput;
             this.menuName = menuName;
+            this.nextMove = nextMove;
         }
 
         public String getMenuName(){
             return menuName;
+        }
+
+        public void execute(Scanner scanner){
+            nextMove.accept(scanner);
         }
 
         public static Menu getSelection(String input){
