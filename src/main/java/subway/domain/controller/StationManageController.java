b58e27678a45b8ad0cc5825f830372061ff00e0d
eoc940 <hgk0000@naver.com>
2020-12-13 02:51:00 +0900

    refactor: StationManagecontroller 내부 메서드 수정

--- src/main/java/subway/domain/controller/StationManageController.java
+++ src/main/java/subway/domain/controller/StationManageController.java
@@ -8,36 +8,24 @@ import java.util.stream.Stream;
 
 public class StationManageController {
 
-    static final String FUNCTION_ONE = "1";
-    static final String FUNCTION_TWO = "2";
-    static final String FUNCTION_THREE = "3";
-    static final String FUNCTION_BACK = "B";
 
     StationManageInput input = new StationManageInput();
 
     public void processEnrollStation(Scanner scanner) {
-        if (input.inputStationManageScreen(scanner).equals(FUNCTION_ONE)) {
-            Station station = new Station(input.inputEnrollStation(scanner));
-            StationRepository.addStation(station);
-        }
+        Station station = new Station(input.inputEnrollStation(scanner));
+        StationRepository.addStation(station);
+
     }
 
     public void processDeleteStation(Scanner scanner) {
-        if (input.inputStationManageScreen(scanner).equals(FUNCTION_TWO)) {
-            String station = input.inputDeleteStation(scanner);
-            StationRepository.deleteStation(station);
-        }
+        String station = input.inputDeleteStation(scanner);
+        StationRepository.deleteStation(station);
+
     }
 
-    public void printAllStations(Scanner scanner) {
-        if (input.inputStationManageScreen(scanner).equals(FUNCTION_THREE)) {
-            Stream<Station> stationStream = StationRepository.stations().stream();
-            stationStream.forEach(station -> System.out.println(station.getName()));
-        }
-    }
-
-    public boolean processGoBack(Scanner scanner) {
-        return input.inputStationManageScreen(scanner).equals(FUNCTION_BACK);
+    public void printAllStations() {
+        Stream<Station> stationStream = StationRepository.stations().stream();
+        stationStream.forEach(station -> System.out.println(station.getName()));
     }
 
 }
