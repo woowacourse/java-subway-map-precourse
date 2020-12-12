bbcab67ba314ae67f320236c1475cc940e15fa74
tomas <cmg14111@gmail.com>
2020-12-12 17:41:20 +0900

    feat(Station): 역 클래스 구현, 이름이 같으면 같은 역

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -3,12 +3,29 @@ package subway.domain;
 public class Station {
     private Name name;
 
-
     public Station(String name) {
         this.name = new Name(name);
     }
 
     public String getName() {
         return name.getName();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        boolean isEqualObject = false;
+        Station station = (Station) object;
+        if (getName().equals(station.getName())) {
+            isEqualObject = true;
+        }
+        return isEqualObject;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int hashCode = 1;
+        hashCode = prime * hashCode + getName().hashCode();
+        return hashCode;
     }
 }
