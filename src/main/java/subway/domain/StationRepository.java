815e2fd9de0dc445a9a3e2fd72884e061013a8a7
hybeom0720 <hybeom@gmail.com>
2020-12-13 00:24:53 +0900

    feat(StationRepository) : 지하철 역 리스트 구현
    수정:
    - 중복 체크하는 기능 추가

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Objects;
 
 public class StationRepository {
+    private static final int ZERO_NUMBER = 0;
+    private static final String OVERLAP_ERROR = "[ERROR] 역의 이름이 중복이 되었습니다.";
     private static final List<Station> stations = new ArrayList<>();
 
     public static List<Station> stations() {
@@ -13,10 +15,20 @@ public class StationRepository {
     }
 
     public static void addStation(Station station) {
+        checkOverlappedStation(station);
         stations.add(station);
     }
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    private static void checkOverlappedStation(Station target) {
+        long isOverlap = stations.stream()
+                .filter(station -> station.compareName(target.getName()))
+                .count();
+        if (isOverlap != ZERO_NUMBER) {
+            throw new IllegalArgumentException(OVERLAP_ERROR);
+        }
     }
 }
