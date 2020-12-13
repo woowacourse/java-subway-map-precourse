09277e428cda4ec78a019a3eee0a9bca8d3189c9
TaewanKimmmm <ktww88@gmail.com>
2020-12-14 01:51:52 +0900

    feat: 지하철 역과 노선을 관리하기 위한 SubwayMapEditor 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,14 @@
 package subway;
 
+import subway.controller.SubwayMapEditor;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        SubwayMapEditor subwayMapEditor = new SubwayMapEditor(scanner);
+        subwayMapEditor.run();
     }
 }
---
+++ src/main/java/subway/controller/SubwayMapEditor.java
@@ -0,0 +1,19 @@
+package subway.controller;
+
+import java.util.Scanner;
+
+public class SubwayMapEditor {
+    private final Scanner scanner;
+
+    public SubwayMapEditor(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void run() {
+        initializeSubwayMap();
+    }
+
+    private void initializeSubwayMap() {
+        
+    }
+}
