604ef7b1d26712232f3e2f515914499e1be92ac2
Ji-Ha <xodhkd36@gmail.com>
2020-12-13 20:16:11 +0900

    feat(Applcation.java): 지하철 노선도를 시작하는 클래스

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,14 @@
 package subway;
 
+import output.ScreenManager;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        ScreenManager screenManager = new ScreenManager(scanner);
+        screenManager.startMainScreen();
     }
 }
