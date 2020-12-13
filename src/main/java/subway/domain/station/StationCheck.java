4e92c8a3c696198be867f6228294015cc6e7bf1d
Gayoung <gyyoon4u@naver.com>
2020-12-13 22:31:44 +0900

    feat: station 이름 '역'으로 끝나는지 예외처리

--- src/main/java/subway/domain/station/StationCheck.java
+++ src/main/java/subway/domain/station/StationCheck.java
@@ -6,11 +6,20 @@ public class StationCheck {
 
     private static final int MIN_STATION_NAME_LENGTH = 2;
 
-    private static final String STATION_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 역이름은 2글자 이상 입력해야합니다.";
+    private static final String STATION_LENGTH_ERROR_MESSAGE = "[ERROR] 역이름은 2글자 이상 입력해야합니다.";
+    public static final String STATION_END_POINT_ERROR_MESSAGE = "[ERROR] 역 이름은 OO역으로 끝나야 합니다.";
+
 
     public static boolean checkStationNameLength(String stationName) {
         if (stationName.length() < MIN_STATION_NAME_LENGTH) {
-            throw new IllegalArgumentException(STATION_NAME_LENGTH_ERROR_MESSAGE);
+            throw new IllegalArgumentException(STATION_LENGTH_ERROR_MESSAGE);
+        }
+        return true;
+    }
+
+    public static boolean checkStationNameEndPoint(String stationName) {
+        if (!stationName.endsWith("역")) {
+            throw new IllegalArgumentException(STATION_END_POINT_ERROR_MESSAGE);
         }
         return true;
     }
