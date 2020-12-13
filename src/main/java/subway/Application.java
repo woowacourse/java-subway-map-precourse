59f2d8f26b6934d9f69f759f7af04cae594297e7
naru <hsik0225@gmail.com>
2020-12-13 21:43:29 +0900

    feat(application): MainController 실행 기능 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,10 @@
 package subway;
 
-import java.util.Scanner;
+import subway.domain.MainController;
 
 public class Application {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        MainController mainController = new MainController();
+        mainController.run();
     }
 }
