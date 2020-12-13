324d579f3b2c79aaaeb9f80288af86739c7c0193
Solji Choi <bssj9307@gmail.com>
2020-12-13 22:24:13 +0900

    feat(manager): 실행 함수 추가

---
+++ src/main/java/subway/SubwayManager.java
@@ -0,0 +1,13 @@
+package subway;
+
+import java.util.Scanner;
+
+public class SubwayManager {
+    public SubwayManager(Scanner scanner) {
+        this.runManager(scanner);
+    }
+
+    public void runManager(Scanner scanner) {
+
+    }
+}
