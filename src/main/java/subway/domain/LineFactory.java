8559bfd4559018cd4b4b67cb49a7ca0baa115bc1
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-14 00:04:31 +0900

    feat & doc : 노선 입력시 상행종점역, 하행종점역 입력에 대한 예외 처리 기능 추가

--- src/main/java/subway/domain/LineFactory.java
+++ src/main/java/subway/domain/LineFactory.java
@@ -4,11 +4,17 @@ public class LineFactory {
     private static final int NAME_MIN_LENGTH = 2;
     private static final String ERR_SHORT_NAME_MSG =
             String.format("[ERROR] 노선의 이름은 %d글자 이상이어야 합니다.", NAME_MIN_LENGTH);
+    public static final String ERR_OTHER_TERMINAL_NOT_EQUALS = "[ERROR] 상행선 종점역과 하행선 종점역은 같을 수 없습니다.";
 
     public static Line makeLine(String name, Station uplineTerminalStation, Station downlineTerminalStation) {
         if (name.length() < NAME_MIN_LENGTH) {
             throw new IllegalArgumentException(ERR_SHORT_NAME_MSG);
         }
+
+        if (uplineTerminalStation.equals(downlineTerminalStation)){
+            throw new IllegalArgumentException(ERR_OTHER_TERMINAL_NOT_EQUALS);
+        }
+
         return new Line(name, uplineTerminalStation, downlineTerminalStation);
     }
 }
