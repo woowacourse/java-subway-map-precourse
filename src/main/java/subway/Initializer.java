4423f21c166aa298c5e1f39a6f688bd3a500c4a7
박우림 <woorimprog@gmail.com>
2020-12-14 07:33:44 +0900

    refector(Initializer) : modify the ArrayList initialize code

--- src/main/java/subway/Initializer.java
+++ src/main/java/subway/Initializer.java
@@ -1,6 +1,6 @@
 package subway;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
@@ -11,42 +11,13 @@ import subway.domain.Station;
 import subway.domain.StationRepository;
 
 public class Initializer {
-	private static final List<String> SECOND_LINE = new ArrayList<>() {
-		{
-			add("교대역");
-			add("강남역");
-			add("역삼역");
-		}
-	};
+	private static final List<String> SECOND_LINE = Arrays.asList("교대역", "강남역", "역삼역");
 	
-	private static final List<String> THIRD_LINE = new ArrayList<>() {
-		{
-			add("교대역");
-			add("남부터미널역");
-			add("양재역");
-			add("매봉역");
-		}
-	};
+	private static final List<String> THIRD_LINE = Arrays.asList("교대역", "남부터미널역", "양재역", "매봉역");
 	
-	private static final List<String> SINBUNDANG_LINE = new ArrayList<>() {
-		{
-			add("강남역");
-			add("양재역");
-			add("양재시민의숲역");
-		}
-	};
+	private static final List<String> SINBUNDANG_LINE = Arrays.asList("강남역", "양재역", "양재시민의숲역");
 	
-	private static final List<String> INITIAL_STATIONS = new ArrayList<>() {
-		{
-			add("교대역");
-			add("강남역");
-			add("역삼역");
-			add("남부터미널역");
-			add("양재역");
-			add("양재시민의숲역");
-			add("매봉역");
-		}
-	};
+	private static final List<String> INITIAL_STATIONS = Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역");
 	
 	private static final HashMap<String, List<String>> INITIAL_LINES = new HashMap<>() {
 		{
