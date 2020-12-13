679bb2e50ec5b44d9c2adc263fc8d86008d7b037
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-13 20:37:19 +0900

    refactor : 하드코딩 값 상수로 변환

--- src/main/java/subway/domain/StationFactory.java
+++ src/main/java/subway/domain/StationFactory.java
@@ -1,9 +1,13 @@
 package subway.domain;
 
 public class StationFactory {
+    private static final int NAME_MIN_LENGTH = 2;
+    private static final String ERR_SHORT_NAME_MSG =
+            String.format("[ERROR] 역의 이름은 %d글자 이상이어야 합니다.", NAME_MIN_LENGTH);
+
     public static Station makeStation(String name) {
-        if (name.length() < 2) {
-            throw new IllegalArgumentException("[ERROR] 역의 이름은 2글자 이상이어야 합니다.");
+        if (name.length() < NAME_MIN_LENGTH) {
+            throw new IllegalArgumentException(ERR_SHORT_NAME_MSG);
         }
         return new Station(name);
     }
--- src/main/java/subway/domain/Stations.java
+++ src/main/java/subway/domain/Stations.java
@@ -6,6 +6,9 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class Stations {
+    private static final String ERR_ALREADY_ADD_STATION_NAME_MSG = "[ERROR] 이미 등록된 역명입니다.";
+    private static final String ERR_NO_SUCH_NAME_STATION_MSG = "[ERROR] 해당 역이 없습니다.";
+
     private final List<Station> stations = new ArrayList<>();
 
     public void addStation(Station station) {
@@ -13,12 +16,12 @@ public class Stations {
             stations.add(station);
             return;
         }
-        throw new IllegalStateException("[ERROR] 이미 등록된 역명입니다.");
+        throw new IllegalStateException(ERR_ALREADY_ADD_STATION_NAME_MSG);
     }
 
     public void deleteStation(String name) {
         if (!stations.removeIf(station -> Objects.equals(station.getName(), name))) {
-            throw new NoSuchElementException("[ERROR] 해당 역이 없습니다.");
+            throw new NoSuchElementException(ERR_NO_SUCH_NAME_STATION_MSG);
         }
     }
 
@@ -30,6 +33,6 @@ public class Stations {
         return stations.stream()
                 .filter(station -> Objects.equals(station.getName(), name))
                 .findFirst()
-                .orElseThrow(() -> new NoSuchElementException("[ERROR] 해당 역이 없습니다."));
+                .orElseThrow(() -> new NoSuchElementException(ERR_NO_SUCH_NAME_STATION_MSG));
     }
 }
