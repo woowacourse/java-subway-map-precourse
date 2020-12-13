9f9c0a949bbfb1073db57249528dc8285b81fa55
Seonho Kim <zxzx235@naver.com>
2020-12-13 22:04:20 +0900

    feat: 구간 등록 기능 추가
    노선, 역이름, 순서 입력 후 구간 등록과 예외 사항 추가.

---
+++ src/main/java/subway/SectionSetting.java
@@ -0,0 +1,50 @@
+package subway;
+
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.StationRepository;
+
+import java.util.Scanner;
+
+public class SectionSetting {
+    public static void add(Scanner scanner) {
+        String lineName = lineName(scanner);
+        LineRepository.lines().forEach(line -> {
+            if (line.getName().equals(lineName)) {
+                String station = station(scanner, line);
+                int order = order(scanner);
+                addStationToLine(line, station, order);
+                Print.infoMessage(Constant.ADD_SECTION_DONE);
+            }
+        });
+    }
+
+    public static String lineName(Scanner scanner) {
+        Print.hashMessage(Constant.ENTER_LINE_SECTION_TO_ADD);
+        String input = scanner.next();
+        System.out.println();
+        return Exceptions.isInLineList(input);
+    }
+
+    public static String station(Scanner scanner, Line line) {
+        Print.hashMessage(Constant.ENTER_STATION_SECTION_TO_ADD);
+        String input = scanner.next();
+        System.out.println();
+        return Exceptions.isAlreadyAddedInThisLine(input, line);
+    }
+
+    public static int order(Scanner scanner) {
+        Print.hashMessage(Constant.ENTER_ORDER_SECTION_TO_ADD);
+        String input = scanner.next();
+        System.out.println();
+        return Exceptions.checkOrderInput(input);
+    }
+
+    public static void addStationToLine(Line line, String stationName, int order) {
+        StationRepository.stations().forEach(station -> {
+            if (station.getName().equals(stationName)) {
+                line.addStation(order, station);
+            }
+        });
+    }
+}
