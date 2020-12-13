f41ae9056876c21aa0d7f5806b5313132eb80364
Ji-Ha <xodhkd36@gmail.com>
2020-12-13 15:13:56 +0900

    style(NoExistStationNameException.java): 오류 메시지 변경

--- src/main/java/exception/NoExistStationNameException.java
+++ src/main/java/exception/NoExistStationNameException.java
@@ -3,7 +3,7 @@ package exception;
 
 public class NoExistStationNameException extends RuntimeException {
     private final static String ERROR = "[ERROR] ";
-    private final static String CAUSE = "이미 존재하지 않는 역입니다.";
+    private final static String CAUSE = "존재하지 않는 역입니다.";
 
     public NoExistStationNameException() {
         super(ERROR + CAUSE);
