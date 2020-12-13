6f995f65d5808b77fbd2bf62cf9e84ee63f90ef5
doorisopen <audauddl2@gmail.com>
2020-12-13 19:03:23 +0900

    feat: 입력값의 공백을 제거한다
    - docs: 기능 구현 목록 수정

--- src/main/java/subway/utils/ParseUtils.java
+++ src/main/java/subway/utils/ParseUtils.java
@@ -5,4 +5,8 @@ public class ParseUtils {
     public static int parseStringToInt(String str) {
         return Integer.parseInt(str);
     }
+
+    public static String parseStringToTrimString(String str) {
+        return str.trim();
+    }
 }
