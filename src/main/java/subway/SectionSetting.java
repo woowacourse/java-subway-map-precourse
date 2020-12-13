46e965edf76def728ca64d7192af720e70032fa2
Seonho Kim <zxzx235@naver.com>
2020-12-13 22:28:11 +0900

    refactor: 메소드명 변경

--- src/main/java/subway/SectionSetting.java
+++ src/main/java/subway/SectionSetting.java
@@ -8,32 +8,32 @@ import java.util.Scanner;
 
 public class SectionSetting {
     public static void add(Scanner scanner) {
-        String lineName = lineName(scanner);
+        String lineName = lineNameToAdd(scanner);
         LineRepository.lines().forEach(line -> {
             if (line.getName().equals(lineName)) {
-                String station = station(scanner, line);
-                int order = orderAdd(scanner, line);
+                String station = stationToAdd(scanner, line);
+                int order = orderToAdd(scanner, line);
                 addStationToLine(line, station, order);
                 Print.infoMessage(Constant.ADD_SECTION_DONE);
             }
         });
     }
 
-    public static String lineName(Scanner scanner) {
+    public static String lineNameToAdd(Scanner scanner) {
         Print.hashMessage(Constant.ENTER_LINE_SECTION_TO_ADD);
         String input = scanner.next();
         System.out.println();
         return Exceptions.isInLineList(input);
     }
 
-    public static String station(Scanner scanner, Line line) {
+    public static String stationToAdd(Scanner scanner, Line line) {
         Print.hashMessage(Constant.ENTER_STATION_SECTION_TO_ADD);
         String input = scanner.next();
         System.out.println();
         return Exceptions.isAlreadyAddedInThisLine(input, line);
     }
 
-    public static int orderAdd(Scanner scanner, Line line) {
+    public static int orderToAdd(Scanner scanner, Line line) {
         Print.hashMessage(Constant.ENTER_ORDER_SECTION_TO_ADD);
         String input = scanner.next();
         System.out.println();
