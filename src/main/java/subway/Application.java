c68181e5c6f22d4dce28d0551ab7fedcd56c304d
bluewood <bluewood.truth@gmail.com>
2020-12-13 18:37:04 +0900

    fix: Application 클래스 수정
    과거 테스트 중 입력했던 값이 남아 있던 부분을 수정함.
    Google Java Style Guide를 준수함.

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,12 +1,11 @@
 package subway;
 
 import java.util.Scanner;
-
 import subway.controller.SubwayMapController;
 
 public class Application {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in, "euc-kr");
+        final Scanner scanner = new Scanner(System.in);
         SubwayMapController.run(scanner);
     }
 }
