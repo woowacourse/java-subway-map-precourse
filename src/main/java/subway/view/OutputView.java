985d51f1278dbea8d54535a9320fdae04c8e31ce
seunghyun <ggyool@naver.com>
2020-12-14 06:47:24 +0900

    feat: 역 관리 화면 출력 기능 구현

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,27 +1,42 @@
 package subway.view;
 
 import subway.domain.screen.MainScreen;
-import subway.domain.screen.Screen;
+import subway.domain.screen.StationManagementScreen;
+
+import java.util.List;
 
 public class OutputView {
     private final static String ERROR_PREFIX = "[ERROR]";
     private final static String INFO_PREFIX = "[INFO]";
     private final static String SPACE = " ";
+    private final static String STATION_LIST_TITLE = "## 역 목록";
 
     public static void showMainScreen() {
-        String message = MainScreen.getInstance().getMessage();
+        String message = MainScreen.getInstance().toString();
         printGuide(message);
+    }
+
+    public static void showStationManagementScreen() {
+        String message = StationManagementScreen.getInstance().toString();
+        printGuide(message);
+    }
+
+    public static void showStationList(List<String> stationNames) {
+        printGuide(STATION_LIST_TITLE);
+        stationNames.stream()
+                .forEach(OutputView::printInfo);
+        lineFeed();
     }
 
     private static void printGuide(String message) {
         println(message);
     }
 
-    private void printError(String message) {
+    private static void printError(String message) {
         println(ERROR_PREFIX + SPACE + message);
     }
 
-    private void printInfo(String message) {
+    private static void printInfo(String message) {
         println(INFO_PREFIX + SPACE + message);
     }
 
@@ -31,5 +46,9 @@ public class OutputView {
 
     private static void println(String message) {
         System.out.println(message);
+    }
+
+    private static void lineFeed() {
+        System.out.println();
     }
 }
