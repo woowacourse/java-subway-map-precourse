984e0ead08535cc07a295fc62b9b3dd1e9817e9e
park seok chae <tjrco56@naver.com>
2020-12-14 00:51:40 +0900

    feat(StationRegisterView): 역 등록하는 기능 추가

--- src/main/java/subway/view/ManagerView.java
+++ src/main/java/subway/view/ManagerView.java
@@ -2,14 +2,15 @@ package subway.view;
 
 public abstract class ManagerView {
 	private static final String TOP_MENU_PREFIX = "\n## ";
-	
+	private static final String INFO_PREFIX = "[INFO] ";
+
 	protected static final String FIRST_MENU = "1";
 	protected static final String SECOND_MENU = "2";
 	protected static final String THIRD_MENU = "3";
 	protected static final String FOURTH_MENU = "4";
 	protected static final String BACK_MENU = "B";
 	protected static final String QUIT_MENU = "Q";
-	
+
 	private static final String MENU_LISTING_SEPARATOR = ". ";
 
 	public abstract void print();
@@ -40,5 +41,9 @@ public abstract class ManagerView {
 
 	public void printQuitMenu(String message) {
 		System.out.println(QUIT_MENU + MENU_LISTING_SEPARATOR + message);
+	}
+
+	public void printInfo(String message) {
+		System.out.println(INFO_PREFIX + message);
 	}
 }
--- src/main/java/subway/view/StationRegisterView.java
+++ src/main/java/subway/view/StationRegisterView.java
@@ -1,8 +1,24 @@
 package subway.view;
 
+import java.util.Scanner;
+
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
 public class StationRegisterView extends ManagerView {
 	private static final String TOP_MENU_MESSAGE = "등록할 역 이름을 입력하세요";
 
+	public StationRegisterView(Scanner scanner) {
+		print();
+		registerStation(scanner);
+		new MainView(scanner);
+	}
+	
+	public void registerStation(Scanner scanner) {
+		String name = scanner.nextLine();
+		StationRepository.addStation(new Station(name));
+	}
+	
 	@Override
 	public void print() {
 		printTopMenu(TOP_MENU_MESSAGE);
