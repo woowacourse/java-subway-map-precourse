5a605ec7ba3fefc1ded2efb0e5a8f6435e6f6dce
park seok chae <tjrco56@naver.com>
2020-12-14 01:05:24 +0900

    feat(StationRemovalVieW): 역 삭제 성공, 실패 메시지 추가

--- src/main/java/subway/view/StationRemovalView.java
+++ src/main/java/subway/view/StationRemovalView.java
@@ -6,15 +6,21 @@ import subway.domain.StationRepository;
 
 public class StationRemovalView extends ManagerView {
 	private static final String TOP_MENU_MESSAGE = "삭제할 역 이름을 입력하세요.";
+	private static final String REMOVE_SUCCESS_MESSAGE = "지하철 역이 삭제되었습니다.";
+	private static final String REMOVE_FAILURE_MESSAGE = "삭제할 지하철 역이 없습니다.";
 	
 	public StationRemovalView(Scanner scanner) {
 		print();
 		removeStation(scanner);
+		new MainView(scanner);
 	}
 	
-	public boolean removeStation(Scanner scanner) {
+	public void removeStation(Scanner scanner) {
 		String name = scanner.nextLine();
-		return StationRepository.deleteStation(name);
+		if(StationRepository.deleteStation(name)) {
+			printInfo(REMOVE_SUCCESS_MESSAGE);
+		}
+		printInfo(REMOVE_FAILURE_MESSAGE);		
 	}
 
 	@Override
