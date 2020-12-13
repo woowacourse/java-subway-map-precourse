379fa165dfc795d2f52d3083d1e2536f2d198a7e
Ji-Ha <xodhkd36@gmail.com>
2020-12-13 12:59:19 +0900

    exception(NoneFunctionException.java): 존재하지 않는 기능에 대한 예외

---
+++ src/main/java/exception/NoneFunctionException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class NoneFunctionException extends RuntimeException{
+    private final static String ERROR = "[ERROR] ";
+    private final static String CAUSE = "선택할 수 없는 기능입니다.";
+    public NoneFunctionException(){
+        super(ERROR + CAUSE);
+    }
+}
