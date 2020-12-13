e8fe42be4ee278d8f179f707d39e26282e0a12f2
operasystem6 <operasystem6@naver.com>
2020-12-14 01:57:37 +0900

    feat: text 모음 클래스 구현

---
+++ src/main/java/subway/view/TextCollection.java
@@ -0,0 +1,23 @@
+package subway.view;
+
+public class TextCollection {
+    public static final String PREFIX = "##";
+    public static final String MAIN_SCREEN_MESSAGE = "메인 화면";
+    public static final String SELECT_FUNCTION_MESSAGE = "원하는 기능을 선택하세요.";
+    public static final String STATION_MANAGEMENT_MESSAGE = "역 관리 화면";
+    public static final String ERROR = "잘못된 입력입니다.";
+    public static final String INFO_TAG = "[INFO] ";
+    public static final String ERROR_TAG = "[ERROR] ";
+
+    public static final String REGISTER_STATION_MESSAGE = "등록할 역 이름을 입력하세요.";
+    public static final String DELETE_STATION_MESSAGE = "삭제할 역 이름을 입력하세요.";
+
+    public static final String REGISTER_LINE_MESSAGE = "등록할 노선 이름을 입력하세요.";
+    public static final String REGISTER_UPWARD_STATION_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
+    public static final String REGISTER_DOWN_STATION_MESSAGE = "등록할 노선의 하행 종점역 이름을 입력하세요.";
+    public static final String DELETE_LINE_MESSAGE = "삭제할 노선 이름을 입력하세요";
+
+    public static final String INPUT_LINE = "노선을 입력하세요.";
+    public static final String INPUT_STATION = "역이름을 입력하세요.";
+    public static final String INPUT_ORDER = "순서를 입력하세요.";
+}
