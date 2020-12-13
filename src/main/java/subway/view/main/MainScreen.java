83a10abc1d36409ea6486c7f37015f1d5b277225
operasystem6 <operasystem6@naver.com>
2020-12-14 01:58:30 +0900

    refactor: 메인 화면 message 부분 재구현

--- src/main/java/subway/view/main/MainScreen.java
+++ src/main/java/subway/view/main/MainScreen.java
@@ -2,20 +2,18 @@ package subway.view.main;
 
 import subway.view.InputView;
 import subway.view.OutputView;
-import subway.view.main.MainMenu;
+import subway.view.TextCollection;
 
 import java.util.Arrays;
 
 public class MainScreen {
-    private static final String MAIN_SCREEN_MESSAGE = "메인 화면";
-    private static final String ERROR = "잘못된 입력입니다.";
 
     public static MainMenu selectMenu() {
         while (true) {
             showMenu();
             String input = InputView.inputFunction();
             if (!MainMenu.isValidInput(input)) {
-                OutputView.printError(ERROR);
+                OutputView.printError(TextCollection.ERROR);
                 continue;
             }
             return MainMenu.findMenuByKey(input);
@@ -23,7 +21,7 @@ public class MainScreen {
     }
 
     private static void showMenu() {
-        InputView.printQuestion(MAIN_SCREEN_MESSAGE);
+        InputView.printQuestion(TextCollection.MAIN_SCREEN_MESSAGE);
         Arrays.stream(MainMenu.values()).forEach(menu -> {
             System.out.println(menu.getKey() + ". " + menu.getTitle());
         });
