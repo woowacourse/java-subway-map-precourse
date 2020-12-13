3033db0f7781a992bab9d1e08bad5f6f6f953e11
sounmind <sounmind777@gmail.com>
2020-12-14 05:45:35 +0900

    feat(SubwayScreen): 메뉴 화면 입출력 인터페이스 구현

---
+++ src/main/java/subway/console/SubwayScreen.java
@@ -0,0 +1,14 @@
+package subway.console;
+
+import java.util.Scanner;
+
+interface SubwayScreen {
+    String MESSAGE_MENU_SELECT = "\n## 원하는 기능을 선택하세요.";
+    String ERROR_MAIN_SCREEN_NOT_VALID_INPUT = "[ERROR] 잘못된 기능 입력입니다.";
+
+    void startProcess(Scanner scanner);
+    void printScreen();
+    String getInput(Scanner scanner);
+    void validateInput(String input);
+    void transfer(Scanner scanner);
+}
