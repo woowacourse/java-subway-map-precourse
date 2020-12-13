8c36f4bce0cecb3d6d25b185afbb0147af36588a
perenok <perenoke@gmail.com>
2020-12-13 21:24:37 +0900

    feat(contorller): MainController
    메인 화면의 목록에 따라 다른 결과물 출력
     - 상수 설정을 통해 1,2,3,4번의 의미 부여
     - 각각의 번호에 맞는 view단 출력
     - 1은 역 관리 화면, 2는 노선 관리 화면
     - 현재 역까지만 구현(차후 업데이트 필요)

--- src/main/java/subway/controller/MainController.java
+++ src/main/java/subway/controller/MainController.java
@@ -1,2 +1,61 @@
-package subway.controller;public class MainController {
+package subway.controller;
+
+import subway.view.MainPage;
+import subway.view.StationPage;
+import utils.ErrorUtils;
+
+import java.util.Scanner;
+
+public class MainController {
+    private static final String STATION_MANAGEMENT = "1";
+    private static final String LINE_MANAGEMENT = "2";
+    private static final String ROUTE_MANAGEMENT = "3";
+    private static final String ROUTE_MAP = "4";
+    private static final String QUIT = "Q";
+
+
+    public static void startMainPage(Scanner scanner){
+        showMainPage();
+        forkPath(chooseItem(scanner), scanner);
+    }
+
+    public static void showMainPage(){
+        MainPage.printMainPage();
+    }
+
+    public static void forkPath(String item, Scanner scanner){
+        if(STATION_MANAGEMENT.equals(item)){
+            StationController.startStationPage(scanner);
+        }
+        if(LINE_MANAGEMENT.equals(item)){
+
+        }
+        if(ROUTE_MANAGEMENT.equals(item)){
+
+        }
+        if(ROUTE_MAP.equals(item)){
+
+        }
+    }
+
+    public static String chooseItem(Scanner scanner){
+        String item = input(scanner);
+        if(!isValidItem(item)){
+            ErrorUtils.printError("잘못된 입력입니다.");
+            startMainPage(scanner);
+        }
+        return item;
+    }
+
+    public static String input(Scanner scanner){
+        return scanner.next();
+    }
+
+    public static boolean isValidItem(String input){
+        if(STATION_MANAGEMENT.equals(input) || LINE_MANAGEMENT.equals(input) ||
+           ROUTE_MANAGEMENT.equals(input) || ROUTE_MAP.equals(input) || QUIT.equalsIgnoreCase(input)){
+            return true;
+        }
+        return false;
+    }
 }
