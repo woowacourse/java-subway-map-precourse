2e9bb0041b4ee175b84e698c7c643802c82625b8
Gayoung <gyyoon4u@naver.com>
2020-12-13 22:43:41 +0900

    feat: 객체동일성이 아닌 논리적 동일성 개념제공을 위한 코드

--- src/main/java/subway/domain/station/Station.java
+++ src/main/java/subway/domain/station/Station.java
@@ -1,9 +1,9 @@
 package subway.domain.station;
 
+import java.util.Objects;
+
 public class Station {
     private String name;
-    public static final int MIN_NAME_SIZE = 2;
-
     public Station(String name) {
         this.name = name;
     }
@@ -16,5 +16,22 @@ public class Station {
         if (StationCheck.checkStationNameLength(stationName) && StationCheck.checkStationNameEndPoint(stationName)) {
         }
         return new Station(stationName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Station station = (Station) o;
+        return Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
