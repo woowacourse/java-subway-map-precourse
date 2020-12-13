aeecafc8bf6db828adb827e6a8319acb675d18f3
operasystem6 <operasystem6@naver.com>
2020-12-14 00:21:22 +0900

    feat: 메인 화면 메뉴 기능 구현

---
+++ src/main/java/subway/view/main/MainMenu.java
@@ -0,0 +1,56 @@
+package subway.view.main;
+
+import subway.repository.LineRepository;
+import subway.view.line.LineView;
+import subway.view.section.SectionView;
+import subway.view.station.StationScreen;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public enum MainMenu {
+    STATION("1", "역 관리", (key) -> StationScreen.selectMenu()),
+    LINE("2", "노선 관리", (key) -> LineView.selectMenu()),
+    SECTION("3", "구간 관리", (key) -> SectionView.selectMenu()),
+    PRINT("4", "지하철 노선도 출력", (key) -> LineRepository.printAll()),
+    QUIT("Q", "종료");
+
+    private String key;
+    private String title;
+    private Consumer<String> expression;
+
+    MainMenu(String key, String title) {
+        this.key = key;
+        this.title = title;
+    }
+
+    MainMenu(String key, String title, Consumer<String> expression) {
+        this.key = key;
+        this.title = title;
+        this.expression = expression;
+    }
+
+    public void request(String key){
+        expression.accept(key);
+    }
+
+    public static MainMenu findMenuByKey(String key) {
+        return Arrays.stream(values())
+                .filter(menu -> menu.getKey().equals(key))
+                .findAny()
+                .get();
+    }
+
+    public static boolean isValidInput(String input) {
+        return Arrays.stream(values())
+                .anyMatch(menu -> menu.getKey().equals(input));
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
