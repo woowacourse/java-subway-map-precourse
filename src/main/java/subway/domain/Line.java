b7b5731ae1fdfc6704de8ca0d2b933bdf6285c19
park seok chae <tjrco56@naver.com>
2020-12-14 00:25:58 +0900

    feat(Line): 노선에서 역의 이름으로 역을 제거하는 기능 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,25 +1,30 @@
 package subway.domain;
 
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class Line {
-    private String name;
-    private LinkedList<Station> stations;
+	private String name;
+	private LinkedList<Station> stations;
 
-    public Line(String name) {
-        this.name = name;
-        stations = new LinkedList<Station>();
-    }
+	public Line(String name) {
+		this.name = name;
+		stations = new LinkedList<Station>();
+	}
 
-    public String getName() {
-        return name;
-    }
-    
-    public LinkedList<Station> getStations(){
-    	return stations;
-    }
-    
-    public void addStation(int index, Station station) {
-    	stations.add(index, station);
-    }
+	public String getName() {
+		return name;
+	}
+
+	public LinkedList<Station> getStations() {
+		return stations;
+	}
+
+	public void addStation(int index, Station station) {
+		stations.add(index, station);
+	}
+
+	public void removeStationByName(String name) {
+		stations.removeIf(station -> Objects.equals(station.getName(), name));
+	}
 }
