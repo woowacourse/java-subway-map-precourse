5777945717dd40755a32519af285f4de7b07f9e6
park seok chae <tjrco56@naver.com>
2020-12-14 01:08:46 +0900

    feat(LineManagerView): 노선 관리 메뉴 입력 받아 다른 메뉴로 이동하는 기능 추가

--- src/main/java/subway/view/LineManagerView.java
+++ src/main/java/subway/view/LineManagerView.java
@@ -1,5 +1,7 @@
 package subway.view;
 
+import java.util.Scanner;
+
 public class LineManagerView extends ManagerView {
 	private static final String TOP_MENU_MESSAGE = "노선 관리 화면";
 	private static final String FIRST_MENU_MESSAGE = "노선 등록";
@@ -7,6 +9,24 @@ public class LineManagerView extends ManagerView {
 	private static final String THIRD_MENU_MESSAGE = "노선 조회";
 	private static final String BACK_MENU_MESSAGE = "돌아가기";
 
+	public LineManagerView(Scanner scanner) {
+		print();
+		selectMenu(scanner);
+	}
+	
+	private void selectMenu(Scanner scanner) {
+		String menuSelected = scanner.nextLine();
+		if (menuSelected.equals(FIRST_MENU)) {
+			new LineRegisterView(scanner);
+		} else if (menuSelected.equals(SECOND_MENU)) {
+			new LineRemovalView(scanner);
+		} else if (menuSelected.equals(THIRD_MENU)) {
+			new LineListView();
+		} else if (menuSelected.equals(BACK_MENU)) {
+			new MainView(scanner);
+		}
+	}
+	
 	@Override
 	public void print() {
 		printTopMenu(TOP_MENU_MESSAGE);
