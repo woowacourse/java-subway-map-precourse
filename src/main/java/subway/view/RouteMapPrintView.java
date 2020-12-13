2c9a979a6550ad710facc00a5c09269ff11075d7
DWL <DWL>
2020-12-14 07:34:08 +0900

    feat: 지하철 노선도 출력 기능 추가

--- src/main/java/subway/view/RouteMapPrintView.java
+++ src/main/java/subway/view/RouteMapPrintView.java
@@ -1,11 +1,19 @@
 package subway.view;
 
 import static subway.resource.TextResource.HEADER_ROUTE_MAP_PRINT_VIEW;
+import static subway.resource.TextResource.PREFIX_INFO;
 
 import java.util.Scanner;
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Sections;
+import subway.domain.Station;
 import subway.view.MainView.OnBackListener;
+import sun.swing.SwingUtilities2.Section;
 
 public class RouteMapPrintView extends View {
+
+    private static final String line = "---";
 
     private OnBackListener onBackListener;
 
@@ -17,6 +25,18 @@ public class RouteMapPrintView extends View {
     @Override
     public void startView() {
         System.out.println(HEADER_ROUTE_MAP_PRINT_VIEW);
+        printRouteMap();
         onBackListener.onBack();
+    }
+
+    private void printRouteMap() {
+        for (Line line : LineRepository.lines()) {
+            System.out.println(PREFIX_INFO + " " + line.getName());
+            System.out.println(line);
+            Sections sections = line.getSections();
+            for (String station : sections.getSections()) {
+                System.out.println(PREFIX_INFO + " " + station);
+            }
+        }
     }
 }
