dfbbe6d87a84ed49a4ceecb52711458600b7b200
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-13 22:59:40 +0900

    feat : 노선 객체에 상행종점역, 하행종점역 필드 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,32 @@
 package subway.domain;
 
+import java.util.Objects;
+
 public class Line {
     private String name;
+    private Station uplineTerminalStation;
+    private Station downlineTerminalStation;
 
-    public Line(String name) {
+    public Line(String name, Station uplineTerminalStation, Station downlineTerminalStation) {
         this.name = name;
+        this.uplineTerminalStation = uplineTerminalStation;
+        this.downlineTerminalStation = downlineTerminalStation;
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Line)) return false;
+        Line line = (Line) o;
+        return Objects.equals(getName(), line.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
 }
