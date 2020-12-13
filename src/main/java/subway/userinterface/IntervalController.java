ee27db577d840fdff9ccfbfb0fd7fb240a8485e2
yunjung <cairofaith@gmail.com>
2020-12-13 17:43:49 +0900

    style(IntervalController): 공백 라인 수정

--- src/main/java/subway/userinterface/IntervalController.java
+++ src/main/java/subway/userinterface/IntervalController.java
@@ -5,7 +5,6 @@ public class IntervalController implements Menu {
     private final static String MENU_NAME = "3. 구간 관리";
     private final static String MENU_KEY = "3";
 
-
     private static IntervalController intervalController;
 
     private IntervalController() {}
@@ -19,7 +18,6 @@ public class IntervalController implements Menu {
     public String getMenuKey() {
         return MENU_KEY;
     }
-
 
     public static Menu getInstance() {
         if (intervalController == null) {
