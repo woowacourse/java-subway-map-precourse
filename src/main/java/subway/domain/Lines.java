c1762097f11cb4b736a60fd0ca412efe59f88d9c
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-13 23:26:23 +0900

    feat : 이름으로 노선을 찾는 기능과 노선의 이름들을 리스트로 반환받는 받는 기능 구현

--- src/main/java/subway/domain/Lines.java
+++ src/main/java/subway/domain/Lines.java
@@ -1,10 +1,15 @@
 package subway.domain;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Lines {
+    private static final String ERR_NO_SUCH_NAME_LINE_MSG = "[ERROR] 해당 노선이 없습니다.";
+
     private List<Line> lines = new ArrayList<>();
 
     public boolean deleteLine(String name) {
@@ -13,6 +18,20 @@ public class Lines {
 
     public void addLine(Line line) {
         lines.add(line);
+    }
+
+    public Line findLine(String name) {
+        return lines.stream()
+                .filter(line -> Objects.equals(line.getName(), name))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException(ERR_NO_SUCH_NAME_LINE_MSG));
+    }
+
+    public List<String> lineNames() {
+        return lines.stream()
+                .map(Line::getName)
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
     }
 
     @Override
