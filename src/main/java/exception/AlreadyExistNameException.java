467a647cbc96e68c4d142b8835dff62001e28aac
Ji-Ha <xodhkd36@gmail.com>
2020-12-13 12:57:38 +0900

    exception(AlreadyExistNameException.java): 이미 존재하는 이름에 대한 예외

---
+++ src/main/java/exception/AlreadyExistNameException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class AlreadyExistNameException extends RuntimeException{
+    private final static String ERROR = "[ERROR] ";
+    private final static String CAUSE = "이미 있는 이름입니다.";
+    public AlreadyExistNameException(){
+        super(ERROR + CAUSE);
+    }
+}
