ea4c362424bb5a076b2aad597c04734e549ffc39
park seok chae <tjrco56@naver.com>
2020-12-13 20:43:07 +0900

    feat(StationManagerView): 역 관리 화면 View 추가

---
+++ src/main/java/subway/view/StationManagerView.java
@@ -0,0 +1,18 @@
+package subway.view;
+
+public class StationManagerView extends ManagerView {
+	private static final String TOP_MENU_MESSAGE = "역 관리 화면";
+	private static final String FIRST_MENU_MESSAGE = "역 등록";
+	private static final String SECOND_MENU_MESSAGE = "역 삭제";
+	private static final String THIRD_MENU_MESSAGE = "역 조회";
+	private static final String BACK_MENU_MESSAGE = "돌아가기";
+	
+	@Override
+	public void print() {
+		printTopMenu(TOP_MENU_MESSAGE);
+		printFirstMenu(FIRST_MENU_MESSAGE);
+		printSecondMenu(SECOND_MENU_MESSAGE);
+		printThirdMenu(THIRD_MENU_MESSAGE);
+		printBackMenu(BACK_MENU_MESSAGE);
+	}
+}
