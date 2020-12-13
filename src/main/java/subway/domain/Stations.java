06d2940a411da504ad2ba382ea6e924a47ed80e5
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-13 20:16:55 +0900

    feat & fix : 지하철역 일급컬렉션에서 오류 수정 및 이름으로 역 찾기, 사이즈 확인 기능 추가
    오류 : 지하철역을 추가한 후에 return 문이 없어 무조건 예외가 발생하는 오류 수정

--- src/main/java/subway/domain/Stations.java
+++ src/main/java/subway/domain/Stations.java
@@ -2,6 +2,7 @@ package subway.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class Stations {
@@ -10,11 +11,23 @@ public class Stations {
     public void addStation(Station station) {
         if (!stations.contains(station)) {
             stations.add(station);
+            return;
         }
         throw new IllegalStateException("[ERROR] 이미 등록된 역명입니다.");
     }
 
     public boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    public int size() {
+        return stations.size();
+    }
+
+    public Station findStation(String name) {
+        return stations.stream()
+                .filter(s -> s.equals(new Station(name)))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("[ERROR] 해당 역이 없습니다."));
     }
 }
