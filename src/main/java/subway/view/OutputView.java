527830d041ea4d19c29029ca9e94e5aaa095de3d
김경록 <goodboy3421@gmail.com>
2020-12-14 00:17:01 +0900

    feat: 지하철 역의 목록을 조회하는 기능 (view단)
    실제 출력을 위한 기능은 구현하지 않았음

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -8,10 +8,11 @@ import java.util.List;
 public class OutputView {
     private static final String INFO_MESSAGE = "[INFO] ";
     private static final String RESULT_VIEW_DIVIDER = "---";
-    public static final String SUBWAY_ROUTE_MAP_MESSAGE = "## 지하철 노선도";
+    private static final String SUBWAY_ROUTE_MAP_LIST_MESSAGE = "## 지하철 노선도";
+    private static final String STATIONS_LIST_MESSAGE = "## 역 목록";
 
     public static void printLines(List<Line> lines) {
-        System.out.println(SUBWAY_ROUTE_MAP_MESSAGE);
+        System.out.println(SUBWAY_ROUTE_MAP_LIST_MESSAGE);
         for (Line line : lines) {
             printLine(line);
         }
@@ -21,6 +22,14 @@ public class OutputView {
         List<Station> stations = line.getStations();
         System.out.println(INFO_MESSAGE + line.getName());
         System.out.println(INFO_MESSAGE + RESULT_VIEW_DIVIDER);
+        for (Station station : stations) {
+            System.out.println(INFO_MESSAGE + station.getName());
+        }
+        System.out.println();
+    }
+
+    public static void printStations(List<Station> stations) {
+        System.out.println(STATIONS_LIST_MESSAGE);
         for (Station station : stations) {
             System.out.println(INFO_MESSAGE + station.getName());
         }
