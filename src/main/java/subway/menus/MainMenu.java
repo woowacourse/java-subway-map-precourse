c445ce68b9c8596203d01ebc66249d1f1ec45cab
tomas <cmg14111@gmail.com>
2020-12-14 05:02:24 +0900

    refactor(MainMenu): 지하철 노선도 출력의 역할을 드러내도록 상수명 변경
    before:
    SHOW_SUBWAYS
    
    after:
    SHOW_SUBWAY_MAP

--- src/main/java/subway/menus/MainMenu.java
+++ src/main/java/subway/menus/MainMenu.java
@@ -6,7 +6,7 @@ public enum MainMenu implements Menu {
     STATION_MANAGEMENT("1", "역 관리"),
     LINE_MANAGEMENT("2", "노선 관리"),
     SECTION_MANAGEMENT("3", "구간 관리"),
-    SHOW_SUBWAYS("4", "지하철 노선도 출력"),
+    SHOW_SUBWAY_MAP("4", "지하철 노선도 출력"),
     EXIT_PROGRAM("Q", "종료");
 
     private String option;
