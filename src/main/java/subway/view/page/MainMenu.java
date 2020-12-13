6630042d6292b89ec1317037c68ebb8c38ab7e92
ecsimsw <ecsimsw@gmail.com>
2020-12-13 16:37:20 +0900

    fix : 출력 문구 오류 수정 라인 출력 오류

--- src/main/java/subway/view/page/MainMenu.java
+++ src/main/java/subway/view/page/MainMenu.java
@@ -28,7 +28,7 @@ public class MainMenu {
     }
 
     public static Menu getMenuSelection(Scanner scanner) {
-        OutputView.printMsg("원하는 기능을 선택하세요.");
+        OutputView.printMsg("원하는 기능을 선택하세요.\n");
         return Menu.getSelection(InputView.getInput(scanner));
     }
 
