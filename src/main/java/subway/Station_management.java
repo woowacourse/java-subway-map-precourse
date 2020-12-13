167bf2f0afc1541cf344b918234bfa8aa769a91e
bbakjae <qkrwogus0724@naver.com>
2020-12-14 00:30:09 +0900

    feat(Station_management.java): 역 관리 기능 구현

--- src/main/java/subway/Station_management.java
+++ src/main/java/subway/Station_management.java
@@ -4,26 +4,35 @@ import subway.domain.Station;
 import subway.domain.StationRepository;
 
 public class Station_management {
-	public void add_station(String name) {
-		Station new_station = new Station(name);
+	public static void add_station(String name) {		
 		for (Station station : StationRepository.stations()) {
 			if (station.getName().equals(name)) {
 				System.out.println("[INFO] 이미 있는 지하철 역입니다.");
 				return;
 			}
 		}
+		Station new_station = new Station(name);
 		StationRepository.addStation(new_station);
 	}
 	
-	public void delete_station(String name) {
+	public static void delete_station(String name) {
 		if (StationRepository.deleteStation(name)) {
 			System.out.println("[INFO] 지하철 역이 삭제되었습니다.");
 			return;
 		}
-		System.out.println("[INFO] 존재하지 않는 지하철 역입니다.");
+		System.out.println("[ERROR] 존재하지 않는 지하철 역입니다.");
 	}
 	
-	public void print_stations() {
+	public static Station get_station_by_name(String name) {
+		for (Station station : StationRepository.stations()) {
+			if(name.equals(station.getName())) {
+				return station;
+			}
+		}
+		return null;
+	}
+	
+	public static void print_stations() {
 		System.out.println("##역 목록");
 		for ( Station station : StationRepository.stations()) {
 			System.out.println("[INFO] "+station.getName());
