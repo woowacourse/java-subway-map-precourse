59b063c89327e945da6e3c6f723936e9449b1201
park seok chae <tjrco56@naver.com>
2020-12-13 18:51:20 +0900

    feat(Line): 등록된 역 정보를 관리하는 stations 멤버 추가, 역 리스트에서 역을 추가하고 제거하는 기능 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,25 @@
 package subway.domain;
 
+import java.util.LinkedList;
+
 public class Line {
     private String name;
+    private LinkedList<Station> stations;
 
     public Line(String name) {
         this.name = name;
+        stations = new LinkedList<Station>();
     }
 
     public String getName() {
         return name;
     }
-
-    // 추가 기능 구현
+    
+    public LinkedList<Station> getStations(){
+    	return stations;
+    }
+    
+    public void addStation(int index, Station station) {
+    	stations.add(index, station);
+    }
 }
