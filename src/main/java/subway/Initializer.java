597e7ea58b1ec3485953677cafa3e50d606e936e
박우림 <woorimprog@gmail.com>
2020-12-14 05:34:06 +0900

    feat(subway) : add the line-initialize-function

--- src/main/java/subway/Initializer.java
+++ src/main/java/subway/Initializer.java
@@ -1,12 +1,41 @@
 package subway;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
 
 public class Initializer {
+	private static final List<String> SECOND_LINE = new ArrayList<>() {
+		{
+			add("교대역");
+			add("강남역");
+			add("역삼역");
+		}
+	};
+	
+	private static final List<String> THIRD_LINE = new ArrayList<>() {
+		{
+			add("교대역");
+			add("남부터미널역");
+			add("양재역");
+			add("매봉역");
+		}
+	};
+	
+	private static final List<String> SINBUNDANG_LINE = new ArrayList<>() {
+		{
+			add("강남역");
+			add("양재역");
+			add("양재시민의숲역");
+		}
+	};
+	
 	private static final List<String> INITIAL_STATIONS = new ArrayList<>() {
 		{
 			add("교대역");
@@ -19,13 +48,28 @@ public class Initializer {
 		}
 	};
 	
+	private static final HashMap<String, List<String>> INITIAL_LINES = new HashMap<>() {
+		{
+			put("2호선", SECOND_LINE);
+			put("3호선", THIRD_LINE);
+			put("신분당선", SINBUNDANG_LINE);
+		}
+	};
+	
 	public static void run() {
 		initStation();
+		initLine();
 	}
 	
 	private static void initStation() {
 		for (String name: INITIAL_STATIONS) {
 			StationRepository.addStation(new Station(name));
+		}
+	}
+	
+	private static void initLine() {
+		for (Entry<String, List<String>> line: INITIAL_LINES.entrySet()) {
+			LineRepository.addLine(new Line(line.getKey(), line.getValue()));
 		}
 	}
 }
--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,14 +1,31 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
     private String upBoundTerminus;
     private String downstreamTerminus;
+    private List<String> stations;
 
+    public Line(String name, List<String> stations) {
+        this.name = name;
+        this.upBoundTerminus = stations.get(0);
+        this.downstreamTerminus = stations.get(stations.size() - 1);
+        this.stations = new ArrayList<>(stations);
+    }
+    
     public Line(String name, String upBoundTerminus, String downstreamTerminus) {
         this.name = name;
         this.upBoundTerminus = upBoundTerminus;
         this.downstreamTerminus = downstreamTerminus;
+        this.stations = new ArrayList<>() {
+        	{
+        		add(upBoundTerminus);
+        		add(downstreamTerminus);
+        	}
+        };
     }
 
     public String getName() {
