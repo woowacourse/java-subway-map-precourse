00df6d817e975c266e3ebf7d5ba05617c79015f7
bepoz <kangsy763@naver.com>
2020-12-13 17:43:43 +0900

    feat: Application에 서비스 시작 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,15 @@
 package subway;
 
+import subway.controller.MainController;
+import subway.view.InputView;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        InputView inputView = new InputView(scanner);
+        new MainController().service(inputView);
     }
 }
