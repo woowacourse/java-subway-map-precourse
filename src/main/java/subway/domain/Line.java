4b1b944374c7d872b90bf0e5fa1546032c0016f6
bbakjae <qkrwogus0724@naver.com>
2020-12-14 00:34:54 +0900

    feat(Line.java): Line 객체 생성 및 생성 메서드 정의

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,8 +1,14 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import subway.Station_management;
+
 public class Line {
     private String name;
-
+    private List<Station> stations = new ArrayList<Station>();
+    
     public Line(String name) {
         this.name = name;
     }
@@ -12,4 +18,24 @@ public class Line {
     }
 
     // 추가 기능 구현
+    public Line(String name, String UP, String DOWN) {
+    	this.name = name;
+    	stations.add(Station_management.get_station_by_name(UP));
+    	stations.add(Station_management.get_station_by_name(DOWN));
+    }
+    
+    public Line(String name, List<String> station_name) {
+    	this.name = name;
+    	for (String station : station_name) {    		
+    		stations.add(Station_management.get_station_by_name(station));
+    	}
+    }
+    
+    public void print_stationOfLine() {
+    	System.out.println("[INFO] "+this.name);
+    	System.out.println("[INFO] ---");
+    	for (Station station : stations) {
+    		System.out.println("[INFO] "+station.getName());
+    	}
+    }
 }
