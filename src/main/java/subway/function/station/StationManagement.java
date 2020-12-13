8c96c4344399f723975c7920106ec5060cbcdd2d
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-12-13 13:33:45 +0900

    feat(역 관리 화면 입력 에러 처리): 1, 2, 3, B 이외의 입력은 에러

--- src/main/java/subway/function/station/StationManagement.java
+++ src/main/java/subway/function/station/StationManagement.java
@@ -1,6 +1,8 @@
 package subway.function.station;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import subway.commonprint.error.CommonErrorPrinter;
 import subway.commonprint.info.CommonInfoPrinter;
 import subway.main.UserSelections;
 import subway.domain.station.Station;
@@ -8,28 +10,37 @@ import subway.domain.station.StationRepository;
 
 public class StationManagement {
     public static void start(Scanner scanner) {
-        printScreen();
-        StationManagementSelectionType type = getStationManagementSelectionType(scanner);
-        resolveStationManagement(type, scanner);
+        String selectionInputPattern = "^[123B]$";
+        while (true) {
+            printScreen();
+            CommonInfoPrinter.printUserFunctionSelectionMessage();
+            String userInput = scanner.nextLine();
+            if (!Pattern.matches(selectionInputPattern, userInput)) {
+                CommonErrorPrinter.printSelectionInputErrorMessage();
+                continue;
+            }
+            StationManagementSelectionType type = getStationManagementSelectionType(userInput);
+            resolveStationManagement(type, scanner);
+            break;
+        }
     }
 
     private static StationManagementSelectionType getStationManagementSelectionType(
-        Scanner scanner) {
-        CommonInfoPrinter.printUserFunctionSelectionMessage();
-        String stationManagementSelectionInput = scanner.nextLine();
-        if (stationManagementSelectionInput.equals(UserSelections.FIRST)) {
+        String userInput) {
+        if (userInput.equals(UserSelections.FIRST)) {
             return StationManagementSelectionType.STATION_REGISTRATION;
         }
-        if (stationManagementSelectionInput.equals(UserSelections.SECOND)) {
+        if (userInput.equals(UserSelections.SECOND)) {
             return StationManagementSelectionType.STATION_DELETE;
         }
-        if (stationManagementSelectionInput.equals(UserSelections.THIRD)) {
+        if (userInput.equals(UserSelections.THIRD)) {
             return StationManagementSelectionType.STATION_PRINT_ALL;
         }
         return StationManagementSelectionType.GO_BACK;
     }
 
-    private static void resolveStationManagement(StationManagementSelectionType type, Scanner scanner) {
+    private static void resolveStationManagement(StationManagementSelectionType type,
+        Scanner scanner) {
         if (type == StationManagementSelectionType.STATION_REGISTRATION) {
             registerNewStation(scanner);
         }
