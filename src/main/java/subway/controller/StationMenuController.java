e7d762fda4b66821dcdbd62b32db1cb6f2ce64fb
김주원 <kjw11077@naver.com>
2020-12-14 00:27:13 +0900

    feat(controller): StationMenuController 어플리케이션 로직 구현

--- src/main/java/subway/controller/StationMenuController.java
+++ src/main/java/subway/controller/StationMenuController.java
@@ -1,6 +1,12 @@
 package subway.controller;
 
+import static subway.view.OutputView.DOT;
+import static subway.view.OutputView.NEWLINE;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import subway.exception.GoBackToPrevControllerException;
+import subway.exception.InvalidChoiceInputException;
 import subway.service.StationService;
 import subway.view.InputView;
 import subway.view.OutputView;
@@ -9,65 +15,38 @@ public class StationMenuController {
 
     public static final String TITLE = "역 관리 화면";
 
-    public static final String REGISTER_STATION = "1";
-    public static final String UNREGISTER_STATION = "2";
-    public static final String LIST_STATIONS = "3";
-    public static final String GO_BACK = "B";
-
     private StationMenuController() {
     }
 
     public static void main() {
-        printMenu();
         while (true) {
+            printMenu();
             try {
                 route(InputView.getInput());
             } catch (GoBackToPrevControllerException e) {
                 return;
             } catch (Exception e) {
                 OutputView.printError(e);
-                return;
+                continue;
             }
+            return;
         }
+    }
+
+    private static void printMenu() {
+        OutputView.printMenu(TITLE, Menu.getMenus());
     }
 
     private static void route(String input) {
-        if (REGISTER_STATION.equals(input)) {
-            registerStation();
-            return;
-        }
-
-        if (UNREGISTER_STATION.equals(input)) {
-            unregisterStation();
-            return;
-        }
-
-        if (LIST_STATIONS.equals(input)) {
-            listStations();
-            return;
-        }
-
-        if (GO_BACK.equals(input)) {
-            throw new GoBackToPrevControllerException();
-        }
-    }
-
-    private static void listStations() {
-        StationService.listAllStations();
-    }
-
-    private static void unregisterStation() {
-        while (true) {
-            try {
-                StationService.removeStationByName(InputView.getInput());
-                return;
-            } catch (Exception e) {
-                OutputView.printError(e);
-            }
-        }
+        Arrays.stream(Menu.values())
+                .filter(menu -> menu.button.equals(input))
+                .findAny()
+                .orElseThrow(InvalidChoiceInputException::new)
+                .goToMenu();
     }
 
     private static void registerStation() {
+        OutputView.printNotice("등록할 역 이름을 입력하세요.");
         while (true) {
             try {
                 StationService.resisterStationByName(InputView.getInput());
@@ -78,7 +57,58 @@ public class StationMenuController {
         }
     }
 
-    private static void printMenu() {
-        OutputView.printInfo(TITLE);
+    private static void unregisterStation() {
+        OutputView.printNotice("삭제할 역 이름을 입력하세요.");
+        while (true) {
+            try {
+                StationService.removeStationByName(InputView.getInput());
+                return;
+            } catch (Exception e) {
+                OutputView.printError(e);
+            }
+        }
+    }
+
+    private static void listStations() {
+        OutputView.print(NEWLINE);
+        OutputView.printNotice("역 목록");
+        StationService.listAllStations();
+    }
+
+    private static void goBack() {
+        throw new GoBackToPrevControllerException();
+    }
+
+
+    enum Menu {
+        REGISTER_STATION("1", "역 등록", StationMenuController::registerStation),
+        UNREGISTER_STATION("2", "역 삭제", StationMenuController::unregisterStation),
+        LIST_STATIONS("3", "역 조회", StationMenuController::listStations),
+        GO_BACK("B", "돌아가기", StationMenuController::goBack);
+
+        private final String button;
+        private final String detail;
+        private final Runnable runnable;
+
+        Menu(String button, String detail, Runnable runnable) {
+            this.button = button;
+            this.detail = detail;
+            this.runnable = runnable;
+        }
+
+        public void goToMenu() {
+            runnable.run();
+        }
+
+        public static String getMenus() {
+            return Arrays.stream(Menu.values())
+                    .map(Menu::toString)
+                    .collect(Collectors.joining(NEWLINE));
+        }
+
+        @Override
+        public String toString() {
+            return button + DOT + detail;
+        }
     }
 }
