55a70a4bcefe7977aacd450300309eb9c7b004a1
perenok <perenoke@gmail.com>
2020-12-14 00:08:58 +0900

    feat(domain): StationRepository
    isExistName과 isLongName 메소드 추가
     - stream().anyMatch를 통해 stations에 name이 같은 게 있는지 확인
     - 정규식 표현을 통해 영문자와 환글 2자 이상인 이름만 가능하도록 체크

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -1,9 +1,8 @@
 package subway.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
@@ -18,5 +17,14 @@ public class StationRepository {
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    public static boolean isExistName(String name){
+        return stations.stream().anyMatch(station -> Objects.equals(station.getName(), name));
+    }
+
+    public static boolean isLongName(String name){
+        String pattern = "^[a-zA-Z가-힣]{2,}$";
+        return Pattern.matches(pattern, name);
     }
 }
