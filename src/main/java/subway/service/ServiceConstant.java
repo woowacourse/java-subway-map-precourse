ccbaf06a9d7cdbc667dd3c8e8ff0383b341d4618
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-12-13 18:14:25 +0900

    refactor: Service 클래스 에러 문구를 가지고 있는 ServiceConstant 인터페이스 추가 및 인터페이스 내 상수 static final 제거

---
+++ src/main/java/subway/service/ServiceConstant.java
@@ -0,0 +1,8 @@
+package subway.service;
+
+public interface ServiceConstant {
+    String LINE_EXIST_ERROR = "\n[ERROR] 노선 목록에 이미 등록되어 있는 노선입니다.";
+    String LINE_NOT_EXIST_ERROR = "\n[ERROR] 노선 목록에 등록되어 있는 노선이 아닙니다.";
+    String STATION_EXIST_ERROR = "\n[ERROR] 역 목록에 이미 등록되어 있는 역입니다.";
+    String STATION_NOT_EXIST_ERROR = "\n[ERROR] 역 목록에 등록되어 있는 역이 아닙니다.";
+}
