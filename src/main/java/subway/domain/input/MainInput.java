003287aa4d4f862bacdcde3e431e4c5d8a6e802f
eoc940 <hgk0000@naver.com>
2020-12-13 20:45:07 +0900

    feat: 메인화면 입력 구현

---
+++ src/main/java/subway/domain/input/MainInput.java
@@ -0,0 +1,36 @@
+package subway.domain.input;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainInput {
+
+    static final String FUNCTION_ONE = "1";
+    static final String FUNCTION_TWO = "2";
+    static final String FUNCTION_THREE = "3";
+    static final String FUNCTION_FOUR = "4";
+    static final String FUNCTION_QUIT = "Q";
+
+    List<String> functionList = new ArrayList<>();
+
+    public List<String> functionList() {
+        functionList.add(FUNCTION_ONE);
+        functionList.add(FUNCTION_TWO);
+        functionList.add(FUNCTION_THREE);
+        functionList.add(FUNCTION_FOUR);
+        functionList.add(FUNCTION_QUIT);
+        return Collections.unmodifiableList(functionList);
+    }
+
+    public String inputMainScreen(Scanner scanner) {
+        String mainChoice = scanner.next();
+        if (functionList().contains(mainChoice)) {
+            return mainChoice;
+        }
+        // 정해진 것만 입력하라는 메시지
+        throw new IllegalArgumentException();
+    }
+
+}
