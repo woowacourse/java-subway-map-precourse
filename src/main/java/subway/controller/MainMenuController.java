bd3ab0f1e24fdbf4c8992ae1c4fa639cce8d25ac
김주원 <kjw11077@naver.com>
2020-12-13 14:47:53 +0900

    refactor(controller): MainMenuController 메소드 이동, 상수 추출 등 리팩토링

--- src/main/java/subway/controller/MainMenuController.java
+++ src/main/java/subway/controller/MainMenuController.java
@@ -1,6 +1,7 @@
 package subway.controller;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 import subway.exception.ExitSystemException;
 import subway.exception.InvalidChoiceInputException;
 import subway.view.InputView;
@@ -9,38 +10,7 @@ import subway.view.OutputView;
 public class MainMenuController {
 
     public static final String TITLE = "메인 화면";
-    public static final String MANAGE_STATION = "1";
-    public static final String MANAGE_LINE = "2";
-    public static final String MANAGE_SECTION = "3";
-    public static final String PRINT_SUBWAY_MAP = "4";
-    public static final String EXIT_SYSTEM = "Q";
-
-    public static final String DOT = ". ";
-
-    enum Menu {
-        MANAGE_STATION("1", "역 관리", StationMenuController::main),
-        MANAGE_LINE("2", "노선 관리", LineMenuController::main),
-        MANAGE_SECTION("3", "구간 관리", SectionMenuController::main),
-        PRINT_SUBWAY_MAP("4", "지하철 노선도 출력", MainMenuController::printSubwayMap),
-        EXIT_SYSTEM("Q", "종료", MainMenuController::exitSystem);
-
-        private final String button;
-        private final String detail;
-        private final Runnable runnable;
-
-        //1. 역 등록
-        Menu(String button, String detail, Runnable runnable) {
-            this.button = button;
-            this.detail = detail;
-            this.runnable = runnable;
-        }
-
-        @Override
-        public String toString() {
-            return button + DOT + detail;
-        }
-    }
-
+    public static final String NEWLINE = "\n";
 
     private MainMenuController() {
     }
@@ -57,17 +27,8 @@ public class MainMenuController {
         }
     }
 
-    private static void printSubwayMap() {
-        // 지하철 노선도 출력
-    }
-
-    private static void exitSystem() {
-        throw new ExitSystemException();
-    }
-
     private static void printMenu() {
-        OutputView.printInfo(TITLE);
-        Arrays.stream(Menu.values()).forEach(menu -> OutputView.print(menu.toString()));
+        OutputView.printInfo(TITLE + NEWLINE + Menu.getMenus());
     }
 
     private static void route(String input) {
@@ -75,22 +36,50 @@ public class MainMenuController {
                 .filter(menu -> menu.button.equals(input))
                 .findAny()
                 .orElseThrow(InvalidChoiceInputException::new)
-                .runnable.run();
+                .goToMenu();
+    }
 
-//        if (MANAGE_STATION.equals(input)) {
-//            StationMenuController.main();
-//        }
-//        if (MANAGE_LINE.equals(input)) {
-//            LineMenuController.main();
-//        }
-//        if (MANAGE_SECTION.equals(input)) {
-//            SectionMenuController.main();
-//        }
-//        if (PRINT_SUBWAY_MAP.equals(input)) {
-//            printSubwayMap();
-//        }
-//        if (EXIT_SYSTEM.equals(input)) {
-//            exitSystem();
-//        }
+    private static void printSubwayMap() {
+        // todo 서비스로 이동
+    }
+
+    private static void exitSystem() {
+        throw new ExitSystemException();
+    }
+
+    enum Menu {
+        MANAGE_STATION("1", "역 관리", StationMenuController::main),
+        MANAGE_LINE("2", "노선 관리", LineMenuController::main),
+        MANAGE_SECTION("3", "구간 관리", SectionMenuController::main),
+        PRINT_SUBWAY_MAP("4", "지하철 노선도 출력", MainMenuController::printSubwayMap),
+        EXIT_SYSTEM("Q", "종료", MainMenuController::exitSystem);
+
+        public static final String DOT = ". ";
+
+        private final String button;
+        private final String detail;
+        private final Runnable runnable;
+
+        //1. 역 등록
+        Menu(String button, String detail, Runnable runnable) {
+            this.button = button;
+            this.detail = detail;
+            this.runnable = runnable;
+        }
+
+        public void goToMenu() {
+            runnable.run();
+        }
+
+        public static String getMenus() {
+            return Arrays.stream(Menu.values())
+                    .map(Menu::toString)
+                    .collect(Collectors.joining(NEWLINE));
+        }
+
+        @Override
+        public String toString() {
+            return button + DOT + detail;
+        }
     }
 }
