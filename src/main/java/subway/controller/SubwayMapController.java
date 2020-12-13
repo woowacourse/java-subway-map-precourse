509bfd14f62e91fd525de30fe090dfc57d3493da
seunghyun <ggyool@naver.com>
2020-12-13 22:49:27 +0900

    feat: controller에 메인화면 실행 코드 작성

--- src/main/java/subway/controller/SubwayMapController.java
+++ src/main/java/subway/controller/SubwayMapController.java
@@ -1,13 +1,46 @@
 package subway.controller;
 
+import subway.domain.command.Command;
+import subway.domain.screen.MainScreen;
+import subway.domain.screen.ScreenType;
 import subway.domain.station.Station;
 import subway.service.StationService;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+import java.util.Scanner;
 
 public class SubwayMapController {
+    private final InputView inputView;
     private final StationService stationService;
+    private ScreenType currentScreen = ScreenType.MAIN;
 
-    public SubwayMapController(StationService stationService) {
-        this.stationService = stationService;
+    public SubwayMapController(Scanner scanner) {
+        inputView = new InputView(scanner);
+        stationService = new StationService();
+    }
+
+    public void run() {
+        while (isRunning()) {
+            runScreen(currentScreen);
+        }
+    }
+
+    public boolean isRunning() {
+        return !currentScreen.isExitScreen();
+    }
+
+    private void runScreen(ScreenType currentScreenType) {
+        runMainScreen(currentScreenType);
+    }
+
+    private void runMainScreen(ScreenType currentScreenType) {
+        if (!currentScreenType.isMainScreen()) {
+            return;
+        }
+        OutputView.showMainScreen();
+        Command command = inputView.inputMainCommand();
+        currentScreen = MainScreen.getInstance().getNextScreen(command);
     }
 
     public void registerStation(Station station) {
