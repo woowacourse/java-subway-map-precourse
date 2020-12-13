8fdd4a1f4d4785b453477b34bd113ec26ca28f12
sounmind <sounmind777@gmail.com>
2020-12-14 02:50:17 +0900

    feat(DataInitialization): 데이터 초기화 기능 구현

---
+++ src/main/java/subway/domain/DataInitialization.java
@@ -0,0 +1,30 @@
+package subway.domain;
+
+public class DataInitialization {
+    private static final String[] initialStation = {"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
+
+    public static void initialize() {
+        initializeStations();
+        initializeLines();
+        inputStationsToLines();
+    }
+
+    private static void inputStationsToLines() {
+        LineRepository.lines().get(0).addStation("강남역", 2); // 2호선
+        LineRepository.lines().get(1).addStation("남부터미널역", 2); // 3호선
+        LineRepository.lines().get(1).addStation("양재역", 3);
+        LineRepository.lines().get(2).addStation("양재역", 2); // 신분당선
+    }
+
+    private static void initializeLines() {
+        LineRepository.initializeLine("2호선", "교대역", "역삼역");
+        LineRepository.initializeLine("3호선", "교대역", "매봉역");
+        LineRepository.initializeLine("신분당선", "강남역", "양재시민의숲역");
+    }
+
+    private static void initializeStations() {
+        for (int i = 0, total = initialStation.length; i < total; i++) {
+            StationRepository.addStation(initialStation[i]);
+        }
+    }
+}
