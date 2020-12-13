36a5d948916adaa070b5357b033661263e03076d
DWL <DWL>
2020-12-14 04:58:11 +0900

    feat: 노선 등록 상행 종점역, 하행 종점역 예외 체크 기능 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -3,15 +3,26 @@ package subway.domain;
 
 import static subway.resource.Config.MIN_LINE_NAME_LENGTH;
 import static subway.resource.TextResource.ERROR_LINE_NAME_LENGTH;
+import static subway.resource.TextResource.ERROR_NOT_EXISTENCE_STATION;
+import static subway.resource.TextResource.ERROR_START_END_STATION_DUPLICATED;
 
 public class Line {
 
     private String name;
 
-    public Line(String name) {
+    public Line(String name, String start, String end) {
         this.name = name;
+
         if (name.length() < MIN_LINE_NAME_LENGTH) {
             throw new IllegalArgumentException(ERROR_LINE_NAME_LENGTH);
+        }
+
+        if (!StationRepository.hasStation(start) || !StationRepository.hasStation(end)) {
+            throw new IllegalArgumentException(ERROR_NOT_EXISTENCE_STATION);
+        }
+
+        if (start.equals(end)) {
+            throw new IllegalArgumentException(ERROR_START_END_STATION_DUPLICATED);
         }
     }
 
@@ -20,4 +31,5 @@ public class Line {
     }
 
     // 추가 기능 구현
+
 }
--- src/test/java/subway/domain/LineTest.java
+++ src/test/java/subway/domain/LineTest.java
@@ -2,6 +2,8 @@ package subway.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static subway.resource.TextResource.ERROR_LINE_NAME_LENGTH;
+import static subway.resource.TextResource.ERROR_NOT_EXISTENCE_STATION;
+import static subway.resource.TextResource.ERROR_START_END_STATION_DUPLICATED;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,9 +13,43 @@ public class LineTest {
     @Test
     public void checkLineNameOverConfigLength() {
         assertThatThrownBy(() -> {
-            Line line = new Line("가");
+            Line line = new Line("가", "강남역", "교대역");
         }).isInstanceOf(IllegalArgumentException.class).hasMessage(
             ERROR_LINE_NAME_LENGTH
+        );
+    }
+
+    @DisplayName("상행 종점역과 하행 종점역은 존재 해야 한다.")
+    @Test
+    public void checkLineStartOrEndStationNotExistence() {
+        StationRepository.addStation(new Station("교대역"));
+        assertThatThrownBy(() -> {
+            Line line = new Line("가나다역", "존재하지않는역", "교대역");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage(
+            ERROR_NOT_EXISTENCE_STATION
+        );
+
+        assertThatThrownBy(() -> {
+            Line line = new Line("가나다역", "교대역", "존재하지않는역");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage(
+            ERROR_NOT_EXISTENCE_STATION
+        );
+
+        assertThatThrownBy(() -> {
+            Line line = new Line("가나다역", "1존재하지않는역", "2존재하지않는역");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage(
+            ERROR_NOT_EXISTENCE_STATION
+        );
+    }
+
+    @DisplayName("상행 종점역과 하행 종점역은 같은 역일 수 없다.")
+    @Test
+    public void checkLineStartAndEndStationSame() {
+        StationRepository.addStation(new Station("교대역"));
+        assertThatThrownBy(() -> {
+            Line line = new Line("가나다역", "교대역", "교대역");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage(
+            ERROR_START_END_STATION_DUPLICATED
         );
     }
 }
