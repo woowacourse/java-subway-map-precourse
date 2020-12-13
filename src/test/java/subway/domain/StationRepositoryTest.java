73a97710b6fb0429ece0f0f6f74a8964939fe321
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-13 18:41:09 +0900

    test & doc : 지하철 역 생성하기 테스트 작성 및 README.md의 오타 수정
    테스트한 내용 : 지하철 역 생성과 지하철 역의 예외처리 기능에 대한 테스트 작성

---
+++ src/test/java/subway/domain/StationRepositoryTest.java
@@ -0,0 +1,24 @@
+package subway.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class StationRepositoryTest {
+    @DisplayName("역을 생성한다")
+    @Test
+    void 역을_생성한다() {
+        String testName = "사당역";
+        assertThat(StationFactory.makeStation(testName)).isEqualTo(new Station(testName));
+    }
+
+    @DisplayName("예외1. 지하철 역 이름이 2글자 미만이면 예외를 발생시킨다.")
+    @Test
+    void 역이름_2글자_예외처리() {
+        assertThatThrownBy(() -> StationFactory.makeStation("일"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 역의 이름은 2글자 이상이어야 합니다.");
+    }
+}
