637119b5856921dc2728663f723bc364d9420ad1
operasystem6 <operasystem6@naver.com>
2020-12-14 01:59:01 +0900

    refactor: InputView 클래스 message 부분 재구현

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -3,8 +3,6 @@ package subway.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String PREFIX = "##";
-    private static final String SELECT_FUNCTION_MESSAGE = "원하는 기능을 선택하세요.";
     private static Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -12,11 +10,11 @@ public class InputView {
     }
 
     public static String inputFunction() {
-        printQuestion(SELECT_FUNCTION_MESSAGE);
+        printQuestion(TextCollection.SELECT_FUNCTION_MESSAGE);
         return scanner.nextLine();
     }
 
     public static void printQuestion(String message) {
-        System.out.printf("%s %s", PREFIX, message);
+        System.out.printf("%s %s", TextCollection.PREFIX, message);
     }
 }
