4129c9e969372ccec656786a186b8ddaa00e4f8b
park seok chae <tjrco56@naver.com>
2020-12-14 00:55:23 +0900

    feat(StationRemovalView): 역 이름을 입력으로 받아 역 삭제하는 기능 추가

--- src/main/java/subway/view/StationRemovalView.java
+++ src/main/java/subway/view/StationRemovalView.java
@@ -1,7 +1,21 @@
 package subway.view;
 
+import java.util.Scanner;
+
+import subway.domain.StationRepository;
+
 public class StationRemovalView extends ManagerView {
 	private static final String TOP_MENU_MESSAGE = "삭제할 역 이름을 입력하세요.";
+	
+	public StationRemovalView(Scanner scanner) {
+		print();
+		removeStation(scanner);
+	}
+	
+	public boolean removeStation(Scanner scanner) {
+		String name = scanner.nextLine();
+		return StationRepository.deleteStation(name);
+	}
 
 	@Override
 	public void print() {
