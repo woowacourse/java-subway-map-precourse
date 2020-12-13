7a84bf044b01d557eb4df80cd46bb2884716336e
eoc940 <hgk0000@naver.com>
2020-12-13 16:20:41 +0900

    feat: 구간 등록할 노선 입력받는 기능 구현

--- src/main/java/subway/domain/input/SectionManageInput.java
+++ src/main/java/subway/domain/input/SectionManageInput.java
@@ -1,2 +1,42 @@
-package subway.domain.input;public class SectionManageInput {
+package subway.domain.input;
+
+import subway.domain.Line;
+import subway.domain.LineRepository;
+
+import java.util.*;
+
+public class SectionManageInput {
+
+    static final String FUNCTION_ONE = "1";
+    static final String FUNCTION_TWO = "2";
+    static final String FUNCTION_BACK = "B";
+
+    List<String> functionList = new ArrayList<>();
+
+    public List<String> functionList() {
+        functionList.add(FUNCTION_ONE);
+        functionList.add(FUNCTION_TWO);
+        functionList.add(FUNCTION_BACK);
+        return Collections.unmodifiableList(functionList);
+    }
+
+    public String inputLineManageScreen(Scanner scanner) throws IllegalArgumentException{
+        String sectionManageChoice = scanner.next();
+        if (functionList().contains(sectionManageChoice)) {
+            return sectionManageChoice;
+        }
+        // 정해진 것만 입력하라는 메시지
+        throw new IllegalArgumentException();
+    }
+
+    public String inputLine(Scanner scanner) throws IllegalArgumentException{
+        String lineName = scanner.next();
+        Optional<Line> searchedLine =LineRepository.lines()
+                .stream().filter(line -> line.getName().equals(lineName)).findAny();
+        if (searchedLine.isPresent()) {
+            return lineName;
+        }
+        //해당 노선 이름이 존재하지 않는다는 메시지
+        throw new IllegalArgumentException();
+    }
 }
