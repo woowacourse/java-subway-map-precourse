ccd59ac5c4bf5c2ce8beb753fef676608fc65a2c
pjy1368 <basejin0810@naver.com>
2020-12-13 11:57:27 +0900

    feat: 역 삭제 기능 구현.

--- src/main/java/subway/domain/menu/submenu/action/DeleteAction.java
+++ src/main/java/subway/domain/menu/submenu/action/DeleteAction.java
@@ -2,8 +2,44 @@ package subway.domain.menu.submenu.action;
 
 import java.util.Scanner;
 
+import subway.domain.StationRepository;
+import subway.domain.menu.constant.CategoryType;
+import subway.domain.menu.constant.CommonMessage;
+import subway.domain.menu.submenu.action.constant.ActionMessage;
+
 public class DeleteAction extends Action {
     public DeleteAction(char order, String category, String actionType, Scanner scanner) {
         super(order, category, actionType, scanner);
+    }
+
+    @Override
+    public void runAction() {
+        printDeleteMessage();
+        String name = inputDelete();
+        if (category.equals(CategoryType.STATION)) {
+            deleteStation(name);
+        }
+        printSuccessMessage();
+    }
+
+    private void printDeleteMessage() {
+        System.out.println(CommonMessage.SHARP + CommonMessage.SHARP + CommonMessage.SPACE + ActionMessage.INPUT_DELETE
+                + CommonMessage.SPACE + category + CommonMessage.SPACE + ActionMessage.INPUT_DELETE_NAME);
+    }
+
+    private String inputDelete() {
+        String name = scanner.nextLine();
+        System.out.println();
+        return name;
+    }
+
+    private void deleteStation(String name) {
+        StationRepository.deleteStation(name);
+    }
+
+    private void printSuccessMessage() {
+        System.out.println(CommonMessage.INFO + CommonMessage.SPACE + ActionMessage.SUCCESS_SUBWAY + CommonMessage.SPACE
+                + category + ActionMessage.SUCCESS_POSTPOSITION + CommonMessage.SPACE + ActionMessage.SUCCESS_DELETE);
+        System.out.println();
     }
 }
