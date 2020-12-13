9cbbe8ef0d7759719312a7dc001da0e620471032
sounmind <sounmind777@gmail.com>
2020-12-14 02:47:41 +0900

    feat(Line): 구간 추가 시 필요한 기능 수정 및 구현(순서, 역 이름 관련)
    - 해당 노선에 구간을 추가할 때 순서를 현실 기준으로 변경
    - 역 이름을 문자열로 받아 해당 역 객체를 얻을 수 있게 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,15 @@
 package subway.domain;
 
 import java.util.LinkedList;
+import java.util.stream.Stream;
 
-class Line {
+public class Line {
     private static final String ERROR_INDEX_NOT_IN_RANGE = "[ERROR] 잘못된 순서입니다.";
     private static final String ERROR_STATION_ALREADY_ON_LINE = "[ERROR] 이미 존재하는 역입니다.";
     private static final String ERROR_STATION_NOT_ON_LINE = "[ERROR] 노선에 해당 역이 없습니다.";
+    private static final String ERROR_STATION_NOT_ON_STATION_REPOSITORY = "[ERROR] 데이터베이스에 등록되지 않은 역입니다.";
     private String name; // 변경 불가능
-    private LinkedList<Station> stationsOnLine; // 해당 노선에 소속된 역(들)
-    private Station upEndStation;
-    private Station downEndStation;
+    private LinkedList<Station> stationsOnLine = new LinkedList<>(); // 해당 노선에 소속된 역(들)
 
     public Line(String name) { // 변경 불가능
         this.name = name;
@@ -20,20 +20,28 @@ class Line {
     }
 
     public void initializeEndStations(Station upEndStation, Station downEndStation) {
-        this.upEndStation = upEndStation;
-        this.downEndStation = downEndStation;
         stationsOnLine.add(upEndStation);
         stationsOnLine.add(downEndStation);
     }
 
-    public void addStation(Station station, int index) {
+    public void addStation(String stationName, int index) {
+        Station station = getStation(stationName);
         if(isStationInLine(station)) {
             throw new IllegalArgumentException(ERROR_STATION_ALREADY_ON_LINE);
         }
-        if(isIndexInRange(index)) {
+        if(!isIndexInRange(index - 1)) { // 순서는 1부터 입력 가능하므로
             throw new IllegalArgumentException(ERROR_INDEX_NOT_IN_RANGE);
         }
-        stationsOnLine.add(index, station);
+        stationsOnLine.add(index - 1, station);
+    }
+
+    private Station getStation(String stationName) {
+        Stream<Station> stationStream = StationRepository.stations().stream();
+        try {
+            return stationStream.filter(station -> station.getName().equals(stationName)).findFirst().get();
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ERROR_STATION_NOT_ON_STATION_REPOSITORY);
+        }
     }
 
     public void deleteStation(Station station) {
