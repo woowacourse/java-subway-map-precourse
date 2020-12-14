8726e5ebd40a4abc510ed4c75567aa73ec8d6d69
taelee <taelee@student.42seoul.kr>
2020-12-14 23:18:26 +0900

    feat(StationManager): add an error handler banning station name below length 2 when station adding

--- src/main/java/subway/StationManager.java
+++ src/main/java/subway/StationManager.java
@@ -3,7 +3,6 @@ package subway;
 import subway.domain.Station;
 import subway.domain.StationRepository;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class StationManager {
@@ -32,28 +31,38 @@ public class StationManager {
     private void addStation() {
         System.out.println("\n## 등록할 역 이름을 입력하세요");
         String station = InputView.askName(scanner);
-        if (isDuplicate(station)) {
-            System.out.println("\n[ERROR] 이미 등록된 역입니다.");
-            run();
+        try {
+            validateOverTwoWords(station);
+            validateNotExisting(station);
+            StationRepository.addStation(new Station(station));
+            System.out.println("[INFO] 지하철 역이 등록되었습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("\n" + e + "\n");
         }
-        StationRepository.addStation(new Station(station));
-        System.out.println("[INFO] 지하철 역이 등록되었습니다.");
+    }
+
+    private void validateNotExisting(String station) {
+        if (StationRepository.hasStation(station)) {
+            throw new IllegalArgumentException("[ERROR] 이미 등록된 역입니다.");
+        }
+    }
+
+    private void validateOverTwoWords(String station) {
+        if (station.length() < 2) {
+            throw new IllegalArgumentException("[ERROR] 역 이름을 2글자 이상이어야합니다.");
+        }
     }
 
     //TODO 노선에 등록된 역은 삭제 안되게 하기
     private void deleteStation() {
         System.out.println("\n## 삭제할 역 이름을 입력하세요");
         String station = InputView.askName(scanner);
-        if (!isDuplicate(station)) {
+        if (!StationRepository.hasStation(station)) {
             System.out.println("\n[ERROR] 존재하지 않는 역입니다.");
             run();
         }
         StationRepository.deleteStation(station);
         System.out.println("[INFO] 지하철 역이 삭제되었습니다.");
-    }
-
-    private boolean isDuplicate(String station) {
-        return StationRepository.hasStation(station);
     }
 
 
