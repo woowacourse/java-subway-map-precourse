ae08179ec3ffaea5bd2b69f37bf41dc6826f4f6a
Jaeyoung Shim <sjy4566@naver.com>
2020-12-14 22:19:25 +0900

    feat(StationManageController): remove station by name

--- src/main/java/controller/StationManageController.java
+++ src/main/java/controller/StationManageController.java
@@ -15,6 +15,8 @@ public class StationManageController {
     public static final String ERROR_INVALID_INPUT = "유효하지 않은 입력입니다. \n";
     public static final String ERROR_DUPLICATE_STATION_NAME = "이미 등록된 역이 존재합니다. \n";
     public static final String ERROR_STATION_NAME_LESS_THAN_TWO = "역이름은 2글자 이상이여야 합니다. \n";
+    public static final String ERROR_STATION_NAME_NOT_EXIST = "해당 역이 존재하지 않습니다. \n";
+    public static final String COMPLETE_REMOVE_STATION = "지하철 역이 삭제되었습니다.";
     public static final String MESSAGE_ADD_COMPLETE = "지하철 역이 등록되었습니다.";
     public static final String INPUT_MESSAGE_FOR_STATION_NAME = "## 등록할 역 이름을 입력하세요.";
     public static final String INPUT_MESSAGE_FOR_STATION_NAME_TO_DELETE = "## 삭제할 역 이름을 입력하세요.";
@@ -57,7 +59,19 @@ public class StationManageController {
     }
 
     private void removeStation() {
+        view.printMessage(INPUT_MESSAGE_FOR_STATION_NAME_TO_DELETE);
+        String input = view.input();
 
+        if (!StationRepository.isExistStation(input)) {
+            view.printMessage(ERROR_STATION_NAME_NOT_EXIST);
+            return;
+        }
+
+        if (StationRepository.deleteStation(input)) {
+            view.printMessage(COMPLETE_REMOVE_STATION);
+        }
+
+        StationRepository.deleteStation(input);
     }
 
     private void searchStation() {
