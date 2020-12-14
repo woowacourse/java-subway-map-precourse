044df3e106213e91adfe937f91491640c04d92df
Wannte <T.wannte@gmail.com>
2020-12-14 14:36:54 +0900

    refactor(SectionService): 입력, 유효성 검사 메소드를 분리. 등록 시 동일한 역이 이미 포함되었는 지 확인 로직 추가

--- src/main/java/subway/service/SectionService.java
+++ src/main/java/subway/service/SectionService.java
@@ -19,36 +19,63 @@ public class SectionService extends BaseService {
     private static final String REGISTER_SECTION_SUCCESS = "구간이 등록되었습니다.";
     private static final String DELETE_SECTION_LINE_NAME_QUESTION = "삭제할 구간의 노선을 입력하세요.";
     private static final String DELETE_SECTION_STATION_NAME_QUESTION = "삭제할 구간의 역을 입력하세요.";
+    private static final String DELETE_SECTION_SUCCESS = "구간이 삭제되었습니다.";
+
+    private static final String ERR_DUPLICATE_STATION_IN_LINE = "동일 노선에 동일한 이름의 역이 이미 있습니다.";
     private static final String ERR_MIN_LINE_LENGTH = "노선의 길이가 짧습니다.";
     private static final String ERR_NO_STATION_ON_LINE = "노선에 입력한 역이 없습니다.";
-    private static final String DELETE_SECTION_SUCCESS = "구간이 삭제되었습니다.";
 
     public static void view() {
         getUserChoiceWithinOptionList(Arrays.asList(SectionOption.values()), HEADER);
     }
 
     public static void register() {
-        OutputView.printQuestion(REGISTER_SECTION_LINE_NAME_QUESTION);
-        Line line = LineRepository.getLine(InputView.getLineName());
-        OutputView.printQuestion(REGISTER_SECTION_STATION_NAME_QUESTION);
-        Station station = StationRepository.getStation(InputView.getStationName());
-        OutputView.printQuestion(REGISTER_SECTION_ORDER_NUMBER_QUESTION);
-        int order = InputView.getOrder();
-        line.add(order - 1, station);
+        Line line = getLineByQuestion(REGISTER_SECTION_LINE_NAME_QUESTION);
+        Station station = getStationByQuestion(REGISTER_SECTION_STATION_NAME_QUESTION);
+        checkDuplicateStationInLine(line, station);
+        int index = getStationIndexToAdd();
+        line.add(index, station);
         OutputView.printInfo(REGISTER_SECTION_SUCCESS);
     }
 
     public static void delete() {
-        OutputView.printQuestion(DELETE_SECTION_LINE_NAME_QUESTION);
-        Line line = LineRepository.getLine(InputView.getLineName());
-        OutputView.printQuestion(DELETE_SECTION_STATION_NAME_QUESTION);
-        Station station = StationRepository.getStation(InputView.getStationName());
+        Line line = getLineByQuestion(DELETE_SECTION_LINE_NAME_QUESTION);
+        Station station = getStationByQuestion(DELETE_SECTION_STATION_NAME_QUESTION);
+        checkLineLengthPossibleToDelete(line);
+        removeStationInLine(line, station);
+        OutputView.printInfo(DELETE_SECTION_SUCCESS);
+    }
+
+    private static Line getLineByQuestion(String question) {
+        OutputView.printQuestion(question);
+        return LineRepository.getLine(InputView.getLineName());
+    }
+
+    private static Station getStationByQuestion(String question) {
+        OutputView.printQuestion(question);
+        return StationRepository.getStation(InputView.getStationName());
+    }
+
+    private static int getStationIndexToAdd() {
+        OutputView.printQuestion(REGISTER_SECTION_ORDER_NUMBER_QUESTION);
+        return InputView.getOrder() - 1;
+    }
+
+    private static void checkDuplicateStationInLine(Line line, Station station) {
+        if (line.hasStation(station)) {
+            throw new IllegalArgumentException(ERR_DUPLICATE_STATION_IN_LINE);
+        }
+    }
+
+    private static void checkLineLengthPossibleToDelete(Line line) {
         if (line.getLength() <= MIN_LINE_LENGTH) {
             throw new IllegalArgumentException(ERR_MIN_LINE_LENGTH);
         }
+    }
+
+    private static void removeStationInLine(Line line, Station station) {
         if (!line.remove(station)) {
             throw new IllegalArgumentException(ERR_NO_STATION_ON_LINE);
         }
-        OutputView.printInfo(DELETE_SECTION_SUCCESS);
     }
 }
