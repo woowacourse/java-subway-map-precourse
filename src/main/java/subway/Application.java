5a81202187d4f327a6ef74b2f761bc14880e80b6
DWL <DWL>
2020-12-14 13:41:34 +0900

    feat: 초기설정 기능 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,12 +1,44 @@
 package subway;
 
 import java.util.Scanner;
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Sections;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 import subway.view.MainView;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        init();
         MainView mainView = new MainView(scanner);
         mainView.startView();
+    }
+
+    private static void init() {
+        StationRepository.addStation(new Station("교대역"));
+        StationRepository.addStation(new Station("강남역"));
+        StationRepository.addStation(new Station("역삼역"));
+        StationRepository.addStation(new Station("남부터미널역"));
+        StationRepository.addStation(new Station("양재역"));
+        StationRepository.addStation(new Station("양재시민의숲역"));
+        StationRepository.addStation(new Station("매봉역"));
+
+        LineRepository.addLine(new Line("2호선", "교대역", "역삼역"));
+        Line line = LineRepository.getLineByName("2호선");
+        Sections sections = line.getSections();
+        sections.addSection("강남역", 1);
+
+        LineRepository.addLine(new Line("3호선", "교대역", "매봉역"));
+        line = LineRepository.getLineByName("3호선");
+        sections = line.getSections();
+        sections.addSection("남부터미널역", 1);
+        sections.addSection("양재역", 2);
+
+        LineRepository.addLine(new Line("신분당선", "강남역", "양재시민의숲역"));
+        line = LineRepository.getLineByName("신분당선");
+        sections = line.getSections();
+        sections.addSection("양재역", 1);
     }
 }
