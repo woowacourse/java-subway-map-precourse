2d2d674920d1b4efadd45007eb2357773900ec6a
bepoz <kangsy763@naver.com>
2020-12-14 23:18:20 +0900

    feat: 테스트케이스 생성 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,15 +1,19 @@
 package subway;
 
+import subway.common.TestCase;
 import subway.controller.MainController;
 import subway.view.InputView;
 
 import java.util.Scanner;
+
+import static subway.common.TestCase.*;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
         InputView inputView = new InputView(scanner);
+        testCaseCreate(inputView);
         new MainController().service(inputView);
     }
 }
---
+++ src/main/java/subway/common/TestCase.java
@@ -0,0 +1,42 @@
+package subway.common;
+
+import subway.domain.Line;
+import subway.domain.Station;
+import subway.repository.LineRepository;
+import subway.view.InputView;
+
+import static subway.repository.LineRepository.*;
+import static subway.repository.StationRepository.addStation;
+import static subway.repository.StationRepository.stations;
+
+
+public class TestCase {
+    public static void testCaseCreate(InputView inputView) {
+        Station kyodae = new Station("교대역");
+        Station kangnam = new Station("강남역");
+        Station yeoksam = new Station("역삼역");
+        Station terminal = new Station("남부터미널역");
+        Station yangjae = new Station("양재역");
+        Station forest = new Station("양재시민의숲역");
+        Station maebong = new Station("매봉역");
+        addStation(kyodae);
+        addStation(kangnam);
+        addStation(yeoksam);
+        addStation(terminal);
+        addStation(yangjae);
+        addStation(forest);
+        addStation(maebong);
+
+        Line line2 = new Line("2호선", kyodae, yeoksam);
+        Line line3 = new Line("3호선", kyodae, maebong);
+        Line lineSinbundang = new Line("신분당선", kangnam, forest);
+        addLine(line2);
+        addLine(line3);
+        addLine(lineSinbundang);
+
+        addSection(line2, kangnam, 1);
+        addSection(line3, yangjae, 1);
+        addSection(line3, terminal, 1);
+        addSection(lineSinbundang, yangjae, 1);
+    }
+}
