056f617303e7c57e5e65d9e394b1198e2c4e1009
operasystem6 <operasystem6@naver.com>
2020-12-14 19:34:12 +0900

    refactor: 번호에 따른 이동 클래스 변경

--- src/main/java/subway/view/main/MainMenu.java
+++ src/main/java/subway/view/main/MainMenu.java
@@ -1,9 +1,9 @@
 package subway.view.main;
 
 import subway.repository.InitialRepository;
-import subway.repository.LineRepository;
+import subway.view.OutputView;
 import subway.view.line.LineScreen;
-import subway.view.section.SectionView;
+import subway.view.section.SectionScreen;
 import subway.view.station.StationScreen;
 
 import java.util.Arrays;
@@ -12,8 +12,8 @@ import java.util.function.Consumer;
 public enum MainMenu {
     STATION("1", "역 관리", (key) -> StationScreen.selectMenu()),
     LINE("2", "노선 관리", (key) -> LineScreen.selectMenu()),
-    SECTION("3", "구간 관리", (key) -> SectionView.selectMenu()),
-    PRINT("4", "지하철 노선도 출력", (key) -> LineRepository.printAllLine()),
+    SECTION("3", "구간 관리", (key) -> SectionScreen.selectMenu()),
+    PRINT("4", "지하철 노선도 출력", (key) -> OutputView.printSubwayLineMap()),
     QUIT("Q", "종료", (key) -> InitialRepository.end());
 
     private String key;
