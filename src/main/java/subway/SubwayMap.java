b72b111c2062fa34a26cf996258d4e0dd578b35c
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-15 03:26:14 +0900

    feat: 지하철 역 초기값 등록 기능 구현

--- src/main/java/subway/SubwayMap.java
+++ src/main/java/subway/SubwayMap.java
@@ -1,8 +1,11 @@
 package subway;
 
+import subway.domain.Station;
+import subway.domain.StationRepository;
 import subway.manager.MainManager;
+import subway.utils.InitStation;
 import subway.view.Input;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SubwayMap {
@@ -14,5 +17,11 @@ public class SubwayMap {
 
     private void init(Scanner scanner) {
         Input.init(scanner);
+        initStation();
+    }
+
+    private void initStation() {
+        Arrays.stream(InitStation.values())
+                .forEach(station -> StationRepository.addStation(new Station(station.name())));
     }
 }
---
+++ src/main/java/subway/utils/InitStation.java
@@ -0,0 +1,5 @@
+package subway.utils;
+
+public enum InitStation {
+    교대역, 강남역, 역삼역, 남부터미널역, 양재역, 양재시민의숲역, 매봉역
+}
