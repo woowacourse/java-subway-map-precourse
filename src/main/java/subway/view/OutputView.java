39e479f2f2965d7678279b5c8fb0c186429241a7
baekchan1024 <dlqhs1024@naver.com>
2020-12-15 07:35:28 +0900

    refactor (OutputView) : modify variable name

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -26,14 +26,14 @@ public class OutputView {
 	private static final String INFOMATION = "[INFO]";
 	private static final String DIVIDE_INFORMATION = "---";
 	private static final String SUBWAY_MAP = "## 지하철 노선도";
-
 	public static final String SELETE_FUNCTION = "## 원하는 기능을 선택하세요.";
+	
 	public static final String REGISTER_STATION = "## 등록할 역 이름을 입력하세요.";
 	public static final String DELETE_STATION = "## 삭제할 역 이름을 입력하세요.";
 	
 	public static final String REGISTER_LINE = "## 등록할 노선 이름을 입력하세요."; 
-	public static final String REGISTER_ASCENDING_END_POINT = "## 등록할 노선의 상행 종점역 이름을 입력하세요.";
-	public static final String REGISTER_DESCENDING_END_POINT = "## 등록할 노선의 하행 종점역 이름을 입력하세요.";
+	public static final String ASCENDING_END_POINT = "## 등록할 노선의 상행 종점역 이름을 입력하세요.";
+	public static final String DESCENDING_END_POINT = "## 등록할 노선의 하행 종점역 이름을 입력하세요.";
 	public static final String DELETE_LINE = "## 삭제할 노선 이름을 입력하세요.";
 	
 	public static final String SELETE_LINE = "## 노선을 입력하세요.";
@@ -48,6 +48,30 @@ public class OutputView {
 		System.out.println(MAIN_LINE_MESSAGE);
 		System.out.println(MAIN_SECTION_MESSAGE);
 		System.out.println(MAIN_MAP_PRINT_MESSAGE);
+		System.out.println(SYSTEM_OUT);
+	}
+	
+	public static void printStationManagementMenu() {
+		System.out.println(STATION_MANAGE_MESSAGE);
+		System.out.println(STATION_REGISTER_MESSAGE);
+		System.out.println(STATION_DELETE_MESSAGE);
+		System.out.println(STATION_PRINT_MESSAGE);
+		System.out.println(BACK_TO_MAIN);
+	}
+	
+	public static void printLineManagementMenu() {
+		System.out.println(SECTION_MANAGE_MESSAGE);
+		System.out.println(LINE_REGISTER_MESSAGE);
+		System.out.println(LINE_DELETE_MESSAGE);
+		System.out.println(LINE_PRINT_MESSAGE);
+		System.out.println(BACK_TO_MAIN);
+	}
+	
+	public static void printSectionManagementMenu() {
+		System.out.println(SELETE_LINE);
+		System.out.println(SECTION_REGISTER_MESSAGE);
+		System.out.println(SECTION_DELETE_MESSAGE);
+		System.out.println(BACK_TO_MAIN);
 	}
 	
 }
