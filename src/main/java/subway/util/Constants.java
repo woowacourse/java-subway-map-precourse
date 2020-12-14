4ca3481ff47682a4e4daf741d7694d6f3039042c
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-14 17:17:30 +0900

    feat(Constants) : 필요한 String 객체 추가 작성

--- src/main/java/subway/util/Constants.java
+++ src/main/java/subway/util/Constants.java
@@ -2,7 +2,7 @@ package subway.util;
 
 public class Constants {
     public static final String ASK_COMMAND = "## 원하는 기능을 선택하세요.";
-    public static final String INFO = "[INFO]";
+    public static final String COMMAND_FAIL = "[ERROR] 선택할 수 없는 기능입니다.\n";
     // 메인 화면
     public static final String MAIN = "## 메인 화면";
     public static final String MAIN_STATION = "1. 역 관리";
@@ -16,21 +16,29 @@ public class Constants {
     public static final String STATION_ADD = "1. 역 등록";
     public static final String STATION_REMOVE = "2. 역 삭제";
     public static final String STATION_VISIT = "3. 역 조회";
+    public static final String STATION_LIST = "## 역 목록";
     public static final String ASK_STATION_ADD = "## 등록할 역 이름을 입력하세요.";
     public static final String ASK_STATION_REMOVE = "## 삭제할 역 이름을 입력하세요.";
-    public static final String STATION_ADD_COMPLETE = "지하철 역이 등록되었습니다.";
-    public static final String STATION_REMOVE_COMPLETE = "지하철 노선이 삭제되었습니다.";
+    public static final String STATION_ADD_COMPLETE = "[INFO] 지하철 역이 등록되었습니다.\n";
+    public static final String STATION_REMOVE_COMPLETE = "[INFO] 지하철 노선이 삭제되었습니다.\n";
+    public static final String STATION_REMOVE_FAIL = "[ERROR] 해당 역은 삭제할 수 없습니다.\n";
+    public static final String STATION_NOT_EXIST = "[ERROR] 해당 역은 존재하지 않습니다.\n";
+    public static final String STATION_EXIST = "[ERROR] 이미 등록된 역이름입니다.\n";
     // 노선 화면
     public static final String LINE_MAIN = "## 노선 관리 화면";
     public static final String LINE_ADD = "1. 노선 등록";
     public static final String LINE_REMOVE = "2. 노선 삭제";
     public static final String LINE_VISIT = "3. 노선 조회";
+    public static final String LINE_LIST = "## 노선 목록";
     public static final String ASK_LINE_ADD = "## 등록할 노선 이름을 입력하세요.";
     public static final String ASK_LINE_REMOVE = "## 삭제할 노선 이름을 입력하세요..";
     public static final String ASK_UPPER_END = "## 등록할 노선의 상행 종점역 이름을 입력하세요.";
     public static final String ASK_LOWER_END = "## 등록할 노선의 하행 종점역 이름을 입력하세요.";
-    public static final String LINE_ADD_COMPLETE = "지하철 노선이 등록되었습니다.";
-    public static final String LINE_REMOVE_COMPLETE = "지하철 노선이 삭제되었습니다.";
+    public static final String LINE_ADD_COMPLETE = "[INFO] 지하철 노선이 등록되었습니다.\n";
+    public static final String LINE_REMOVE_COMPLETE = "[INFO] 지하철 노선이 삭제되었습니다.\n";
+    public static final String LINE_REMOVE_FAIL = "[ERROR] 해당 노선은 삭제할 수 없습니다.\n";
+    public static final String LINE_EXIST = "[ERROR] 이미 등록된 노선이름입니다.\n";
+    public static final String LINE_NOT_EXIST = "[ERROR] 해당 노선은 존재하지 않습니다.\n";
     // 구간 화면
     public static final String SECTION_MAIN = "## 구간 관리 화면";
     public static final String SECTION_ADD = "1. 구간 등록";
@@ -40,8 +48,8 @@ public class Constants {
     public static final String ASK_SECTION_ADD_ORDER = "## 순서를 입력하세요.";
     public static final String ASK_SECTION_REMOVE_LINE = "## 삭제할 구간의 노선을 입력하세요.";
     public static final String ASK_SECTION_REMOVE_STATION = "## 삭제할 구간의 역을 입력하세요.";
-    public static final String SECTION_ADD_COMPLETE = "구간이 등록되었습니다.";
-    public static final String SECTION_REMOVE_COMPLETE = "구간이 삭제되었습니다.";
+    public static final String SECTION_ADD_COMPLETE = "[INFO] 구간이 등록되었습니다.\n";
+    public static final String SECTION_REMOVE_COMPLETE = "[INFO] 구간이 삭제되었습니다.\n";
 
     public static void printMain() {
         System.out.println(MAIN);
@@ -68,6 +76,7 @@ public class Constants {
         System.out.println(LINE_MAIN);
         System.out.println(LINE_ADD);
         System.out.println(LINE_REMOVE);
+        System.out.println(LINE_VISIT);
         System.out.println(GO_BACK);
         System.out.print("\n");
         System.out.println(Constants.ASK_COMMAND);
