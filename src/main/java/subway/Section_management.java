3ae718c93f9458b82c4f7c1121a7cd57763575dd
bbakjae <qkrwogus0724@naver.com>
2020-12-14 23:11:38 +0900

    feat(Section_management.java): 예외 처리 구현

--- src/main/java/subway/Section_management.java
+++ src/main/java/subway/Section_management.java
@@ -1,17 +1,30 @@
 package subway;
 
 import subway.domain.Line;
+import subway.domain.Station;
 
 public class Section_management {
-	
+
 	public static void add_section(String line_name, String Station_name, int position) {
 		Line line = Line_management.get_line_by_name(line_name);
-		line.getStations().add(position-1, Station_management.get_station_by_name(Station_name));
+		Station station = Station_management.get_station_by_name(Station_name);
+		if (station != null) {
+			line.getStations().add(position - 1, Station_management.get_station_by_name(Station_name));
+			System.out.println("[INFO] 구간이 등록되었습니다.");
+			return;
+		}
+		System.out.println("[ERROR] 존재하지 않는 지하철 역입니다.");
 	}
-	
+
 	public static void delete_section(String line_name, String Station_name) {
 		Line line = Line_management.get_line_by_name(Station_name);
-		line.getStations().remove(Station_management.get_station_by_name(Station_name));
+		Station station = Station_management.get_station_by_name(Station_name);
+		if (station != null) {			
+			line.getStations().remove(Station_management.get_station_by_name(Station_name));
+			System.out.println("[INFO] 구간이 삭제되었습니다.");
+			return;
+		}
+		System.out.println("[ERROR] 존재하지 않는 지하철역입니다.");
 	}
-	
+
 }
