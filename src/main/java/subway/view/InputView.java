3a2f9214423c57627c141ff1b54d24403f480a47
Wannte <T.wannte@gmail.com>
2020-12-14 20:16:23 +0900

    feat(inputView): 역과 노선에 이름에 제약사항을 추가
    한글 또는 숫자로만 구성이 될 수 있고, 공백은 중간 부분에 한번만 허용한다.

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -7,9 +7,10 @@ public class InputView {
     private static final String ERR_CONTAIN_SPACE = "입력값이 공백을 포함하면 안됩니다.";
     private static final String ERR_NOT_INTEGER = "입력값은 정수값이여야 합니다.";
     private static final String ERR_NOT_POSITIVE = "입력값은 양수여야 합니다";
+    private static final String ERR_NOT_SUBWAY_LINE_NAME_PATTERN = "잘못된 형식의 이름입니다.";
     private static final int MIN_STATION_LENGTH = 2;
     private static final int MIN_LINE_LENGTH = 2;
-    private static final String SPACE = " ";
+    final private static String SUBWAY_LINE_NAME_PATTERN = "^[가-힣0-9]+[\\s]?[가-힣0-9]+$";
     private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() {
@@ -38,14 +39,14 @@ public class InputView {
     public static String getStationName() {
         String stationName = scanner.nextLine().strip();
         validateStringLength(stationName, MIN_STATION_LENGTH);
-        validateNotContainSpace(stationName);
+        validateSubwayNamePattern(stationName);
         return stationName;
     }
 
     public static String getLineName() {
         String lineName = scanner.nextLine().strip();
         validateStringLength(lineName, MIN_LINE_LENGTH);
-        validateNotContainSpace(lineName);
+        validateSubwayNamePattern(lineName);
         return lineName;
     }
 
@@ -55,9 +56,9 @@ public class InputView {
         }
     }
 
-    private static void validateNotContainSpace(String string) {
-        if (string.contains(SPACE)) {
-            throw new IllegalArgumentException(ERR_CONTAIN_SPACE);
+    public static void validateSubwayNamePattern(String string) {
+        if (!string.matches(SUBWAY_LINE_NAME_PATTERN)) {
+            throw new IllegalArgumentException(ERR_NOT_SUBWAY_LINE_NAME_PATTERN);
         }
     }
 }
