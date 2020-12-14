a4ebb0c3315c368db3ac962a6b6d0704242e299d
Eugine Park <pkeugine@gmail.com>
2020-12-15 02:38:00 +0900

    style(InfoMessage): separate line

--- src/main/java/subway/view/InfoMessage.java
+++ src/main/java/subway/view/InfoMessage.java
@@ -3,6 +3,7 @@ package subway.view;
 public class InfoMessage {
 
     public static final String LABEL = "[INFO] ";
+
     public static final String STATION_ADDED = LABEL + "지하철 역이 등록되었습니다.";
     public static final String STATION_DELETED = LABEL + "지하철 역이 삭제되었습니다.";
     public static final String LINE_ADDED = LABEL + "지하철 노선이 등록되었습니다.";
