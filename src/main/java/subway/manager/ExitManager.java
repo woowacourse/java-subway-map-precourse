14bdfa3c9cb8b3957dae5d6703f3201653557411
김경록 <goodboy3421@gmail.com>
2020-12-14 18:02:15 +0900

    refactor: System exit 함수로 프로그램을 종료하는 구조 개선
    System.exit(0); 으로 프로그램을 종료하지 않아도, 자연스럽게 프로그램을 종료할 수 있어, 제거

--- src/main/java/subway/manager/ExitManager.java
+++ src/main/java/subway/manager/ExitManager.java
@@ -5,11 +5,9 @@ import subway.view.OutputView;
 import java.util.Scanner;
 
 public class ExitManager implements Manager{
-    private static final int EXIT = 0;
 
     @Override
     public void execute(Scanner scanner) {
         OutputView.printExit();
-        System.exit(EXIT);
     }
 }
