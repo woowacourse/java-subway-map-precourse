748d75bbc699f9ecea0ca71d04a9a07417cd663a
NewWisdom <wlgp2500@gmail.com>
2020-12-15 02:10:51 +0900

    refactor(resource): 에러 메세지와 출력 메세지를 enum 클래스로 분리

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,23 +1,17 @@
 package subway.view;
 
+import subway.view.resource.ErrorCode;
+import subway.view.resource.Message;
+
 import java.util.List;
 import java.util.Map;
 
 public class OutputView {
     private static final String HEADER_SHARP = "## ";
     private static final String HEADER_INFO = "[INFO] ";
+    private static final String ERROR_HEADER = "[ERROR] ";
     private static final String PARTITION = "---";
     private static final String LIST_TITLE = " 목록";
-    private static final String INPUT_FUNCTION_INDEX_MESSAGE = "원하는 기능을 선택하세요.";
-    private static final String INPUT_REGISTER_VALUE_MESSAGE = "등록할 %s 이름을 입력하세요.";
-    private static final String REGISTER_SUCCESS_MESSAGE = "지하철 %s이 등록되었습니다.";
-    private static final String INPUT_DELETE_VALUE_MESSAGE = "삭제할 %s 이름을 입력하세요.";
-    private static final String DELETE_SUCCESS_MESSAGE = "지하철 %s이 삭제되었습니다.";
-    private static final String INPUT_FIRST_STATION_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
-    private static final String INPUT_LAST_STATION_MESSAGE = "등록할 노선의 하행 종점역 이름을 입력하세요.";
-    private static final String INPUT_VALUE_MESSAGE = "%s 이름을 입력하세요.";
-    private static final String INPUT_INDEX_MESSAGE = "순서를 입력하세요.";
-    private static final String INPUT_VALUE_OF_DELETE_SECTION = "삭제할 구간의 %s을 입력하세요.";
 
     public static void printTitle(String functionHeader) {
         System.out.println();
@@ -33,56 +27,6 @@ public class OutputView {
         for (String function : functionList) {
             System.out.println(function);
         }
-        System.out.println();
-    }
-
-    public static void printInputFunctionIndex() {
-        System.out.println(HEADER_SHARP + INPUT_FUNCTION_INDEX_MESSAGE);
-    }
-
-    public static void printInputRegisterValue(String screenName) {
-        System.out.println();
-        System.out.printf(HEADER_SHARP + INPUT_REGISTER_VALUE_MESSAGE + "\n", screenName);
-    }
-
-    public static void printRegisterFirstStation() {
-        System.out.println();
-        System.out.println(HEADER_SHARP + INPUT_FIRST_STATION_MESSAGE);
-    }
-
-    public static void printRegisterLastStation() {
-        System.out.println();
-        System.out.println(HEADER_SHARP + INPUT_LAST_STATION_MESSAGE);
-    }
-
-    public static void printRegisterSuccess(String screenName) {
-        System.out.println();
-        System.out.printf(HEADER_INFO + REGISTER_SUCCESS_MESSAGE + "\n", screenName);
-    }
-
-    public static void printInputDeleteValue(String screenNam) {
-        System.out.println();
-        System.out.printf(HEADER_SHARP + INPUT_DELETE_VALUE_MESSAGE + "\n", screenNam);
-    }
-
-    public static void printDeleteSuccess(String screenNam) {
-        System.out.println();
-        System.out.printf(HEADER_INFO + DELETE_SUCCESS_MESSAGE + "\n", screenNam);
-    }
-
-    public static void printInputValue(String screenName) {
-        System.out.println();
-        System.out.printf(HEADER_SHARP + INPUT_VALUE_MESSAGE + "\n", screenName);
-    }
-
-    public static void printInputIndex() {
-        System.out.println();
-        System.out.println(HEADER_SHARP + INPUT_INDEX_MESSAGE);
-    }
-
-    public static void printInputValueOfDeleteSection(String screenName) {
-        System.out.println();
-        System.out.printf(HEADER_SHARP + INPUT_VALUE_OF_DELETE_SECTION + "\n", screenName);
     }
 
     public static void printSubwayMap(Map<String, List<String>> subwayMap) {
@@ -94,6 +38,24 @@ public class OutputView {
             }
             System.out.println();
         }
+    }
+
+    public static void printMessage(Message message){
+        printEnter();
+        System.out.println(HEADER_SHARP + message);
+    }
+
+    public static void printResultMessage(Message message){
+        printEnter();
+        System.out.println(HEADER_INFO + message);
+    }
+    public static void printErrorMessage(ErrorCode message) {
+        printEnter();
+        System.out.println(ERROR_HEADER + message);
+    }
+
+    public static void printEnter(){
+        System.out.println();
     }
 
 }
---
+++ src/main/java/subway/view/resource/Message.java
@@ -0,0 +1,32 @@
+package subway.view.resource;
+
+public enum Message {
+    INPUT_FUNCTION_INDEX("원하는 기능을 선택하세요."),
+    INPUT_REGISTER_STATION_NAME("등록할 역 이름을 입력하세요."),
+    INPUT_REGISTER_LINE_NAME("등록할 노선 이름을 입력하세요."),
+    REGISTER_STATION_SUCCESS("지하철 역이 등록되었습니다."),
+    INPUT_DELETE_STATION_NAME("삭제할 역 이름을 입력하세요."),
+    INPUT_DELETE_LINE_NAME("삭제할 노선 이름을 입력하세요."),
+    DELETE_STATION_SUCCESS("지하철 역이 삭제되었습니다."),
+    DELETE_LINE_SUCCESS("지하철 노선이 삭제되었습니다."),
+    DELETE_SECTION_SUCCESS("지하철 구간이 삭제되었습니다."),
+    REGISTER_LINE_SUCCESS("지하철 노선이 등록되었습니다."),
+    INPUT_REGISTER_FIRST_STATION("등록할 노선의 상행 종점역 이름을 입력하세요."),
+    INPUT_REGISTER_LAST_STATION("등록할 노선의 하행 종점역 이름을 입력하세요."),
+    REGISTER_SECTION_SUCCESS("구간이 등록되었습니다."),
+    INPUT_LINE("노선을 입력하세요."),
+    INPUT_INDEX("순서를 입력하세요."),
+    INPUT_LINE_OF_DELETE_SECTION("삭제할 구간의 노선을 입력하세요."),
+    INPUT_STATION_OF_DELETE_SECTION("삭제할 구간의 역을 입력하세요.");
+
+    private String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString(){
+        return message;
+    }
+}
