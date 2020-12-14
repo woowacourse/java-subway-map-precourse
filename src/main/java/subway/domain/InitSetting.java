e276314a2662d34c1887b5ac98578bb68836ed72
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-14 23:27:06 +0900

    초기 설정 클래스 추가

---
+++ src/main/java/subway/domain/InitSetting.java
@@ -0,0 +1,33 @@
+package subway.domain;
+
+public class InitSetting {
+
+    private static String[] initStation = {"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
+    private static String[][] initLine = {{"2호선", "교대역", "역삼역"}, {"3호선", "교대역", "매봉역"}, {"신분당선", "강남역", "양재시민의숲역"}};
+    private static String[][] initSection = {{"강남역"}, {"남부터미널역", "양재역"}, {"양재역"}};
+
+    public static void initSetting() {
+        initStation();
+        initLine();
+    }
+
+    private static void initStation() {
+        for (String name : initStation) {
+            StationRepository.addStation(new Station(name));
+        }
+    }
+
+    private static void initLine(){
+        for (int i = 0; i < initLine.length; i++) {
+            String line = initLine[i][0];
+            String firstStation = initLine[i][1];
+            String lastStation = initLine[i][2];
+            String[] sections = initSection[i];
+            LineRepository.addLine(line, firstStation, lastStation);
+            int idx = Constants.START_ORDER;
+            for (String section : sections) {
+                LineRepository.addSection(idx++, initLine[i][0], section);
+            }
+        }
+    }
+}
