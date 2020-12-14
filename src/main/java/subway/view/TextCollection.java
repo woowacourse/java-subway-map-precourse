86c90a3c02f74380ed6c4506efc92be80f78f0c5
operasystem6 <operasystem6@naver.com>
2020-12-14 19:29:21 +0900

    refactor: text 데이터 모음 클래스

--- src/main/java/subway/view/TextCollection.java
+++ src/main/java/subway/view/TextCollection.java
@@ -2,22 +2,48 @@ package subway.view;
 
 public class TextCollection {
     public static final String PREFIX = "##";
+    public static final String SEPARATOR = "---";
     public static final String MAIN_SCREEN_MESSAGE = "메인 화면";
-    public static final String SELECT_FUNCTION_MESSAGE = "원하는 기능을 선택하세요.";
     public static final String STATION_MANAGEMENT_MESSAGE = "역 관리 화면";
-    public static final String ERROR = "잘못된 입력입니다.";
+    public static final String STATION_LIST_MESSAGE = "역 목록";
+    public static final String LINE_LIST_MESSAGE = "노선 목록";
+    public static final String SUBWAY_LINE_MAP_MESSAGE = "지하철 노선도";
+
     public static final String INFO_TAG = "[INFO] ";
     public static final String ERROR_TAG = "[ERROR] ";
 
+    public static final String SELECT_FUNCTION_MESSAGE = "원하는 기능을 선택하세요.";
     public static final String REGISTER_STATION_MESSAGE = "등록할 역 이름을 입력하세요.";
     public static final String DELETE_STATION_MESSAGE = "삭제할 역 이름을 입력하세요.";
+    public static final String REGISTERED_STATION_MESSAGE = "지하철 역이 등록되었습니다.";
+    public static final String DELETED_STATION_MESSAGE = "지하철 역이 삭제되었습니다.";
 
+    public static final String LINE_MANAGEMENT_MESSAGE = "노선 관리 화면";
     public static final String REGISTER_LINE_MESSAGE = "등록할 노선 이름을 입력하세요.";
     public static final String REGISTER_UPWARD_STATION_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
     public static final String REGISTER_DOWN_STATION_MESSAGE = "등록할 노선의 하행 종점역 이름을 입력하세요.";
-    public static final String DELETE_LINE_MESSAGE = "삭제할 노선 이름을 입력하세요";
+    public static final String DELETE_LINE_MESSAGE = "삭제할 노선 이름을 입력하세요.";
+    public static final String DELETED_LINE_MESSAGE = "지하철 노선이 삭제되었습니다.";
+    public static final String REGISTERED_LINE_MESSAGE = "지하철 노선이 등록되었습니다.";
 
-    public static final String INPUT_LINE = "노선을 입력하세요.";
-    public static final String INPUT_STATION = "역이름을 입력하세요.";
-    public static final String INPUT_ORDER = "순서를 입력하세요.";
+    public static final String SECTION_MANAGEMENT_MESSAGE = "구간 관리 화면";
+    public static final String INPUT_LINE_MESSAGE = "노선을 입력하세요.";
+    public static final String INPUT_STATION_MESSAGE = "역이름을 입력하세요.";
+    public static final String INPUT_ORDER_MESSAGE = "순서를 입력하세요.";
+    public static final String REGISTERED_SECTION_MESSAGE = "구간이 등록 되었습니다.";
+    public static final String DELETE_SECTION_LINE_MESSAGE = "삭제할 구간의 노선을 입력하세요.";
+    public static final String DELETE_SECTION_STATION_MESSAGE = "삭제할 구간의 역을 입력하세요.";
+    public static final String DELETED_SECTION_MESSAGE = "구간이 삭제되었습니다.";
+
+    public static final String TWO_OR_MORE_LETTERS_MESSAGE = "이름은 2글자 이상이어야 합니다.";
+    public static final String EXISTING_STATION_MESSAGE = "이미 등록된 역 이름입니다.";
+    public static final String EXISTING_LINE_MESSAGE = "이미 등록된 노선 이름입니다.";
+    public static final String SAME_NAME_UP_DOWN_MESSAGE = "상행 종점역과 하행 종점역의 이름이 같습니다";
+    public static final String NOT_NUMBER_MESSAGE = "숫자가 아닌 값을 입력하였습니다.";
+    public static final String WRONG_MENU_INPUT_MESSAGE = "선택할 수 없는 기능입니다.";
+    public static final String NOT_EXIST_STATION_MESSAGE = "존재하지 않는 역입니다.";
+    public static final String NOT_EXIST_LINE_MESSAGE = "존재하지 않는 노선입니다.";
+    public static final String NOT_EXIST_SECTION_MESSAGE = "존재하지 않는 구간입니다.";
+    public static final String AT_LEAST_STATIONS = "노선에 포함된 역이 두개 이하입니다.";
+    public static final String OUT_OF_LINE = "노선 밖의 범위입니다.";
 }
