ea9a5880dc6dac129f8beac6aa94c9746fc6264d
baekchan1024 <dlqhs1024@naver.com>
2020-12-15 06:40:18 +0900

    feat (OutputView) : main menu output

---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,30 @@
+package subway.view;
+
+public class OutputView {
+	private static final String MAIN_MESSAGE = "## 메인 화면";
+	private static final String MAIN_STATION_MESSAGE = "1. 역 관리";
+	private static final String MAIN_LINE_MESSAGE = "2. 노선 관리";
+	private static final String MAIN_SECTION_MESSAGE = "3. 구간 관리";
+	private static final String MAIN_MAP_PRINT_MESSAGE = "4. 지하철 노선도 출력";
+
+	private static final String STATION_MANAGE_MESSAGE = "## 역 관리 화면";
+	private static final String STATION_REGISTER_MESSAGE = "1. 역 등록";
+	private static final String STATION_DELETE_MESSAGE = "2. 역 삭제";
+	private static final String STATION_PRINT_MESSAGE = "3. 역 조회";
+
+	private static final String LINE_MANAGE_MESSAGE = "## 노선 관리 화면";
+	private static final String LINE_REGISTER_MESSAGE = "1. 노선 등록";
+	private static final String LINE_DELETE_MESSAGE = "2. 노선 삭제";
+	private static final String LINE_PRINT_MESSAGE = "3. 노선 조회";
+
+	private static final String SECTION_MANAGE_MESSAGE = "## 구간 관리 화면";
+	private static final String SECTION_REGISTER_MESSAGE = "1. 구간 등록";
+	private static final String SECTION_DELETE_MESSAGE = "2. 구간 삭제";
+
+	private static final String SYSTEM_OUT = "Q. 종료\n";
+	private static final String BACK_TO_MAIN = "B. 돌아가기\n";
+	private static final String INFOMATION = "[INFO]";
+	private static final String DIVIDE_INFORMATION = "---";
+
+	public static final String SELETE_FUNCTION = "## 원하는 기능을 선택하세요.";
+}
