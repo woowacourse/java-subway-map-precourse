f81c45df20470caf40bc465a14b46cb35327a3c6
Gayoung <gyyoon4u@naver.com>
2020-12-14 14:01:26 +0900

    feat: line이 존재하는지 노선이름으로 확인

--- src/main/java/subway/domain/list/LineRepository.java
+++ src/main/java/subway/domain/list/LineRepository.java
@@ -12,6 +12,7 @@ public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
 
     public static final String LINE_DUPLICATE_ERROR_MESSAGE = "[ERROR] 이미 등록된 노선 이름입니다. 중복되지 않는 역이름을 입력해주세요.";
+    public static final String LINE_CANNOT_FIND_ERROR_MESSAGE = "[ERROR] 존재하지 않는 노선입니다.";
 
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
@@ -23,6 +24,12 @@ public class LineRepository {
         }
         lines.add(lineName);
         return lines;
+    }
+
+    // 구간 입력할 때 존재하는 노선인지 확인
+    public static Line findLineName(String lineName) {
+        return lines.stream().filter(line -> line.getName().equals(lineName))
+                .findAny().orElseThrow(() -> new IllegalArgumentException(LINE_CANNOT_FIND_ERROR_MESSAGE));
     }
 
     public static void addLine(Line line) {
