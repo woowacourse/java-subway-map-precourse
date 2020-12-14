a6a35b4bba403029f856722e6940e0aec5cc4e15
mungto <daum7766@naver.com>
2020-12-14 17:47:52 +0900

    feat(Line): create initLineInStation Function
    Add a function to add an endpoint

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,12 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
+
     private String name;
+    private List<Station> lineInStationList = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -13,5 +18,10 @@ public class Line {
 
     public boolean isEqualName(String name) {
         return name.equals(this.name);
+    }
+
+    public void initLineInStation(Station firstStation, Station lastStation) {
+        lineInStationList.add(firstStation);
+        lineInStationList.add(lastStation);
     }
 }
