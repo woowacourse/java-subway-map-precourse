7de235a2ed8b1ed61011e49b0cf3a4a311bc07ea
TaewanKimmmm <ktww88@gmail.com>
2020-12-15 01:01:24 +0900

    feat: 메인 화면 유저 입력에 대한 예외처리기능 추가

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -4,13 +4,47 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String SELECT_MESSAGE = "## 원하는 기능을 선택하세요.";
+    private static final String CANNOT_SELECT_MESSAGE = "[ERROR] 선택할 수 없는 기능입니다.";
+    private static final char ONE = '1';
+    private static final char FOUR = '4';
+    private static final char QUIT = 'Q';
 
     private InputView() {
     }
 
     public static void getUserSelection(Scanner scanner) {
         System.out.println(SELECT_MESSAGE);
-        String userInput = scanner.next();
+        String userInput = scanner.nextLine();
+
+        try {
+            validateUserInput(userInput);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            getUserSelection(scanner);
+        }
     }
 
+    private static void validateUserInput(String userInput) {
+        validateUserInputLength(userInput);
+        char characterizedUserInput = userInput.charAt(0);
+        if (!isUserInputQuit(characterizedUserInput)) {
+            validateUserInputRange(characterizedUserInput);
+        }
+    }
+
+    private static void validateUserInputLength(String userInput) {
+        if (userInput.length() == 0) {
+            throw new IllegalArgumentException(CANNOT_SELECT_MESSAGE);
+        }
+    }
+
+    private static boolean isUserInputQuit(char userInput) {
+        return Character.toUpperCase(userInput) == QUIT;
+    }
+
+    private static void validateUserInputRange(char userInput) {
+        if ((userInput < ONE || userInput > FOUR)) {
+            throw new IllegalArgumentException(CANNOT_SELECT_MESSAGE);
+        }
+    }
 }
