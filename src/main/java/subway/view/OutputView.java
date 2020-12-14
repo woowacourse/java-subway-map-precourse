95ed2df5e5e9feaedab3b73116cc0d27ab4310ab
doorisopen <audauddl2@gmail.com>
2020-12-14 16:44:45 +0900

    style: 출력 형식 변경
    - 개행 스타일 변경

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -3,8 +3,7 @@ package subway.view;
 public class OutputView {
 
     public static void printMainMenu() {
-        System.out.println();
-        System.out.println("## 메인 화면");
+        System.out.println("\n## 메인 화면");
         System.out.println("1. 역 관리");
         System.out.println("2. 노선 관리");
         System.out.println("3. 구간 관리");
@@ -13,8 +12,7 @@ public class OutputView {
     }
 
     public static void printStationManagementMenu() {
-        System.out.println();
-        System.out.println("## 역 관리 화면");
+        System.out.println("\n## 역 관리 화면");
         System.out.println("1. 역 등록");
         System.out.println("2. 역 삭제");
         System.out.println("3. 역 조회");
@@ -22,8 +20,7 @@ public class OutputView {
     }
 
     public static void printLineStationManagementMenu() {
-        System.out.println();
-        System.out.println("## 노선 관리 화면");
+        System.out.println("\n## 노선 관리 화면");
         System.out.println("1. 노선 등록");
         System.out.println("2. 노선 삭제");
         System.out.println("3. 노선 조회");
@@ -31,40 +28,33 @@ public class OutputView {
     }
 
     public static void printSectionManagementMenu() {
-        System.out.println();
-        System.out.println("## 구간 관리 화면");
+        System.out.println("\n## 구간 관리 화면");
         System.out.println("1. 구간 등록");
         System.out.println("2. 구간 삭제");
         System.out.println("B. 돌아가기");
     }
 
     public static void printAddStationSuccessMessage() {
-        System.out.println();
-        System.out.println("[INFO] 지하철 역이 등록되었습니다.");
+        System.out.println("\n[INFO] 지하철 역이 등록되었습니다.");
     }
 
     public static void printDeleteStationSuccessMessage() {
-        System.out.println();
-        System.out.println("[INFO] 지하철 역이 삭제되었습니다.");
+        System.out.println("\n[INFO] 지하철 역이 삭제되었습니다.");
     }
 
     public static void printAddLineStationSuccessMessage() {
-        System.out.println();
-        System.out.println("[INFO] 지하철 노선이 등록되었습니다.");
+        System.out.println("\n[INFO] 지하철 노선이 등록되었습니다.");
     }
 
     public static void printDeleteLineStationSuccessMessage() {
-        System.out.println();
-        System.out.println("[INFO] 지하철 노선이 삭제되었습니다.");
+        System.out.println("\n[INFO] 지하철 노선이 삭제되었습니다.");
     }
 
     public static void printAddSectionSuccessMessage() {
-        System.out.println();
-        System.out.println("[INFO] 구간이 등록되었습니다.");
+        System.out.println("\n[INFO] 구간이 등록되었습니다.");
     }
 
     public static void printDeleteSectionSuccessMessage() {
-        System.out.println();
-        System.out.println("[INFO] 구간이 삭제되었습니다.");
+        System.out.println("\n[INFO] 구간이 삭제되었습니다.");
     }
 }
