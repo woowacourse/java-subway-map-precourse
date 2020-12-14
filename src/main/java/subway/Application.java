0228511e77eccfcf595dedbd93c8459177489dde
park seok chae <tjrco56@naver.com>
2020-12-14 17:17:11 +0900

    feat(StationNameVaildator): 역 이름 타당성 체크하는 유틸 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -2,9 +2,11 @@ package subway;
 
 import java.util.Scanner;
 
+import subway.view.MainView;
+
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        new MainView(scanner);
     }
 }
