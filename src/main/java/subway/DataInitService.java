f0c0de62975a04cf75e1ed1accbc8c9c4fc69006
mungto <daum7766@naver.com>
2020-12-14 14:41:08 +0900

    feat(DataInitService): create DataInitService class
    create DataInitService and a constant

---
+++ src/main/java/subway/DataInitService.java
@@ -0,0 +1,19 @@
+package subway;
+
+public class DataInitService {
+
+    private static final String GYODAE_STATION = "교대역";
+    private static final String GANGNAM_STATION = "강남역";
+    private static final String YEOKSAM_STATION = "역삼역";
+    private static final String SOUTH_TERMIANL_STATION = "남부터미널역";
+    private static final String YANGJAE_STATION = "양재역";
+    private static final String YANGJAE_CITIZEN_FOREST_STATION = "양재시민의숲역";
+    private static final String MAEBONG_STATION = "매봉역";
+    private static final String LINE_TOW = "2호선";
+    private static final String LINE_THREE = "3호선";
+    private static final String LINE_SINBUNDANG = "신분당선";
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+
+    
+}
