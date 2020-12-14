81cdcef1b0e1bd238abb984b3cec994c99c60864
Jinho <jh8579@gmail.com>
2020-12-14 22:52:55 +0900

    feat(domain/station) : add boolean value and equal method to distinct registered station

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,15 +1,42 @@
 package subway.domain;
 
+import java.util.Objects;
+
 public class Station {
     private String name;
+    private boolean isRegistered;
 
     public Station(String name) {
         this.name = name;
+        this.isRegistered = false;
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    public boolean isRegistered() {
+        return isRegistered;
+    }
+
+    public void setRegistered(boolean registered) {
+        isRegistered = registered;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Station)) {
+            return false;
+        }
+        Station station = (Station) o;
+        return Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
