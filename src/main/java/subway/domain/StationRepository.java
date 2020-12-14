a896be4516d68ec6207d944e2e413e4f43de4aae
mungto <daum7766@naver.com>
2020-12-14 15:57:04 +0900

    feat(StationService): create isEqualStationName Function
    Function to find out if there is a station with the same name in the station

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class StationRepository {
+
     private static final List<Station> stations = new ArrayList<>();
 
     public static List<Station> stations() {
@@ -18,5 +19,14 @@ public class StationRepository {
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    public static boolean isEqualStationName(String name) {
+        for (Station station : stations) {
+            if (station.isEqualName(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
