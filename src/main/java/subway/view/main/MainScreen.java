ca6accafeaa68fdf140a3366f57fc0667e9e23d6
operasystem6 <operasystem6@naver.com>
2020-12-14 16:53:12 +0900

    refactor: printQuestion 메소드 (InputView -> OutputView)

--- src/main/java/subway/view/main/MainScreen.java
+++ src/main/java/subway/view/main/MainScreen.java
@@ -2,6 +2,7 @@ package subway.view.main;
 
 import subway.exception.SubwayException;
 import subway.view.InputView;
+import subway.view.OutputView;
 import subway.view.TextCollection;
 
 import java.util.Arrays;
@@ -9,7 +10,6 @@ import java.util.Arrays;
 public class MainScreen {
 
     public static MainMenu selectMenu() {
-        showMenu();
         String input = InputView.inputFunction();
         if (!MainMenu.isValidInput(input)) {
             throw new SubwayException(TextCollection.WRONG_MENU_INPUT_MESSAGE);
@@ -17,8 +17,8 @@ public class MainScreen {
         return MainMenu.findMenuByKey(input);
     }
 
-    private static void showMenu() {
-        InputView.printQuestion(TextCollection.MAIN_SCREEN_MESSAGE);
+    public static void showMenu() {
+        OutputView.printQuestion(TextCollection.MAIN_SCREEN_MESSAGE);
         Arrays.stream(MainMenu.values()).forEach(menu -> {
             System.out.println(menu.getKey() + ". " + menu.getTitle());
         });
