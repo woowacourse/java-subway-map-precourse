38b4b59634e403e5928739b2941209d9b8f54f40
483759 <483759@naver.com>
2020-12-14 12:08:24 +0900

    refactor : remove redundant code in RouteRepository

--- src/main/java/subway/domain/RouteRepository.java
+++ src/main/java/subway/domain/RouteRepository.java
@@ -6,48 +6,60 @@ import java.util.List;
 import java.util.Objects;
 
 public class RouteRepository {
+
     private static final List<Route> routes = new ArrayList<>();
 
-    public static List<Route> routes(){return Collections.unmodifiableList(routes);}
-
-    public static void addRoute(Route route){routes.add(route);}
-
-    public static void addStationToLine(String lineName, Station station,int index){
-        for(Route route: routes){
-            if(route.matchLineName(lineName))
-                route.insertStation(station,index);
-        }
+    public static List<Route> routes() {
+        return Collections.unmodifiableList(routes);
     }
 
-    public static void deleteRoute(String lineName,String stationName){
-        for(Route route:routes){
-            if(route.matchLineName(lineName)){
-                route.deleteStation(stationName);
-                return;
+    public static void addRoute(Route route) {
+        routes.add(route);
+    }
+
+    public static Route getRouteMatchingName(String lineName) {
+        for (Route route : routes) {
+            if (route.matchLineName(lineName)) {
+                return route;
             }
         }
+        return null;
     }
 
-    public static boolean isLineIncluded(String lineName){
-        for(Route route: routes){
-            if(route.matchLineName(lineName))
-                return true;
+    public static void addStationToLine(String lineName, Station station, int index) {
+        Route route = getRouteMatchingName(lineName);
+        if (route != null) {
+            getRouteMatchingName(lineName).insertStation(station, index);
+        }
+    }
+
+    public static void deleteRoute(String lineName, String stationName) {
+        Route route = getRouteMatchingName(lineName);
+        if (route != null) {
+            getRouteMatchingName(lineName).deleteStation(stationName);
+        }
+    }
+
+    public static boolean isLineIncluded(String lineName) {
+        Route route = getRouteMatchingName(lineName);
+        if (route != null) {
+            return true;
         }
         return false;
     }
 
-    public static boolean doesLineIncludeStation(String lineName, String stationName){
-        for(Route route: routes){
-            if(route.matchLineName(lineName))
-                return route.doesIncludeStation(stationName);
+    public static boolean doesLineIncludeStation(String lineName, String stationName) {
+        Route route = getRouteMatchingName(lineName);
+        if (route != null) {
+            return route.doesIncludeStation(stationName);
         }
         return false;
     }
 
-    public static int numberOfStationsLineHave(String lineName){
-        for(Route route: routes){
-            if(route.matchLineName(lineName))
-                return route.getNumberOfStations();
+    public static int numberOfStationsLineHave(String lineName) {
+        Route route = getRouteMatchingName(lineName);
+        if (route != null) {
+            return route.getNumberOfStations();
         }
         return -1;
     }
