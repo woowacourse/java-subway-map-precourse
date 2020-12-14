a96aa50452aebe7d26028e5b4b665e2522586597
operasystem6 <operasystem6@naver.com>
2020-12-14 16:51:51 +0900

    feat: 노선 입력, 삭제, 조회, 백업 기능 구현 (예외처리 포함)

---
+++ src/main/java/subway/service/LineService.java
@@ -0,0 +1,63 @@
+package subway.service;
+
+import subway.domain.Line;
+import subway.domain.Station;
+import subway.repository.LineRepository;
+import subway.repository.StationRepository;
+import subway.utils.ValidateUtils;
+import subway.view.InputView;
+import subway.view.OutputView;
+import subway.view.TextCollection;
+
+public class LineService {
+    private static LineService lineService;
+
+    public static LineService getInstance() {
+        if (lineService == null) {
+            lineService = new LineService();
+        }
+        return lineService;
+    }
+
+    public boolean insert() {
+        OutputView.printQuestion(TextCollection.REGISTER_LINE_MESSAGE);
+        String lineName = InputView.inputValue();
+        Line line = new Line(lineName);
+        ValidateUtils.isDuplicatedUpDownStation(addUpwardSection(line), addDownSection(line));
+        LineRepository.addLine(line);
+        OutputView.printInformation(TextCollection.REGISTERED_STATION_MESSAGE);
+        return true;
+    }
+
+    public boolean delete() {
+        OutputView.printQuestion(TextCollection.DELETE_LINE_MESSAGE);
+        String lineName = InputView.inputValue();
+        LineRepository.deleteLineByName(lineName);
+        OutputView.printInformation(TextCollection.DELETED_LINE_MESSAGE);
+        return true;
+    }
+
+    public boolean search() {
+        OutputView.printQuestion(TextCollection.LINE_LIST_MESSAGE);
+        LineRepository.printAllLine();
+        return true;
+    }
+
+    public boolean backup() {
+        return true;
+    }
+
+    private static Station addUpwardSection(Line line) {
+        OutputView.printQuestion(TextCollection.REGISTER_UPWARD_STATION_MESSAGE);
+        Station upwardStation = StationRepository.searchStation(InputView.inputValue());
+        line.addSection(upwardStation);
+        return upwardStation;
+    }
+
+    private static Station addDownSection(Line line) {
+        OutputView.printQuestion(TextCollection.REGISTER_DOWN_STATION_MESSAGE);
+        Station downStation = StationRepository.searchStation(InputView.inputValue());
+        line.addSection(downStation);
+        return downStation;
+    }
+}
