e01a83077be509c2479bc2b6d35a5c1ec3d44d5e
doorisopen <audauddl2@gmail.com>
2020-12-15 01:17:03 +0900

    refactor: 애러 이후 프로그램을 종료하지 않는다
    - 에러 문구 출력후 프로그램을 종료하지 않고 입력 요청을한다
    - 공백 제거 로직 수정

--- src/main/java/subway/service/InputService.java
+++ src/main/java/subway/service/InputService.java
@@ -13,89 +13,109 @@ public class InputService extends InputValidation {
 
     public String inputSelectMenu(Scanner scanner, MenuType menuType) {
         inputSelectMenuRequestMessage();
-        String menu = scanner.nextLine();
-        validateMenuRange(menuType.getKeys(), menu);
-        return parseStringToTrimString(menu);
+        String menu = parseStringToTrimString(scanner.nextLine());
+        if (!validateMenuRange(menuType.getKeys(), menu)) {
+            return inputSelectMenu(scanner, menuType);
+        }
+        return menu;
     }
 
     public String inputAddStationName(Scanner scanner) {
         inputAddStationNameRequestMessage();
-        String stationName = scanner.nextLine();
-        validateNameLengthIsMoreThan2(stationName);
-        validateStationNameIsDuplicate(stationName);
-        return parseStringToTrimString(stationName);
+        String stationName = parseStringToTrimString(scanner.nextLine());
+        if (!validateNameLengthIsMoreThan2(stationName) || !validateStationNameIsDuplicate(stationName)) {
+            return inputAddStationName(scanner);
+        }
+        return stationName;
     }
 
     public String inputDeleteStationName(Scanner scanner, LineStationRepository lineStation) {
         inputDeleteStationNameRequestMessage();
-        String stationName = scanner.nextLine();
-        validateStationNameIsContains(stationName);
-        validateStationIsContainsLineStation(stationName, lineStation);
-        return parseStringToTrimString(stationName);
+        String stationName = parseStringToTrimString(scanner.nextLine());
+        if (!validateStationNameIsContains(stationName) || !validateStationIsContainsLineStation(stationName, lineStation)) {
+            return inputDeleteStationName(scanner, lineStation);
+        }
+        return stationName;
     }
 
     public String inputAddLineName(Scanner scanner) {
         inputAddLineNameRequestMessage();
-        String lineName = scanner.nextLine();
-        validateNameLengthIsMoreThan2(lineName);
-        validateLineNameIsDuplicate(lineName);
-        return parseStringToTrimString(lineName);
+        String lineName = parseStringToTrimString(scanner.nextLine());
+        if (!validateNameLengthIsMoreThan2(lineName) || !validateLineNameIsDuplicate(lineName)) {
+            return inputAddLineName(scanner);
+        }
+        return lineName;
     }
 
     public String inputDeleteLineName(Scanner scanner) {
         inputDeleteLineNameRequestMessage();
-        String lineName = scanner.nextLine();
-        validateLineNameIsContains(lineName);
-        return parseStringToTrimString(lineName);
+        String lineName = parseStringToTrimString(scanner.nextLine());
+        if (!validateLineNameIsContains(lineName)) {
+            return inputDeleteLineName(scanner);
+        }
+        return lineName;
     }
 
     public String inputAddStartStationName(Scanner scanner) {
         inputAddStartStationNameRequestMessage();
-        String stationName = scanner.nextLine();
-        validateStationNameIsContains(stationName);
-        return parseStringToTrimString(stationName);
+        String stationName = parseStringToTrimString(scanner.nextLine());
+        if (!validateStationNameIsContains(stationName)) {
+            return inputAddStartStationName(scanner);
+        }
+        return stationName;
     }
 
     public String inputAddEndStationName(Scanner scanner) {
         inputAddEndStationNameRequestMessage();
-        String stationName = scanner.nextLine();
-        validateStationNameIsContains(stationName);
-        return parseStringToTrimString(stationName);
+        String stationName = parseStringToTrimString(scanner.nextLine());
+        if (!validateStationNameIsContains(stationName)) {
+            return inputAddEndStationName(scanner);
+        }
+        return stationName;
     }
 
     public String inputLineNameToAddSection(Scanner scanner) {
         inputLineNameToAddSectionRequestMessage();
-        String lineName = scanner.nextLine();
-        validateLineNameIsContains(lineName);
-        return parseStringToTrimString(lineName);
+        String lineName = parseStringToTrimString(scanner.nextLine());
+        if (!validateLineNameIsContains(lineName)) {
+            return inputLineNameToAddSection(scanner);
+        }
+        return lineName;
     }
 
     public String inputStationNameToAddSection(Scanner scanner) {
         inputStationNameToAddSectionRequestMessage();
-        String stationName = scanner.nextLine();
-        validateStationNameIsContains(stationName);
-        return parseStringToTrimString(stationName);
+        String stationName = parseStringToTrimString(scanner.nextLine());
+        if (!validateStationNameIsContains(stationName)) {
+            return inputStationNameToAddSection(scanner);
+        }
+        return stationName;
     }
 
     public String inputPositionToAddSection(Scanner scanner) {
         inputPositionToAddSectionRequestMessage();
-        String position = scanner.nextLine();
-        validatePositionIsDigit(position);
-        return parseStringToTrimString(position);
+        String position = parseStringToTrimString(scanner.nextLine());
+        if (!validatePositionIsDigit(position)) {
+            return inputPositionToAddSection(scanner);
+        }
+        return position;
     }
 
     public String inputLineNameToDeleteSection(Scanner scanner, LineStationRepository lineStation) {
         inputLineNameToDeleteSectionRequestMessage();
-        String lineName = scanner.nextLine();
-        validateLineNameIsContains(lineName);
-        validateStationSizeOfLineIsMoreThan2(lineName, lineStation);
-        return parseStringToTrimString(lineName);
+        String lineName = parseStringToTrimString(scanner.nextLine());
+        if (!validateLineNameIsContains(lineName) || !validateStationSizeOfLineIsMoreThan2(lineName, lineStation)) {
+            return inputLineNameToDeleteSection(scanner, lineStation);
+        }
+        return lineName;
     }
 
     public String inputStationNameToDeleteSection(Scanner scanner) {
         inputStationNameToDeleteSectionRequestMessage();
-        String stationName = scanner.nextLine();
-        validateStationNameIsContains(stationName);
-        return parseStringToTrimString(stationName);
+        String stationName = parseStringToTrimString(scanner.nextLine());
+        if (!validateStationNameIsContains(stationName)) {
+            return inputStationNameToDeleteSection(scanner);
+        }
+        return stationName;
     }
 }
--- src/main/java/subway/utils/InputValidation.java
+++ src/main/java/subway/utils/InputValidation.java
@@ -13,52 +13,66 @@ public class InputValidation {
     private static final int SECTION_POINT_ONE = 1;
     private static final int SECTION_POINT_ZERO = 0;
 
-    public void validateMenuRange(List<String> menuRange, String menu) {
+    public boolean validateMenuRange(List<String> menuRange, String menu) {
         boolean isContains = menuRange.contains(menu);
         if (!isContains) {
-            throw new IllegalArgumentException("[ERROR] 선택할 수 없는 기능입니다.");
+            System.out.println("\n[ERROR] 선택할 수 없는 기능입니다.");
+            return false;
         }
+        return true;
     }
 
-    public void validateNameLengthIsMoreThan2(String name) {
+    public boolean validateNameLengthIsMoreThan2(String name) {
         if (name.length() < NAME_MIN_LENGTH) {
-            throw new IllegalArgumentException("[ERROR] 이름의 길이는 최소 2이상이어야 합니다.");
+            System.out.println("\n[ERROR] 이름의 길이는 최소 2이상이어야 합니다.");
+            return false;
         }
+        return true;
     }
 
-    public void validateStationNameIsDuplicate(String name) {
+    public boolean validateStationNameIsDuplicate(String name) {
         Station findStation = StationRepository.findStation(name);
         if (!Objects.equals(findStation, null)) {
-            throw new IllegalArgumentException("[ERROR] 이미 등록된 역 이름입니다.");
+            System.out.println("\n[ERROR] 이미 등록된 역 이름입니다.");
+            return false;
         }
+        return true;
     }
 
-    public void validateStationNameIsContains(String name) {
+    public boolean validateStationNameIsContains(String name) {
         Station findStation = StationRepository.findStation(name);
         if (Objects.equals(findStation, null)) {
-            throw new IllegalArgumentException("[ERROR] 존재하지 않는 역입니다.");
+            System.out.println("\n[ERROR] 존재하지 않는 역입니다.");
+            return false;
         }
+        return true;
     }
 
-    public void validateLineNameIsDuplicate(String name) {
+    public boolean validateLineNameIsDuplicate(String name) {
         Line findLine = LineRepository.findLine(name);
         if (!Objects.equals(findLine, null)) {
-            throw new IllegalArgumentException("[ERROR] 이미 등록된 노선 이름입니다.");
+            System.out.println("\n[ERROR] 이미 등록된 노선 이름입니다.");
+            return false;
         }
+        return true;
     }
 
-    public void validateLineNameIsContains(String name) {
+    public boolean validateLineNameIsContains(String name) {
         Line findLine = LineRepository.findLine(name);
         if (Objects.equals(findLine, null)) {
-            throw new IllegalArgumentException("[ERROR] 존재하지 않는 노선입니다.");
+            System.out.println("\n[ERROR] 존재하지 않는 노선입니다.");
+            return false;
         }
+        return true;
     }
 
-    public void validatePositionIsDigit(String position) {
+    public boolean validatePositionIsDigit(String position) {
         boolean isDigit = position.chars().allMatch(Character::isDigit);
         if (!isDigit) {
-            throw new IllegalArgumentException("[ERROR] 순서는 숫자만 입력 가능합니다.");
+            System.out.println("\n[ERROR] 순서는 숫자만 입력 가능합니다.");
+            return false;
         }
+        return true;
     }
 
     public int validatePositionIsOutOfRange(String lineName, String position, LineStationRepository lineStation) {
@@ -73,17 +87,21 @@ public class InputValidation {
         return pos;
     }
 
-    public void validateStationSizeOfLineIsMoreThan2(String lineName, LineStationRepository lineStation) {
+    public boolean validateStationSizeOfLineIsMoreThan2(String lineName, LineStationRepository lineStation) {
         int stationSizeOfLine = lineStation.getStationSizeOfLine(LineRepository.findLine(lineName));
         if (stationSizeOfLine <= MIN_COUNT_OF_DELETE_SECTION) {
-            throw new IllegalArgumentException("[ERROR] 노선에 포함된 역이 두개 이하입니다.");
+            System.out.println("\n[ERROR] 노선에 포함된 역이 두개 이하입니다.");
+            return false;
         }
+        return true;
     }
 
-    public void validateStationIsContainsLineStation(String stationName, LineStationRepository lineStation) {
+    public boolean validateStationIsContainsLineStation(String stationName, LineStationRepository lineStation) {
         Station findStation = StationRepository.findStation(stationName);
         if (lineStation.findStationInLine(findStation)) {
-            throw new IllegalArgumentException("[ERROR] 노선에 등록된 역은 삭제할 수 없습니다.");
+            System.out.println("\n[ERROR] 노선에 등록된 역은 삭제할 수 없습니다.");
+            return false;
         }
+        return true;
     }
 }
