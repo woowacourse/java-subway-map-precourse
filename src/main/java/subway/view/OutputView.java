bd1d80f50522e362e950576ec91b7c4a9037141d
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-15 07:05:43 +0900

    refactor: OutputView 불필요한 내용 삭제 및 empty data 조회에 대한 예외 구현

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -15,12 +15,13 @@ public class OutputView {
             "\n## 노선 관리 화면\n1. 노선 등록\n2. 노선 삭제\n3. 노선 조회\nB. 돌아가기";
     private static final String SECTION_MANAGEMENT_DISPLAY =
             "\n## 구간 관리 화면\n1. 구간 등록\n2. 구간 삭제\nB. 돌아가기";
-    private static final String SUCCESS_MESSAGE_FORMAT = "\n[INFO] 지하철 %s이 %s되었습니다.";
-    private static final String NAMES_PRINT_HEADER_FORMAT = "\n## %s 목록";
+    private static final String EMPTY_DATA_MESSAGE = "등록된 데이터가 없어 출력이 불가능합니다.";
     private static final String SUBWAY_MAP_PRINT_HEADER = "\n## 지하철 노선도";
     private static final String SUBWAY_MAP_DELIMITER = "\n[INFO] ---";
+    private static final String INFORMATION_SUCCESS_MESSAGE_FORMAT = "\n[INFO] 지하철 %s이 %s되었습니다.\n";
     private static final String INFORMATION_MESSAGE_FORMAT = "\n[INFO] %s";
     private static final String ERROR_MESSAGE_FORMAT = "\n[ERROR] %s\n";
+    private static final String NAMES_PRINT_HEADER_FORMAT = "\n## %s 목록";
 
     private OutputView() {
     }
@@ -45,40 +46,38 @@ public class OutputView {
     }
 
     public static void printErrorMessage(RuntimeException runtimeException) {
-        String errorMessage = runtimeException.getMessage();
-        System.out.printf(ERROR_MESSAGE_FORMAT, errorMessage);
+        System.out.printf(ERROR_MESSAGE_FORMAT, runtimeException.getMessage());
     }
 
     public static void printSuccessMessage(ManagementType managementType, FunctionType functionType) {
-        if (functionType == FunctionType.READ) {
-            return;
-        }
-        String message = String.format(SUCCESS_MESSAGE_FORMAT, managementType.toString(), functionType.toString());
-        System.out.println(message);
+        System.out.printf(INFORMATION_SUCCESS_MESSAGE_FORMAT, managementType.toString(), functionType.toString());
     }
 
     public static void printNames(ManagementType managementType, List<String> names) {
-        String headerMessage = String.format(NAMES_PRINT_HEADER_FORMAT, managementType.toString());
-        System.out.println(headerMessage);
+        if (names.isEmpty()) {
+            System.out.printf(ERROR_MESSAGE_FORMAT, EMPTY_DATA_MESSAGE);
+            return;
+        }
+        System.out.printf(NAMES_PRINT_HEADER_FORMAT, managementType.toString());
         names.forEach(name -> System.out.printf(INFORMATION_MESSAGE_FORMAT, name));
         System.out.println();
     }
 
     public static void printSubwayMap(List<SubwayMapDto> subwayMapDtos) {
+        if (subwayMapDtos.isEmpty()) {
+            System.out.printf(ERROR_MESSAGE_FORMAT, EMPTY_DATA_MESSAGE);
+            return;
+        }
         System.out.print(SUBWAY_MAP_PRINT_HEADER);
-        subwayMapDtos.forEach(OutputView::printEachSubwayLine);
+        subwayMapDtos.forEach(OutputView::printSubwayMapOfEachLine);
     }
 
-    public static void printEachSubwayLine(SubwayMapDto subwayMapDto) {
+    public static void printSubwayMapOfEachLine(SubwayMapDto subwayMapDto) {
         String lineName = subwayMapDto.getLineName();
         System.out.printf(INFORMATION_MESSAGE_FORMAT, lineName);
         System.out.print(SUBWAY_MAP_DELIMITER);
-        List<String> stationNamesOfLine = subwayMapDto.getStationNames();
-        stationNamesOfLine.forEach(OutputView::printEachStationName);
+        subwayMapDto.getStationNames()
+                .forEach(stationName -> System.out.printf(INFORMATION_MESSAGE_FORMAT, stationName));
         System.out.println();
-    }
-
-    private static void printEachStationName(String stationName) {
-        System.out.printf(INFORMATION_MESSAGE_FORMAT, stationName);
     }
 }
