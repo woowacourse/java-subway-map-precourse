a52be4fdf9732d8dfff8b56c6293d71cd883f066
Gayoung <gyyoon4u@naver.com>
2020-12-14 12:57:23 +0900

    feat: line이름 예외처리후 값 가져오기+논리적동일성 제공

--- src/main/java/subway/domain/list/Line.java
+++ src/main/java/subway/domain/list/Line.java
@@ -1,5 +1,10 @@
 package subway.domain.list;
 
+import subway.domain.station.Station;
+import subway.domain.station.StationCheck;
+
+import java.util.Objects;
+
 public class Line {
     private String name;
 
@@ -11,5 +16,26 @@ public class Line {
         return name;
     }
 
-    // 추가 기능 구현
+    public static Line getLine(String lineName) {
+        if (LineCheck.checkLineNameLength(lineName) && LineCheck.checkLineNameEndPoint(lineName)) {
+        }
+        return new Line(lineName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Line line = (Line) o;
+        return Objects.equals(name, line.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
