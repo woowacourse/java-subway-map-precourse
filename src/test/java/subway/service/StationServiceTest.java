572d8a721f57eae3b440bd7596cd6cb9fa8aa566
tomas <cmg14111@gmail.com>
2020-12-14 23:07:41 +0900

    test(StationServiceTest): 역 삭제 테스트, 존재하지 않는 역을 삭제 시도시 예외 발생 테스트

--- src/test/java/subway/service/StationServiceTest.java
+++ src/test/java/subway/service/StationServiceTest.java
@@ -18,13 +18,29 @@ class StationServiceTest {
         Assertions.assertEquals(true, StationRepository.stations().contains(yangsan));
     }
 
+//    @Test
+//    public void 역_중복_예외_테스트() {
+//        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+//            StationRepository.addStation(yangsan);
+//            StationService stationService = new StationService(scanner);
+//            stationService.isDuplicatedStation(new Station("양산역"));
+//        });
+//        Assertions.assertEquals("\n[ERROR] 해당 역은 이미 등록되었습니다.", exception.getMessage());
+//    }
+
     @Test
-    public void 역_중복_예외_테스트() {
+    public void 역_삭제_테스트() {
+        StationRepository.deleteStation("양산역");
+        Assertions.assertEquals(false, StationRepository.stations().contains(yangsan));
+    }
+
+    @Test
+    public void 역_삭제할_때_존재하지_않으면_예외_테스트() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
             StationRepository.addStation(yangsan);
             StationService stationService = new StationService(scanner);
-            stationService.isDuplicatedStation(new Station("양산역"));
+            stationService.isNotExistStation(new Station("증산역"));
         });
-        Assertions.assertEquals("\n[ERROR] 해당 역은 이미 등록되었습니다.", exception.getMessage());
+        Assertions.assertEquals("\n[ERROR] 삭제할 역이 존재하지 않습니다.", exception.getMessage());
     }
 }
