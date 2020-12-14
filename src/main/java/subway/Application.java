6ab4b9785fd9ed1d01cd77a603f08c1f86c3dbd1
mungto <daum7766@naver.com>
2020-12-14 23:06:22 +0900

    refactor(Application): call userInterfaceView start Function

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -3,9 +3,12 @@ package subway;
 import java.util.Scanner;
 
 public class Application {
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
         DataInitService.init();
+        UserInterfaceView userInterfaceView = new UserInterfaceView(scanner);
+        userInterfaceView.start();
     }
 }
