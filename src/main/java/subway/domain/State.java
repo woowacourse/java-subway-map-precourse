f497f21c733dd79170f0ca27e54cce277e01b104
lns13301 <rkdrbgks22@ks.ac.kr>
2020-12-14 21:44:01 +0900

    refactor: State(enum) 의 불필요한 index 제거

--- src/main/java/subway/domain/State.java
+++ src/main/java/subway/domain/State.java
@@ -1,25 +1,19 @@
 package subway.domain;
 
 public enum State {
-    QUIT(-1),
-    MAIN_SCENE(0),
-    STATION_SCENE(1),
-    LINE_SCENE(2),
-    SECTION_SCENE(3),
-    MAP_SCENE(4),
-    STATION_ADD(11),
-    STATION_REMOVE(12),
-    STATION_INQUIRY(13),
-    LINE_ADD(21),
-    LINE_REMOVE(22),
-    LINE_INQUIRY(23),
-    SECTION_ADD(31),
-    SECTION_REMOVE(32),
-    SECTION_INQUIRY(33);
-
-    private int index;
-
-    State(int index) {
-        this.index = index;
-    }
+    QUIT,
+    MAIN_SCENE,
+    STATION_SCENE,
+    LINE_SCENE,
+    SECTION_SCENE,
+    MAP_SCENE,
+    STATION_ADD,
+    STATION_REMOVE,
+    STATION_INQUIRY,
+    LINE_ADD,
+    LINE_REMOVE,
+    LINE_INQUIRY,
+    SECTION_ADD,
+    SECTION_REMOVE,
+    SECTION_INQUIRY
 }
