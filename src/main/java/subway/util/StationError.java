b15f8484e45295be74cab0bb6ce1c0be2b0dde08
Gomding <backchida@gmail.com>
2020-12-15 00:54:02 +0900

    feat : 역을 등록할 때 역이름이 2자리 이상인지 검사하는 기능 추가

--- src/main/java/subway/util/StationError.java
+++ src/main/java/subway/util/StationError.java
@@ -6,11 +6,20 @@ import java.util.List;
 
 public class StationError {
 
+    private static final int SIZE = 2;
+
     public static boolean checkDuplicateName(String name, List<Station> stations) {
         for(Station station : stations) {
             if (station.getName().equals(name)) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    public static boolean checkValidName(String name) {
+        if (name.length() >= SIZE) {
+            return true;
         }
         return false;
     }
