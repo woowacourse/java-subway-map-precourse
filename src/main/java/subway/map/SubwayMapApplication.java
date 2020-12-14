92f0fe782ac5196df6c96a804a9793c97d429d41
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-15 05:45:06 +0900

    refactor: SubwayMapApplication 기존의 컨트롤러 요청 작업을 ControllerMapper로 위임

--- src/main/java/subway/map/SubwayMapApplication.java
+++ src/main/java/subway/map/SubwayMapApplication.java
@@ -1,24 +1,19 @@
 package subway.map;
 
-import subway.controller.SubwayMapController;
+import subway.controller.ControllerMapper;
 import subway.domain.type.FunctionType;
 import subway.domain.type.ManagementType;
-import subway.dto.LineDto;
-import subway.dto.SectionDto;
-import subway.dto.SubwayMapDto;
 import subway.view.InputView;
 import subway.view.OutputView;
-
-import java.util.List;
 
 public class SubwayMapApplication {
 
     private final InputView inputView;
-    private final SubwayMapController subwayMapController;
+    private final ControllerMapper controllerMapper;
 
-    public SubwayMapApplication(InputView inputView, SubwayMapController subwayMapController) {
+    public SubwayMapApplication(InputView inputView, ControllerMapper controllerMapper) {
         this.inputView = inputView;
-        this.subwayMapController = subwayMapController;
+        this.controllerMapper = controllerMapper;
     }
 
     public void run() {
@@ -26,96 +21,40 @@ public class SubwayMapApplication {
         while (managementType.isRunning()) {
             OutputView.printMainDisplay();
             managementType = inputView.inputManagementType();
-            moveToMatchingManagementDisplay(managementType);
+            executeExceptionHandler(managementType);
         }
     }
 
-    private void moveToMatchingManagementDisplay(ManagementType managementType) {
-        if (managementType == ManagementType.EXIT) {
-            return;
-        }
-        if (managementType == ManagementType.SUBWAY_MAP_PRINT) {
-            printSubwayMapInformation(managementType);
+    private void executeExceptionHandler(ManagementType managementType) {
+        if (!isDetailManagementDisplayRequired(managementType)) {
             return;
         }
         try {
-            showManagementDisplay(managementType);
+            activateDetailManagementDisplay(managementType);
         } catch (RuntimeException runtimeException) {
             OutputView.printErrorMessage(runtimeException);
-            moveToMatchingManagementDisplay(managementType);
+            executeExceptionHandler(managementType);
         }
     }
 
-    private void printSubwayMapInformation(ManagementType managementType) {
-        if (managementType == ManagementType.SUBWAY_MAP_PRINT) {
-            List<SubwayMapDto> subwayMapDtos = subwayMapController.getSubwayMapDtos();
-            OutputView.printSubwayMap(subwayMapDtos);
+    private boolean isDetailManagementDisplayRequired(ManagementType managementType) {
+        if (managementType == ManagementType.EXIT) {
+            return false;
         }
-        if (managementType == ManagementType.STATION) {
-            List<String> stationNames = subwayMapController.getStationNames();
-            OutputView.printNames(managementType, stationNames);
+        if (managementType == ManagementType.PRINT_SUBWAY_MAP) {
+            controllerMapper.delegateRequestToController(managementType, FunctionType.READ);
+            return false;
         }
-        if (managementType == ManagementType.LINE) {
-            List<String> lineNames = subwayMapController.getLineNames();
-            OutputView.printNames(managementType, lineNames);
-        }
+        return true;
     }
 
-    private void showManagementDisplay(ManagementType managementType) {
+    private void activateDetailManagementDisplay(ManagementType managementType) {
         OutputView.printManagementDisplay(managementType);
         FunctionType functionType = inputView.inputFunctionType(managementType);
         if (functionType == FunctionType.BACK) {
             return;
         }
-        if (functionType == FunctionType.READ) {
-            printSubwayMapInformation(managementType);
-            return;
-        }
-        executeMatchingManagement(managementType, functionType);
-    }
-
-    private void executeMatchingManagement(ManagementType managementType, FunctionType functionType) {
-        if (managementType == ManagementType.STATION) {
-            executeStationManagement(functionType);
-        }
-        if (managementType == ManagementType.LINE) {
-            executeLineManagement(functionType);
-        }
-        if (managementType == ManagementType.SECTION) {
-            executeSectionManagement(functionType);
-        }
-    }
-
-    private void executeStationManagement(FunctionType functionType) {
-        String stationName = inputView.inputStationName(functionType);
-        if (functionType == FunctionType.REGISTER) {
-            subwayMapController.addStationByName(stationName);
-        }
-        if (functionType == FunctionType.DELETE) {
-            subwayMapController.deleteStationByName(stationName);
-        }
-        OutputView.printStationManagementSuccessMessage(functionType);
-    }
-
-    private void executeLineManagement(FunctionType functionType) {
-        LineDto lineDto = inputView.inputLineRequest(functionType);
-        if (functionType == FunctionType.REGISTER) {
-            subwayMapController.addLine(lineDto);
-        }
-        if (functionType == FunctionType.DELETE) {
-            subwayMapController.deleteLine(lineDto);
-        }
-        OutputView.printLineManagementSuccessMessage(functionType);
-    }
-
-    private void executeSectionManagement(FunctionType functionType) {
-        SectionDto sectionDto = inputView.inputSectionRequest(functionType);
-        if (functionType == FunctionType.REGISTER) {
-            subwayMapController.addSection(sectionDto);
-        }
-        if (functionType == FunctionType.DELETE) {
-            subwayMapController.deleteSection(sectionDto);
-        }
-        OutputView.printSectionManagementSuccessMessage(functionType);
+        controllerMapper.delegateRequestToController(managementType, functionType);
+        OutputView.printSuccessMessage(managementType, functionType);
     }
 }
