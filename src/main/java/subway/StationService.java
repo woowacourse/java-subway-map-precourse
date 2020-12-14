2c82172cdbcb7f3d9d853d52fc03147c2758ba86
mungto <daum7766@naver.com>
2020-12-14 15:23:15 +0900

    feat(StationService): create addStation Function
    create addStation Fucntion and Length condition check

--- src/main/java/subway/StationService.java
+++ src/main/java/subway/StationService.java
@@ -1,5 +1,20 @@
 package subway;
 
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
 public class StationService {
-    
+
+    public static final int MIN_STATION_NAME_LENGTH = 2;
+
+    public static boolean addStation(String name) {
+
+        if (name.length() < MIN_STATION_NAME_LENGTH) {
+            // TODO - 길이에러 출력함수 호출하기
+            return false;
+        }
+        StationRepository.addStation(new Station(name));
+        // TODO - 완료출력함수 호출하기
+        return true;
+    }
 }
