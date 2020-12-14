d0049f79d4ba8df7db542b9f1e3640ee59742e47
pika96 <52442949+pika96@users.noreply.github.com>
2020-12-14 23:26:48 +0900

    User 인스턴스 생성 메소드 추가

--- src/main/java/subway/domain/SubwayManagement.java
+++ src/main/java/subway/domain/SubwayManagement.java
@@ -3,20 +3,19 @@ package subway.domain;
 public class SubwayManagement {
 
     private User user;
-    private static String[] initStation = {"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
-    private static String[][] initLine = {{"2호선", "교대역", "역삼역"}, {"3호선", "교대역", "매봉역"}, {"신분당선", "강남역", "양재시민의숲역"}};
-    private static String[][] initSection = {{"강남역"}, {"남부터미널역", "양재역"}, {"양재역"}};
 
     public void start() {
-
-        initStats();
-
-        user = new User();
+        createUserInstance();
+        InitSetting.initSetting();
 
         while (true) {
             printScreen.printMainScreen();
             mainFunction(user.getInput());
         }
+    }
+
+    private void createUserInstance() {
+        user = new User();
     }
 
     private void mainFunction(String select) {
@@ -158,18 +157,5 @@ public class SubwayManagement {
         printScreen.printAllSubwayLine();
     }
 
-    private static void initStats() {
-        for (String name : initStation) {
-            StationRepository.addStation(new Station(name));
-        }
 
-        for (int i = 0; i < initLine.length; i++) {
-            LineRepository.addLine(initLine[i][0], initLine[i][1], initLine[i][2]);
-            String[] sections = initSection[i];
-            for (String section : sections) {
-                int idx = 2;
-                LineRepository.addSection(idx, initLine[i][0], section);
-            }
-        }
-    }
 }
