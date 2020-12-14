0ad4f7c39c3a44862c2a5c8de6e46be095e1816e
Sehwan_Jang <whilter08@gmail.com>
2020-12-14 16:10:12 +0900

    feat: 전체 어플리케이션 작동기능 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,13 @@
 package subway;
 
+import subway.userinterface.UserController;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        UserController.runApp(scanner);
+        scanner.close();
     }
 }
