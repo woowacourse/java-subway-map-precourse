f16cc38e9f44adb2983bc98be0e1ddf056fd5114
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2020-12-14 23:05:12 +0900

    fix(DuplicatedNameException) : 메세지 문구 수정

--- src/main/java/subway/exception/DuplicatedNameException.java
+++ src/main/java/subway/exception/DuplicatedNameException.java
@@ -1,7 +1,7 @@
 package subway.exception;
 
 public class DuplicatedNameException extends IllegalArgumentException {
-    private static final String message = "[ERROR] 이미 등록된 역 이름입니다. ";
+    private static final String message = "[ERROR] 이미 등록된 이름입니다. ";
 
     @Override
     public String getMessage() {
