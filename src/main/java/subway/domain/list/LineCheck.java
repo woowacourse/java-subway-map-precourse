9bd905fe75844939842c34004cc6c6bbdfc448f3
Gayoung <gyyoon4u@naver.com>
2020-12-14 12:53:34 +0900

    feat: line 이름 '선'또는 '호선'으로 끝나는지 예외처리

--- src/main/java/subway/domain/list/LineCheck.java
+++ src/main/java/subway/domain/list/LineCheck.java
@@ -2,15 +2,21 @@ package subway.domain.list;
 
 public class LineCheck {
 
-    private String lineName;
-
     private static final int MIN_LINE_NAME_LENGTH = 2;
 
     private static final String LINE_LENGTH_ERROR_MESSAGE = "[ERROR] 노선 이름은 2글자 이상 입력해야합니다.";
+    public static final String LINE_END_POINT_ERROR_MESSAGE = "[ERROR] 노선이름은 OO선 또는 O호선으로 끝나야 합니다.";
 
     public static boolean checkLineNameLength(String lineName) {
         if (lineName.length() < MIN_LINE_NAME_LENGTH) {
             throw new IllegalArgumentException(LINE_LENGTH_ERROR_MESSAGE);
+        }
+        return true;
+    }
+
+    public static boolean checkLineNameEndPoint(String lineName) {
+        if (!lineName.endsWith("선") || !lineName.endsWith("호선")) {
+            throw new IllegalArgumentException(LINE_END_POINT_ERROR_MESSAGE);
         }
         return true;
     }
