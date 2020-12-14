d522162cf34031a88ff652beb7f71e919651b12d
mungto <daum7766@naver.com>
2020-12-14 15:37:23 +0900

    feat(OutPut): create printStationCreateMessage Function

--- src/main/java/subway/OutPut.java
+++ src/main/java/subway/OutPut.java
@@ -5,9 +5,14 @@ public class OutPut {
     public static final String INFO = "[INFO] ";
     public static final String ERROR = "[ERROR] ";
     private static final String STATION_NAME_LENGTH_ERROR = "지하철 역의 이름은 2글자 이상이여야 합니다.\n";
+    private static final String STATION_CREATE_MESSAGE = "지하철 역이 등록되었습니다.\n";
 
     public static void printStationNameLengthError() {
         System.out.println(ERROR + STATION_NAME_LENGTH_ERROR);
+    }
+
+    public static void printStationCreateMessage() {
+        System.out.println(INFO + STATION_CREATE_MESSAGE);
     }
 
 }
