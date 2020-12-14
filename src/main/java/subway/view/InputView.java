6789f3b1ccf745606de3c86c8856181ef2ae47ef
operasystem6 <operasystem6@naver.com>
2020-12-14 11:41:54 +0900

    refactor: InputView inputValue 메소드 구현

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -3,18 +3,20 @@ package subway.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner scanner;
-
-    public InputView(Scanner scanner) {
-        this.scanner = scanner;
-    }
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String inputFunction() {
         printQuestion(TextCollection.SELECT_FUNCTION_MESSAGE);
         return scanner.nextLine();
     }
 
-    public static void printQuestion(String message) {
-        System.out.printf("%s %s", TextCollection.PREFIX, message);
+    public static String inputValue() {
+        return scanner.nextLine();
     }
+
+    public static void printQuestion(String message) {
+        System.out.println();
+        System.out.printf("%s %s\n", TextCollection.PREFIX, message);
+    }
+
 }
