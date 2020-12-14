838bcab6db959871d1af66fcfde476f8d6bb4ad8
Gomding <backchida@gmail.com>
2020-12-15 03:56:54 +0900

    feat : 메인메뉴 화면 출력 추가

--- src/main/java/subway/menu/MainMenu.java
+++ src/main/java/subway/menu/MainMenu.java
@@ -12,7 +12,38 @@ public class MainMenu {
     private static final String QUIT = "Q. 종료";
     private static final String CHOICE_MESSAGE = "## 원하는 기능을 선택하세요.";
 
-    public void start(Scanner scanner) {
+    private Scanner scanner;
 
+    public void start(Scanner scanner) {
+        this.scanner = scanner;
+        String input = "";
+        while (!input.equals("Q")) {
+            printMainMenu();
+            input = scanner.nextLine();
+            switch (input) {
+                case "1" :
+                    break;
+                case "2" :
+                    break;
+                case "3" :
+                    break;
+                case "4" :
+                    break;
+                case "Q" :
+                    break;
+            }
+        }
+    }
+
+    private void printMainMenu() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(MAIN_TITLE).append("\n")
+                .append(MENU1).append("\n")
+                .append(MENU2).append("\n")
+                .append(MENU3).append("\n")
+                .append(MENU4).append("\n")
+                .append(QUIT).append("\n\n")
+                .append(CHOICE_MESSAGE).append("\n");
+        System.out.println(sb);
     }
 }
