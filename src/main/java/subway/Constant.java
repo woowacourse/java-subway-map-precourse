b8a62c811f2b2b0404bec3c5ae50dfb3139b8a10
Seonho Kim <zxzx235@naver.com>
2020-12-14 20:18:39 +0900

    feat: 초기 설정하는 기능 추가

--- src/main/java/subway/Constant.java
+++ src/main/java/subway/Constant.java
@@ -7,15 +7,26 @@ import java.util.List;
 public class Constant {
     static final String[] defaultStations = {"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
     static final String[] defaultLines = {"2호선", "3호선", "신분당선"};
+    static final String[][] defaultSetting = {
+            {defaultStations[0], defaultStations[1], defaultStations[2]},
+            {defaultStations[0], defaultStations[3], defaultStations[4], defaultStations[6]},
+            {defaultStations[1], defaultStations[4], defaultStations[5]}
+    };
 
     private static final List<String> defaultStationList = Arrays.asList(defaultStations);
     private static final List<String> defaultLineList = Arrays.asList(defaultLines);
+    private static final List<String[]> defaultSettingList = Arrays.asList(defaultSetting);
 
     public static List<String> defaultStationList() {
         return Collections.unmodifiableList(defaultStationList);
     }
+
     public static List<String> defaultLineList() {
         return Collections.unmodifiableList(defaultLineList);
+    }
+
+    public static List<String[]> defaultSettingList() {
+        return Collections.unmodifiableList(defaultSettingList);
     }
 
     static final String[] mainMenuItems = {"1. 역 관리", "2. 노선 관리", "3. 구간 관리", "4. 지하철 노선도 출력", "Q. 종료"};
--- src/main/java/subway/DefaultData.java
+++ src/main/java/subway/DefaultData.java
@@ -1,12 +1,30 @@
 package subway;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
 
 public class DefaultData {
-    public static void input(){
+    public static void setting(){
+        station();
+        line();
+    }
+
+    public static void station() {
         Constant.defaultStationList().forEach(station -> {
             StationRepository.addStation(new Station(station));
         });
+    }
+
+    public static void line() {
+        for (int i = 0; i < Constant.defaultLineList().size(); i++) {
+            Line line = new Line(Constant.defaultLineList().get(i));
+            for (int j = 0; j < Constant.defaultSettingList().get(i).length; j++) {
+                LineSetting.addStationToLine(line, Constant.defaultSettingList().get(i)[j]);
+            }
+            LineRepository.addLine(line);
+        }
+
     }
 }
