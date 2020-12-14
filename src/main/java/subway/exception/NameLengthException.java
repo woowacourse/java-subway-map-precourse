91d4057f903747861e31926b984b1df95ba8ed96
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-15 02:52:54 +0900

    feat: 지하철역 등록 시 검증로직 추가

---
+++ src/main/java/subway/exception/NameLengthException.java
@@ -0,0 +1,9 @@
+package subway.exception;
+
+public class NameLengthException extends IllegalArgumentException{
+    private static final String message = "[ERROR] 이름은 2글자 이상입니다.";
+
+    public NameLengthException() {
+        super(message);
+    }
+}
--- src/main/java/subway/service/StationService.java
+++ src/main/java/subway/service/StationService.java
@@ -2,10 +2,27 @@ package subway.service;
 
 import subway.domain.Station;
 import subway.domain.StationRepository;
+import subway.exception.NameLengthException;
+import subway.exception.StationNameDuplicateException;
 
 public class StationService {
+    private static final int MIN_NAME_LENGTH = 2;
 
     public static void save(Station station) {
+        validateNameLength(station.getName());
+        validateDuplicateName(station.getName());
         StationRepository.addStation(station);
+    }
+
+    private static void validateNameLength(String name) {
+        if(name.length() < MIN_NAME_LENGTH) {
+            throw new NameLengthException();
+        }
+    }
+
+    private static void validateDuplicateName(String name) {
+        if(StationRepository.findByName(name).isPresent()) {
+            throw new StationNameDuplicateException();
+        }
     }
 }
