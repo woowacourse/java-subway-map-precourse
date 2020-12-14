91b995d8382e4a25164028ce91b0d5d81589720c
seunghyun <ggyool@naver.com>
2020-12-14 20:19:46 +0900

    refactor: static과 final 키워드 위치 변경

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -7,11 +7,11 @@ import subway.domain.screen.StationManagementScreen;
 import java.util.List;
 
 public class OutputView {
-    private final static String ERROR_PREFIX = "[ERROR]";
-    private final static String INFO_PREFIX = "[INFO]";
-    private final static String SPACE = " ";
-    private final static String STATION_LIST_TITLE = "## 역 목록";
-    private final static String LINE_LIST_TITLE = "## 노선 목록";
+    private static final String ERROR_PREFIX = "[ERROR]";
+    private static final String INFO_PREFIX = "[INFO]";
+    private static final String SPACE = " ";
+    private static final String STATION_LIST_TITLE = "## 역 목록";
+    private static final String LINE_LIST_TITLE = "## 노선 목록";
 
     public static void showMainScreen() {
         String message = MainScreen.getInstance().toString();
