67c26297a603a15adae74ea5403d0cbb9c562ab0
park seok chae <tjrco56@naver.com>
2020-12-14 17:43:18 +0900

    feat(LineRegisterView): 노선 등록할 때 해당하는 역이 없을 때 메시지 출력하도록 수정

--- src/main/java/subway/view/LineRegisterView.java
+++ src/main/java/subway/view/LineRegisterView.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 import subway.domain.Line;
 import subway.domain.LineRepository;
 import subway.domain.Station;
+import subway.domain.StationRepository;
 
 public class LineRegisterView extends ManagerView {
 	private static final String INPUT_LINE_NAME_MESSAGE = "등록할 노선 이름을 입력하세요.";
 	private static final String INPUT_FIRST_STATION_NAME_MESSAGE = "등록할 노선의 상행 종점역 이름을 입력하세요.";
 	private static final String INPUT_LAST_STATION_NAME_MESSAGE = "등록할 노선의 하행 종점역 이름을 입력하세요.";
-
+	private static final String NOT_FOUND_STATION_MESSAGE = "입력한 역을 찾을 수 없습니다.";
+	
 	public LineRegisterView(Scanner scanner) {
 		print();
 		registerLine(scanner);
@@ -20,16 +22,19 @@ public class LineRegisterView extends ManagerView {
 	public void registerLine(Scanner scanner) {
 		String name = scanner.nextLine();
 		Line line = new Line(name);
+		try {
+			printTopMenu(INPUT_FIRST_STATION_NAME_MESSAGE);
+			Station firstStation = StationRepository.getStationByName(scanner.nextLine());
+			line.addFirstStation(firstStation);
 
-		printTopMenu(INPUT_FIRST_STATION_NAME_MESSAGE);
-		String firstStationName = scanner.nextLine();
-		line.addFirstStation(new Station(firstStationName));
-
-		printTopMenu(INPUT_LAST_STATION_NAME_MESSAGE);
-		String lastStationName = scanner.nextLine();
-		line.addLastStation(new Station(lastStationName));
-
-		LineRepository.addLine(line);
+			printTopMenu(INPUT_LAST_STATION_NAME_MESSAGE);
+			Station lastStation = StationRepository.getStationByName(scanner.nextLine());
+			line.addLastStation(lastStation);
+			
+			LineRepository.addLine(line);
+		} catch (Exception e) {
+			printInfo(NOT_FOUND_STATION_MESSAGE);
+		}
 	}
 
 	@Override
