aad60f2c4f3e3058d971ce4511d5ad92fca967de
binghe819 <binghe819@naver.com>
2020-12-14 22:33:47 +0900

    fix: 초기 세팅 값 수정

--- src/main/java/subway/station/Station.java
+++ src/main/java/subway/station/Station.java
@@ -6,14 +6,10 @@ public class Station {
     private String name;
 
     public Station(String name) {
-        /** TODO: 아래 코드 리펙토링 커밋하기 */
-//        InputValidator.validateMoreThanTwoWords(name);
         this.name = name;
     }
 
     public String getName() {
         return name;
     }
-
-    // 추가 기능 구현
 }
--- src/main/java/subway/station/StationRepository.java
+++ src/main/java/subway/station/StationRepository.java
@@ -7,7 +7,7 @@ public class StationRepository {
 
     private static final String ALREADY_EXIST_STATION_ERROR_MESSAGE = "이미 등록된 역 이름입니다.";
     private static final String NOT_EXIST_STATION_ERROR_MESSAGE = "존재하지 않는 역입니다.";
-    private static final String[] DEFAULT_STATION_NAMES = {"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역", "잠실역"};
+    private static final String[] DEFAULT_STATION_NAMES = {"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
 
     private final List<Station> stations = Arrays.stream(DEFAULT_STATION_NAMES).map(Station::new).collect(Collectors.toList());
 
