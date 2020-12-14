5e85b59423367835411337168d91e2d3f711bcdf
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-12-14 20:28:44 +0900

    feat 메인 입력 받는 로직 설정 및 상수 집합 관리하는 Constant 클래스 설정

--- src/main/java/subway/subwaymanager/MainSelectManager.java
+++ src/main/java/subway/subwaymanager/MainSelectManager.java
@@ -1,9 +1,28 @@
 package subway.subwaymanager;
 
+import subway.utils.InputView;
 import subway.utils.OutputView;
+
+import static subway.utils.Constant.*;
 
 public class MainSelectManager {
     public static void mainSelectManager() {
+        while (true) {
             OutputView.printMainContents();
+            String inputMainSelect = InputView.inputSelect();
+
+            if (inputMainSelect.equals(CONTENTS_NUMBER_FIRST)) {
+            }
+            if (inputMainSelect.equals(CONTENTS_NUMBER_SECOND)) {
+            }
+            if (inputMainSelect.equals(CONTENTS_NUMBER_THIRD)) {
+            }
+            if (inputMainSelect.equals(CONTENTS_NUMBER_FOURTH)) {
+            }
+            if (inputMainSelect.equals(CONTENTS_NUMBER_QUIT)) {
+                System.out.println("종료합니다.");
+                break;
+            }
+        }
     }
 }
---
+++ src/main/java/subway/utils/Constant.java
@@ -0,0 +1,10 @@
+package subway.utils;
+
+public class Constant {
+    public static final String CONTENTS_NUMBER_FIRST = "1";
+    public static final String CONTENTS_NUMBER_SECOND = "2";
+    public static final String CONTENTS_NUMBER_THIRD = "3";
+    public static final String CONTENTS_NUMBER_FOURTH = "4";
+    public static final String CONTENTS_NUMBER_QUIT = "Q";
+    public static final String CONTENTS_NUMBER_BACK = "B";
+}
