aceaa7c8671fc5da8b19e845b2bd49effd2ce352
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2020-12-14 19:24:29 +0900

    feat(subway-view): 등록된 노선이 없을때 출력할 안내 구현

--- src/main/java/subway/view/mapprintoutput/MapPrintInfoView.java
+++ src/main/java/subway/view/mapprintoutput/MapPrintInfoView.java
@@ -7,9 +7,14 @@ import subway.view.InfoView;
 import subway.view.OutputView;
 
 public class MapPrintInfoView extends InfoView {
+    private static final String NO_LINE_INFO = "등록된 지하철 노선이 없습니다.";
     private static String BORDER_LINE = "---";
 
     public static void printMap() {
+        if(LineRepository.lines().size() == 0) {
+            printNoLineInfo();
+            return;
+        }
         for (Line line : LineRepository.lines()) {
             System.out.println(INFO + line.getName());
             System.out.println(INFO + BORDER_LINE);
@@ -18,5 +23,9 @@ public class MapPrintInfoView extends InfoView {
             }
             OutputView.printNewLine();
         }
+    }
+
+    public static void printNoLineInfo() {
+        printInfo(NO_LINE_INFO);
     }
 }
