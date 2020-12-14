93b47003c9d8d81a05f23b1508b0212960f62763
operasystem6 <operasystem6@naver.com>
2020-12-14 19:28:45 +0900

    refactor: 최소 역 2개, postion 변수 숫자 구분, 노선 범위 검사해주는 메소드 구현

--- src/main/java/subway/utils/ValidateUtils.java
+++ src/main/java/subway/utils/ValidateUtils.java
@@ -5,39 +5,55 @@ import subway.domain.Station;
 import subway.exception.SubwayException;
 import subway.repository.LineRepository;
 import subway.repository.StationRepository;
+import subway.view.TextCollection;
+
+import java.util.List;
 
 public class ValidateUtils {
     private static int STANDARD_TWO = 2;
-    private static final String TWO_OR_MORE_LETTERS_MESSAGE = "이름은 2글자 이상이어야 합니다.";
-    private static final String EXISTING_STATION_MESSAGE = "이미 등록된 역 이름입니다.";
-    private static final String EXISTING_LINE_MESSAGE = "이미 등록된 노선 이름입니다.";
-    private static final String SAME_NAME_UP_DOWN_MESSAGE = "상행 종점역과 하행 종점역의 이름이 같습니다";
 
     public static void isTwoOrMoreLetters(String input) {
         if (input.length() < STANDARD_TWO) {
-            throw new SubwayException(TWO_OR_MORE_LETTERS_MESSAGE);
+            throw new SubwayException(TextCollection.TWO_OR_MORE_LETTERS_MESSAGE);
         }
     }
 
     public static void isDuplicatedStation(String input) {
         if (StationRepository.stations().stream()
                 .anyMatch(station -> station.getName().equals(input))) {
-            throw new SubwayException(EXISTING_STATION_MESSAGE);
+            throw new SubwayException(TextCollection.EXISTING_STATION_MESSAGE);
         }
     }
-
 
     public static void isDuplicatedLine(String input) {
         if (LineRepository.lines().stream()
                 .anyMatch(line -> line.getName().equals(input))) {
-            throw new SubwayException(EXISTING_LINE_MESSAGE);
+            throw new SubwayException(TextCollection.EXISTING_LINE_MESSAGE);
         }
     }
 
     public static void isDuplicatedUpDownStation(Station upwardStation, Station downStation) {
         if (upwardStation.equals(downStation)) {
-            throw new SubwayException(SAME_NAME_UP_DOWN_MESSAGE);
+            throw new SubwayException(TextCollection.SAME_NAME_UP_DOWN_MESSAGE);
         }
+    }
 
+    public static void isAtLeastTwoStations(Line line) {
+        if (line.getSections().size() <= STANDARD_TWO) {
+            throw new SubwayException(TextCollection.AT_LEAST_STATIONS);
+        }
+    }
+
+    public static void isNumber(String number) {
+        String regExp = "^[0-9]+$";
+        if (!number.matches(regExp)) {
+            throw new SubwayException(TextCollection.NOT_NUMBER_MESSAGE);
+        }
+    }
+
+    public static void isPositionOutOfLine(List<Station> section, int position) {
+        if (section.size() < position) {
+            throw new SubwayException(TextCollection.OUT_OF_LINE);
+        }
     }
 }
