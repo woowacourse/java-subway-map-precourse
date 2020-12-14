db5d24d869f6ed18414aeb7d59d6947c1632845f
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-12-15 02:40:55 +0900

    fix: 노선 추가 버그 수정
    노선 추가시 제대로 전체 노선에 포함되지 않던 버그를 수정

--- src/main/java/subway/LineController.java
+++ src/main/java/subway/LineController.java
@@ -70,8 +70,9 @@ public class LineController {
         newLine = new Line(lineName);
         newLine.addStation(StationRepository.getStationFromName(upTerminus));
         newLine.addStation(StationRepository.getStationFromName(downTerminus));
-
+        LineRepository.addLine(newLine);
         OutputView.lineAddSuccessPrint();
+
         return true;
     }
 
@@ -88,6 +89,7 @@ public class LineController {
             OutputView.stationNameDeleteErrorPrint();
             return false;
         }
+
 
         OutputView.stationDeleteSuccessPrint();
         return true;
