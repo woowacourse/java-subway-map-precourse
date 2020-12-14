9a5528eefedd52d56ffb049e27b46677b7deced0
operasystem6 <operasystem6@naver.com>
2020-12-14 16:57:48 +0900

    refactor: 중복된 노선, 상행 종점역-하행 종점역 이름 검사

--- src/main/java/subway/utils/ValidateUtils.java
+++ src/main/java/subway/utils/ValidateUtils.java
@@ -1,12 +1,17 @@
 package subway.utils;
 
+import subway.domain.Line;
+import subway.domain.Station;
 import subway.exception.SubwayException;
+import subway.repository.LineRepository;
 import subway.repository.StationRepository;
 
 public class ValidateUtils {
     private static int STANDARD_TWO = 2;
-    private static final String TWO_OR_MORE_LETTERS_MESSAGE = "지하철 역 이름은 2글자 이상이어야 합니다.";
-    private static final String EXISTING_STATION_MESSAGE = "이미 등록된 역 입니다.";
+    private static final String TWO_OR_MORE_LETTERS_MESSAGE = "이름은 2글자 이상이어야 합니다.";
+    private static final String EXISTING_STATION_MESSAGE = "이미 등록된 역 이름입니다.";
+    private static final String EXISTING_LINE_MESSAGE = "이미 등록된 노선 이름입니다.";
+    private static final String SAME_NAME_UP_DOWN_MESSAGE = "상행 종점역과 하행 종점역의 이름이 같습니다";
 
     public static void isTwoOrMoreLetters(String input) {
         if (input.length() < STANDARD_TWO) {
@@ -16,8 +21,23 @@ public class ValidateUtils {
 
     public static void isDuplicatedStation(String input) {
         if (StationRepository.stations().stream()
-            .anyMatch(station -> station.getName().equals(input))){
+                .anyMatch(station -> station.getName().equals(input))) {
             throw new SubwayException(EXISTING_STATION_MESSAGE);
         }
+    }
+
+
+    public static void isDuplicatedLine(String input) {
+        if (LineRepository.lines().stream()
+                .anyMatch(line -> line.getName().equals(input))) {
+            throw new SubwayException(EXISTING_LINE_MESSAGE);
+        }
+    }
+
+    public static void isDuplicatedUpDownStation(Station upwardStation, Station downStation) {
+        if (upwardStation.equals(downStation)) {
+            throw new SubwayException(SAME_NAME_UP_DOWN_MESSAGE);
+        }
+
     }
 }
