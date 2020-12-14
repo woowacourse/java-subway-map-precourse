5b0074035acf0fb33300a83a8ba8d127f18fa5e7
DH Lim <dlarkfl2@gamil.com>
2020-12-14 12:39:09 +0900

    feat(Application): 로직 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,16 @@
 package subway;
 
+import subway.controller.SubwayMap;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        DummyDataSetting.run();
+
+        SubwayMap subwayMap = new SubwayMap();
+        subwayMap.Run(scanner);
     }
 }
