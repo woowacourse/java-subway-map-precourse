c8867207c4c3c9afe3bebae9cf8b2ad5d3010dd1
operasystem6 <operasystem6@naver.com>
2020-12-14 19:35:47 +0900

    refactor: 구간 기능을 위한 구간 관련 기능 추가 (등록, 삭제)

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,6 +1,10 @@
 package subway.domain;
 
+import subway.exception.SubwayException;
+import subway.repository.LineRepository;
 import subway.repository.StationRepository;
+import subway.utils.ValidateUtils;
+import subway.view.TextCollection;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,17 +17,32 @@ public class Line {
         this.name = name;
     }
 
+    public void addSection(Station station) {
+        if (StationRepository.stations().contains(station)) {
+            sections.add(station);
+        }
+    }
+
+    public void addSectionWithPosition(int position, Station station) {
+        if (StationRepository.stations().contains(station)) {
+            ValidateUtils.isPositionOutOfLine(sections, position);
+            sections.add(position - 1, station);
+        }
+    }
+
+    public void deleteSection(Station inputStation) {
+        Line line = LineRepository.searchLine(name);
+        ValidateUtils.isAtLeastTwoStations(line);
+        if (!(sections.removeIf(station -> inputStation == station))) {
+            throw new SubwayException(TextCollection.NOT_EXIST_SECTION_MESSAGE);
+        }
+    }
+
     public String getName() {
         return name;
     }
 
     public List<Station> getSections() {
         return sections;
-    }
-
-    public void addSection(Station station) {
-        if (StationRepository.stations().contains(station)) {
-            sections.add(station);
-        }
     }
 }
