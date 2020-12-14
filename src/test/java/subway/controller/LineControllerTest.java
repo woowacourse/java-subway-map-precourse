2c461440147ab127dad3034bbf1923240431c3ec
ecsimsw <ecsimsw@gmail.com>
2020-12-14 22:02:15 +0900

    refactor : 초기 데이터 설정 기능 분리 (DummyData)

--- src/test/java/subway/controller/LineControllerTest.java
+++ src/test/java/subway/controller/LineControllerTest.java
@@ -1,48 +1,16 @@
 package subway.controller;
 
 import org.junit.jupiter.api.Test;
+import subway.DummyData;
 import subway.domain.*;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LineControllerTest {
-    private static final String[] initialStation = new String[]{"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
-    private static final String[] initialLine = new String[]{"2호선", "3호선", "신분당선"};
-
-    private void initialize() {
-        List<Station> stations = Arrays.stream(initialStation)
-                .map(Name::new)
-                .map(Station::create)
-                .collect(Collectors.toList());
-
-        List<Name> lineNames = Arrays.stream(initialLine)
-                .map(Name::new)
-                .collect(Collectors.toList());
-
-        stations.stream().forEach(StationRepository::addStation);
-
-        Line ihoseon = Line.create(lineNames.get(0), stations.get(0), stations.get(2));
-        ihoseon.addStation(new Order(1), stations.get(1));
-
-        Line samhoseon = Line.create(lineNames.get(1), stations.get(0), stations.get(6));
-        samhoseon.addStation(new Order(1), stations.get(3));
-        samhoseon.addStation(new Order(2), stations.get(4));
-
-        Line sinboondang = Line.create(lineNames.get(2), stations.get(1), stations.get(5));
-        sinboondang.addStation(new Order(1), stations.get(4));
-
-        LineRepository.addLine(ihoseon);
-        LineRepository.addLine(samhoseon);
-        LineRepository.addLine(sinboondang);
-    }
 
     @Test
     void addLine() {
-        initialize();
+        DummyData.initialize();
 
         int beforeSize = LineRepository.lines().size();
         Station start = StationRepository.getByName(new Name("매봉역"));
@@ -54,7 +22,7 @@ class LineControllerTest {
 
     @Test
     void addLine_Expect_Exception() {
-        initialize();
+        DummyData.initialize();
 
         // 중복 노선 이름 불가능
         assertThrows(Exception.class, () -> {
@@ -80,7 +48,7 @@ class LineControllerTest {
 
     @Test
     void deleteLine() {
-        initialize();
+        DummyData.initialize();
 
         int beforeSize = LineRepository.lines().size();
         LineRepository.remove(LineRepository.getByName(new Name("3호선")));
@@ -90,7 +58,7 @@ class LineControllerTest {
 
     @Test
     void deleteLine_Expect_Exception() {
-        initialize();
+        DummyData.initialize();
 
         // 존재하지 않는 노선 제거 불가능
         assertThrows(Exception.class, () -> {
