a37110bca200f3e59a95aad781a50dd8b71b5746
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2020-12-14 16:22:30 +0900

    refactor(DomainIsNotExistedException) : 메세지 수정

--- src/main/java/subway/exception/DomainIsNotExistedException.java
+++ src/main/java/subway/exception/DomainIsNotExistedException.java
@@ -1,7 +1,7 @@
 package subway.exception;
 
 public class DomainIsNotExistedException extends Exception {
-    private static final String message = "해당 도메인은 존재하지 않습니다";
+    private static final String message = "[ERROR] 해당 도메인은 존재하지 않습니다";
 
     @Override
     public String getMessage() {
