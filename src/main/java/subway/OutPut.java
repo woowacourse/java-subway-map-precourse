2b6e9e9a9443ef155e6daeadb2d0515284da35c6
mungto <daum7766@naver.com>
2020-12-14 16:05:34 +0900

    feat(OutPut): create StationNameDuplicateError Function

--- src/main/java/subway/OutPut.java
+++ src/main/java/subway/OutPut.java
@@ -6,6 +6,7 @@ public class OutPut {
     public static final String ERROR = "[ERROR] ";
     private static final String STATION_NAME_LENGTH_ERROR = "지하철 역의 이름은 2글자 이상이여야 합니다.\n";
     private static final String STATION_CREATE_MESSAGE = "지하철 역이 등록되었습니다.\n";
+    private static final String STATION_NAME_DUPLICATE_ERROR = "지하철 역의 이름은 중복될 수 없습니다.\n";
 
     public static void printStationNameLengthError() {
         System.out.println(ERROR + STATION_NAME_LENGTH_ERROR);
@@ -13,6 +14,10 @@ public class OutPut {
 
     public static void printStationCreateMessage() {
         System.out.println(INFO + STATION_CREATE_MESSAGE);
+    }
+
+    public static void printStationNameDuplicateError() {
+        System.out.println(ERROR + STATION_NAME_DUPLICATE_ERROR);
     }
 
 }
