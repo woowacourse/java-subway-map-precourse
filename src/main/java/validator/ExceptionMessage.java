aa0d6549fb98162b9557173ec816c0119f723701
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-14 21:15:32 +0900

    feat: 매직코드 상수로 모음

--- src/main/java/validator/ExceptionMessage.java
+++ src/main/java/validator/ExceptionMessage.java
@@ -7,6 +7,9 @@ public class ExceptionMessage {
     public static final String NOT_EXIST_DELETE_STATION = "[ERROR] 삭제할 역이 존재하지 않습니다.";
     public static final String LINE_NAME_OVER_TWO = "[ERROR] 노선 이름은 2글자 이상이여야 합니다.";
     public static final String LINE_NAME_EXISTS = "[ERROR] 이미 등록된 노선 이름입니다.";
-    public static final String NOT_EXIST_LINE = "[ERROR] 삭제할 노선이 존재하지 않습니다.";
-    public static final String NOT_EXIST_STATION_FOR_LINE = "[ERROR] 등록할 역이 존재하지 않습니다.";
+    public static final String NOT_EXIST_LINE = "[ERROR] 노선이 존재하지 않습니다.";
+    public static final String NOT_EXIST_STATION = "[ERROR] 역이 존재하지 않습니다.";
+    public static final String SAME_STATION_IN_LINE = "[ERROR] 노선에 같은 역이 존재합니다";
+    public static final String NOT_VALID_ORDER = "[ERROR] 넣을 수 없는 구간 순서 입니다";
+    public static final String DO_NOT_DELETE_SECTION = "[ERROR] 노선에 구간이 2개 이하 이므로 구간을 삭제할 수 없습니다.";
 }
