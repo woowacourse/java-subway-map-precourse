b90163ecdf1071f8b0e82193cb0f03a3bc3fb53b
Beenie93 <seongbeen93@naver.com>
2020-12-14 22:29:11 +0900

    refactor(Apllication): 초기 설정 및 프로그램 실행 구문 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,17 @@
 package subway;
 
+import subway.controller.MainController;
+import subway.service.InitializationService;
+import subway.view.outputview.MainOutputView;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        MainOutputView mainOutputView = new MainOutputView();
+        MainController mainController = new MainController(scanner, mainOutputView);
+        InitializationService.init();
+        mainController.run();
     }
 }
