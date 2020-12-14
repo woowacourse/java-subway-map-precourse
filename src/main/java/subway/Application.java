04e850b4ae594790a0bce40c9fb2f97d29c9aa5a
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-14 23:41:18 +0900

    refactor: 클래스 이름 변경

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -6,8 +6,7 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
-        Subway subway = new Subway();
+        SubwayMap subway = new SubwayMap();
         subway.run(scanner);
-
     }
 }
---
+++ src/main/java/subway/SubwayMap.java
@@ -0,0 +1,12 @@
+package subway;
+
+import subway.manager.MainManager;
+import java.util.Scanner;
+
+public class SubwayMap {
+
+    public void run(Scanner scanner) {
+        MainManager mainManager = new MainManager();
+        mainManager.run(scanner);
+    }
+}
