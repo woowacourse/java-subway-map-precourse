9d870482f1669966505139c7c2f083ff1d00bf58
박우림 <woorimprog@gmail.com>
2020-12-15 01:59:56 +0900

    refactor(view) : add the number-constants for the selector-number-string

--- src/main/java/subway/util/View.java
+++ src/main/java/subway/util/View.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 import subway.Subway;
 
 public class View {
+	private final String FIRST = "1";
+	private final String SECOND = "2";
+	private final String THIRD = "3";
+	private final String FOURTH = "4";
+	private final String QUITE = "Q";
+	private final String BACK = "B";
+	
 	private Scanner scanner;
 	private String selector;
 	
@@ -15,7 +22,7 @@ public class View {
 	public void main() {
 		Output.mainView();
 		selector = Input.nextLine(scanner);
-		if (selector.equals("Q")) {
+		if (selector.equals(QUITE)) {
 			Output.print(Message.EXIT);
 			System.exit(0);
 		}
@@ -23,16 +30,16 @@ public class View {
 	}
 	
 	private void moveManagementViewBySelector(String selector) {
-		if (selector.equals("1")) {
+		if (selector.equals(FIRST)) {
 			stationManagement();
 			return;
-		} else if (selector.equals("2")) {
+		} else if (selector.equals(SECOND)) {
 			lineManagement();
 			return;
-		} else if (selector.equals("3")) {
+		} else if (selector.equals(THIRD)) {
 			sectionManagement();
 			return;
-		} else if (selector.equals("4")) {
+		} else if (selector.equals(FOURTH)) {
 			Output.title(Message.SUBWAY_LINE_VIEW);
 			Subway.show();
 			return;
@@ -43,7 +50,7 @@ public class View {
 	private void stationManagement() {
 		Output.stationManagement();
 		selector = Input.nextLine(scanner);
-		if (selector.equals("B")) {
+		if (selector.equals(BACK)) {
 			Output.print(Message.BACK_TO_MAIN);
 			return;
 		}
@@ -51,13 +58,13 @@ public class View {
 	}
 	
 	private void moveStationViewBySelector(String selector) {
-		if (selector.equals("1")) {
+		if (selector.equals(FIRST)) {
 			ViewManager.createStation();
 			return;
-		} else if (selector.equals("2")) {
+		} else if (selector.equals(SECOND)) {
 			ViewManager.removeStation();
 			return;
-		} else if (selector.equals("3")) {
+		} else if (selector.equals(THIRD)) {
 			Subway.readStation();
 			return;
 		}
@@ -67,7 +74,7 @@ public class View {
 	private void lineManagement() {
 		Output.lineManagement();
 		selector = Input.nextLine(scanner);
-		if (selector.equals("B")) {
+		if (selector.equals(BACK)) {
 			Output.print(Message.BACK_TO_MAIN);
 			return;
 		}
@@ -75,13 +82,13 @@ public class View {
 	}
 	
 	private void moveLineViewBySelector(String selector) {
-		if (selector.equals("1")) {
+		if (selector.equals(FIRST)) {
 			ViewManager.createLine();
 			return;
-		} else if (selector.equals("2")) {
+		} else if (selector.equals(SECOND)) {
 			ViewManager.removeLine();
 			return;
-		} else if (selector.equals("3")) {
+		} else if (selector.equals(THIRD)) {
 			Subway.readLine();
 			return;
 		}
@@ -91,7 +98,7 @@ public class View {
 	private void sectionManagement() {
 		Output.sectionManagement();
 		selector = Input.nextLine(scanner);
-		if (selector.equals("B")) {
+		if (selector.equals(BACK)) {
 			Output.print(Message.BACK_TO_MAIN);
 			return;
 		}
@@ -99,10 +106,10 @@ public class View {
 	}
 	
 	private void moveSectionViewBySelector(String selector) {
-		if (selector.equals("1")) {
+		if (selector.equals(FIRST)) {
 			ViewManager.createSection();
 			return;
-		} else if (selector.equals("2")) {
+		} else if (selector.equals(SECOND)) {
 			ViewManager.removeSection();
 			return;
 		}
