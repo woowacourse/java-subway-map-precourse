8f5bb4b79046a51a0ebffeecc59400140d489662
park seok chae <tjrco56@naver.com>
2020-12-14 16:42:00 +0900

    feat(SectionRemovalView): 노선이름과 역이름을 입력으로 받아 구간 삭제하는 기능 추가

--- src/main/java/subway/view/SectionRemovalView.java
+++ src/main/java/subway/view/SectionRemovalView.java
@@ -1,10 +1,32 @@
 package subway.view;
 
+import java.util.Scanner;
+
+import subway.domain.Line;
+import subway.domain.LineRepository;
+
 public class SectionRemovalView extends ManagerView {
-	private static final String TOP_MENU_MESSAGE = "삭제할 구간의 노선을 입력하세요.";
+	private static final String LINE_INPUT_MESSAGE = "노선을 입력하세요.";
+	private static final String STATION_NAME_INPUT_MESSAGE = "역이름을 입력하세요.";
+
+	public SectionRemovalView(Scanner scanner) {
+		print();
+		removeSection(scanner);
+		new MainView(scanner);
+	}
+
+	public void removeSection(Scanner scanner) {
+		String lineName = scanner.nextLine();
+		Line line = LineRepository.getLineByName(lineName);
+
+		printTopMenu(STATION_NAME_INPUT_MESSAGE);
+		String stationName = scanner.nextLine();
+
+		line.removeStationByName(stationName);
+	}
 
 	@Override
 	public void print() {
-		printTopMenu(TOP_MENU_MESSAGE);
-	}	
+		printTopMenu(LINE_INPUT_MESSAGE);
+	}
 }
