be9f1fc5f10564a3be66a7c3b5cd8d516cb64008
KJunseo <max9106@naver.com>
2020-12-14 22:05:34 +0900

    refactor: 사용되지 않는 상수 제거

--- src/main/java/subway/station/StationRepository.java
+++ src/main/java/subway/station/StationRepository.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Objects;
 
 public class StationRepository {
-    private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String NOT_EXIST = ERROR_PREFIX + "등록되지 않은 역입니다.";
     private static final List<Station> stations = new ArrayList<>();
 
     public static List<Station> stations() {
