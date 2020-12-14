e4548d1b8480eda3967a11130bf21dde9b227986
ecsimsw <ecsimsw@gmail.com>
2020-12-14 22:21:42 +0900

    feat : 역 삭제 테스트 코드 작성

--- src/test/java/subway/controller/StationControllerTest.java
+++ src/test/java/subway/controller/StationControllerTest.java
@@ -27,4 +27,31 @@ class StationControllerTest {
             StationRepository.addStation(Station.create(new Name("교대역")));
         });
     }
+
+    @Test
+    void deleteStation() {
+        DummyData.initialize();
+
+        StationRepository.addStation(Station.create(new Name("죽전역")));
+        int beforeSize = StationRepository.getStationNames().size();
+        StationRepository.remove(StationRepository.getByName(new Name("죽전역")));
+        int afterSize = StationRepository.getStationNames().size();
+        assertEquals(beforeSize - 1, afterSize);
+    }
+
+    @Test
+    void deleteStation_Expect_Exception() {
+        DummyData.initialize();
+
+        // 노선에 추가된 역 삭제 불가능
+        assertThrows(Exception.class, () -> {
+            Station station = StationRepository.getByName(new Name("교대역"));
+            StationRepository.remove(station);
+        });
+
+        // 존재하지 않는 역 삭제 불가능
+        assertThrows(Exception.class, () -> {
+            StationRepository.remove(StationRepository.getByName(new Name("죽전역")));
+        });
+    }
 }
