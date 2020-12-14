36c7ff70bcc9b70122252ec7ef82431e4781eebb
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-14 18:29:14 +0900

    feat : 메인메뉴를 출력하는 반복하는 기능 추가
    todo : Enum의 값으로 동적으로 메뉴를 출력하도록 수정 예정

--- src/main/java/subway/controller/SubwayController.java
+++ src/main/java/subway/controller/SubwayController.java
@@ -2,10 +2,13 @@ package subway.controller;
 
 import subway.menu.MainMenu;
 import subway.view.InputView;
+import subway.view.OutputView;
 
 public class SubwayController {
   public static void run() {
       while(true){
+          //TODO : Menu enum에서 값을 추출하여 동적으로 메뉴를 출력하도록 수정
+          OutputView.showMainMenu();
           MainMenu.findByCommand(InputView.nextLine()).run();
       }
   }
--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,6 +1,19 @@
 package subway.view;
 
 public class OutputView {
+    public static void showMainMenu(){
+        StringBuffer mainMenus = new StringBuffer();
+        mainMenus.append("## 메인화면").append(System.lineSeparator())
+                .append("1. 역 관리").append(System.lineSeparator())
+                .append("2. 노선 관리").append(System.lineSeparator())
+                .append("3. 구간 관리").append(System.lineSeparator())
+                .append("4. 지하철 노선도 출력").append(System.lineSeparator())
+                .append("Q. 종료").append(System.lineSeparator())
+                .append("---");
+
+        System.out.println(mainMenus.toString());
+    }
+    
     public static void println(String message){
         System.out.println(message);
     }
