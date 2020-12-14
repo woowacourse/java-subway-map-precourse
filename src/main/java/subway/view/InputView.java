555931a21f3d28989a49f19ec6776bc403263eee
TaewanKimmmm <ktww88@gmail.com>
2020-12-15 02:43:19 +0900

    fix: 예외처리 버그 수정

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -6,21 +6,21 @@ public class InputView {
     private static final String SELECT_MESSAGE = "## 원하는 기능을 선택하세요.";
     private static final String CANNOT_SELECT_MESSAGE = "[ERROR] 선택할 수 없는 기능입니다.";
     private static final char ONE = '1';
+    private static final char THREE = '3';
     private static final char FOUR = '4';
     private static final char QUIT = 'Q';
 
     private InputView() {
     }
 
-    public static void getUserSelection(Scanner scanner) {
+    public static void getMainScreenUserSelection(Scanner scanner) {
         System.out.println(SELECT_MESSAGE);
         String userInput = scanner.nextLine();
-
         try {
             validateUserInput(userInput);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            getUserSelection(scanner);
+            getMainScreenUserSelection(scanner);
         }
     }
 
@@ -33,7 +33,7 @@ public class InputView {
     }
 
     private static void validateUserInputLength(String userInput) {
-        if (userInput.length() == 0) {
+        if (userInput.length() != 1) {
             throw new IllegalArgumentException(CANNOT_SELECT_MESSAGE);
         }
     }
