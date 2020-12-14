f2fd72ff1c1958c8efa7f4cba63bdc07af7c55c3
seunghyun <ggyool@naver.com>
2020-12-14 19:52:16 +0900

    feat: Line 객체 생성과 유효성을 검사하는 기능 추가

--- src/main/java/subway/domain/line/Line.java
+++ src/main/java/subway/domain/line/Line.java
@@ -1,10 +1,27 @@
 package subway.domain.line;
 
+import subway.domain.station.Station;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Line {
     private String name;
+    private List<Station> sections;
 
     public Line(String name) {
+    private Line(String name, List<Station> sections) {
         this.name = name;
+        this.sections = sections;
+    }
+
+    public static Line createLine(String name, String upwardEndStationName, String downwardEndStationName) {
+        LineValidator.validate(name, upwardEndStationName, downwardEndStationName);
+        List<Station> sections = new ArrayList<>();
+        sections.add(new Station(upwardEndStationName));
+        sections.add(new Station(downwardEndStationName));
+        return new Line(name, sections);
     }
 
     public String getName() {
---
+++ src/main/java/subway/domain/line/LineValidator.java
@@ -0,0 +1,57 @@
+package subway.domain.line;
+
+import java.util.regex.Pattern;
+
+public class LineValidator {
+    private static final int MIN_NAME_LENGTH = 2;
+    private static final int MAX_NAME_LENGTH = 100;
+    private static final String TILDE = "~";
+    private static final String SPACE = " ";
+    private static final String OPEN_BRACKET = "(";
+    private static final String CLOSE_BRACKET = ")";
+    private static final String NAME_LENGTH_ERROR_MESSAGE
+            = "노선의 이름은 " + MIN_NAME_LENGTH + TILDE + MAX_NAME_LENGTH + " 글자여야 합니다.";
+    private static final String NAME_LETTER_TYPE_ERROR_MESSAGE
+            = "노선의 이름은 한글, 영어, 숫자, 공백만을 포함해야 합니다.";
+    private static final String NAME_SPACE_ERROR_MESSAGE
+            = "노선의 이름에는 공백이 2자 연속으로 올 수 없습니다.";
+    private static final String SAME_END_STATION_ERROR_MESSAGE
+            = "상행 종점역과 하행 종점역은 같을 수 없습니다.";
+
+    static void validate(String name, String upwardEndStationName, String downwardEndStationName) {
+        validateNameLength(name);
+        validateNameLetterType(name);
+        validateNameContainsTwoOrMoreSpace(name);
+        validateEndStationDifference(upwardEndStationName, downwardEndStationName);
+    }
+
+    private static void validateNameLength(String name) {
+        if (name.length() < MIN_NAME_LENGTH || MAX_NAME_LENGTH < name.length()) {
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE + getNameWithBrackets(name));
+        }
+    }
+
+    private static void validateNameLetterType(String name) {
+        String regex = "^[0-9a-zA-Z가-힣\\s]*$";              // 한글,영어,숫자,공백 허용
+
+        if (!Pattern.matches(regex, name)) {
+            throw new IllegalArgumentException(NAME_LETTER_TYPE_ERROR_MESSAGE + getNameWithBrackets(name));
+        }
+    }
+
+    private static void validateNameContainsTwoOrMoreSpace(String name) {
+        if (name.contains(SPACE + SPACE)) {
+            throw new IllegalArgumentException(NAME_SPACE_ERROR_MESSAGE + getNameWithBrackets(name));
+        }
+    }
+
+    private static void validateEndStationDifference(String upwardEndStationName, String downwardEndStationName) {
+        if (upwardEndStationName.equals(downwardEndStationName)) {
+            throw new IllegalArgumentException(SAME_END_STATION_ERROR_MESSAGE + getNameWithBrackets(upwardEndStationName));
+        }
+    }
+
+    private static String getNameWithBrackets(String name) {
+        return SPACE + OPEN_BRACKET + name + CLOSE_BRACKET;
+    }
+}
