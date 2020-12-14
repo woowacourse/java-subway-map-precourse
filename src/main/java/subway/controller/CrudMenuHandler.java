33cd67ab20bf123e22ae853f5eca8e7108507365
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-14 18:32:13 +0900

    feat : 세부 메뉴를 출력하기 위한 밑작업
    메뉴 별 세부내용인 add, delete, show 메뉴를 출력하기 위한 CrudMenu 추가
    해당 메뉴 호출 시 동작을 정의한 CrudMenuHandler 추가

---
+++ src/main/java/subway/controller/CrudMenuHandler.java
@@ -0,0 +1,12 @@
+package subway.controller;
+
+public class CrudMenuHandler {
+    public static void add(){
+    }
+
+    public static void delete(){
+    }
+
+    public static void show(){
+    }
+}
