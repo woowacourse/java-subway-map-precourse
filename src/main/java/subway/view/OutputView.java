4c5aa12684d4c3faed436989ddd777c313533adb
JungYoonSung <ybook2012@gmail.com>
2020-12-14 16:17:22 +0900

    feat : 역 관리 화면 출력 기능

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -12,6 +12,17 @@ public class OutputView {
         functionSelectView();
     }
 
+    public static void stationManageView(){
+        System.out.println();
+        System.out.println("## 역 관리 화면");
+        System.out.println("1. 역 등록");
+        System.out.println("2. 역 삭제");
+        System.out.println("3. 역 조회");
+        System.out.println("B. 돌아가기");
+        System.out.println();
+        functionSelectView();
+    }
+
     public static void functionSelectView(){
         System.out.println("## 원하는 기능을 선택하세요.");
     }
