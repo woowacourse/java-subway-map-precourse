ab5020477facc0d6bd8437ae6e2e00d532a31e8a
baekchan1024 <dlqhs1024@naver.com>
2020-12-15 07:30:46 +0900

    feat (InputView) : user station and line input function implementation

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -5,5 +5,34 @@ import java.util.Scanner;
 public class InputView {
 	private static final Scanner scanner = new Scanner(System.in); 
 	
+	public static String getSelectFunction() {
+		System.out.println(OutputView.SELETE_FUNCTION);
+		String select = scanner.nextLine();
+		return select;
+	}
+	
+	public static String getRegisterStation() {
+		System.out.println(OutputView.REGISTER_STATION);
+		String name = scanner.nextLine();
+		return name;
+	}
+	
+	public static String getRegisterLine() {
+		System.out.println(OutputView.REGISTER_LINE);
+		String name = scanner.nextLine();
+		return name;
+	}
+	
+	public static String getDeleteStation() {
+		System.out.println(OutputView.DELETE_STATION);
+		String name = scanner.nextLine();
+		return name;
+	}
+	
+	public static String getDeleteLine() {
+		System.out.println(OutputView.DELETE_LINE);
+		String name = scanner.nextLine();
+		return name;
+	}
 	
 }
