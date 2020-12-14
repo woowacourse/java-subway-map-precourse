d35c0d15ce3bb8899232eef2e39ca77e631a94c4
JungYoonSung <ybook2012@gmail.com>
2020-12-14 16:18:07 +0900

    feat : 역 이름 중복 확인 기능, 메인 메뉴 인풋 검증 기능

--- src/main/java/subway/utils/Validator.java
+++ src/main/java/subway/utils/Validator.java
@@ -1,8 +1,30 @@
 package subway.utils;
 
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Validator {
     private static final int NAME_MINIMUM_LENGTH = 2;
-    public static boolean isValidName(String name){
+
+    public static boolean isValidNameLength(String name) {
         return name.length() >= NAME_MINIMUM_LENGTH;
+    }
+
+    public static boolean isNonDuplicateStationName(String name) {
+        List<Station> stationList = StationRepository.stations();
+        for (Station station : stationList) {
+            String stationName = station.getName();
+            if (stationName.equals(name)) return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidMainMenuInput(String inputMsg) {
+        List<String> validMainMsgList = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "Q"));
+        return validMainMsgList.contains(inputMsg);
     }
 }
