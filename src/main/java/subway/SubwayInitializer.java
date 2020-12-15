5fc53876d937408afbb5b621d010e07508c29f40
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-15 19:17:30 +0900

    refactor : 시작시 데이터 세팅하는 기능 리팩토링
    - 역들의 이름 리스트와 역이름 및 순서를 담은 csvData로 초기데이터를 입력할 수 있도록 리팩토링

--- src/main/java/subway/SubwayInitializer.java
+++ src/main/java/subway/SubwayInitializer.java
@@ -1,29 +1,55 @@
 package subway;
 
-import subway.domain.SectionRepository;
+import subway.domain.StationFactory;
+import subway.domain.StationRepository;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static subway.domain.LineFactory.makeLine;
 import static subway.domain.LineRepository.addLine;
 import static subway.domain.LineRepository.findLine;
-import static subway.domain.StationFactory.makeStation;
-import static subway.domain.StationRepository.addStation;
+import static subway.domain.SectionRepository.addSection;
 import static subway.domain.StationRepository.findStationByName;
 
 public class SubwayInitializer {
-    public static void init(){
-        addStation(makeStation("교대역"));
-        addStation(makeStation("강남역"));
-        addStation(makeStation("역삼역"));
-        addStation(makeStation("남부터미널역"));
-        addStation(makeStation("양재역"));
-        addStation(makeStation("양재시민의숲역"));
-        addStation(makeStation("매봉역"));
-        addLine(makeLine("2호선", findStationByName("교대역"), findStationByName("역삼역")));
-        addLine(makeLine("3호선", findStationByName("교대역"), findStationByName("매봉역")));
-        addLine(makeLine("신분당선", findStationByName("강남역"), findStationByName("양재시민의숲역")));
-        SectionRepository.addSection(findLine("2호선"), findStationByName("강남역"),1);
-        SectionRepository.addSection(findLine("3호선"), findStationByName("양재역"),1);
-        SectionRepository.addSection(findLine("3호선"), findStationByName("남부터미널역"),1);
-        SectionRepository.addSection(findLine("신분당선"), findStationByName("양재역"),1);
+    public static final String COMMA = ",";
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+
+    public static void init() {
+        List<String> stations = Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역");
+        List<String> lineAndSequence = Arrays.asList(
+                "2호선,교대역,강남역,역삼역"
+                , "3호선,교대역,남부터미널역,양재역,매봉역"
+                , "신분당선,강남역,양재역,양재시민의숲역"
+        );
+        makeStations(stations);
+        makeLinesFromCsvData(lineAndSequence);
+    }
+
+    private static void makeLinesFromCsvData(List<String> lineAndSequence) {
+        lineAndSequence.stream()
+                .map(csvData -> Arrays.asList(csvData.split(COMMA)))
+                .forEach(SubwayInitializer::makeLines);
+    }
+
+    private static void makeLines(List<String> lineAndStations) {
+        String lineName = lineAndStations.get(ZERO);
+        addLine(makeLine(lineName,
+                findStationByName(lineAndStations.get(ONE)),
+                findStationByName(lineAndStations.get(TWO))));
+
+        for (int i = THREE; i < lineAndStations.size(); i++) {
+            addSection(findLine(lineName), findStationByName(lineAndStations.get(i)), i - 1);
+        }
+    }
+
+    private static void makeStations(List<String> stations) {
+        stations.stream()
+                .map(StationFactory::makeStation)
+                .forEach(StationRepository::addStation);
     }
 }
