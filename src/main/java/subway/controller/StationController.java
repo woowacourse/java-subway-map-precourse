dd5c2f7da8e2118da6e69d4697572d7065564b35
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-15 17:00:18 +0900

    feat(StationController): 역 관리 화면 메뉴(등록, 삭제, 조회, 돌아가기)  선택 시 동작되는 기능 구현

--- src/main/java/subway/controller/StationController.java
+++ src/main/java/subway/controller/StationController.java
@@ -1,6 +1,14 @@
 package subway.controller;
 
+import subway.domain.Station;
+import subway.domain.StationRepository;
+import subway.view.InputView;
+import subway.view.OutputViewOfError;
+import subway.view.OutputViewOfInfo;
+
 public class StationController {
+    private static final String STATION_LIST_MESSAGE = "## 역 목록";
+
     private static StationController stationController;
 
     public static StationController getInstance() {
@@ -12,14 +20,35 @@ public class StationController {
 
 
     public Boolean registerStation() {
+        String name = InputView.registerStationName();
+        Station station = new Station(name);
+        boolean result = StationRepository.addStation(station);
+        if (result) {
+            OutputViewOfInfo.registerStationComplete();
+        }
+        return result;
     }
 
     public Boolean deleteStation() {
+        String name = InputView.deleteStationName();
+        boolean result = StationRepository.deleteStation(name);
+        if (result) {
+            OutputViewOfInfo.deleteStationComplete();
+        }
+        if (!result) {
+            OutputViewOfError.cannotDeleteError();
+        }
+        return result;
     }
 
     public Boolean inquiryStation() {
+        System.out.println(STATION_LIST_MESSAGE);
+        StationRepository.printStationList();
+        System.out.println();
+        return true;
     }
 
     public Boolean back() {
+        return true;
     }
 }
