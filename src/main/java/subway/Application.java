2dc31605b97f8b613a6403f2f3c63b773e4cb5c7
xrabcde <joara124@ewhain.net>
2020-12-15 18:09:05 +0900

    feat(Application): 메인화면 기능 목록과 입력 안내문구 출력 및 메인기능 입력 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -13,6 +13,23 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         initialize();
+        showMainMenu();
+        System.out.println(inputMainFunction(scanner));
+    }
+
+    public static void showMainMenu() {
+        System.out.println("## 메인 화면");
+        System.out.println("1. 역 관리");
+        System.out.println("2. 노선 관리");
+        System.out.println("3. 구간 관리");
+        System.out.println("4. 지하철 노선도 출력");
+        System.out.println("Q. 종료");
+        System.out.println("\n## 원하는 기능을 선택하세요.");
+    }
+
+    public static int inputMainFunction(Scanner kbd) {
+        int input = kbd.nextInt();
+        return input;
     }
 
     public static void initialize() {
