4252c49fa4b3a54cdbf983fbda4a1815bf210fe9
Gomding <backchida@gmail.com>
2020-12-15 23:20:41 +0900

    refactor : SubwayMapMenu 메소드 분리

--- src/main/java/subway/menu/SubwayMapMenu.java
+++ src/main/java/subway/menu/SubwayMapMenu.java
@@ -8,13 +8,21 @@ public class SubwayMapMenu {
 
     public void printSubwayMap() {
         System.out.println("## 지하철 노선도");
+        printLines();
+        System.out.println();
+    }
+
+    private void printLines() {
         for (Line line : LineRepository.lines()) {
             System.out.println("\n[ INFO ] " + line.getName());
             System.out.println("[ INFO ] ---");
-            for (Station station : line.stationList()) {
-                System.out.println("[ INFO ] " + station.getName());
-            }
+            printStations(line);
         }
-        System.out.println();
+    }
+
+    private void printStations(Line line) {
+        for (Station station : line.stationList()) {
+            System.out.println("[ INFO ] " + station.getName());
+        }
     }
 }
