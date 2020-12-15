8888537d557a09755d38f90998dd81b38033bfef
mungto <daum7766@naver.com>
2020-12-15 22:14:17 +0900

    feat(OutPut): create printLine Function

--- src/main/java/subway/OutPut.java
+++ src/main/java/subway/OutPut.java
@@ -83,4 +83,8 @@ public class OutPut {
     public static void printSectionDeleteMessage() {
         System.out.println(INFO + SECTION_DELETE_MESSAGE);
     }
+
+    public static void printNextLine() {
+        System.out.println();
+    }
 }
