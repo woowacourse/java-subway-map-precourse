832bd365479a3410061c45e8ad66e0939c634668
김주원 <kjw11077@naver.com>
2020-12-15 13:39:22 +0900

    feat(exception): 예외 클래스 추가

--- src/main/java/subway/exception/DuplicatedStationInOneLineException.java
+++ src/main/java/subway/exception/DuplicatedStationInOneLineException.java
@@ -5,6 +5,6 @@ public class DuplicatedStationInOneLineException extends RuntimeException {
     private static final String MESSAGE = "한 노선에 두 개 이상의 역은 존재할 수 없습니다. 다시 입력해주세요. 입력값: (%s)";
 
     public DuplicatedStationInOneLineException(String stationName) {
-        super(MESSAGE + stationName);
+        super(String.format(MESSAGE, stationName));
     }
 }
---
+++ src/main/java/subway/exception/InvalidIndexException.java
@@ -0,0 +1,10 @@
+package subway.exception;
+
+public class InvalidIndexException extends RuntimeException{
+
+    private static final String MESSAGE = "입력한 순서가 구간의 범위를 벗어났습니다. 다시 입력해주세요. 입력값: (%s)";
+
+    public InvalidIndexException(String index) {
+        super(String.format(MESSAGE, index));
+    }
+}
