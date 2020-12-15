daeee610199dbd1850ad499a345bb5d1b99dd0aa
perenok <perenoke@gmail.com>
2020-12-15 22:33:54 +0900

    feat(LineController, LineRepository): implements methods
    예외조건으로 경우를 나눠 예외에 걸릴때마다 각자의 메세지 출력
    모든 조건을 만족하는 경우 등록, 삭제, 조회 진행

--- src/main/java/subway/controller/LineController.java
+++ src/main/java/subway/controller/LineController.java
@@ -1,59 +1,128 @@
 package subway.controller;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 import subway.view.LinePage;
-import subway.view.StationPage;
-import utils.ErrorUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LineController {
-    private static final String ADD_Line = "1";
-    private static final String DELETE_Line = "2";
-    private static final String SELECT_Line = "3";
+    private static final String ADD_LINE = "1";
+    private static final String DELETE_LINE = "2";
+    private static final String SELECT_LINE = "3";
     private static final String BACK = "B";
+    private static final LinePage LINE_PAGE = new LinePage();
 
-    public static void startStationPage(Scanner scanner){
-        LinePage.printLineManagementPage();
+
+    public static void startLinePage(Scanner scanner) {
+        LINE_PAGE.printLineManagementPage();
         forkPath(chooseItem(scanner), scanner);
     }
 
-    public static void forkPath(String item, Scanner scanner){
-        if(ADD_Line.equals(item)){
+    public static void forkPath(String item, Scanner scanner) {
+        if (ADD_LINE.equals(item)) {
             startInsertLine(scanner);
         }
-        if(DELETE_Line.equals(item)){
+        if (DELETE_LINE.equals(item)) {
             startDeleteLine(scanner);
         }
-        if(SELECT_Line.equals(item)){
+        if (SELECT_LINE.equals(item)) {
             startSelectLine(scanner);
         }
-        if(BACK.equals(item)){
+        if (BACK.equals(item)) {
             MainController.startMainPage(scanner);
         }
     }
 
-    public static String chooseItem(Scanner scanner){
+    public static String chooseItem(Scanner scanner) {
         String item = input(scanner);
-        if(!isValidItem(item)){
-            ErrorUtils.printError("잘못된 입력입니다.");
-            startStationPage(scanner);
+        if (!isValidItem(item)) {
+            LINE_PAGE.printWrongItemError();
+            startLinePage(scanner);
         }
         return item;
     }
 
-    public static String input(Scanner scanner){
+    public static String input(Scanner scanner) {
         return scanner.next();
     }
 
-    public static boolean isValidItem(String item){
-        if(ADD_Line.equals(item) || DELETE_Line.equals(item) ||
-                SELECT_Line.equals(item) || BACK.equals(item)){
-            return true;
+    public static boolean isValidItem(String item) {
+        return ADD_LINE.equals(item) || DELETE_LINE.equals(item) ||
+                SELECT_LINE.equals(item) || BACK.equals(item);
+    }
+
+    public static void startInsertLine(Scanner scanner) {
+        LINE_PAGE.printAddLinePage();
+        String lineName = scanner.next();
+        if (isValidName(lineName)) {
+            List<Station> stations = new ArrayList<>();
+            LINE_PAGE.printAddUpperTerminalPage();
+            insertTerminal(stations, scanner);
+            LINE_PAGE.printAddLowerTerminalPage();
+            insertTerminal(stations, scanner);
+            insertLine(lineName, stations, scanner);
         }
+        startLinePage(scanner);
+    }
+
+    public static void insertLine(String lineName, List<Station> stations, Scanner scanner) {
+        Line line = new Line(lineName, stations);
+        LineRepository.addLine(line);
+        LINE_PAGE.printCompleteAdd();
+        MainController.startMainPage(scanner);
+    }
+
+    public static boolean isValidName(String name) {
+        if (LineRepository.isLongName(name)) {
+            if (!LineRepository.isExistName(name)) {
+                return true;
+            }
+            LINE_PAGE.printDuplicateError();
+            return false;
+        }
+        LINE_PAGE.printShortNameError();
         return false;
     }
 
+    public static void insertTerminal(List<Station> stations, Scanner scanner) {
+        String StationName = input(scanner);
+        if (!StationRepository.isExistName(StationName)) {
+            LINE_PAGE.printNullStationError();
+            startLinePage(scanner);
+            return;
+        }
+        Station upperStation = new Station(StationName);
+        stations.add(upperStation);
+    }
 
+    public static void startDeleteLine(Scanner scanner) {
+        LINE_PAGE.printDeleteLinePage();
+        deleteLine(scanner);
+    }
 
+    public static void deleteLine(Scanner scanner) {
+        String LineName = input(scanner);
+        if (LineRepository.isExistName(LineName)) {
+            if (LineRepository.deleteLineByName(LineName)) {
+                LINE_PAGE.printCompleteDelete();
+                MainController.startMainPage(scanner);
+                return;
+            }
+            LINE_PAGE.printFailToDeleteError();
+            startLinePage(scanner);
+            return;
+        }
+        LINE_PAGE.printNullLineError();
+        startLinePage(scanner);
+    }
 
+    public static void startSelectLine(Scanner scanner) {
+        LINE_PAGE.printSelectLinesPage(LineRepository.lines());
+        MainController.startMainPage(scanner);
+    }
 }
--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
@@ -18,5 +19,41 @@ public class LineRepository {
 
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static Line selectOneLineByName(String name) {
+        for (Line line : lines) {
+            if (name.equals(line.getName())) {
+                return line;
+            }
+        }
+        return null;
+    }
+
+    public static void addStationByIndex(int index, Line line, String stationName) {
+        Line target = selectOneLineByName(line.getName());
+        target.addRoute(index, stationName);
+    }
+
+    public static void deleteStationByName(Line line, String stationName) {
+        Line target = selectOneLineByName(line.getName());
+        target.deleteRoute(stationName);
+    }
+
+    public static int countStationNumInLine(Line line) {
+        return line.getStationNumber();
+    }
+
+    public static boolean isDuplicateStationInLine(Line line, String stationName) {
+        return line.isExistStationInLine(stationName);
+    }
+
+    public static boolean isExistName(String name) {
+        return lines.stream().anyMatch(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static boolean isLongName(String name) {
+        String pattern = "^[0-9a-zA-Z가-힣]{2,}$";
+        return Pattern.matches(pattern, name);
     }
 }
