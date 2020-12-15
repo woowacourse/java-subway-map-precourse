4f181574d25a07753742f8ccbef1843cd1ceecbb
KJunseo <max9106@naver.com>
2020-12-15 20:19:12 +0900

    refactor: 노선 찾는 메소드 간소화

--- src/main/java/subway/line/LineService.java
+++ src/main/java/subway/line/LineService.java
@@ -6,9 +6,6 @@ import subway.station.Station;
 import java.util.List;
 
 public class LineService {
-    private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String NOT_EXIST = ERROR_PREFIX + "등록되지 않은 노선입니다.";
-
     public static void addLine(Line line) {
         LineRepository.addLine(line);
     }
@@ -26,11 +23,8 @@ public class LineService {
     }
 
     public static Line findLine(String lineName) {
-        Line line = LineRepository.findByName(lineName);
-        if (line == null) {
-            throw new IllegalArgumentException(NOT_EXIST);
-        }
-        return line;
+        CheckRegisteredLine.validation(lineName);
+        return LineRepository.findByName(lineName);
     }
 
     public static List<Line> allLine() {
