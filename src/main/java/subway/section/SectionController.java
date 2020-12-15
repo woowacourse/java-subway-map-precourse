7ff46f14334c0bf22849bd373676912e0a0d5056
KJunseo <max9106@naver.com>
2020-12-15 14:41:27 +0900

    refactor: 지하철 구간 관리 입출력을 모두 controller에서 담당

--- src/main/java/subway/section/SectionController.java
+++ src/main/java/subway/section/SectionController.java
@@ -1,6 +1,13 @@
 package subway.section;
 
-import subway.common.CommonService;
+import subway.line.Line;
+import subway.line.LineService;
+import subway.line.validation.CheckAlreadyRegisteredStation;
+import subway.line.validation.CheckNotExistStation;
+import subway.line.validation.CheckRightSectionNumber;
+import subway.main.SubwayController;
+import subway.station.Station;
+import subway.station.StationService;
 import subway.view.InputView;
 import subway.view.section.SectionManagementView;
 
@@ -17,7 +24,7 @@ public class SectionController {
 
         while (true) {
             SectionManagementView.showSectionManagementMenu();
-            char option = CommonService.selectOption(optionList, inputView);
+            char option = SubwayController.selectOption(optionList, inputView);
 
             if (option == GO_BACK) {
                 break;
@@ -40,22 +47,66 @@ public class SectionController {
     }
 
     private static boolean addNewSection(InputView inputView) {
-        SectionManagementView.askAddSectionLineName();
-        String lineName = inputView.lineName();
-        boolean success = SectionService.addSection(lineName, inputView);
-        if (success) {
-            SectionManagementView.addSectionComplete();
+        boolean success = false;
+        try {
+            Line line = getAddSectionLine(inputView);
+            Station station = getAddSectionStation(line, inputView);
+            int sectionOrder = getAddSectionOrder(line, inputView);
+            success = SectionService.addSection(line, station, sectionOrder);
+            if (success) {
+                SectionManagementView.addSectionComplete();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
         return success;
     }
 
-    private static boolean deleteSection(InputView inputView) {
-        SectionManagementView.askDeleteSectionLineName();
+    private static Line getAddSectionLine(InputView inputView) {
+        SectionManagementView.askAddSectionLineName();
         String lineName = inputView.lineName();
-        boolean success = SectionService.deleteSection(lineName, inputView);
-        if (success) {
-            SectionManagementView.deleteSectionComplete();
+        return LineService.findLine(lineName);
+    }
+
+    private static Station getAddSectionStation(Line line, InputView inputView) {
+        SectionManagementView.askAddStationName();
+        String stationName = inputView.stationName();
+        CheckAlreadyRegisteredStation.validation(line, stationName);
+        return StationService.findStation(stationName);
+    }
+
+    private static int getAddSectionOrder(Line line, InputView inputView) {
+        SectionManagementView.askStationOrder();
+        String number = inputView.sectionNumber();
+        CheckRightSectionNumber.validation(line, number);
+        return Integer.parseInt(number);
+    }
+
+    private static boolean deleteSection(InputView inputView) {
+        boolean success = false;
+        try {
+            Line line = getDeleteSectionLine(inputView);
+            Station station = getDeleteSectionStation(line, inputView);
+            success = SectionService.deleteSection(line, station);
+            if (success) {
+                SectionManagementView.deleteSectionComplete();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
         return success;
+    }
+
+    private static Line getDeleteSectionLine(InputView inputView) {
+        SectionManagementView.askDeleteSectionLineName();
+        String lineName = inputView.lineName();
+        return LineService.findLine(lineName);
+    }
+
+    private static Station getDeleteSectionStation(Line line, InputView inputView) {
+        SectionManagementView.askDeleteSectionStation();
+        String stationName = inputView.stationName();
+        CheckNotExistStation.validation(line, stationName);
+        return StationService.findStation(stationName);
     }
 }
--- src/main/java/subway/section/SectionService.java
+++ src/main/java/subway/section/SectionService.java
@@ -1,68 +1,21 @@
 package subway.section;
 
 import subway.line.Line;
-import subway.line.LineService;
-import subway.line.validation.CheckAlreadyRegisteredStation;
-import subway.line.validation.CheckNotExistStation;
-import subway.line.validation.CheckRightSectionNumber;
 import subway.station.Station;
-import subway.station.StationService;
-import subway.view.InputView;
-import subway.view.section.SectionManagementView;
 
 public class SectionService {
     private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String NOT_EXIST = ERROR_PREFIX + "등록되지 않은 노선입니다.";
     private static final String STATION_NUMBER_LACK = ERROR_PREFIX + "등록된 역이 2개 이하이므로 삭제할 수 없습니다.";
 
-    public static boolean addSection(String lineName, InputView inputView) {
-        boolean isAdd = false;
-        try {
-            Line line = LineService.findLine(lineName);
-            Station station = getSectionStation(line, inputView);
-            int sectionNumber = getSectionPosition(line, inputView);
-            line.addSection(station, sectionNumber);
-            isAdd = true;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+    public static boolean addSection(Line line, Station station, int order) {
+        line.addSection(station, order);
+        return true;
+    }
+
+    public static boolean deleteSection(Line line, Station station) {
+        if (!line.deleteSection(station)) {
+            throw new IllegalArgumentException(STATION_NUMBER_LACK);
         }
-        return isAdd;
-    }
-
-    private static Station getSectionStation(Line line, InputView inputView) {
-        SectionManagementView.askAddStationName();
-        String stationName = inputView.stationName();
-        CheckAlreadyRegisteredStation.validation(line, stationName);
-        return StationService.findStation(stationName);
-    }
-
-    private static int getSectionPosition(Line line, InputView inputView) {
-        SectionManagementView.askStationOrder();
-        String number = inputView.sectionNumber();
-        CheckRightSectionNumber.validation(line, number);
-        return Integer.parseInt(number);
-    }
-
-    public static boolean deleteSection(String lineName, InputView inputView) {
-        boolean isDelete = false;
-        try {
-            Line line = LineService.findLine(lineName);
-            Station station = getDeleteSectionStation(line, inputView);
-            isDelete = line.deleteSection(station);
-            if (!isDelete) {
-                throw new IllegalArgumentException(STATION_NUMBER_LACK);
-            }
-
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        return isDelete;
-    }
-
-    private static Station getDeleteSectionStation(Line line, InputView inputView) {
-        SectionManagementView.askDeleteSectionStation();
-        String stationName = inputView.stationName();
-        CheckNotExistStation.validation(line, stationName);
-        return StationService.findStation(stationName);
+        return true;
     }
 }
