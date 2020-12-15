ab18007860030539b23ffff68954696810e03db9
Injun Son <ison@sfu.ca>
2020-12-15 19:25:42 +0900

    feat: 노선에 포함된 역이 두개 이하일 때 삭제 예외처리

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -31,12 +31,15 @@ public class Line {
     }
 
     public void insertStation(String stationName, int order) {
-        stationLine.add(order, new Station(stationName));
+        if (order > stationLine.toArray().length + 1) {
+            order = stationLine.toArray().length + 1;
+        }
+        stationLine.add(order-1, new Station(stationName));
     }
 
     public boolean deleteStation(String stationName) {
         for (Station station : stationLine) {
-            if (station.isSameName(stationName)) {
+            if (station.isSameName(stationName) && stationLine.toArray().length > 2) {
                 stationLine.remove(station);
                 return true;
             }
