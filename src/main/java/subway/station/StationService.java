43c4af35c77049f8fbd2e33a02949244547b5836
KJunseo <max9106@naver.com>
2020-12-15 20:19:54 +0900

    refactor: 역 찾는 메소드 간소화

--- src/main/java/subway/station/StationService.java
+++ src/main/java/subway/station/StationService.java
@@ -6,9 +6,6 @@ import subway.station.validation.CheckRegisteredStation;
 import java.util.List;
 
 public class StationService {
-    private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String NOT_EXIST = ERROR_PREFIX + "등록되지 않은 역입니다.";
-
     public static boolean addStation(String stationName) {
         Station station = new Station(stationName);
         StationRepository.addStation(station);
@@ -22,11 +19,8 @@ public class StationService {
     }
 
     public static Station findStation(String stationName) {
-        Station station = StationRepository.findByName(stationName);
-        if (station == null) {
-            throw new IllegalArgumentException(NOT_EXIST);
-        }
-        return station;
+        CheckRegisteredStation.validation(stationName);
+        return StationRepository.findByName(stationName);
     }
 
     public static List<Station> AllStation() {
