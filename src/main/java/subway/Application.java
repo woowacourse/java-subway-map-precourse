729d0373ed2b6b8e8f905f21e1c326a802d28e63
xrabcde <joara124@ewhain.net>
2020-12-15 23:55:43 +0900

    refactor: StationManage 리팩토링

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -21,7 +21,7 @@ public class Application {
     public static void goSubMenu(String input, Scanner kbd) {
         System.out.println();
         if (input.equals(Constants.STATION_MENU))
-            manageStation(kbd);
+            StationManage.manageStation(kbd);
         if (input.equals(Constants.LINE_MENU))
             manageLine(kbd);
         if (input.equals(Constants.SECTION_MENU))
@@ -30,19 +30,6 @@ public class Application {
             View.displayAllLines(kbd);
         if (input.equalsIgnoreCase(Constants.FINISH_PROGRAM))
             finishProgram();
-    }
-
-    public static void manageStation(Scanner kbd) {
-        View.showStationMenu();
-        String input = InputView.inputFunction(kbd, Constants.SUB_FUNCTIONS);
-        if (input.equals(Constants.ADD_MENU))
-            addStation(kbd);
-        if (input.equals(Constants.DELETE_MENU))
-            deleteStation(kbd);
-        if (input.equals(Constants.SEARCH_MENU))
-            searchStation(kbd);
-        if (input.equalsIgnoreCase(Constants.GO_BACK_MENU))
-            startProgram(kbd);
     }
 
     public static void manageLine(Scanner kbd) {
@@ -56,41 +43,6 @@ public class Application {
             searchLine(kbd);
         if (input.equalsIgnoreCase(Constants.GO_BACK_MENU))
             startProgram(kbd);
-    }
-
-    public static void addStation(Scanner kbd) {
-        try {
-            System.out.println("\n## 등록할 역 이름을 입력하세요.");
-            String stationName = kbd.nextLine();
-            Errors.checkSameStation(stationName);
-            Errors.checkTextLength(stationName);
-            StationRepository.addStation(new Station(stationName));
-            System.out.println("\n[INFO] 지하철 역이 등록되었습니다.");
-            startProgram(kbd);
-        } catch (Exception e) {
-            startProgram(kbd);
-        }
-    }
-
-    public static void deleteStation(Scanner kbd) {
-        try {
-            System.out.println("\n## 삭제할 역 이름을 입력하세요.");
-            String stationName = kbd.nextLine();
-            Errors.checkExistStation(stationName);
-            Errors.checkInLine(stationName);
-            StationRepository.deleteStation(stationName);
-            System.out.println("\n[INFO] 지하철 역이 삭제되었습니다.");
-            startProgram(kbd);
-        } catch (Exception e) {
-            startProgram(kbd);
-        }
-    }
-
-    public static void searchStation(Scanner kbd) {
-        System.out.println("\n## 역 목록");
-        for (Station station : StationRepository.stations())
-            System.out.println("[INFO] " + station.getName());
-        startProgram(kbd);
     }
 
     public static void addLine(Scanner kbd) {
