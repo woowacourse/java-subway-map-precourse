e9f5fe248cdceb292110de1442124da6eebd2a77
sakjung <58401309+sakjung@users.noreply.github.com>
2020-12-15 16:55:49 +0900

    docs(README.md): Add additional explanation on programme's entire process flow

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -2,7 +2,10 @@ package subway.domain;
 
 import subway.view.LineMessages;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 public class LineRepository {
 	private static final List<Line> lines = new ArrayList<>();
