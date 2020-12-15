28baaf80767e7d2250c236b838587463d65b9173
yunjung <cairofaith@gmail.com>
2020-12-15 15:55:37 +0900

    fix(IntervalService): 이미 구간으로 등록된 역 재등록 예외처리

--- src/main/java/subway/service/IntervalService.java
+++ src/main/java/subway/service/IntervalService.java
@@ -6,10 +6,20 @@ import subway.domain.Station;
 public class IntervalService {
     private static final int MIN_LINE_SIZE = 2;
     private static final String LINE_UNDER_TWO = "\n[ERROR] 노선에 등록된 역이 2개 이하 입니다.";
+    private static final String DUPLICATED_INTERVAL = "\n[ERROR] 이미 등록되어 있는 구간 입니다.";
 
-    public void registerInterval(Line currLine, Station inputStation, int position) {
+    public void registerInterval(Line currLine, Station inputStation, int position) throws
+            IllegalArgumentException {
+        validateDuplicatedStationInterval(currLine, inputStation);
         currLine.addIntervals(inputStation, position);
         inputStation.addRegisteredLine(currLine);
+    }
+
+    private void validateDuplicatedStationInterval(
+            Line currLine, Station inputStation) throws IllegalArgumentException {
+        if (currLine.intervals().contains(inputStation)) {
+            throw new IllegalArgumentException(DUPLICATED_INTERVAL);
+        }
     }
 
     public void deleteInterval(Line currLine, Station deleteStation) throws
