e2d04f754b16810c8a7743944725e0f5f9bcfb42
amy choi <cgc8016@ewhain.net>
2020-12-15 23:37:37 +0900

    feat(Line): add another generator for line

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,17 +1,39 @@
 package subway.domain;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Line {
-    private String name;
+    private final String name;
+    private Station upwardTerminal;
+    private Station downwardTerminal;
+    private LinkedList<Station> terminals;
 
     public Line(String name) {
         this.name = name;
+        terminals = new LinkedList<>();
+    }
+    public Line(String name, Station upwardTerminal, Station downwardTerminal) {
+        this.name = name;
+        this.upwardTerminal = upwardTerminal;
+        this.downwardTerminal = downwardTerminal;
+        terminals = new LinkedList<Station>(Arrays.asList(upwardTerminal, downwardTerminal));
+
     }
 
     public String getName() {
         return name;
     }
 
-    public void print() {
+    public void printName() {
         System.out.println("[INFO] "+this.name);
+    }
+
+    public void printAllInfo() {
+        printName();
+        System.out.println("---");
+        for(Station terminal : terminals) {
+            terminal.print();
+        }
     }
 }
