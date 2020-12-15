516634d78745401d1b0a9e3f303d3c19ba78d01e
xorud2592 <xorud2592@gmail.com>
2020-12-15 23:26:32 +0900

    노선삭제

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -28,7 +28,9 @@ public class Application {
 	private static final String ERROR_CREATE_LINE_MESSAGE = "[ERROR] 이미 등록된 노선 이름입니다.";
 	private static final String INPUT_START_LINE = "## 등록할 노선의 상행 종점역 이름을 입력하세요.";
 	private static final String INPUT_END_LINE = "## 등록할 노선의 하행 종점역 이름을 입력하세요.";
-	
+	private static final String DELETE_LINE_INPUT = "\n## 삭제할 노선 이름을 입력하세요.";
+	private static final String DELETE_LINE_MESSAGE = "[INFO] 지하철 노선이 삭제되었습니다.";
+	private static final String ERROR_DELETE_LINE_MESSAGE = "[ERROR] 없는 지하철노선 이름입니다.";
 	private static final String ERROR_INPUT_MESSAGE = "[ERROR] 선택할 수 없는 기능입니다.";
 	private static final String END_PROGRAM_MESSAGE = "##프로그램을 종료한다.";
 
@@ -179,7 +181,7 @@ public class Application {
 				return;
 			}
 			if (Select.equals("2")) {
-				deleteStation(scanner, stationRepositiory, lineRepositiory);
+				deleteLine(scanner, lineRepositiory);
 				return;
 			}
 			if (Select.equals("3")) {
@@ -218,9 +220,21 @@ public class Application {
 		System.out.println(ERROR_CREATE_STATION_MESSAGE + "\n");
 	}
 
+	public static void deleteLine(Scanner scanner, LineRepository lineRepositiory)
+	{
+		System.out.println(DELETE_LINE_INPUT);
+		String line = scanner.nextLine();
+		if (lineRepositiory.findStation(line) != null) {
+			lineRepositiory.deleteLineByName(line);
+			System.out.println(DELETE_LINE_MESSAGE + "\n");
+			return;
+		}
+		System.out.println(ERROR_DELETE_LINE_MESSAGE + "\n");
+	}
 	public static void statusLine(LineRepository lineRepositiory) {
 		lineRepositiory.viewLine();
 	}
+	
 	
 	public static void showStatus(LineRepository lineRepositiory) {
 		lineRepositiory.viewLineRepository();
