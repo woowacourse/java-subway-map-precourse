28a7f6bee85dcc6342ba790633c697fc7ac708da
perenok <perenoke@gmail.com>
2020-12-15 22:31:13 +0900

    feat(Applcation): use startProgram method
    생성자 대신 MainController.startProgram(scanner)을 이용해서 실행

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,13 @@
 package subway;
 
+import subway.controller.MainController;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        MainController.startProgram(scanner);
+        scanner.close();
     }
 }
