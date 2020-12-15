9970bae2fe32534e4c19dab8e01b7ffc397bd876
perenok <perenoke@gmail.com>
2020-12-15 22:11:03 +0900

    feat(MaonController): add Constructor and selectRouteMap method
    노선도 출력을 위해 루트맵을 형식에 맞춰 출력하는 메소드 작성
    생성자를 통해 초기설정을 진행
    초기설정 클래스를 호출해서 설정함

--- src/main/java/subway/controller/MainController.java
+++ src/main/java/subway/controller/MainController.java
@@ -1,8 +1,8 @@
 package subway.controller;
 
+import subway.InitialSetting;
+import subway.domain.LineRepository;
 import subway.view.MainPage;
-import subway.view.StationPage;
-import utils.ErrorUtils;
 
 import java.util.Scanner;
 
@@ -12,50 +12,53 @@ public class MainController {
     private static final String ROUTE_MANAGEMENT = "3";
     private static final String ROUTE_MAP = "4";
     private static final String QUIT = "Q";
+    private static final MainPage MAIN_PAGE = new MainPage();
 
+    public MainController(Scanner scanner) {
+        InitialSetting initialSetting = new InitialSetting();
+        startMainPage(scanner);
+    }
 
-    public static void startMainPage(Scanner scanner){
-        showMainPage();
+    public static void startMainPage(Scanner scanner) {
+        MAIN_PAGE.printMainPage();
         forkPath(chooseItem(scanner), scanner);
     }
 
-    public static void showMainPage(){
-        MainPage.printMainPage();
-    }
-
-    public static void forkPath(String item, Scanner scanner){
-        if(STATION_MANAGEMENT.equals(item)){
+    public static void forkPath(String item, Scanner scanner) {
+        if (STATION_MANAGEMENT.equals(item)) {
             StationController.startStationPage(scanner);
         }
-        if(LINE_MANAGEMENT.equals(item)){
-
+        if (LINE_MANAGEMENT.equals(item)) {
+            LineController.startLinePage(scanner);
         }
-        if(ROUTE_MANAGEMENT.equals(item)){
-
+        if (ROUTE_MANAGEMENT.equals(item)) {
+            RouteMapController.startRouteMapPage(scanner);
         }
-        if(ROUTE_MAP.equals(item)){
-
+        if (ROUTE_MAP.equals(item)) {
+            selectRouteMap(scanner);
         }
     }
 
-    public static String chooseItem(Scanner scanner){
+    public static void selectRouteMap(Scanner scanner) {
+        MAIN_PAGE.printRouteMap(LineRepository.lines());
+        startMainPage(scanner);
+    }
+
+    public static String chooseItem(Scanner scanner) {
         String item = input(scanner);
-        if(!isValidItem(item)){
-            ErrorUtils.printError("잘못된 입력입니다.");
+        if (!isValidItem(item)) {
+            MAIN_PAGE.printWrongItemError();
             startMainPage(scanner);
         }
         return item;
     }
 
-    public static String input(Scanner scanner){
+    public static String input(Scanner scanner) {
         return scanner.next();
     }
 
-    public static boolean isValidItem(String input){
-        if(STATION_MANAGEMENT.equals(input) || LINE_MANAGEMENT.equals(input) ||
-           ROUTE_MANAGEMENT.equals(input) || ROUTE_MAP.equals(input) || QUIT.equalsIgnoreCase(input)){
-            return true;
-        }
-        return false;
+    public static boolean isValidItem(String input) {
+        return STATION_MANAGEMENT.equals(input) || LINE_MANAGEMENT.equals(input) ||
+                ROUTE_MANAGEMENT.equals(input) || ROUTE_MAP.equals(input) || QUIT.equalsIgnoreCase(input);
     }
 }
