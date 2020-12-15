c39d5a1c845d98d35f42d1704f05a7db1a5d4446
perenok <perenoke@gmail.com>
2020-12-15 22:16:41 +0900

    feat(InfoUtils): simplify Class
    FORMAT에 맞춰 받은 메세지를 출력하는 기능만 남겨두고 필요할 때 view 클래스에서 출력문장을 받아 출력

--- src/main/java/utils/InfoUtils.java
+++ src/main/java/utils/InfoUtils.java
@@ -1,15 +1,9 @@
-package subway.utils;
+package utils;
 
 public class InfoUtils {
     private static final String FORMAT = "[INFO] %s";
-    private String msg;
 
-    public void print(Object arg){
+    public static void printInfo(Object arg) {
         System.out.println(String.format(FORMAT, arg));
-    }
-
-    public void printAddStation(){
-        msg = "지하철 역이 등록되었습니다";
-        print(msg);
     }
 }
