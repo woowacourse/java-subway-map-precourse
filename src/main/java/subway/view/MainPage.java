acfba457da8c01510c3d008ee1525b9601c3d44c
perenok <perenoke@gmail.com>
2020-12-15 22:12:15 +0900

    feat(MainPage): update methods
    지하철 노선도 출력을 위한 printRouteMap method 추가
    List<Line> lines를 받아 각각의 노선에 대한 역들을 출력

--- src/main/java/subway/view/MainPage.java
+++ src/main/java/subway/view/MainPage.java
@@ -1,8 +1,15 @@
 package subway.view;
 
 
+import subway.domain.Line;
+import subway.domain.Station;
+import utils.ErrorUtils;
+import utils.InfoUtils;
+
+import java.util.List;
+
 public class MainPage {
-    public static void printMainPage(){
+    public void printMainPage() {
         System.out.println("\n## 메인 화면");
         System.out.println("1. 역 관리");
         System.out.println("2. 노선 관리");
@@ -10,5 +17,21 @@ public class MainPage {
         System.out.println("4. 지하철 노선도 출력");
         System.out.println("Q. 종료");
         System.out.println("\n## 원하는 기능을 선택하세요.");
+    }
+
+    public void printRouteMap(List<Line> lines) {
+        System.out.println("\n## 지하철 노선도");
+        for (Line line : lines) {
+            InfoUtils.printInfo(line.getName());
+            InfoUtils.printInfo("---");
+            for (Station station : line.getStations()) {
+                InfoUtils.printInfo(station.getName());
+            }
+            System.out.println();
+        }
+    }
+
+    public void printWrongItemError() {
+        ErrorUtils.printError("잘못된 입력입니다.");
     }
 }
