042c74e1851bcf032366616c1e12800a9c77bb49
park <msmn1729@gmail.com>
2020-12-15 22:03:07 +0900

    feat: 노선을 관리할 수 있도록 메소드 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,35 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> section = new ArrayList<>();
 
-    public Line(String name) {
+    public Line(String name, Station upward, Station downward) {
         this.name = name;
+        addSection(upward);
+        addSection(downward);
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    public void addSection(Station station) {
+        section.add(station);
+    }
+
+    public void addSection(int index, Station station) {
+        section.add(index, station);
+    }
+
+    public List<Station> getSection() {
+        return section;
+    }
+
+    public void deleteSection(int index) {
+        section.remove(index);
+    }
 }
