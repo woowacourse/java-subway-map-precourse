2b4fbe10594c0a5d4d0011e0ae57f6c40dad2d0b
baekchan1024 <dlqhs1024@naver.com>
2020-12-15 17:00:53 +0900

    refactor (Line) : add line constructor

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -2,14 +2,18 @@ package subway.domain;
 
 public class Line {
     private String name;
+    private String ascendingEndPoint;
+    private String descendingEndPoint;
 
-    public Line(String name) {
+    public Line(String name, String ascendingEndPoint, String descendingEndPoint) {
         this.name = name;
+        this.ascendingEndPoint = ascendingEndPoint;
+        this.descendingEndPoint = descendingEndPoint;
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    // 異붽� 湲곕뒫 援ы쁽
 }
