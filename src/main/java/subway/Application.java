62f95f5ef6fdc9ef50beabe95df2c9618b20ac82
DH Lim <dlarkfl2@gamil.com>
2020-12-15 20:49:01 +0900

    style: 불필요한 개행 삭제

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -9,7 +9,6 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
         DummyDataSetting.run();
-
         SubwayMap subwayMap = new SubwayMap();
         subwayMap.Run(scanner);
     }
