163a9000ce75967026a9dd2e6844772d9e86c2d1
Jaeyoung Shim <sjy4566@naver.com>
2020-12-15 14:46:12 +0900

    feat(LineManageController): remove line

--- src/main/java/controller/LineManageController.java
+++ src/main/java/controller/LineManageController.java
@@ -8,13 +8,18 @@ import view.LineManageView;
 
 public class LineManageController {
     private static final String INPUT_ADD_LINE = "1";
+    private static final String INPUT_REMOVE_LINE = "2";
+    private static final String INPUT_MESSAGE_LINE_NAME_TO_DELETE = "## 삭제할 노선 이름을 입력하세요.";
     private static final String INPUT_MESSAGE_LINE_NAME = "## 등록할 노선 이름을 입력하세요.";
     private static final String INPUT_MESSAGE_UPWARD_DESTINATION = "## 등록할 노선의 상행 종점역 이름을 입력하세요.";
     private static final String INPUT_MESSAGE_DOWNWARD_DESTINATION = "## 등록할 노선의 하행 종점역 이름을 입력하세요.";
     private static final String COMPLETE_ADD_LINE = "\n[INFO] 지하철 노선이 등록되었습니다.\n";
+    private static final String COMPLETE_REMOVE_LINE = "\n[INFO] 지하철 노선이 삭제되었습니다.\n";
     private static final String ERROR_EXIST_LINE = "\n[ERROR] 이미 존재하는 노선입니다.\n";
     private static final String ERROR_NOT_EXIST_STATION = "\n[ERROR] 상행 종점역 또는 하행 종점역이 존재하지 않니다.\n";
+    private static final String ERROR_NOT_EXIST_LINE_TO_DELETE = "\n[ERROR] 삭제할 노선이 존재하지 않니다.\n";
     private static final String ERROR_INPUT_MORE_THAN_TWO = "\n[ERROR] 2글자 이상 입력해주세요.\n";
+
     private LineManageView view;
 
     public LineManageController(LineManageView view) {
@@ -29,9 +34,12 @@ public class LineManageController {
         if (input.equals(INPUT_ADD_LINE)) {
             addLine();
         }
+        if (input.equals(INPUT_REMOVE_LINE)) {
+            removeLine();
+        }
     }
 
-    public void addLine() {
+    private void addLine() {
         String lineName = inputFromView(INPUT_MESSAGE_LINE_NAME);
         String upwardDestination = inputFromView(INPUT_MESSAGE_UPWARD_DESTINATION);
         String downwardDestination = inputFromView(INPUT_MESSAGE_DOWNWARD_DESTINATION);
@@ -48,7 +56,7 @@ public class LineManageController {
         view.printMessage(COMPLETE_ADD_LINE);
     }
 
-    public boolean validateAddLineInputs(String lineName, String upwardDestination,
+    private boolean validateAddLineInputs(String lineName, String upwardDestination,
             String downwardDestination) {
         if (LineRepository.isExistLine(lineName)) {
             view.printMessage(ERROR_EXIST_LINE);
@@ -66,7 +74,19 @@ public class LineManageController {
         return true;
     }
 
-    public String inputFromView(String inputHint) {
+    private void removeLine() {
+        String lineNameToDelete = inputFromView(INPUT_MESSAGE_LINE_NAME_TO_DELETE);
+
+        if (!LineRepository.isExistLine(lineNameToDelete)) {
+            view.printMessage(ERROR_NOT_EXIST_LINE_TO_DELETE);
+            return;
+        }
+
+        LineRepository.deleteLineByName(lineNameToDelete);
+        view.printMessage(COMPLETE_REMOVE_LINE);
+    }
+
+    private String inputFromView(String inputHint) {
         view.printMessage(inputHint);
         return view.input();
     }
