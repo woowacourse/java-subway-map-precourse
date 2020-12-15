b1b277531d3be4ad08f5b24cfa564bc3962b99a6
DH Lim <dlarkfl2@gamil.com>
2020-12-15 20:40:43 +0900

    refactor: 상행/하행 종점 멤버변수와 관련 메소드 삭제
    구간의 집합(sections)에서 처음과 끝이 각각 상행, 하행 종점을 의미하므로 불필요한 중복 삭제

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -16,12 +16,9 @@ public class Line {
     }
 
     // 추가 기능 구현
-    private static final int FROM_ORDER_TO_INDEX = 1;//구간 순서 와 인덱스 보정값 네이밍
-    private Station upwardStation; //상행 종착점
-    private Station downwardStation; // 하행 종착점
+    private static final int FROM_ORDER_TO_INDEX = 1;
     private List<Station> sections = new LinkedList<>();
 
-    // 구간 관련 메소드 시작
     public List<Station> getSections() {
         return sections;
     }
@@ -29,31 +26,12 @@ public class Line {
     public void addSectionByStation(Station station) {
         sections.add(station);
     }
-    // orderOfRoute-1 == index
+
     public void addSectionByOrderAndStation(int orderOfRoute, Station station) {
-        sections.add(orderOfRoute-FROM_ORDER_TO_INDEX,station);
+        sections.add(orderOfRoute - FROM_ORDER_TO_INDEX, station);
     }
 
     public boolean deleteSectionByName(String name) {
         return sections.removeIf(station -> Objects.equals(station.getName(), name));
     }
-    // 구간 관련 메소드 끝
-
-    // 상행, 하행 getter setter 시작
-    public Station getUpwardStation() {
-        return upwardStation;
-    }
-
-    public void setUpwardStation(Station upwardStation) {
-        this.upwardStation = upwardStation;
-    }
-
-    public Station getDownwardStation() {
-        return downwardStation;
-    }
-
-    public void setDownwardStation(Station downwardStation) {
-        this.downwardStation = downwardStation;
-    }
-    // 상행, 하행 getter setter 끝
 }
