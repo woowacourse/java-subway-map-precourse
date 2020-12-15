39bb85521f6bea81429ee3c5c0d13d505898e38b
100-yeeun <100yeeun@gmail.com>
2020-12-15 14:09:20 +0900

    feat: 시작 입출력 view 구현
    필요 script정리한 ScriptUtils 생성
    main, station, line, section 담은 Category Enum 생성
    MainController를 이용해 View 호출

---
+++ src/main/java/utils/Category.java
@@ -0,0 +1,5 @@
+package utils;
+
+public enum Category {
+    MAIN, STATION, LINE, SECTION
+}
---
+++ src/main/java/utils/ScriptUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class ScriptUtils {
+
+    public static final String MAIN_MENU = "## 메인 화면\n"
+        + "1. 역 관리\n"
+        + "2. 노선 관리\n"
+        + "3. 구간 관리\n"
+        + "4. 지하철 노선도 출력\n"
+        + "Q. 종료\n";
+    public static final String ASK_SELECTION = "## 원하는 기능을 선택하세요.";
+    public static final String ERROR_OUT_OF_VALUE = "[ERROR] 선택할 수 없는 기능입니다.";
+}
