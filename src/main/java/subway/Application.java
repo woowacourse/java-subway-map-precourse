aa93616ff8caad9adc7ce30c774ad4b125561edf
김주원 <kjw11077@naver.com>
2020-12-15 18:37:59 +0900

    docs(README): 기능 목록 작성

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -7,5 +7,12 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         MainMenuController.main(scanner);
+
+        // todo
+        // - 예외 처리
+        // - 테스트
+        // - 중복제거 상수, 메소드, 구현코드 등
+        // - static -> 객체화
+        // - 패키지*/
     }
 }
