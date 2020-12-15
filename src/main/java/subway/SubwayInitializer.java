4bb2782acf5a44a7cfd735a55b18ee313012bc24
김주원 <kjw11077@naver.com>
2020-12-15 21:53:31 +0900

    fix(domain): 역 제거 기능
    노선에 포함된 역이 두개 이하일 때는 역을 제거할 수 없다.

--- src/main/java/subway/SubwayInitializer.java
+++ src/main/java/subway/SubwayInitializer.java
@@ -3,6 +3,7 @@ package subway;
 import java.util.Arrays;
 import subway.domain.Line;
 import subway.domain.Station;
+import subway.domain.StationRepository;
 import subway.service.LineService;
 
 public class SubwayInitializer {
@@ -10,13 +11,13 @@ public class SubwayInitializer {
     public static final String LINE_2호선 = "2호선";
     public static final String LINE_3호선 = "3호선";
     public static final String LINE_신분당선 = "신분당선";
-    public static final String 교대역 = "교대역";
-    public static final String 강남역 = "강남역";
-    public static final String 역삼역 = "역삼역";
-    public static final String 남부터미널역 = "남부터미널역";
-    public static final String 양재역 = "양재역";
-    public static final String 양재시민의숲역 = "양재시민의숲역";
-    public static final String 매봉역 = "매봉역";
+    public static final String STATION_교대역 = "교대역";
+    public static final String STATION_강남역 = "강남역";
+    public static final String STATION_역삼역 = "역삼역";
+    public static final String STATION_남부터미널역 = "남부터미널역";
+    public static final String STATION_양재역 = "양재역";
+    public static final String STATION_양재시민의숲역 = "양재시민의숲역";
+    public static final String STATION_매봉역 = "매봉역";
 
     public static void init() {
         initStations();
@@ -25,22 +26,26 @@ public class SubwayInitializer {
     }
 
     private static void initStations() {
-        Arrays.asList(교대역, 강남역, 역삼역, 남부터미널역, 양재역, 양재시민의숲역, 매봉역)
+        Arrays.asList(STATION_교대역, STATION_강남역, STATION_역삼역, STATION_남부터미널역, STATION_양재역,
+                STATION_양재시민의숲역, STATION_매봉역)
                 .forEach(name -> Station.from(name).save());
     }
 
     private static void initLines() {
         Arrays.asList(
-                Line.of(LINE_2호선, Station.from(교대역), Station.from(역삼역)),
-                Line.of(LINE_3호선, Station.from(교대역), Station.from(매봉역)),
-                Line.of(LINE_신분당선, Station.from(강남역), Station.from(양재시민의숲역)))
+                Line.of(LINE_2호선, StationRepository.findByName(STATION_교대역), StationRepository.findByName(
+                        STATION_역삼역)),
+                Line.of(LINE_3호선, StationRepository.findByName(STATION_교대역), StationRepository.findByName(
+                        STATION_매봉역)),
+                Line.of(LINE_신분당선, StationRepository.findByName(STATION_강남역), StationRepository.findByName(
+                        STATION_양재시민의숲역)))
                 .forEach(Line::save);
     }
 
     private static void initSections() {
-        LineService.insertSection(LINE_2호선, 강남역, 1);
-        LineService.insertSection(LINE_3호선, 남부터미널역, 1);
-        LineService.insertSection(LINE_3호선, 양재역, 2);
-        LineService.insertSection(LINE_신분당선, 양재역, 1);
+        LineService.insertSection(LINE_2호선, STATION_강남역, 1);
+        LineService.insertSection(LINE_3호선, STATION_남부터미널역, 1);
+        LineService.insertSection(LINE_3호선, STATION_양재역, 2);
+        LineService.insertSection(LINE_신분당선, STATION_양재역, 1);
     }
 }
