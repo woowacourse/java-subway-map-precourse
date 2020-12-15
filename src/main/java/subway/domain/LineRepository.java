97dfff9c137bb9bcbe907222978ed352c5f3d1e6
xrabcde <joara124@ewhain.net>
2020-12-15 22:12:01 +0900

    feat(LineRepository): 중복된 노선이 있는지 검사하는 isExist 메소드 구현

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -21,4 +21,8 @@ public class LineRepository {
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
     }
+
+    public static boolean isExist(String name) {
+        return lines.stream().map(Line::getName).anyMatch(x -> x.equals(name));
+    }
 }
