47f2ab41624a8fd454f5f016978db7b2700affb5
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-15 16:49:40 +0900

    fix: 역 관리 화면에서 메뉴 입력 받고 해당 기능 실행할 수 있도록 컨트롤러와 연결

--- src/main/java/subway/view/StationMenu.java
+++ src/main/java/subway/view/StationMenu.java
@@ -1,14 +1,16 @@
 package subway.view;
 
+import subway.controller.StationController;
+
 import java.util.Arrays;
 import java.util.function.Supplier;
 
 public class StationMenu {
     public enum StationView{
-        REGISTER_STATION("1", "역 등록", () -> ),
-        DELETE_STATION("2", "역 삭제", () -> ),
-        INQUIRY_STATION("3", "역 조회", () -> ),
-        BACK("B", "돌아가기", () ->);
+        REGISTER_STATION("1", "역 등록", () -> StationController.getInstance().registerStation()),
+        DELETE_STATION("2", "역 삭제", () -> StationController.getInstance().deleteStation()),
+        INQUIRY_STATION("3", "역 조회", () -> StationController.getInstance().inquiryStation()),
+        BACK("B", "돌아가기", () -> StationController.getInstance().back());
 
         private String key;
         private String menuList;
