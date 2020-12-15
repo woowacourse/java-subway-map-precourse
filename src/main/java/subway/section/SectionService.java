762ac8d66569b0b4330dbe26d77d4146084e21ca
KJunseo <max9106@naver.com>
2020-12-15 20:20:51 +0900

    refactor: 구간 삭제 기능 간소화

--- src/main/java/subway/section/SectionService.java
+++ src/main/java/subway/section/SectionService.java
@@ -1,21 +1,16 @@
 package subway.section;
 
 import subway.line.Line;
+import subway.line.validation.SatisfyLineMinimumStation;
 import subway.station.Station;
 
 public class SectionService {
-    private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String STATION_NUMBER_LACK = ERROR_PREFIX + "등록된 역이 2개 이하이므로 삭제할 수 없습니다.";
-
     public static boolean addSection(Line line, Station station, int order) {
         line.addSection(station, order);
         return true;
     }
 
     public static boolean deleteSection(Line line, Station station) {
-        if (!line.deleteSection(station)) {
-            throw new IllegalArgumentException(STATION_NUMBER_LACK);
-        }
-        return true;
+        return SatisfyLineMinimumStation.validation(line, station);
     }
 }
