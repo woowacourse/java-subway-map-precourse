607e3103a74270d292dd94fd9ddd27210ad593dc
xorud2592 <xorud2592@gmail.com>
2020-12-15 19:56:14 +0900

    test

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        // TODO: �봽濡쒓렇�옩 援ы쁽
+        System.out.println("aa");
     }
 }
