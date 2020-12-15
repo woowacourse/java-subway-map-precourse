55af3ce8f9197e15ca0c1fca3b48617eccb263b6
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-12-15 17:46:40 +0900

    refactor: OutputView 일부 변수명 및 출력 메소드 수정

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -14,8 +14,8 @@ public class OutputView {
     private static final String PRINT_LIST_MESSAGE = "\n## %s 목록\n";
     private static final String PRINT_SUBWAY_MAP_MESSAGE = "\n## 지하철 노선도";
     private static final String PRINT_LIST = "[INFO] %s\n";
-    private static final String ADD_MESSAGE = "\n[INFO] %s이 등록되었습니다.\n";
-    private static final String DELETE_MESSAGE = "\n[INFO] %s이 삭제되었습니다.\n";
+    private static final String ADD_SUCCESS_MESSAGE = "\n[INFO] %s이 등록되었습니다.\n";
+    private static final String DELETE_SUCCESS_MESSAGE = "\n[INFO] %s이 삭제되었습니다.\n";
 
 
     private OutputView() {
@@ -25,13 +25,14 @@ public class OutputView {
         if (!category.equals(SECTION)) {
             category = SUBWAY + category;
         }
-        System.out.printf(ADD_MESSAGE, category);
+        System.out.printf(ADD_SUCCESS_MESSAGE, category);
     }
+
     public static void printDeleteMessage(String category) {
         if (!category.equals(SECTION)) {
             category = SUBWAY + category;
         }
-        System.out.printf(DELETE_MESSAGE, category);
+        System.out.printf(DELETE_SUCCESS_MESSAGE, category);
     }
 
     public static void printStationList(String category) {
@@ -52,10 +53,9 @@ public class OutputView {
         System.out.println(PRINT_SUBWAY_MAP_MESSAGE);
         List<Line> lines = LineRepository.lines();
         for (int i = 0; i < lines.size(); i++) {
-            Line line = lines.get(i);
-            System.out.printf(PRINT_LIST, line.getName().getName());
+            System.out.printf(PRINT_LIST, lines.get(i).getName().getName());
             System.out.printf(PRINT_LIST, DASH);
-            line.getStations().stream()
+            lines.get(i).getStations().stream()
                     .map(Station::getName)
                     .forEach(name -> System.out.printf(PRINT_LIST, name.getName()));
             if (i != lines.size() - 1) {
