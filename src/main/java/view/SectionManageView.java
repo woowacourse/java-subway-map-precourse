16ea0acbbba855601aa399ff1c531fcc979b083d
Jaeyoung Shim <sjy4566@naver.com>
2020-12-15 15:20:17 +0900

    feat(SectionManageView): input value for section manage menu

--- src/main/java/view/SectionManageView.java
+++ src/main/java/view/SectionManageView.java
@@ -5,6 +5,8 @@ import controller.SectionManageController;
 
 public class SectionManageView {
     private static final String MENU = "## 구간 관리 화면\n" + "1. 구간 등록\n" + "2. 구간 삭제\n" + "B. 돌아가기";
+    private static final String INPUT_MESSAGE = "## 원하는 기능을 선택하세요.";
+
     private Scanner scanner;
     private SectionManageController controller;
 
@@ -18,6 +20,8 @@ public class SectionManageView {
 
     public void run() {
         showMenu();
+        printMessage(INPUT_MESSAGE);
+        String input = input();
     }
 
     private void showMenu() {
@@ -26,5 +30,9 @@ public class SectionManageView {
 
     public void printMessage(String s) {
         System.out.println(s);
+    }
+
+    public String input() {
+        return scanner.nextLine();
     }
 }
