1f04b3d58ef4a45ea2d0995a823c4bc591475d06
TaewanKimmmm <ktww88@gmail.com>
2020-12-15 18:59:19 +0900

    feat: 초기 세팅 기능 추가

--- src/main/java/subway/domain/SubwayMap.java
+++ src/main/java/subway/domain/SubwayMap.java
@@ -12,24 +12,64 @@ public class SubwayMap {
     private static final String YANGJAE_CITIZEN_FOREST_STATION = "양재시민의숲역";
     private static final String MAEBONG_STATION = "매봉역";
 
-    public void initialize() {
-        initializeStation();
-        initializeLine();
+    private Line lineNumberTwo;
+    private Line lineNumberThree;
+    private Line lineNewBoondang;
+    private Station gyodaeStation;
+    private Station gangnamStation;
+    private Station yeoksamStation;
+    private Station nambuTerminalStation;
+    private Station yangjaeStation;
+    private Station yangjaeCitizenForestStation;
+    private Station maebongStation;
+
+    public SubwayMap() {
+        initialize();
     }
 
-    private void initializeStation() {
-        StationRepository.addStation(new Station(GYODAE_STATION));
-        StationRepository.addStation(new Station(GANGNAM_STATION));
-        StationRepository.addStation(new Station(YEOKSAM_STATION));
-        StationRepository.addStation(new Station(NAMBU_TERMINAL_STATION));
-        StationRepository.addStation(new Station(YANGJAE_STATION));
-        StationRepository.addStation(new Station(YANGJAE_CITIZEN_FOREST_STATION));
-        StationRepository.addStation(new Station(MAEBONG_STATION));
+    public void initialize() {
+        initializeLine();
+        initializeStation();
+        initializeMap();
     }
 
     private void initializeLine() {
-        LineRepository.addLine(new Line(LINE_NUMBER_TWO));
-        LineRepository.addLine(new Line(LINE_NUMBER_THREE));
-        LineRepository.addLine(new Line(LINE_NEW_BOONDANG));
+        lineNumberTwo = new Line(LINE_NUMBER_TWO);
+        lineNumberThree = new Line(LINE_NUMBER_THREE);
+        lineNewBoondang = new Line(LINE_NEW_BOONDANG);
+        LineRepository.addLine(lineNumberTwo);
+        LineRepository.addLine(lineNumberThree);
+        LineRepository.addLine(lineNewBoondang);
     }
+
+    private void initializeStation() {
+        gyodaeStation = new Station(GYODAE_STATION);
+        gangnamStation = new Station(GANGNAM_STATION);
+        yeoksamStation = new Station(YEOKSAM_STATION);
+        nambuTerminalStation = new Station(NAMBU_TERMINAL_STATION);
+        yangjaeStation = new Station(YANGJAE_STATION);
+        yangjaeCitizenForestStation = new Station(YANGJAE_CITIZEN_FOREST_STATION);
+        maebongStation = new Station(MAEBONG_STATION);
+        StationRepository.addStation(gyodaeStation);
+        StationRepository.addStation(gangnamStation);
+        StationRepository.addStation(yeoksamStation);
+        StationRepository.addStation(nambuTerminalStation);
+        StationRepository.addStation(yangjaeStation);
+        StationRepository.addStation(yangjaeCitizenForestStation);
+        StationRepository.addStation(maebongStation);
+    }
+
+    private void initializeMap() {
+        lineNumberTwo.registerStation(gyodaeStation);
+        lineNumberTwo.registerStation(gangnamStation);
+        lineNumberTwo.registerStation(yeoksamStation);
+        lineNumberThree.registerStation(gyodaeStation);
+        lineNumberThree.registerStation(nambuTerminalStation);
+        lineNumberThree.registerStation(yangjaeStation);
+        lineNumberThree.registerStation(maebongStation);
+        lineNewBoondang.registerStation(gangnamStation);
+        lineNewBoondang.registerStation(yangjaeStation);
+        lineNewBoondang.registerStation(yangjaeCitizenForestStation);
+    }
+
 }
