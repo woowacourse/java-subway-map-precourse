68919a2eae6eb934b77deadc2433508efa36d268
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-15 20:47:06 +0900

    feat: 잘못된 에러 출력문 수정

--- src/main/java/subway/Exception/StationException/ShorterThanMinStationNameException.java
+++ src/main/java/subway/Exception/StationException/ShorterThanMinStationNameException.java
@@ -1,7 +1,7 @@
 package subway.Exception.StationException;
 
 public class ShorterThanMinStationNameException extends IllegalArgumentException{
-    private static final String MESSAGE = "[ERROR] 이미 등록된 역 이름입니다.";
+    private static final String MESSAGE = "[ERROR] 역 이름은 2글자 이상이여야 합니다.";
 
     public ShorterThanMinStationNameException() {
         super(MESSAGE);
