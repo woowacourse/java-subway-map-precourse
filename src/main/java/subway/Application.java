d01c43016ead354068561484c22167bf7e5b501f
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-15 20:40:12 +0900

    feat(Application) : 메인함수 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,19 @@
 package subway;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.util.MainManager;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        MainManager mainManager = new MainManager();
+        mainManager.appMain(scanner);
+
     }
 }
