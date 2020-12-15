9fa3d028d1387f057e49755bcd7a6fc2c7e18cf4
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-12-15 22:29:16 +0900

    refactor 구간 삭제 리펙토링

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -9,7 +9,7 @@ public class Line {
     private final List<Station> stations;
 
     public Line(String name, List<Station> stations) {
-        if(name.length() < MIN_STATION_AND_LINE_NAME_LENGTH) {
+        if (name.length() < MIN_STATION_AND_LINE_NAME_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 노선 이름은 2글자 이상이여야 합니다.");
         }
         this.name = name;
@@ -30,5 +30,14 @@ public class Line {
 
     public boolean equalsName(String lineName) {
         return this.name.equals(lineName);
+    }
+
+    public void deleteStation(String stationName) {
+        for (Station station : this.stations) {
+            if (station.equalsName(stationName)) {
+                this.stations.remove(station);
+                return;
+            }
+        }
     }
 }
--- src/main/java/subway/subwaymanager/SectionManager.java
+++ src/main/java/subway/subwaymanager/SectionManager.java
@@ -8,8 +8,6 @@ import subway.utils.InputView;
 import subway.utils.OutputView;
 import subway.validators.ValidateSectionSelect;
 
-import java.util.List;
-
 import static subway.utils.Constant.*;
 
 public class SectionManager {
@@ -44,37 +42,34 @@ public class SectionManager {
     public static void addSectionLineOfStation(String lineName, String stationName, int indexNum) {
         Line line = LineRepository.getLineByName(lineName);
         Station station = StationRepository.getStationByName(stationName);
-        validateAddSectionLineOfStation(line,station);
+        validateAddSectionLineOfStation(line, station);
         line.addSectionStation(station, indexNum);
     }
 
     public static void validateAddSectionLineOfStation(Line line, Station station) {
-        if(line == null) {
+        if (line == null) {
             throw new IllegalArgumentException("[ERROR] 라인이 존재하지 않습니다. 라인을 먼저 만들고 시도 바랍니다.");
         }
-        if(station == null) {
+        if (station == null) {
             throw new IllegalArgumentException("[ERROR] 역이 존재하지 않습니다. 역을 먼저 만들고 시도 바랍니다.");
         }
     }
 
     private static void deleteSection() {
         String deleteSectionLine = InputView.inputDeleteSectionLine();
-        List<Line> lines = LineRepository.getLines();
-        for (int i = 0; i < lines.size(); i++) {
-            if (lines.get(i).getName().equals(deleteSectionLine)) {
-                deleteStationOfSection(lines, i);
-            }
-        }
+        Line line = LineRepository.getLineByName(deleteSectionLine);
+        validateDeleteSection(line);
+        String deleteStation = InputView.inputDeleteSectionStation();
+        line.deleteStation(deleteStation);
         OutputView.printEmptyLine();
     }
 
-    private static void deleteStationOfSection(List<Line> lines, int i) {
-        String deleteStation = InputView.inputDeleteSectionStation();
-        for (int j = 0; j < lines.get(i).getStations().size(); j++) {
-            if (lines.get(i).getStations().get(j).getName().equals(deleteStation)) {
-                StationRepository.sectionDeleteStation(deleteStation);
-            }
+    private static void validateDeleteSection(Line line) {
+        if (line == null) {
+            throw new IllegalArgumentException("[ERROR] 라인이 존재하지 않습니다. 라인을 먼저 만들고 시도 바랍니다.");
         }
-        OutputView.printDeleteMassage();
+        if (line.getStations().size() <= MIN_SECTION_DELETE_STATION_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 역이 두개 이하일 경우 삭제 불가능 합니다.");
+        }
     }
 }
