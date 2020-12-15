25fc2d3c02165cfa3a17f075f5951dddf3b393a6
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-12-15 16:58:06 +0900

    Revert "Revert "docs(README) : 예외 사항 문서 작성""
    This reverts commit 1f4ceb32518ab6111b2eb7a6d9c5d52f525d4e72.

--- src/main/java/subway/domain/SectionRepository.java
+++ src/main/java/subway/domain/SectionRepository.java
@@ -1,11 +1,16 @@
 package subway.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 public class SectionRepository {
     private static final List<Section> sections = new ArrayList<>();
+
+    public static List<Section> sections() {
+        return Collections.unmodifiableList(sections);
+    }
 
     public static void addSection(Section section) {
         sections.add(section);
