47ad479953d1f172f62f04f9af12ec015152c439
baekchan1024 <dlqhs1024@naver.com>
2020-12-15 21:45:58 +0900

    feat (OutputView) : add output function

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,5 +1,10 @@
 package subway.view;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
 public class OutputView {
 	private static final String MAIN_MESSAGE = "## 메인 화면";
 	private static final String MAIN_STATION_MESSAGE = "1. 역 관리";
@@ -27,21 +32,21 @@ public class OutputView {
 	private static final String DIVIDE_INFORMATION = "---";
 	private static final String SUBWAY_MAP = "## 지하철 노선도";
 	public static final String SELETE_FUNCTION = "## 원하는 기능을 선택하세요.";
-	
+
 	public static final String REGISTER_STATION = "## 등록할 역 이름을 입력하세요.";
 	public static final String DELETE_STATION = "## 삭제할 역 이름을 입력하세요.";
-	
-	public static final String REGISTER_LINE = "## 등록할 노선 이름을 입력하세요."; 
+
+	public static final String REGISTER_LINE = "## 등록할 노선 이름을 입력하세요.";
 	public static final String ASCENDING_END_POINT = "## 등록할 노선의 상행 종점역 이름을 입력하세요.";
 	public static final String DESCENDING_END_POINT = "## 등록할 노선의 하행 종점역 이름을 입력하세요.";
 	public static final String DELETE_LINE = "## 삭제할 노선 이름을 입력하세요.";
-	
+
 	public static final String SELETE_LINE = "## 노선을 입력하세요.";
 	public static final String SELETE_STATION = "## 역이름을 입력하세요.";
 	public static final String REGISTER_SEQUENCE = "## 순서를 입력하세요.";
 	public static final String DELETE_SECTION_LINE = "## 삭제할 구간의 노선을 입력하세요.";
 	public static final String DELETE_SECTION_STATION = "## 삭제할 구간의 역을 입력하세요.";
-	
+
 	public static void printMainMenu() {
 		System.out.println(MAIN_MESSAGE);
 		System.out.println(MAIN_STATION_MESSAGE);
@@ -50,7 +55,7 @@ public class OutputView {
 		System.out.println(MAIN_MAP_PRINT_MESSAGE);
 		System.out.println(SYSTEM_OUT);
 	}
-	
+
 	public static void printStationManagementMenu() {
 		System.out.println(STATION_MANAGE_MESSAGE);
 		System.out.println(STATION_REGISTER_MESSAGE);
@@ -58,7 +63,7 @@ public class OutputView {
 		System.out.println(STATION_PRINT_MESSAGE);
 		System.out.println(BACK_TO_MAIN);
 	}
-	
+
 	public static void printLineManagementMenu() {
 		System.out.println(SECTION_MANAGE_MESSAGE);
 		System.out.println(LINE_REGISTER_MESSAGE);
@@ -66,12 +71,42 @@ public class OutputView {
 		System.out.println(LINE_PRINT_MESSAGE);
 		System.out.println(BACK_TO_MAIN);
 	}
-	
+
 	public static void printSectionManagementMenu() {
 		System.out.println(SELETE_LINE);
 		System.out.println(SECTION_REGISTER_MESSAGE);
 		System.out.println(SECTION_DELETE_MESSAGE);
 		System.out.println(BACK_TO_MAIN);
 	}
-	
+
+	public static void printTotalMap() {
+		System.out.println(SUBWAY_MAP);
+		for (Line line : LineRepository.lines()) {
+			printCombineInfo(line.getName());
+			printCombineInfo(DIVIDE_INFORMATION);
+			printConbineInfoLine(line);
+		}
+	}
+
+	private static void printCombineInfo(String message) {
+		System.out.println(INFOMATION + message);
+	}
+
+	private static void printConbineInfoLine(Line line) {
+		for (String stationName : line.getStationInLine()) {
+			printCombineInfo(stationName);
+		}
+	}
+
+	public static void printTotalStation() {
+		for (Station station : StationRepository.stations()) {
+			printCombineInfo(station.getName());
+		}
+	}
+
+	public static void printTotalLine() {
+		for (Line line : LineRepository.lines()) {
+			printCombineInfo(line.getName());
+		}
+	}
 }
