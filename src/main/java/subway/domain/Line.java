0289c4c53f06c9c1f93263aa8aec3106af36d480
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-12-15 23:02:22 +0900

    refactor: add new fields and methods in Line

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,12 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
+
     private String name;
+    private List<Station> section = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -11,5 +16,32 @@ public class Line {
         return name;
     }
 
-    // 추가 기능 구현
+    public void addStationInLine(int i, Station station) {
+        station.changeStatus();
+        section.add(i - 1, station);
+    }
+
+    public void addStationListInLine(List<String> stationNames) {
+        for (int i = 0; i < stationNames.size(); i++) {
+            Station station = new Station(stationNames.get(i));
+            station.changeStatus();
+            section.add(station);
+        }
+    }
+
+    public void deleteStationInLine(Station station) {
+        section.remove(station);
+    }
+
+    public List<String> getSectionName() {
+        List<String> sectionName = new ArrayList<>();
+        for (int i = 0; i < section.size(); i++) {
+            sectionName.add(section.get(i).getName());
+        }
+        return sectionName;
+    }
+
+    public int getSectionSize() {
+        return section.size();
+    }
 }
