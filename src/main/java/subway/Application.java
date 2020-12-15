9407e6f787581a8a87bf3f349021214b1185b98d
xrabcde <joara124@ewhain.net>
2020-12-15 18:22:06 +0900

    feat(Application): 메인 기능 입력 예외처리를 위해 displayErrorMessage 메소드 구현 및 예외처리

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Application {
+    public static final int FUNCTION_INPUT_ERROR = 0;
+    public static final List<String> mainFunctions = Arrays.asList("1", "2", "3", "4", "Q");
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         initialize();
@@ -24,12 +26,26 @@ public class Application {
         System.out.println("3. 구간 관리");
         System.out.println("4. 지하철 노선도 출력");
         System.out.println("Q. 종료");
-        System.out.println("\n## 원하는 기능을 선택하세요.");
     }
 
-    public static int inputMainFunction(Scanner kbd) {
-        int input = kbd.nextInt();
+    public static String inputMainFunction(Scanner kbd) {
+        String input = "0";
+        boolean check = false;
+        while(!check) {
+            System.out.println("\n## 원하는 기능을 선택하세요.");
+            input = kbd.nextLine();
+            check = checkMainInput(input);
+        }
         return input;
+    }
+
+    public static boolean checkMainInput(String input) {
+        boolean check = true;
+        if (!mainFunctions.contains(input)) {
+            displayErrorMessage(FUNCTION_INPUT_ERROR);
+            check = false;
+        }
+        return check;
     }
 
     public static void initialize() {
@@ -50,5 +66,10 @@ public class Application {
         LineRepository.addLine(line3, Arrays.asList("교대역", "남부터미널역", "양재역", "매봉역"));
         Line lineSinbundang = new Line("신분당선");
         LineRepository.addLine(lineSinbundang, Arrays.asList("강남역", "양재역", "양재시민의숲역"));
+    }
+
+    public static void displayErrorMessage(int errorCase) {
+        if (errorCase == FUNCTION_INPUT_ERROR)
+            System.out.println("[ERROR] 선택할 수 없는 기능입니다.");
     }
 }
