1e147ad9f6632329ce05eac4b0487f9dd2064ccf
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-12-15 17:42:08 +0900

    refactor : menu enum을 새 패키지에 추가, 기존 menu클래스 삭제 및 enum 분할

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,7 +1,7 @@
 package subway.view;
 
 import subway.Constant;
-import subway.domain.Menu;
+import subway.domain.menu.MainMenu;
 
 public class OutputView {
 
@@ -11,10 +11,10 @@ public class OutputView {
     public static void printMainView(){
         System.out.print(Constant.VIEW_HEADER);
         System.out.println(Constant.MAIN_VIEW_HEADER);
-        for(Menu.MainMenu menu : Menu.MainMenu.values()) {
+        for(MainMenu menu : MainMenu.values()) {
             System.out.printf(Constant.VIEW_FORMAT, menu.getOrder(), menu.getMessage());
         }
-        System.out.println("");
+        System.out.println();
     }
 
     public static void printManagementView(String menu){
