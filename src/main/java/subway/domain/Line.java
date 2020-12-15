b931fcdfd9ebeec4fd61ba7174ba17a46959e5dc
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-15 17:25:59 +0900

    feat(Line): 상행 종점역과 하행 종점역을 입력 받아 역에 추가하는 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,10 @@
 package subway.domain;
 
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations;
 
     public Line(String name) {
         this.name = name;
@@ -12,4 +15,9 @@ public class Line {
     }
 
     // 추가 기능 구현
+
+    public void addStreamStation(String upstreamStation, String downstreamStation) {
+        stations.add(new Station(upstreamStation));
+        stations.add(new Station(downstreamStation));
+    }
 }
