cd52380677f771642848346cf99c89372662a3a9
Solji Choi <bssj9307@gmail.com>
2020-12-15 18:29:40 +0900

    feat(init): 초기 설정 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,11 +1,14 @@
 package subway;
 
+import subway.managers.SubwayManager;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        new InitSubway().initialSetting();
         new SubwayManager(scanner);
     }
 }
