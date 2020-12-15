9157979e4703c3f9242055b56bf38862b80776cc
perenok <perenoke@gmail.com>
2020-12-15 22:16:18 +0900

    feat(ErrorUtils): simplify Class
    FORMAT에 맞춰 받은 메세지를 출력하는 기능만 남겨두고 필요할 때 view 클래스에서 출력문장을 받아 출력

--- src/main/java/utils/ErrorUtils.java
+++ src/main/java/utils/ErrorUtils.java
@@ -1,30 +1,10 @@
-package subway.utils;
+package utils;
 
 public class ErrorUtils {
-    private static final String FORMAT = "[ERROR] %s";
-    private String msg;
+    private static final String FORMAT = "\n[ERROR] %s";
 
-    public void print(Object arg){
+    public static void printError(Object arg) {
         System.out.println(String.format(FORMAT, arg));
     }
 
-    public void printWrongInputError(){
-        msg = "잘못된 입력입니다.";
-        print(msg);
-    }
-
-    public void printEmptyError(){
-        msg = "선택할 수 없는 기능입니다.";
-        print(msg);
-    }
-
-    public void printDuplicateStationError(){
-        msg = "이미 등록된 역 이름입니다.";
-        print(msg);
-    }
-
-    public void printDuplicateLineError(){
-        msg = "이미 등록된 노선 이름입니다.";
-        print(msg);
-    }
 }
