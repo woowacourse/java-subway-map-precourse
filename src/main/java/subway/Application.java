2b277faeda43fe198e2d0f1640cee60061c0953b
이동건 <tnpdlzi@naver.com>
2020-12-15 15:40:27 +0900

    feat(Validator) : 첫 번호 선택 검증 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -5,6 +5,7 @@ import subway.line.LineUtils;
 import subway.line.SectionUtils;
 import subway.station.StationUtils;
 import subway.station.SubwayUtils;
+import subway.validator.Validator;
 
 import java.util.Scanner;
 
@@ -22,35 +23,39 @@ public class Application {
     }
 
     public static void selectUtils(Scanner scanner, StationUtils stationUtils, LineUtils lineUtils, SectionUtils sectionUtils, SubwayUtils subwayUtils) {
+        final int MAIN_QUIT = 0;
+
         while (true) {
-            System.out.println("## 메인 화면\n" +
-                    "1. 역 관리\n" +
-                    "2. 노선 관리\n" +
-                    "3. 구간 관리\n" +
-                    "4. 지하철 노선도 출력\n" +
-                    "Q. 종료\n");
+            String chosen;
+            int chosenNumber;
+
+            System.out.println("## 메인 화면\n" + "1. 역 관리\n" + "2. 노선 관리\n" + "3. 구간 관리\n" + "4. 지하철 노선도 출력\n" + "Q. 종료\n");
             System.out.println("## 원하는 기능을 선택하세요.");
-            String chosen = scanner.next();
-            if (chosen.equals("Q")) {
+            chosen = scanner.next();
+            chosenNumber = Validator.isMainInputRight(chosen);
+            if (chosenNumber == MAIN_QUIT) {
                 break;
             }
-            choose(chosen, stationUtils, lineUtils, sectionUtils, subwayUtils);
+            choose(chosenNumber, stationUtils, lineUtils, sectionUtils, subwayUtils);
         }
     }
 
-    public static void choose(String chosen, StationUtils stationUtils, LineUtils lineUtils, SectionUtils sectionUtils, SubwayUtils subwayUtils) {
-        int number = Integer.parseInt(chosen);
+    public static void choose(int chosenNumber, StationUtils stationUtils, LineUtils lineUtils, SectionUtils sectionUtils, SubwayUtils subwayUtils) {
+        final int MAIN_STATION = 1;
+        final int MAIN_LINE = 2;
+        final int MAIN_SECTION = 3;
+        final int MAIN_SUBWAY = 4;
 
-        if (number == 1) {
+        if (chosenNumber == MAIN_STATION) {
             stationUtils.play();
         }
-        if (number == 2) {
+        if (chosenNumber == MAIN_LINE) {
             lineUtils.play();
         }
-        if (number == 3) {
+        if (chosenNumber == MAIN_SECTION) {
             sectionUtils.play();
         }
-        if (number == 4) {
+        if (chosenNumber == MAIN_SUBWAY) {
             subwayUtils.play();
         }
     }
--- src/main/java/subway/validator/Validator.java
+++ src/main/java/subway/validator/Validator.java
@@ -4,7 +4,10 @@ public class Validator {
     private static final String UNABLE_INPUT = "[ERROR] 선택할 수 없는 기능입니다.";
     private static final int MIN_STATION_AND_LINE_NUMBER = 1;
     private static final int MAX_STATION_AND_LINE_NUMBER = 3;
+    private static final int MIN_MAIN_NUMBER = 1;
+    private static final int MAX_MAIN_NUMBER = 4;
     private static final int BACK_NUMBER = 0;
+    private static final int QUIT_NUMBER = 0;
 
 
     public Validator() {
@@ -20,6 +23,24 @@ public class Validator {
         try {
             integerNumber = Integer.parseInt(number);
             if (!(integerNumber >= MIN_STATION_AND_LINE_NUMBER && integerNumber <= MAX_STATION_AND_LINE_NUMBER)) {
+                throw new IllegalArgumentException(UNABLE_INPUT);
+            }
+            return integerNumber;
+        } catch (Exception e) {
+            throw new IllegalArgumentException(UNABLE_INPUT);
+        }
+    }
+
+    public static int isMainInputRight(String number) {
+        int integerNumber;
+
+        if (number.equals("Q")) {
+            return QUIT_NUMBER;
+        }
+
+        try {
+            integerNumber = Integer.parseInt(number);
+            if (!(integerNumber >= MIN_MAIN_NUMBER && integerNumber <= MAX_MAIN_NUMBER)) {
                 throw new IllegalArgumentException(UNABLE_INPUT);
             }
             return integerNumber;
