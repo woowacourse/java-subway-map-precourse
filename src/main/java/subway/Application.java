612c83e6f1e42465aedfdfdae47fa11aa8c7d106
seunghyun <ggyool@naver.com>
2020-12-15 20:50:40 +0900

    feat: 어플리케이션 실행 코드 작성

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,21 @@
 package subway;
 
+import subway.controller.SubwayMapController;
+import subway.initializer.SubwayMapInitializer;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+
+        SubwayMapInitializer initializer = new SubwayMapInitializer();
+        SubwayMapController subwayMapController = new SubwayMapController(scanner);
+        try {
+            initializer.run();
+            subwayMapController.run();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
