c99416b948c9a7bcae8119a53ec71a41aa9506a4
xorud2592 <xorud2592@gmail.com>
2020-12-15 22:27:11 +0900

    역삭제추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -11,12 +11,15 @@ public class Application {
 	private static final String[] DEFAULT_STATIONS = { "교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역" };
 	private static final String[] DEFAULT_LINES = { "2호선", "3호선", "신분당선" };
 	private static final String MAIN_SELECT_MESSAGE = "## 메인화면\n" + "1. 역 관리\n" + "2. 노선 관리\n" + "3. 구간 관리\n"
-			+ "4. 지하철 노선도 출력" +"\n" +"Q. 종료\n" + "\n" + "## 원하는 기능을 선택하세요.";
-	private static final String LINE_SELECT_MESSAGE = "\n## 역 관리 화면\n" + "1. 역 등록\n" + "2. 역 삭제\n" + "3. 역 조회\n"
+			+ "4. 지하철 노선도 출력" + "\n" + "Q. 종료\n" + "\n" + "## 원하는 기능을 선택하세요.";
+	private static final String STATION_SELECT_MESSAGE = "\n## 역 관리 화면\n" + "1. 역 등록\n" + "2. 역 삭제\n" + "3. 역 조회\n"
 			+ "B. 돌아가기" + "\n\n" + "## 원하는 기능을 선택하세요.";
 
-	private static final String NEW_LINE_MESSAGE = "\n## 등록할 역 이름을 입력하세요.";
-	private static final String CREATE_LINE_MESSAGE = "[INFO] 지하철 역이 등록되었습니다.";
+	private static final String NEW_STATION_MESSAGE = "\n## 등록할 역 이름을 입력하세요.";
+	private static final String CREATE_STATION_MESSAGE = "[INFO] 지하철 역이 등록되었습니다.";
+	private static final String DELETE_STATION_INPUT = "\n## 삭제할 역 이름을 입력하세요.";
+	private static final String DELETE_STATION_MESSAGE = "[INFO] 지하철 역이 삭제되었습니다.";
+	private static final String ERROR_INPUT_MESSAGE = "[ERROR] 잘못된 입력입니다. 다시 입력해주세요";
 	
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
@@ -74,24 +77,43 @@ public class Application {
 
 	public static void stationManagement(Scanner scanner, StationRepository stationRepositiory,
 			LineRepository lineRepositiory) {
-		System.out.println(LINE_SELECT_MESSAGE);
-		String Select = scanner.next();
-		if (Select.equals("1")) {
-			newStation(scanner, stationRepositiory, lineRepositiory);
-		}
-		if(Select.equals("3")) {
-			statusStation(stationRepositiory);
+		while (true) {
+			System.out.println(STATION_SELECT_MESSAGE);
+			String Select = scanner.next();
+			if (Select.equals("1")) {
+				newStation(scanner, stationRepositiory, lineRepositiory);
+				return;
+			}
+			if (Select.equals("2")) {
+				deleteStation(scanner, stationRepositiory, lineRepositiory);
+				return;
+			}
+			if (Select.equals("3")) {
+				statusStation(stationRepositiory);
+				return;
+			}
+			if (Select.equals("B")) {
+				return;
+			}
+			System.out.println(ERROR_INPUT_MESSAGE);
 		}
 	}
 
 	public static void newStation(Scanner scanner, StationRepository stationRepositiory,
 			LineRepository lineRepositiory) {
-		System.out.println(NEW_LINE_MESSAGE);
+		System.out.println(NEW_STATION_MESSAGE);
 		String station = scanner.next();
 		createStation(stationRepositiory, station);
-		System.out.println(CREATE_LINE_MESSAGE + "\n");
+		System.out.println(CREATE_STATION_MESSAGE + "\n");
 	}
 
+	public static void deleteStation(Scanner scanner, StationRepository stationRepositiory,
+			LineRepository lineRepositiory) {
+		System.out.println(DELETE_STATION_INPUT);
+		String station = scanner.next();
+		stationRepositiory.deleteStation(station);
+		System.out.println(DELETE_STATION_MESSAGE + "\n");
+	}
 	public static void statusStation(StationRepository stationRepositiory) {
 		stationRepositiory.viewStations();
 	}
