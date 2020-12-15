a5f294fb039ae8a9581a7b34dafdadb517ba1a23
TaewanKimmmm <ktww88@gmail.com>
2020-12-15 21:42:02 +0900

    refactor: 상수 코딩 수정

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -10,6 +10,7 @@ public class LineRepository {
     private static final String INFO_WITH_BORDER = "[INFO] ---";
     private static final String LINE_LIST = "## 노선 목록";
     private static final String SUBWAY_MAP = "## 지하철 노선도";
+    private static final String LINE_NOT_EXIST = "[ERROR] 존재하지 않는 노선입니다.";
 
     private static final List<Line> lines = new ArrayList<>();
 
@@ -18,12 +19,29 @@ public class LineRepository {
     }
 
     public static void addLine(Line line) {
-        // TODO 노선 이름 중복 확인할 것
         lines.add(line);
     }
 
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static void isLineExist(String lineName) {
+        boolean flag = false;
+        for (Line line : lines) {
+            flag = line.getName().equals(lineName);
+        }
+        if (!flag) {
+            throw new IllegalArgumentException(LINE_NOT_EXIST);
+        }
+    }
+
+    public static void printLines() {
+        System.out.println(LINE_LIST);
+        for (Line line : lines) {
+            System.out.println(INFO + line.getName());
+        }
+        System.out.println();
     }
 
     public static void printMap() {
@@ -34,23 +52,5 @@ public class LineRepository {
             System.out.println(INFO_WITH_BORDER);
             line.printStations();
         }
-    }
-
-    public static void isLineExist(String lineName) {
-        boolean flag = false;
-        for (Line line : lines) {
-            flag = line.getName().equals(lineName);
-        }
-        if (!flag) {
-            throw new IllegalArgumentException("[ERROR] 존재하지 않는 노선입니다.");
-        }
-    }
-
-    public static void printLines() {
-        System.out.println(LINE_LIST);
-        for (Line line : lines) {
-            System.out.println(INFO + line.getName());
-        }
-        System.out.println();
     }
 }
