7908b83af8c7d8021962743746d02df67b2c53dc
DH Lim <dlarkfl2@gamil.com>
2020-12-15 20:39:00 +0900

    style: 불필요한 주석, 개행 삭제

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -5,32 +5,12 @@ import java.util.Scanner;
 public class InputView {
     private String inputData;
 
-
     public String getInputData() {
         return inputData;
     }
 
-//    public void run(Scanner scanner) {
-//        OutputView.guideChooseFunction();
-//        scanData(scanner);
-//    }
-
     public void scanData(Scanner scanner) {
         inputData = scanner.nextLine();
         System.out.println();
-
-        /*
-        * 구간 재정의 할것. 역과 역사이를 구간. 구간이 모여서 노선.
-        *
-        * 에러코드 받으면 다시 실행
-        *
-        * 화면단 구성
-        * 메인화면 stack 1
-        * 기능선택 stack 2
-        *
-        * */
     }
-
-
-
 }
