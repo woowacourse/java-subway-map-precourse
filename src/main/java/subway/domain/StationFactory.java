6f7356bbc865369f3dff8f37e97f6fcf93577887
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-12-15 11:11:58 +0900

    feat : 역 및 라인 생성시 예외처리 추가
    예외 : 이름이 ~역 혹은 ~선으로 끝나지 않으면 예외

--- src/main/java/subway/domain/StationFactory.java
+++ src/main/java/subway/domain/StationFactory.java
@@ -4,13 +4,16 @@ public class StationFactory {
     private static final int NAME_MIN_LENGTH = 2;
     private static final String ERR_SHORT_NAME_MSG =
             String.format("[ERROR] 역의 이름은 %d글자 이상이어야 합니다.", NAME_MIN_LENGTH);
+    private static final String ERR_WRONG_STATION_NAME_SUFFIX = "[ERROR] 역의 이름은 ~역 형태여야 합니다.";
+    public static final String STATION = "역";
 
     public static Station makeStation(String name) {
         if (name.length() < NAME_MIN_LENGTH) {
             throw new IllegalArgumentException(ERR_SHORT_NAME_MSG);
         }
+        if (!name.endsWith(STATION)){
+            throw new IllegalArgumentException(ERR_WRONG_STATION_NAME_SUFFIX);
+        }
         return new Station(name);
     }
-
-
 }
