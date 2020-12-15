d1118d51b728cdd291b769ad9e160473684bc781
amy choi <cgc8016@ewhain.net>
2020-12-16 00:49:46 +0900

    feat(log): add guide print method and fix error body

--- src/main/java/log/ErrorCase.java
+++ src/main/java/log/ErrorCase.java
@@ -1,11 +1,11 @@
 package log;
 
 public class ErrorCase {
-    public static final String FUNCTION_INPUT_ERROR = "선택할 수 없는 기능입니다.\n";
-    public static final String ALREADY_EXIST_ERROR = "이미 등록된 역/노선 이름입니다.\n";
-    public static final String NAME_LENGTH_ERROR = "이름을 2글자 이상 입력해주세요.\n";
-    public static final String NO_SUCH_NAME_ERROR = "등록되지 않은 역/노선 이름입니다.\n";
-    public static final String HAS_IN_LINE_ERROR = "노선에 등록된 역은 삭제할 수 없습니다.\n";
+    public static final String FUNCTION_INPUT_ERROR = "선택할 수 없는 기능입니다.";
+    public static final String ALREADY_EXIST_ERROR = "이미 등록된 역/노선 이름입니다.";
+    public static final String NAME_LENGTH_ERROR = "역/노선의 이름은 최소 2자 이상이어야 합니다.";
+    public static final String NO_SUCH_NAME_ERROR = "등록되지 않은 역/노선 이름입니다.";
+    public static final String HAS_IN_LINE_ERROR = "노선에 등록된 역은 삭제할 수 없습니다.";
     public static final String TERMINAL_LENGTH_ERROR = "노선의 역 수는 2개 이상이어야 합니다.";
 
 }
--- src/main/java/log/Logger.java
+++ src/main/java/log/Logger.java
@@ -1,9 +1,13 @@
 package log;
 
 public class Logger {
+    public static final String LEVEL_GUIDE = "\n## ";
+    public static final String LEVEL_ERROR = "\n[ERROR] ";
+    public static final String LEVEL_INFO = "\n[INFO] ";
 
-    public static final String LEVEL_ERROR = "[ERROR] ";
-    public static final String LEVEL_INFO = "[INFO] ";
+    public static void guidePrint(String errorBody) {
+        System.out.println(LEVEL_GUIDE + errorBody);
+    }
 
     public static void errorPrint(String errorBody) {
         System.out.println(LEVEL_ERROR + errorBody);
@@ -20,6 +24,16 @@ public class Logger {
             + "3. 구간 관리\n"
             + "4. 지하철 노선도 출력\n"
             + "Q. 종료\n"
+            + "\n"
+            + "## 원하는 기능을 선택하세요.");
+    }
+
+    public static void displayLineManageScreen() {
+        System.out.println("\n## 노선 관리 화면\n"
+            + "1. 노선 등록\n"
+            + "2. 노선 삭제\n"
+            + "3. 노선 조회\n"
+            + "B. 돌아가기\n"
             + "\n"
             + "## 원하는 기능을 선택하세요.");
     }
