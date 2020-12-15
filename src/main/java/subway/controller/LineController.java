570ac8ee7edfc5b59f779f7d051b4c27b1e8a611
thisisyoungbin <thisisyoungbin@gmail.com>
2020-12-15 17:24:40 +0900

    feat(LineController): 노선 등록 기능 구현 및 유효성 판별, 예외 처리시 문자 에러메시지 출력 기능 구현

---
+++ src/main/java/subway/controller/LineController.java
@@ -0,0 +1,76 @@
+package subway.controller;
+
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.StationRepository;
+import subway.view.InputView;
+import subway.view.OutputViewOfError;
+import subway.view.OutputViewOfInfo;
+
+public class LineController {
+    private static final String INQUIRY_LINE_MESSAGE = "## 역 목록";
+
+    private static LineController lineController;
+    private String lineName;
+    private String stationName;
+    private String order;
+    private String upstreamStation;
+    private String downstreamStation;
+    private static final int MIN_NAME_LENGTH = 2;
+
+
+    public static LineController getInstance() {
+        if (lineController==null) {
+            lineController = new LineController();
+        }
+        return lineController;
+    }
+
+    public Boolean registerLine() {
+        if (!validLineInput()) {
+            return false;
+        }
+        Line line = new Line(lineName);
+        line.addStreamStation(upstreamStation, downstreamStation);
+        boolean result = LineRepository.addLine(line);
+        if (result) {
+            OutputViewOfInfo.registerLineComplete();
+        }
+        return result;
+    }
+
+    private boolean validLineInput() {
+        if ((lineName = InputView.registerLineName()).length() < MIN_NAME_LENGTH) {
+            OutputViewOfError.isNotValidNameLength();
+            return false;
+        }
+        if (isNotValidStreamStationName(upstreamStation=InputView.upstreamStationName())) {
+            return false;
+        }
+        if (isNotValidStreamStationName(downstreamStation=InputView.downstreamStationName())) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isNotValidStreamStationName(String s) {
+        if (s.length() < MIN_NAME_LENGTH) {
+            OutputViewOfError.isNotValidNameLength();
+            return false;
+        }
+        if(StationRepository.stations().stream().noneMatch(station -> station.getName().equals(s))) {
+            OutputViewOfError.isNotExistStation();
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean deleteLine() {
+    }
+
+    public Boolean inquiryLine() {
+    }
+
+    public Boolean back() {
+    }
+}
