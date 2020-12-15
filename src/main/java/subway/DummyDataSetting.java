81cd2d4ef49688ea2735df9dd70932ad5928f0a8
DH Lim <dlarkfl2@gamil.com>
2020-12-15 20:48:34 +0900

    refactor: 반복문 최소화

--- src/main/java/subway/DummyDataSetting.java
+++ src/main/java/subway/DummyDataSetting.java
@@ -5,8 +5,10 @@ import subway.domain.LineRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
 
-public class DummyDataSetting {
+import java.util.Arrays;
+import java.util.List;
 
+public class DummyDataSetting {
 
     public static void run() {
         /*
@@ -17,35 +19,28 @@ public class DummyDataSetting {
          * - 3호선: 교대역 - 남부터미널역 - 양재역 - 매봉역
          * - 신분당선: 강남역 - 양재역 - 양재시민의숲역
          */
+        List<String> stationNames = Arrays.asList("교대역", "강남역", "역삼역", "남부터미널역", "양재역",
+                "양재시민의숲역", "매봉역");
+        for (String name : stationNames) {
+            StationRepository.addStation(new Station(name));
+        }
 
-        StationRepository.addStation(new Station("교대역"));
-        StationRepository.addStation(new Station("강남역"));
-        StationRepository.addStation(new Station("역삼역"));
-        StationRepository.addStation(new Station("남부터미널역"));
-        StationRepository.addStation(new Station("양재역"));
-        StationRepository.addStation(new Station("양재시민의숲역"));
-        StationRepository.addStation(new Station("매봉역"));
-
-        LineRepository.addLine(new Line("2호선"));
-        LineRepository.addLine(new Line("3호선"));
-        LineRepository.addLine(new Line("신분당선"));
+        List<String> lineNames = Arrays.asList("2호선", "3호선", "신분당선");
+        for (String name : lineNames) {
+            LineRepository.addLine(new Line(name));
+        }
 
         LineRepository.getLine("2호선").addSectionByStation(StationRepository.getStation("교대역"));
         LineRepository.getLine("2호선").addSectionByStation(StationRepository.getStation("강남역"));
         LineRepository.getLine("2호선").addSectionByStation(StationRepository.getStation("역삼역"));
+
         LineRepository.getLine("3호선").addSectionByStation(StationRepository.getStation("교대역"));
         LineRepository.getLine("3호선").addSectionByStation(StationRepository.getStation("남부터미널역"));
         LineRepository.getLine("3호선").addSectionByStation(StationRepository.getStation("양재역"));
         LineRepository.getLine("3호선").addSectionByStation(StationRepository.getStation("매봉역"));
+
         LineRepository.getLine("신분당선").addSectionByStation(StationRepository.getStation("강남역"));
         LineRepository.getLine("신분당선").addSectionByStation(StationRepository.getStation("양재역"));
         LineRepository.getLine("신분당선").addSectionByStation(StationRepository.getStation("양재시민의숲역"));
-//        for(Line line : LineRepository.lines()) {
-//            System.out.println(line.getName());
-//            for(Station station : line.getRoute()) {
-//                System.out.println(station.getName());
-//            }
-//            System.out.println();
-//        }
     }
 }
