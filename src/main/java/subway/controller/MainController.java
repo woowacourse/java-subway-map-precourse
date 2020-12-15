84eac4e6d2e346c025229e890ea59e929219652e
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-12-15 23:38:15 +0900

    feat(main-controller,view): display main screen

--- src/main/java/subway/controller/MainController.java
+++ src/main/java/subway/controller/MainController.java
@@ -1,8 +1,32 @@
 package subway.controller;
 
+import subway.controller.function.MainFunction;
+import subway.controller.message.MainMessage;
+import subway.controller.screen.SubwayScreen;
+import subway.controller.validation.MainValidator;
+import subway.view.SubwayView;
+
+import java.util.Objects;
+
 public class MainController {
+    private static boolean isQuit(String functionCode) {
+        return Objects.equals(MainFunction.QUIT.getCode(), functionCode);
+    }
+
     public static void run() {
         while (true) {
+            SubwayView.displayScreen(SubwayScreen.MAIN);
+            SubwayView.notice(MainMessage.SELECT_FUNCTION);
+            String functionCode = MainValidator.validateMainFunction(SubwayView.userInput());
+
+            if (isQuit(functionCode)) {
+                return;
+            }
+
+            MainFunction.callBy(functionCode);
         }
+    }
+
+    public static void redirect() {
     }
 }
--- src/main/java/subway/view/SubwayView.java
+++ src/main/java/subway/view/SubwayView.java
@@ -1,12 +1,85 @@
 package subway.view;
 
+import subway.controller.function.Function;
+import subway.controller.message.Message;
+import subway.controller.screen.Screen;
+import subway.domain.Section;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class SubwayView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String NOTICE_FORMAT = "## %s";
+    private static final String INFO_FORMAT = "[INFO] %s";
+    private static final String ERROR_FORMAT = "[ERROR] %s";
+    private static final String FUNCTION_FORMAT = "%s. %s";
+    private static final String HYPHEN_DIVIDER = "---";
 
     public static String userInput() {
-        return scanner.nextLine();
+        String userInput = scanner.nextLine();
+        newLine();
+        return userInput;
+    }
+
+    public static void printSubwayMap(Section section) {
+        info(section.getLine().getName());
+        printHyphenDivider();
+        section.getStations().stream()
+                .forEach(station -> info(station.getName()));
+        newLine();
+    }
+
+    public static void printFunction(Function function) {
+        String formatted = String.format(FUNCTION_FORMAT, function.getCode(), function.getTitle());
+        println(formatted);
+    }
+
+    public static void printFunctionList(List<Function> functions) {
+        functions.stream()
+                .forEach(function -> printFunction(function));
+        newLine();
+    }
+
+    public static void displayScreen(Screen screen) {
+        notice(screen.getTitle());
+        printFunctionList(screen.getFunctions());
+    }
+
+    public static void notice(String message) {
+        String formatted = String.format(NOTICE_FORMAT, message);
+        println(formatted);
+    }
+
+    public static void notice(Message message) {
+        String formatted = String.format(NOTICE_FORMAT, message.getContent());
+        println(formatted);
+    }
+
+    public static void info(String message) {
+        String formatted = String.format(INFO_FORMAT, message);
+        println(formatted);
+    }
+
+    public static void info(Message message) {
+        String formatted = String.format(INFO_FORMAT, message.getContent());
+        println(formatted);
+        newLine();
+    }
+
+    public static void error(String message) {
+        String formatted = String.format(ERROR_FORMAT, message);
+        println(formatted);
+    }
+
+    public static void error(Message message) {
+        String formatted = String.format(ERROR_FORMAT, message.getContent());
+        println(formatted);
+        newLine();
+    }
+
+    public static void printHyphenDivider() {
+        System.out.println(HYPHEN_DIVIDER);
     }
 
     public static void print(String message) {
