c7766fc0426657ef8bc4dfe26dbbfaed5f96aa09
Jinho <jh8579@gmail.com>
2020-12-15 18:10:07 +0900

    feat(domain/line) : add List for registering station in line

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,60 @@
 package subway.domain;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Line {
     private String name;
+    private List<Station> stationList;
 
-    public Line(String name) {
+    public Line(String name, List<Station> stationList) {
         this.name = name;
+        this.stationList = stationList;
+    }
+
+    public void addStation(Station station, int order) {
+        stationList.add(order, station);
+    }
+
+    public void deleteStation(Station station) {
+        stationList.remove(station);
+    }
+
+    public boolean isStationInLine(String name) {
+        for(Station s : stationList) {
+            if(s.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    public List<Station> getStationList() {
+        return stationList;
+    }
+
+    public int getStationSize() {
+        return stationList.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Line)) {
+            return false;
+        }
+        Line line = (Line) o;
+        return Objects.equals(name, line.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
