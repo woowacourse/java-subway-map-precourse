3551171f8e4323b4c2225ea12843c9c7748ec16a
perenok <perenoke@gmail.com>
2020-12-15 22:32:35 +0900

    feat(Line): add List<Station> stations and implements methods
    List<Station> stations을 추가하여 각 노선에 해당하는 역의 정보를 저장
    이를 이용해 노선도에 저장 또는 조회를 용이하게 함

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,43 @@
 package subway.domain;
 
-public class Line {
-    private String name;
+import java.util.List;
+import java.util.Objects;
 
-    public Line(String name) {
+public class Line {
+    private final String name;
+    private final List<Station> stations;
+
+    public Line(String name, List<Station> stations) {
         this.name = name;
+        this.stations = stations;
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public int getStationNumber() {
+        return stations.size();
+    }
+
+    public boolean isExistStationInLine(String stationName) {
+        return stations.stream().anyMatch(station -> Objects.equals(station.getName(), stationName));
+    }
+
+    public void addRoute(int index, String StationName) {
+        Station station = new Station(StationName);
+        if (index == getStationNumber()) {
+            stations.add(station);
+            return;
+        }
+        stations.add(index, station);
+    }
+
+    public void deleteRoute(String stationName) {
+        stations.removeIf(station -> Objects.equals(station.getName(), stationName));
+    }
 }
