e64fc55145e99de18dc4124e97a6fd6600e78081
amy choi <cgc8016@ewhain.net>
2020-12-16 22:06:42 +0900

    feat(ERrorCase): add error cases

--- src/main/java/log/ErrorCase.java
+++ src/main/java/log/ErrorCase.java
@@ -1,11 +1,12 @@
 package log;
 
 public class ErrorCase {
-    public static final String FUNCTION_INPUT_ERROR = "선택할 수 없는 기능입니다.";
-    public static final String ALREADY_EXIST_ERROR = "이미 등록된 역/노선 이름입니다.";
-    public static final String NAME_LENGTH_ERROR = "역/노선의 이름은 최소 2자 이상이어야 합니다.";
-    public static final String NO_SUCH_NAME_ERROR = "등록되지 않은 역/노선 이름입니다.";
-    public static final String HAS_IN_LINE_ERROR = "노선에 등록된 역은 삭제할 수 없습니다.";
-    public static final String TERMINAL_LENGTH_ERROR = "노선의 역 수는 2개 이상이어야 합니다.";
 
+    public static final String FUNCTION_INPUT_ERROR = "선택할 수 없는 기능입니다. ";
+    public static final String ALREADY_EXIST_ERROR = "이미 등록된 역/노선 이름입니다. ";
+    public static final String NAME_LENGTH_ERROR = "역/노선의 이름은 최소 2자 이상이어야 합니다. ";
+    public static final String NO_SUCH_NAME_ERROR = "등록되지 않은 역/노선 이름입니다. ";
+    public static final String INVALID_POSITION_ERROR = "잘못된 위치값입니다. ";
+    public static final String HAS_IN_LINE_ERROR = "노선에 등록된 역은 삭제할 수 없습니다. ";
+    public static final String TERMINAL_LENGTH_ERROR = "노선의 역 수는 2개 이상이어야 합니다. ";
 }
