f9fe54db8cb11a1951272ed3dc90e778a15c61dd
Seungho Baek <weirdbb91@gmail.com>
2020-12-17 23:31:01 +0900

    feat(Section): 구간 추가

---
+++ src/main/java/subway/domain/Section.java
@@ -0,0 +1,13 @@
+package subway.domain;
+
+public class Section {
+    private String name;
+
+    public Section(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
