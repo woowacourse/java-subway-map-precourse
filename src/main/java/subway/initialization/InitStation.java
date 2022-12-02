3bf9f2fe037bb17da55c75b0a076e039b3955180
jmj005 <jmj00005@naver.com>
2022-12-03 02:54:17 +0900

    feat(InitStation): 초기 역 설정 기능 구현

---
+++ src/main/java/subway/initialization/InitStation.java
@@ -0,0 +1,24 @@
+package subway.initialization;
+
+import subway.domain.Station;
+
+public enum InitStation {
+    KYODAE(new Station("교대역")),
+    GANGNAM(new Station("강남역")),
+    YEOKSAM(new Station("역삼역")),
+    NAMBU_TERMINAL(new Station("남부터미널역")),
+    YANGJAE(new Station("양재역")),
+    YANGJAE_FOREST(new Station("양재시민의숲역")),
+    MAEBONG(new Station("매봉역"));
+
+    private final Station stationName;
+
+    InitStation(Station stationName) {
+        this.stationName = stationName;
+    }
+
+    public Station getStationName() {
+        return stationName;
+    }
+}
+
