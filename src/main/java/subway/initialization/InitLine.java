8bdeab49d84ab21351e011920ceb4efef8edc0c0
jmj005 <jmj00005@naver.com>
2022-12-03 02:54:40 +0900

    feat(InitStation): 초기 노선 설정 기능 구현

---
+++ src/main/java/subway/initialization/InitLine.java
@@ -0,0 +1,19 @@
+package subway.initialization;
+
+import subway.domain.Line;
+
+public enum InitLine {
+    LINE_2(new Line("2호선")),
+    LINE_3(new Line("3호선")),
+    LINE_SINBUNDANG(new Line("신분당선"));
+
+    private final Line lineName;
+
+    InitLine(Line lineName) {
+        this.lineName = lineName;
+    }
+
+    public Line getLineName() {
+        return lineName;
+    }
+}
