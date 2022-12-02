de5b05c89df9edf1206b4f938d528e0da9a46178
jmj005 <jmj00005@naver.com>
2022-12-03 02:39:35 +0900

    feat(StationRepository): 역 추가 유효성 확인 기능 구현

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -13,10 +13,28 @@ public class StationRepository {
     }
 
     public static void addStation(Station station) {
+        validate(station);
         stations.add(station);
     }
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    private static void validate(Station station) {
+        validateStationNameRange(station);
+        validateDuplicationStation(station);
+    }
+
+    private static void validateStationNameRange(Station station) {
+        if (station.getName().length() < 2) {
+            throw new IllegalArgumentException("[ERROR] 지하철 역 이름은 두 글자 이상이어야 합니다.");
+        }
+    }
+
+    private static void validateDuplicationStation(Station station) {
+        if (stations.contains(station)) {
+            throw new IllegalArgumentException("[ERROR] 지하철 역 이름은 중복될 수 없습니다.");
+        }
     }
 }
