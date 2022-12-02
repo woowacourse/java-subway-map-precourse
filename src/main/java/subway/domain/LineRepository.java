dd500974c8076952ce3402e005bef162632ada80
jmj005 <jmj00005@naver.com>
2022-12-03 02:41:11 +0900

    feat(LineRepository): 노선 추가 유효성 확인 기능 구현

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class LineRepository {
+
     private static final List<Line> lines = new ArrayList<>();
 
     public static List<Line> lines() {
@@ -13,10 +14,28 @@ public class LineRepository {
     }
 
     public static void addLine(Line line) {
+        validate(line);
         lines.add(line);
     }
 
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    private static void validate(Line line) {
+        validateLineNameRange(line);
+        validateDuplicationLine(line);
+    }
+
+    private static void validateLineNameRange(Line line) {
+        if (line.getName().length() < 2) {
+            throw new IllegalArgumentException("[ERROR] 지하철 노선은 두 글자 이상이어야 합니다.");
+        }
+    }
+
+    private static void validateDuplicationLine(Line line) {
+        if (lines().contains(line)) {
+            throw new IllegalArgumentException("[ERROR] 지하철 노선은 중복될 수 없습니다.");
+        }
     }
 }
