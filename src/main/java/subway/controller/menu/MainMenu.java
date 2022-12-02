b6977953dcb7b4e4fc215e39c21b0cf818ac7ee3
jmj005 <jmj00005@naver.com>
2022-12-02 20:41:57 +0900

    feat(MainMenu): 메인 화면 선택 기능 구현

---
+++ src/main/java/subway/controller/menu/MainMenu.java
@@ -0,0 +1,51 @@
+package subway.controller.menu;
+
+import java.util.Arrays;
+import java.util.Objects;
+import subway.controller.LineController;
+import subway.controller.SectionController;
+import subway.controller.StationController;
+
+public enum MainMenu implements Menu {
+
+    PRINT_MAIN("##"," 메인 화면",null),
+    SELECT_STATION_MANAGEMENT("1",". 역 관리",StationController::stationRun),
+    SELECT_LINE_MANAGEMENT("2",". 노선 관리",LineController::lineRun),
+    SELECT_SECTION_MANAGEMENT("3",". 구간 관리",SectionController::sectionRun),
+    PRINT_SUBWAY_MAP("4",". 지하철 노선도 출력",null),
+    QUIT("Q",". 종료",null);
+
+    private final String number;
+    private final String message;
+    private final Runnable function;
+
+    MainMenu(String number, String message, Runnable function) {
+        this.number = number;
+        this.message = message;
+        this.function = function;
+    }
+
+    @Override
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public Runnable getFunction() {
+        return function;
+    }
+
+    public static void callFunction(String input) {
+        Arrays.stream(MainMenu.values())
+                .filter(function -> Objects.equals(function.getNumber(), input))
+                .findAny()
+                .get()
+                .getFunction()
+                .run();
+    }
+}
