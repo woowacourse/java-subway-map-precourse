e7e4faeab8d5f9ba01c3166bd141e7fc2825c4c2
jmj005 <jmj00005@naver.com>
2022-12-04 03:10:57 +0900

    refactor(StationMenu): StationMenu refactoring

--- src/main/java/subway/controller/menu/StationMenu.java
+++ src/main/java/subway/controller/menu/StationMenu.java
@@ -1,16 +1,22 @@
 package subway.controller.menu;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import subway.controller.MainController;
+import subway.controller.StationController;
+import subway.view.OutputView;
 
 public enum StationMenu implements Menu {
 
-    PRINT_STATION("##", " 역 관리 화면", null),
-    SELECT_STATION_UPDATE("1", ". 역 등록", null),
-    SELECT_STATION_REMOVE("2", ". 역 삭제", null),
-    SELECT_PRINT_STATION_LIST("3", ". 역 조회", null),
-    BACK("B", ". 돌아가기", null);
+    PRINT_STATION("\n##", " 역 관리 화면", null),
+    SELECT_STATION_UPDATE("1", ". 역 등록", new StationController()::addStation),
+    SELECT_STATION_REMOVE("2", ". 역 삭제", new StationController()::removeStation),
+    SELECT_PRINT_STATION_LIST("3", ". 역 조회", new OutputView()::printStationList),
+    BACK("B", ". 돌아가기", new MainController()::runMenu);
 
+    private static final String ERROR_MESSAGE = "선택할 수 없는 기능입니다.";
     private final String number;
     private final String message;
     private final Runnable function;
@@ -36,11 +42,17 @@ public enum StationMenu implements Menu {
         return function;
     }
 
+    public static List<String> getMenu() {
+        return Arrays.stream(StationMenu.values())
+                .map(e -> e.getNumber() + e.getMessage())
+                .collect(Collectors.toList());
+    }
+
     public static void callFunction(String input) {
         StationMenu result = Arrays.stream(StationMenu.values())
-                .filter(function -> Objects.equals(function.getNumber(), input))
+                .filter(e -> Objects.equals(e.getNumber(), input))
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 선택할 수 없는 기능입니다."));
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE));
         result.getFunction()
                 .run();
     }
