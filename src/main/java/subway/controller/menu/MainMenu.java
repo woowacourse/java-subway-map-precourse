04237bb12c031ed6b4391659471b0a44bb51457b
jmj005 <jmj00005@naver.com>
2022-12-04 03:10:25 +0900

    refactor(MainMenu): MainMenu refactoring

--- src/main/java/subway/controller/menu/MainMenu.java
+++ src/main/java/subway/controller/menu/MainMenu.java
@@ -1,7 +1,9 @@
 package subway.controller.menu;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import subway.controller.LineController;
 import subway.controller.SectionController;
 import subway.controller.StationController;
@@ -9,12 +11,13 @@ import subway.controller.StationController;
 public enum MainMenu implements Menu {
 
     PRINT_MAIN("##"," 메인 화면",null),
-    SELECT_STATION_MANAGEMENT("1",". 역 관리",StationController::stationRun),
-    SELECT_LINE_MANAGEMENT("2",". 노선 관리",LineController::lineRun),
-    SELECT_SECTION_MANAGEMENT("3",". 구간 관리",SectionController::sectionRun),
+    SELECT_STATION_MANAGEMENT("1",". 역 관리", new StationController()::stationRun),
+    SELECT_LINE_MANAGEMENT("2",". 노선 관리",  new LineController()::lineRun),
+    SELECT_SECTION_MANAGEMENT("3",". 구간 관리",new SectionController()::sectionRun),
     PRINT_SUBWAY_MAP("4",". 지하철 노선도 출력",null),
     QUIT("Q",". 종료",null);
 
+    private static final String ERROR_MESSAGE = "선택할 수 없는 기능입니다.";
     private final String number;
     private final String message;
     private final Runnable function;
@@ -40,11 +43,17 @@ public enum MainMenu implements Menu {
         return function;
     }
 
-    public static void callFunction(String input) {
+    public static List<String> getMenu() {
+        return Arrays.stream(MainMenu.values())
+                .map(e -> e.getNumber() + e.getMessage())
+                .collect(Collectors.toList());
+    }
+
+    public static void callMainFunction(String input) {
         MainMenu result = Arrays.stream(MainMenu.values())
                 .filter(function -> Objects.equals(function.getNumber(), input))
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 선택할 수 없는 기능입니다."));
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE));
         result.getFunction()
                 .run();
     }
