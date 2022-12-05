8631fe8c3c6a2519a2727ec6d489410b2577d7cb
jmj005 <jmj00005@naver.com>
2022-12-05 20:28:06 +0900

    feat(LineService): 노선 추가 기능 구현

---
+++ src/main/java/subway/service/LineService.java
@@ -0,0 +1,48 @@
+package subway.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.SectionRepository;
+import subway.domain.Station;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+public class LineService {
+
+    private final String INPUT_UPDATE_LINE_NAME = "## 등록할 노선 이름을 입력하세요.";
+    private final String INPUT_UP_BOUND_STATION_NAME = "## 등록할 노선의 상행 종점역 이름을 입력하세요.";
+    private final String INPUT_DOWN_BOUND_STATION_NAME = "## 등록할 노선의 하행 종점역 이름을 입력하세요.";
+    private final String INPUT_REMOVE_LINE = "## 삭제할 노선 이름을 입력하세요.";
+    private final String INFO_REMOVE_LINE = "지하철 노선이 삭제되었습니다.";
+    private final String ERROR_NON_EXIST_LINE = "노선이 존재하지 않습니다.";
+
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public void inputAddLine() {
+        Line inputLine = new Line(inputView.getInput(INPUT_UPDATE_LINE_NAME));
+        try {
+            LineRepository.addLine(inputLine);
+            SectionRepository.addSections(inputLine, getUpBoundDownBound());
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e);
+        }
+    }
+
+    private List<Station> getUpBoundDownBound() {
+        List<Station> upBoundDownBound = new ArrayList<>();
+        upBoundDownBound.add(inputUpBoundStation());
+        upBoundDownBound.add(inputDownBoundStation());
+        return upBoundDownBound;
+    }
+
+    private Station inputUpBoundStation() {
+        return new Station(inputView.getInput(INPUT_UP_BOUND_STATION_NAME));
+    }
+
+    private Station inputDownBoundStation() {
+        return new Station(inputView.getInput(INPUT_DOWN_BOUND_STATION_NAME));
+    }
+}
